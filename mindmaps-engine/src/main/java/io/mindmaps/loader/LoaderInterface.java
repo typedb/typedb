c60463449c8e09f7e7896b92fcf93aa658bb7efd
Alexandra Orth <alexandra@Alexandras-MacBook-Pro.local>
2016-08-16 17:34:22 +0100

    beginning of loading interface

--- mindmaps-engine/src/main/java/io/mindmaps/loader/LoaderInterface.java
+++ mindmaps-engine/src/main/java/io/mindmaps/loader/LoaderInterface.java
@@ -1,7 +1,38 @@
 package io.mindmaps.loader;
 
+import io.mindmaps.graql.Var;
+
+import java.util.Collection;
+
 /**
- * Created by alexandra on 8/16/16.
+ * Loader to perform bulk loading into the graph
  */
-public class LoaderInterface {
+public interface LoaderInterface {
+
+    /**
+     * Add a single var to the queue
+     * @param var to be loaded
+     */
+    public void addToQueue(Var var);
+
+    /**
+     * Add multiple vars to the queue. These should be inserted in one transaction.
+     * @param vars to be loaded
+     */
+    public void addToQueue(Collection<Var> vars);
+
+    /**
+     * Add the given query to the queue to load
+     * @param vars to be loaded
+     */
+    public void addToQueue(String vars);
+
+    /**
+     * Set the size of the each transaction in terms of number of vars.
+     * @param size number of vars in each transaction
+     */
+    public void setBatchSize(int size);
+
+
+
 }
