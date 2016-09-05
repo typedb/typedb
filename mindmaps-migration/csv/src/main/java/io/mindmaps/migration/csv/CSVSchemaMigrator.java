cd193a52e1f03d8726cc19ff0d60caeef27a713e
Alexandra Orth <alexandra@mindmaps.io>
2016-09-05 14:27:12 +0100

    integrating CSV migrator (#8829)

---
+++ mindmaps-migration/csv/src/main/java/io/mindmaps/migration/csv/CSVSchemaMigrator.java
@@ -0,0 +1,106 @@
+package io.mindmaps.migration.csv;
+
+
+import com.google.common.collect.Lists;
+import io.mindmaps.core.Data;
+import io.mindmaps.core.MindmapsGraph;
+import io.mindmaps.graql.Var;
+import io.mindmaps.loader.Loader;
+import org.apache.commons.csv.CSVParser;
+
+import java.util.Collection;
+import java.util.Map;
+
+import static io.mindmaps.graql.Graql.var;
+
+/**
+ * Migrator for migrating a CSV schema into a Mindmaps Ontology
+ */
+public class CSVSchemaMigrator {
+
+    private Namer namer;
+    private String entityName;
+    private CSVParser parser;
+    private MindmapsGraph graph;
+
+    /**
+     * Create a CSVSchemaMigrator to migrate the schemas in the given parsers
+     */
+    public CSVSchemaMigrator(){
+        this.namer = new Namer() {};
+    }
+
+    /**
+     * @param entityName name to be given to the migrated entity
+     * @param parser CSVparser of the file to migrate
+     */
+    public CSVSchemaMigrator configure(String entityName, CSVParser parser){
+        this.entityName = entityName;
+        this.parser = parser;
+        return this;
+    }
+
+    /**
+     * Set a graph from which to get information
+     * @param graph a Mindmaps graph
+     */
+    public CSVSchemaMigrator graph(MindmapsGraph graph){
+        this.graph = graph;
+        return this;
+    }
+
+    /**
+     * Migrate a CSV schema into a Mindmaps ontology
+     * @return var patterns representing the migrated Mindmaps ontology
+     */
+    public Collection<Var> migrate(){
+        return migrateEntitySchema(entityName, parser);
+    }
+
+    /**
+     * Migrate the entire schema into the given loader.
+     * @param loader Loader to migrate into.
+     */
+    public void migrate(Loader loader){
+        loader.addToQueue(migrate());
+        loader.flush();
+        loader.waitToFinish();
+    }
+
+    /**
+     * Migrate a CSV parser as an entity type
+     * @param entityType user-provided type of the entity representing the table
+     * @param parser the parser to migrate
+     * @return var patterns representing the resource and the entity
+     */
+    public Collection<Var> migrateEntitySchema(String entityType, CSVParser parser){
+        Var type = var().isa("entity-type").id(entityType);
+
+        Collection<Var> collection = Lists.newArrayList(type);
+
+        Map<String, Integer> headers = parser.getHeaderMap();
+        headers.keySet().stream()
+                .map(header -> migrateAsResource(entityType, header))
+                .forEach(collection::addAll);
+
+        return collection;
+    }
+
+    /**
+     * Migrate a column as a resource type
+     * @param ownerType id of the owning type
+     * @param otherType  name of the column (becomes the id of the resource type)
+     * @return
+     */
+    public Collection<Var> migrateAsResource(String ownerType, String otherType){
+        // create the vars
+        Var resourceType = var().id(namer.resourceName(otherType))
+                .datatype(Data.STRING)
+                .isa("resource-type");
+
+        Var hasResource = var().id(ownerType).hasResource(namer.resourceName(otherType));
+
+        // insert
+        return Lists.newArrayList(resourceType, hasResource);
+    }
+}
---
+++ mindmaps-migration/csv/src/test/java/io/mindmaps/migration/csv/CSVSchemaMigratorTest.java
@@ -0,0 +1,210 @@
+package io.mindmaps.migration.csv;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import io.mindmaps.MindmapsTransaction;
+import io.mindmaps.api.CommitLogController;
+import io.mindmaps.api.GraphFactoryController;
+import io.mindmaps.api.TransactionController;
+import io.mindmaps.core.Data;
+import io.mindmaps.core.MindmapsGraph;
+import io.mindmaps.core.model.*;
+import io.mindmaps.factory.GraphFactory;
+import io.mindmaps.loader.BlockingLoader;
+import io.mindmaps.util.ConfigProperties;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class CSVSchemaMigratorTest {
+
+    private String GRAPH_NAME;
+
+    private MindmapsGraph graph;
+    private MindmapsTransaction transaction;
+    private static CSVSchemaMigrator migrator;
+    private BlockingLoader loader;
+    private Namer namer = new Namer() {};
+
+    @BeforeClass
+    public static void start(){
+        Logger logger = (Logger) org.slf4j.LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
+        logger.setLevel(Level.INFO);
+
+        System.setProperty(ConfigProperties.CONFIG_FILE_SYSTEM_PROPERTY,ConfigProperties.TEST_CONFIG_FILE);
+        System.setProperty(ConfigProperties.CURRENT_DIR_SYSTEM_PROPERTY, System.getProperty("user.dir")+"/../");
+
+        new TransactionController();
+        new CommitLogController();
+        new GraphFactoryController();
+
+        migrator = new CSVSchemaMigrator();
+    }
+
+    @Before
+    public void setup(){
+        GRAPH_NAME = ConfigProperties.getInstance().getProperty(ConfigProperties.DEFAULT_GRAPH_NAME_PROPERTY);
+
+        loader = new BlockingLoader(GRAPH_NAME);
+        graph = GraphFactory.getInstance().getGraphBatchLoading(GRAPH_NAME);
+        transaction = graph.getTransaction();
+    }
+
+    @After
+    public void shutdown(){
+        graph.clear();
+        graph.close();
+    }
+
+    @Test
+    public void icijEntityTest() throws IOException {
+        migrator.configure("entity", parser("icij/Entities.csv"))
+                .migrate(loader);
+
+        // Check entity type and resources
+        EntityType entity = transaction.getEntityType("entity");
+        assertNotNull(entity);
+
+        assertResourceRelationExists("name", Data.STRING, entity);
+        assertResourceRelationExists("original_name", Data.STRING, entity);
+        assertResourceRelationExists("former_name", Data.STRING, entity);
+        assertResourceRelationExists("jurisdiction", Data.STRING, entity);
+        assertResourceRelationExists("jurisdiction_description", Data.STRING, entity);
+        assertResourceRelationExists("company_type", Data.STRING, entity);
+        assertResourceRelationExists("address", Data.STRING, entity);
+        assertResourceRelationExists("internal_id", Data.STRING, entity);
+        assertResourceRelationExists("incorporation_date", Data.STRING, entity);
+        assertResourceRelationExists("inactivation_date", Data.STRING, entity);
+        assertResourceRelationExists("struck_off_date", Data.STRING, entity);
+        assertResourceRelationExists("dorm_date", Data.STRING, entity);
+        assertResourceRelationExists("status", Data.STRING, entity);
+        assertResourceRelationExists("service_provider", Data.STRING, entity);
+        assertResourceRelationExists("ibcRUC", Data.STRING, entity);
+        assertResourceRelationExists("country_codes", Data.STRING, entity);
+        assertResourceRelationExists("countries", Data.STRING, entity);
+        assertResourceRelationExists("note", Data.STRING, entity);
+        assertResourceRelationExists("valid_until", Data.STRING, entity);
+        assertResourceRelationExists("node_id", Data.STRING, entity);
+        assertResourceRelationExists("sourceID", Data.STRING, entity);
+    }
+
+    @Test
+    public void icijAddressTest() throws IOException {
+        migrator.configure("address", parser("icij/Addresses.csv"))
+                .migrate(loader);
+
+        // Check address type and resources
+        EntityType address = transaction.getEntityType("address");
+        assertNotNull(address);
+
+        assertResourceRelationExists("icij_id", Data.STRING, address);
+        assertResourceRelationExists("valid_until", Data.STRING, address);
+        assertResourceRelationExists("country_codes", Data.STRING, address);
+        assertResourceRelationExists("countries", Data.STRING, address);
+        assertResourceRelationExists("node_id", Data.STRING, address);
+        assertResourceRelationExists("sourceID", Data.STRING, address);
+    }
+
+    @Test
+    public void icijOfficerTest() throws IOException {
+        migrator.configure("officer", parser("icij/Officers.csv"))
+                .migrate(loader);
+
+        // check officers type and resources
+        EntityType officer = transaction.getEntityType("officer");
+        assertNotNull(officer);
+
+        assertResourceRelationExists("name", Data.STRING, officer);
+        assertResourceRelationExists("icij_id", Data.STRING, officer);
+        assertResourceRelationExists("valid_until", Data.STRING, officer);
+        assertResourceRelationExists("country_codes", Data.STRING, officer);
+        assertResourceRelationExists("countries", Data.STRING, officer);
+        assertResourceRelationExists("node_id", Data.STRING, officer);
+        assertResourceRelationExists("sourceID", Data.STRING, officer);
+    }
+
+    @Test
+    public void icijIntermediaryTest() throws IOException {
+        migrator.configure("intermediary", parser("icij/Intermediaries.csv"))
+                .migrate(loader);
+
+        // check intermediaries type and resources
+        EntityType intermediary = transaction.getEntityType("intermediary");
+        assertNotNull(intermediary);
+
+        assertResourceRelationExists("name", Data.STRING, intermediary);
+        assertResourceRelationExists("internal_id", Data.STRING, intermediary);
+        assertResourceRelationExists("address", Data.STRING, intermediary);
+        assertResourceRelationExists("valid_until", Data.STRING, intermediary);
+        assertResourceRelationExists("country_codes", Data.STRING, intermediary);
+        assertResourceRelationExists("countries", Data.STRING, intermediary);
+        assertResourceRelationExists("status", Data.STRING, intermediary);
+        assertResourceRelationExists("node_id", Data.STRING, intermediary);
+        assertResourceRelationExists("sourceID", Data.STRING, intermediary);
+    }
+
+    @Test
+    public void icijRelationshipTest() throws IOException {
+        migrator.configure("relationship", parser("icij/all_edges.csv"))
+                .migrate(loader);
+
+        // check relations were created
+        EntityType relationship = transaction.getEntityType("relationship");
+        assertNotNull(relationship);
+
+        assertResourceRelationExists("node_1", Data.STRING, relationship);
+        assertResourceRelationExists("rel_type", Data.STRING, relationship);
+        assertResourceRelationExists("node_2", Data.STRING, relationship);
+    }
+
+    private ResourceType assertResourceExists(String name, Data datatype) {
+        ResourceType resourceType = transaction.getResourceType(name);
+        assertNotNull(resourceType);
+        assertEquals(datatype.getName(), resourceType.getDataType().getName());
+        return resourceType;
+    }
+
+    private void assertResourceRelationExists(String name, Data datatype, Type owner){
+        String resourceName = namer.resourceName(name);
+        ResourceType resource = assertResourceExists(resourceName, datatype);
+
+        RelationType relationType = transaction.getRelationType("has-" + resourceName);
+        RoleType roleOwner = transaction.getRoleType("has-" + resourceName + "-owner");
+        RoleType roleOther = transaction.getRoleType("has-" + resourceName + "-value");
+
+        assertNotNull(relationType);
+        assertNotNull(roleOwner);
+        assertNotNull(roleOther);
+
+        assertEquals(relationType, roleOwner.relationType());
+        assertEquals(relationType, roleOther.relationType());
+
+        assertTrue(owner.playsRoles().contains(roleOwner));
+        assertTrue(resource.playsRoles().contains(roleOther));
+    }
+
+    private CSVParser parser(String fileName){
+        File file = new File(CSVSchemaMigratorTest.class.getClassLoader().getResource(fileName).getPath());
+
+        CSVParser csvParser = null;
+        try {
+            csvParser = CSVParser.parse(file.toURI().toURL(),
+                    StandardCharsets.UTF_8, CSVFormat.DEFAULT.withHeader());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return csvParser;
+    }
+}
