1970185b6b918460211261117efd4342e3d018e3
Felix Chapman <aelred717@gmail.com>
2016-09-08 11:33:43 +0100

    Make Graql shell display ConceptExceptions from core

--- mindmaps-graql-shell/src/test/java/io/mindmaps/shell/GraqlShellTest.java
+++ mindmaps-graql-shell/src/test/java/io/mindmaps/shell/GraqlShellTest.java
@@ -233,6 +233,20 @@ public class GraqlShellTest {
     }
 
     @Test
+    public void testDuplicateRelation() throws IOException {
+        ByteArrayOutputStream err = new ByteArrayOutputStream();
+        testShell(
+                "insert R isa relation-type, has-role R1, has-role R2; R1 isa role-type; R2 isa role-type;\n" +
+                "insert X isa entity-type, plays-role R1, plays-role R2;\n" +
+                "insert x isa X; (R1 x, R2 x) isa R\n" +
+                "insert x isa X; (R1 x, R2 x) isa R\n",
+                err
+        );
+
+        assertThat(err.toString().toLowerCase(), allOf(containsString("exists"), containsString("relation")));
+    }
+
+    @Test
     public void testLimit() throws IOException {
         String result = testShell("match $x isa concept-type limit 1\n");
 
