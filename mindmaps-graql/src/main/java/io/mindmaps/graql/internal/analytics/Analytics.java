a724b05ee315974e4611b12dcde797fda7b7c41c
Sheldon <sheldon@mindmaps.io>
2016-09-08 18:37:19 +0100

    arggghhhh finally

--- mindmaps-graql/src/main/java/io/mindmaps/graql/internal/analytics/Analytics.java
+++ mindmaps-graql/src/main/java/io/mindmaps/graql/internal/analytics/Analytics.java
@@ -62,11 +62,6 @@ public class Analytics {
             Sets.newHashSet(degree, GraqlType.HAS_RESOURCE.getId(degree));
 
     /**
-     * A reference to the Mindmaps Graph that this subgraph belongs to.
-     */
-    private final MindmapsGraph graph;
-
-    /**
      * The graph computer.
      */
     private final MindmapsComputer computer;
@@ -81,7 +76,7 @@ public class Analytics {
      */
     public Analytics(String keySpace) {
         this.keySpace = keySpace;
-        graph = MindmapsClient.getGraph(this.keySpace);
+        MindmapsGraph graph = MindmapsClient.getGraphBatchLoading(this.keySpace);
         computer = MindmapsClient.getGraphComputer(this.keySpace);
 
         // collect meta-types to exclude them as they do not have instances
@@ -119,7 +114,6 @@ public class Analytics {
      */
     public Analytics(String keySpace, Set<Type> types) {
         this.keySpace = keySpace;
-        graph = MindmapsClient.getGraph(this.keySpace);
         computer = MindmapsClient.getGraphComputer(this.keySpace);
 
         // use ako relations to add subtypes of the provided types
@@ -147,6 +141,7 @@ public class Analytics {
     public Map<Instance, Long> degrees() {
         Map<Instance, Long> allDegrees = new HashMap<>();
         ComputerResult result = computer.compute(new DegreeVertexProgram(allTypes));
+        MindmapsGraph graph = MindmapsClient.getGraphBatchLoading(keySpace);
         result.graph().traversal().V().forEachRemaining(v -> {
             if (v.keys().contains(DegreeVertexProgram.DEGREE)) {
                 Instance instance = graph.getInstance(v.value(ITEM_IDENTIFIER.name()));
@@ -172,6 +167,7 @@ public class Analytics {
     }
 
     private void insertOntology(String resourceTypeId, ResourceType.DataType resourceDataType) {
+        MindmapsGraph graph = MindmapsClient.getGraphBatchLoading(keySpace);
         ResourceType resource = graph.putResourceType(resourceTypeId, resourceDataType);
         RoleType degreeOwner = graph.putRoleType(GraqlType.HAS_RESOURCE_OWNER.getId(resourceTypeId));
         RoleType degreeValue = graph.putRoleType(GraqlType.HAS_RESOURCE_VALUE.getId(resourceTypeId));
@@ -206,8 +202,9 @@ public class Analytics {
         return Analytics.analyticsElements.contains(getVertexType(vertex));
     }
 
-    public static String persistResource(MindmapsGraph mindmapsGraph, Vertex vertex,
+    public static String persistResource(String keySpace, Vertex vertex,
                                          String resourceName, long value) {
+        MindmapsGraph mindmapsGraph = MindmapsClient.getGraphBatchLoading(keySpace);
         ResourceType<Long> resourceType = mindmapsGraph.getResourceType(resourceName);
         RoleType resourceOwner = mindmapsGraph.getRoleType(GraqlType.HAS_RESOURCE_OWNER.getId(resourceName));
         RoleType resourceValue = mindmapsGraph.getRoleType(GraqlType.HAS_RESOURCE_VALUE.getId(resourceName));
@@ -229,13 +226,10 @@ public class Analytics {
                     .putRolePlayer(resourceOwner, instance)
                     .putRolePlayer(resourceValue, resource);
 
-            while (true) {
-                try {
-                    mindmapsGraph.commit();
-                    break;
-                } catch (Exception e) {
-                    throw new RuntimeException(ErrorMessage.BULK_PERSIST.getMessage(resourceType,e.getMessage()),e);
-                }
+            try {
+                mindmapsGraph.commit();
+            } catch (Exception e) {
+                throw new RuntimeException(ErrorMessage.BULK_PERSIST.getMessage(resourceType,e.getMessage()),e);
             }
             return null;
         }
@@ -248,18 +242,15 @@ public class Analytics {
         if (!relations.isEmpty()) {
             String oldAssertionId = relations.get(0).getId();
 
-            while (true) {
-                Resource<Long> resource = mindmapsGraph.putResource(value, resourceType);
+            Resource<Long> resource = mindmapsGraph.putResource(value, resourceType);
 
-                mindmapsGraph.addRelation(relationType)
-                        .putRolePlayer(resourceOwner, instance)
-                        .putRolePlayer(resourceValue, resource);
-                try {
-                    mindmapsGraph.commit();
-                    break;
-                } catch (Exception e) {
-                    throw new RuntimeException(ErrorMessage.BULK_PERSIST.getMessage(resourceType,e.getMessage()),e);
-                }
+            mindmapsGraph.addRelation(relationType)
+                    .putRolePlayer(resourceOwner, instance)
+                    .putRolePlayer(resourceValue, resource);
+            try {
+                mindmapsGraph.commit();
+            } catch (Exception e) {
+                throw new RuntimeException(ErrorMessage.BULK_PERSIST.getMessage(resourceType,e.getMessage()),e);
             }
 
             return oldAssertionId;
