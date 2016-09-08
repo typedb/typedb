9f192cdf1e467817af20bdb466e019e45e384422
Felix Chapman <aelred717@gmail.com>
2016-09-08 09:54:35 +0100

    Fix rebase

---
+++ mindmaps-graql/src/main/java/io/mindmaps/graql/internal/query/InsertQueryInternal.java
@@ -0,0 +1,36 @@
+/*
+ * MindmapsDB - A Distributed Semantic Database
+ * Copyright (C) 2016  Mindmaps Research Ltd
+ *
+ * MindmapsDB is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MindmapsDB is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MindmapsDB. If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ */
+
+package io.mindmaps.graql.internal.query;
+
+import io.mindmaps.graql.admin.InsertQueryAdmin;
+import io.mindmaps.graql.admin.VarAdmin;
+
+import java.util.Collection;
+
+/**
+ * Internal class for handling an InsertQuery
+ */
+public interface InsertQueryInternal extends InsertQueryAdmin {
+
+    /**
+     * @return a collection of Vars to insert, including any nested vars
+     */
+    Collection<VarAdmin> getAllVars();
+
+}
