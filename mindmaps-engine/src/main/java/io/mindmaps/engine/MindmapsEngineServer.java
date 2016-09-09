a18f4e5cd0517d8b43f5dedd834a64eb2cd31bf6
Alexandra Orth <alexandra@mindmaps.io>
2016-09-09 10:44:27 +0100

    Updating copyright

--- mindmaps-engine/src/main/java/io/mindmaps/engine/MindmapsEngineServer.java
+++ mindmaps-engine/src/main/java/io/mindmaps/engine/MindmapsEngineServer.java
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with MindmapsDB. If not, see <http://www.gnu.org/licenses/gpl.txt>.
  */
+
 package io.mindmaps.engine;
 
 import ch.qos.logback.classic.Level;
