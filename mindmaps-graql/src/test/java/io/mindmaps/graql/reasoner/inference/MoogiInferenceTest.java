f28a26c05b8519b66ab7eebe56e296c65c69dc0e
Kasper Piskorski <kasper@mindmaps.io>
2016-09-20 13:16:33 +0100

    resolved conflicts 2

--- mindmaps-graql/src/test/java/io/mindmaps/graql/reasoner/inference/MoogiInferenceTest.java
+++ mindmaps-graql/src/test/java/io/mindmaps/graql/reasoner/inference/MoogiInferenceTest.java
@@ -6,19 +6,7 @@ import io.mindmaps.graql.Graql;
 import io.mindmaps.graql.MatchQuery;
 import io.mindmaps.graql.QueryBuilder;
 import io.mindmaps.graql.Reasoner;
-<<<<<<< HEAD
-import io.mindmaps.graql.internal.reasoner.query.QueryAnswers;
-import io.mindmaps.graql.reasoner.graphs.GenericGraph;
-import org.junit.BeforeClass;
-import org.junit.Ignore;
-import org.junit.Test;
 
-import static io.mindmaps.graql.internal.reasoner.Utility.printAnswers;
-import static io.mindmaps.graql.internal.reasoner.Utility.printMatchQueryResults;
-import static org.junit.Assert.assertEquals;
-
-public class MoogiInferenceTest{
-=======
 import io.mindmaps.graql.reasoner.graphs.GenericGraph;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -26,7 +14,6 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class MoogiInferenceTest {
->>>>>>> 7067d4a7520f36db8e4b5e8c13c1e80d80d1cf7b
 
     private static MindmapsGraph graph;
     private static Reasoner reasoner;
@@ -40,11 +27,7 @@ public class MoogiInferenceTest {
 
     @BeforeClass
     public static void setUpClass() {
-<<<<<<< HEAD
-        graph = GenericGraph.getGraph(schemaFile/*, entityFile, assertionFile2, assertionFile, ruleFile*/);
-=======
         graph = GenericGraph.getGraph(schemaFile, entityFile, assertionFile2, assertionFile, ruleFile);
->>>>>>> 7067d4a7520f36db8e4b5e8c13c1e80d80d1cf7b
         reasoner = new Reasoner(graph);
         qb = Graql.withGraph(graph);
     }
@@ -56,20 +39,11 @@ public class MoogiInferenceTest {
                              "$x isa movie; $y isa genre; $y has description 'science fiction'; select $x";
         MatchQuery query = qb.parseMatch(queryString);
 
-<<<<<<< HEAD
-        QueryAnswers answers = reasoner.resolve(query);
-
-=======
->>>>>>> 7067d4a7520f36db8e4b5e8c13c1e80d80d1cf7b
         String explicitQuery = "match (production-with-genre: $x, genre-of-production: $y) isa has-genre;"+
                             "$y has description 'Sci-Fi' or $y has description 'science fiction' or $y has description 'Science Fiction';" +
                             "$x isa movie; select $x";
 
-<<<<<<< HEAD
-        assertEquals(answers, Sets.newHashSet(qb.parseMatch(explicitQuery)));
-=======
         assertEquals(reasoner.resolve(query), Sets.newHashSet(qb.parseMatch(explicitQuery)));
->>>>>>> 7067d4a7520f36db8e4b5e8c13c1e80d80d1cf7b
         assertQueriesEqual(reasoner.resolveToQuery(query), qb.parseMatch(explicitQuery));
     }
 
