c9354d7cb858d22a5127152192f4c7b0726b78a8
Denis Igorevich Lobanov <denis.lobanov@zooei.com>
2016-09-20 01:25:29 +0100

    Bug #9595 Rename Shell tab to Console

--- mindmaps-dashboard/src/components/visualiser.vue
+++ mindmaps-dashboard/src/components/visualiser.vue
@@ -55,7 +55,7 @@ along with MindmapsDB. If not, see <http://www.gnu.org/licenses/gpl.txt>.
             <div class="tabs-container">
                 <ul class="nav nav-tabs">
                     <li class="active"><a data-toggle="tab" href="#tab-1" aria-expanded="true">Visualiser</a></li>
-                    <li class=""><a data-toggle="tab" href="#tab-2" aria-expanded="false">Shell</a></li>
+                    <li class=""><a data-toggle="tab" href="#tab-2" aria-expanded="false">Console</a></li>
                 </ul>
                 <div class="tab-content">
                     <div id="tab-1" class="tab-pane active">
