273f1bce048fd91b5fbbc44729da339f9112cef1
Filipe Peliz Pinto Teixeira <fppintoteixeira@gmail.com>
2016-09-27 08:06:17 +0100

    Change another test to statically create controllers

--- mindmaps-engine/src/test/java/io/mindmaps/engine/controller/CommitLogControllerTest.java
+++ mindmaps-engine/src/test/java/io/mindmaps/engine/controller/CommitLogControllerTest.java
@@ -54,16 +54,18 @@ public class CommitLogControllerTest {
     private Cache cache;
 
     @BeforeClass
-    public static void startController() {
+    public static void setUpController() throws InterruptedException {
+        Spark.stop();
+        Thread.sleep(5000);
         System.setProperty(ConfigProperties.CONFIG_FILE_SYSTEM_PROPERTY, ConfigProperties.TEST_CONFIG_FILE);
+        Util.setRestAssuredBaseURI(ConfigProperties.getInstance().getProperties());
+        new CommitLogController();
+        new GraphFactoryController();
+        Thread.sleep(5000);
     }
 
     @Before
     public void setUp() throws Exception {
-        new CommitLogController();
-        new GraphFactoryController();
-        Util.setRestAssuredBaseURI(ConfigProperties.getInstance().getProperties());
-
         cache = Cache.getInstance();
 
         String commitLog = "{\n" +
@@ -89,8 +91,6 @@ public class CommitLogControllerTest {
     @After
     public void takeDown() throws InterruptedException {
         cache.getCastingJobs().clear();
-        Spark.stop();
-        Thread.sleep(5000);
     }
 
     @Test
