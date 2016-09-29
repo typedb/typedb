a1c51608ac8fe7946e5a4b4f838782a5a0c708c2
Filipe Peliz Pinto Teixeira <fppintoteixeira@gmail.com>
2016-09-29 12:07:07 +0100

    More concurrency tests

--- mindmaps-test/src/test/java/io/mindmaps/core/ConcurrencyTest.java
+++ mindmaps-test/src/test/java/io/mindmaps/core/ConcurrencyTest.java
@@ -25,25 +25,33 @@ import io.mindmaps.concept.EntityType;
 import io.mindmaps.concept.RelationType;
 import io.mindmaps.concept.RoleType;
 import io.mindmaps.exception.MindmapsValidationException;
-import org.javatuples.Pair;
-import org.junit.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Future;
 
-import static io.mindmaps.IntegrationUtils.graphWithNewKeyspace;
 import static io.mindmaps.IntegrationUtils.startTestEngine;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 /**
  *
  */
 public class ConcurrencyTest {
-
-    String keyspace;
-    MindmapsGraph graph;
-
-    long startTime;
+    private final String ROLE_1 = "role1";
+    private final String ROLE_2 = "role2";
+    private final String ENTITY_TYPE = "Entity Type";
+    private final String RELATION_TYPE = "Relation Type";
+    private MindmapsGraph graph;
 
     @BeforeClass
     public static void startController() throws Exception {
@@ -51,58 +59,106 @@ public class ConcurrencyTest {
     }
 
     @Before
-    public void setUp() throws InterruptedException {
-        Pair<MindmapsGraph, String> result = graphWithNewKeyspace();
-        graph = result.getValue0();
-        keyspace = result.getValue1();
+    public void setUp() throws InterruptedException, MindmapsValidationException {
+        graph = Mindmaps.factory(Mindmaps.DEFAULT_URI, UUID.randomUUID().toString().replaceAll("-", "")).getGraph();
+        createOntology(graph);
+    }
+    private void createOntology(MindmapsGraph graph) throws MindmapsValidationException {
+        RoleType role1 = graph.putRoleType(ROLE_1);
+        RoleType role2 = graph.putRoleType(ROLE_2);
+        graph.putEntityType(ENTITY_TYPE).playsRole(role1).playsRole(role2);
+        graph.putRelationType(RELATION_TYPE).hasRole(role1).hasRole(role2);
+        graph.commit();
     }
 
     @After
     public void cleanGraph() {
+        graph.rollback();
+        assertEquals(2, graph.getEntityType(ENTITY_TYPE).instances().size());
+        assertEquals(1, graph.getRelationType(RELATION_TYPE).instances().size());
         graph.clear();
-        graph.close();
     }
 
-    @Ignore
     @Test
-    public void testSameGraphMultipleThreads() throws InterruptedException {
-        int numberOfThread = 3;
-        graph.close();
-        ExecutorService executor = Executors.newFixedThreadPool(numberOfThread);
-        for (int i=0;i<numberOfThread;i++) {
-            executor.submit(() -> {
-                try {
-                    createGraph();
-                } catch (MindmapsValidationException e) {
-                    e.printStackTrace();
-                }
-            });
+    public void testWritingTheSameDataSequentially() throws MindmapsValidationException {
+        writeData();
+        assertEquals(2, graph.getEntityType(ENTITY_TYPE).instances().size());
+        assertEquals(1, graph.getRelationType(RELATION_TYPE).instances().size());
+
+        for(int i = 0; i < 10; i ++){
+            boolean exceptionThrown = false;
+            try {
+                writeData();
+            } catch (Exception e){
+                exceptionThrown = true;
+            }
+            assertFalse(exceptionThrown);
         }
-        executor.awaitTermination(5000, TimeUnit.MILLISECONDS);
-
-        MindmapsGraph graph = Mindmaps.factory(Mindmaps.DEFAULT_URI, keyspace).getGraph();
-
-        graph.getRoleType("related1");
-        graph.getRoleType("related2");
-        graph.getEntityType("thing");
-        graph.getRelationType("relationship");
-        graph.getEntity("e1");
-        graph.getEntity("e2");
-        graph.getRelation("relation1");
     }
+    private void writeData() throws MindmapsValidationException {
+        EntityType entityType = graph.getEntityType(ENTITY_TYPE);
+        RelationType relationType = graph.getRelationType(RELATION_TYPE);
+        RoleType role1 = graph.getRoleType(ROLE_1);
+        RoleType role2 = graph.getRoleType(ROLE_2);
 
-    public void createGraph() throws MindmapsValidationException {
-        MindmapsGraph graph = Mindmaps.factory(Mindmaps.DEFAULT_URI, keyspace).getGraph();
-        RoleType related1 = graph.putRoleType("related1");
-        RoleType related2 = graph.putRoleType("related2");
-
-        EntityType thing = graph.putEntityType("thing").playsRole(related1).playsRole(related2);
-        RelationType relationship = graph.putRelationType("relationship").hasRole(related1).hasRole(related2);
-
-        Entity e1 = graph.putEntity("e1", thing);
-        Entity e2 = graph.putEntity("e2", thing);
-        graph.putRelation("relation1",relationship).putRolePlayer(related1,e1).putRolePlayer(related2,e2);
+        Entity e1 = graph.putEntity("e1", entityType);
+        Entity e2 = graph.putEntity("e2", entityType);
+        graph.putRelation("relation1",relationType).putRolePlayer(role1,e1).putRolePlayer(role2,e2);
 
         graph.commit();
     }
+
+    @Test
+    public void testWritingTheSameDataConcurrentlyWithRetriesOnFailureAndInitialDataWrite() throws ExecutionException, InterruptedException, MindmapsValidationException {
+        writeData();
+        concurrentWriteSuper();
+    }
+
+    @Ignore //This is ignored because we end up with duplicates. These duplicates will be resolved by removing IDs or a merging process
+    @Test
+    public void testWritingTheSameDataConcurrentlyWithRetriesOnFailure() throws ExecutionException, InterruptedException {
+        concurrentWriteSuper();
+    }
+    private void concurrentWriteSuper() throws ExecutionException, InterruptedException {
+        Set<Future> futures = new HashSet<>();
+        ExecutorService pool = Executors.newFixedThreadPool(10);
+
+        for(int i = 0; i < 20; i ++){
+            futures.add(pool.submit(this::concurrentWrite));
+        }
+
+        int numFinished = 0;
+        for (Future future : futures) {
+            future.get();
+            numFinished ++;
+        }
+
+        assertEquals(20, numFinished);
+    }
+
+    private void concurrentWrite(){
+        final int MAX_FAILURE_COUNT = 10;
+        boolean doneWriting = false;
+        int failureCount = 0;
+        while(!doneWriting){
+            try{
+                writeData();
+                doneWriting = true;
+            } catch (Exception e){
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+
+                graph.rollback(); //It's interesting that we need this after a validation failure. Maybe we should auto rollback after validation failure
+
+                failureCount++;
+                if(failureCount >= MAX_FAILURE_COUNT){
+                    throw new RuntimeException("Too many failures", e);
+                }
+            }
+        }
+    }
+
 }
