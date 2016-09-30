a2430d96e5ac8752261ef2d1041bdba84b8c0eae
Kasper Piskorski <muydeemer@o2.pl>
2016-09-30 19:23:26 +0200

    reasoning test (#317)

--- mindmaps-migration/owl/src/test/java/test/io/mindmaps/migration/owl/TestTransitiveProperties.java
+++ mindmaps-migration/owl/src/test/java/test/io/mindmaps/migration/owl/TestTransitiveProperties.java
@@ -1,5 +1,11 @@
 package test.io.mindmaps.migration.owl;
 
+import io.mindmaps.concept.Concept;
+import io.mindmaps.graql.MatchQuery;
+import io.mindmaps.graql.QueryBuilder;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -13,12 +19,17 @@ import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLNamedIndividual;
 import org.semanticweb.owlapi.model.OWLOntology;
 
+
+import io.mindmaps.graql.Graql;
+import static org.junit.Assert.assertEquals;
+
+
 public class TestTransitiveProperties extends TestOwlMindMapsBase {
 	private IRI baseIri = IRI.create("http://www.co-ode.org/roberts/family-tree.owl");
 	private OWLOntology family = null;
 	
 	@Before
-	public void loadShakespeare() {
+	public void loadFamily() {
         try {
             family = loadOntologyFromResource("/io/mindmaps/migration/owl/samples/family.owl");               
             migrator.ontology(family).graph(graph).migrate();
@@ -31,7 +42,7 @@ public class TestTransitiveProperties extends TestOwlMindMapsBase {
 	}
 	
     @Test
-    public void testSubpropertyInference() {
+    public void testTransitiveInference() {
     	Reasoner reasoner = new Reasoner(new Configuration(), family);
     	IRI person = baseIri.resolve("#Person");
     	IRI hasAncestor = baseIri.resolve("#hasAncestor");
@@ -41,7 +52,20 @@ public class TestTransitiveProperties extends TestOwlMindMapsBase {
     					df.getOWLObjectHasValue(df.getOWLObjectProperty(hasAncestor), 
     											df.getOWLNamedIndividual(baseIri.resolve("#John"))));
     	Set<OWLNamedIndividual> owlResult = reasoner.getInstances(expr).entities().collect(Collectors.toSet());
-    	System.out.println(owlResult);
+		Set<Map<String, Concept>> OWLanswers = new HashSet<>();
+		owlResult.forEach(result -> {
+			Map<String, Concept> resultMap = new HashMap<>();
+			resultMap.put("x", migrator.entity(result));
+			OWLanswers.add(resultMap);
+		});
+
+		QueryBuilder qb = Graql.withGraph(migrator.graph());
+		io.mindmaps.graql.Reasoner mmReasoner = new io.mindmaps.graql.Reasoner(migrator.graph());
+
+		String queryString = "match $x isa tPerson;$y id 'eJohn';" +
+							 "(owl-subject-op-hasAncestor: $x, owl-object-op-hasAncestor: $y) isa op-hasAncestor; select $x;";
+		MatchQuery query = qb.parseMatch(queryString);
+		assertEquals(mmReasoner.resolve(query), OWLanswers);
     }
 
 }
