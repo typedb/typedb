92afd6621a1316dc8206dfb8f28fe7a92074ea8d
Filipe Peliz Pinto Teixeira <fppintoteixeira@gmail.com>
2016-10-03 12:54:40 +0100

    Create new engine embedded config to support integration tests

--- mindmaps-engine/src/main/java/io/mindmaps/engine/util/ConfigProperties.java
+++ mindmaps-engine/src/main/java/io/mindmaps/engine/util/ConfigProperties.java
@@ -33,9 +33,9 @@ import java.util.Properties;
 
 public class ConfigProperties {
 
-    public static final String DEFAULT_CONFIG_FILE = "../../conf/main/mindmaps-engine.properties";
+    public static final String DEFAULT_CONFIG_FILE = "../conf/main/mindmaps-engine.properties";
     public static final String TEST_CONFIG_FILE = "../../conf/test/mindmaps-engine-test.properties";
-    public static final String EMBEDDED_CONFIG_FILE = "../../conf/main/mindmaps-engine.properties";
+    public static final String EMBEDDED_CONFIG_FILE = "../../conf/test/mindmaps-engine-embedded.properties";
     public static final String DEFAULT_LOG_CONFIG_FILE = "../conf/main/logback.xml";
 
     public static final String GRAPH_CONFIG_PROPERTY = "graphdatabase.config";
