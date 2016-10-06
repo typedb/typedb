ce491ab3524b3222115573f4ecb334629abfb391
Filipe Peliz Pinto Teixeira <fppintoteixeira@gmail.com>
2016-10-06 16:36:22 +0100

    Proper naming for Titan Tests

--- mindmaps-test/titan-test/src/test/java/io/mindmaps/TitanGraqlShellTests.java
+++ mindmaps-test/titan-test/src/test/java/io/mindmaps/TitanGraqlShellTests.java
@@ -8,97 +8,97 @@ import java.io.IOException;
 public class TitanGraqlShellTests extends MindmapsTitanTestBase{
     //Graql Shell Tests
     @Test
-    public void GraqlShellIT_1() throws IOException {
+    public void GraqlShellIT_testStartAndExitShell() throws IOException {
         GraqlShellIT.testStartAndExitShell(graph.getKeyspace());
     }
 
     @Test
-    public void GraqlShellIT_2() throws IOException {
+    public void GraqlShellIT_testHelpOption() throws IOException {
         GraqlShellIT.testHelpOption(graph.getKeyspace());
     }
 
     @Test
-    public void GraqlShellIT_3() throws IOException {
+    public void GraqlShellIT_testVersionOption() throws IOException {
         GraqlShellIT.testVersionOption(graph.getKeyspace());
     }
 
     @Test
-    public void GraqlShellIT_4() throws IOException {
+    public void GraqlShellIT_testExecuteOption() throws IOException {
         GraqlShellIT.testExecuteOption(graph.getKeyspace());
     }
 
     @Test
-    public void GraqlShellIT_5() throws IOException {
+    public void GraqlShellIT_testFileOption() throws IOException {
         GraqlShellIT.testFileOption(graph.getKeyspace());
     }
 
     @Test
-    public void GraqlShellIT_6() throws IOException {
+    public void GraqlShellIT_testMatchQuer() throws IOException {
         GraqlShellIT.testMatchQuery(graph.getKeyspace());
     }
 
     @Test
-    public void GraqlShellIT_7() throws IOException {
+    public void GraqlShellIT_testAskQuery() throws IOException {
         GraqlShellIT.testAskQuery(graph.getKeyspace());
     }
 
     @Test
-    public void GraqlShellIT_8() throws IOException {
+    public void GraqlShellIT_testInsertQuery() throws IOException {
         GraqlShellIT.testInsertQuery(graph.getKeyspace());
     }
 
     @Test
-    public void GraqlShellIT_10() throws IOException {
+    public void GraqlShellIT_testInsertOutput() throws IOException {
         GraqlShellIT.testInsertOutput(graph.getKeyspace());
     }
 
     @Test
-    public void GraqlShellIT_11() throws IOException {
+    public void GraqlShellIT_testAutocomplete() throws IOException {
         GraqlShellIT.testAutocomplete(graph.getKeyspace());
     }
 
     @Test
-    public void GraqlShellIT_12() throws IOException {
+    public void GraqlShellIT_testAutocompleteShellCommand() throws IOException {
         GraqlShellIT.testAutocompleteShellCommand(graph.getKeyspace());
     }
 
     @Test
-    public void GraqlShellIT_13() throws IOException {
+    public void GraqlShellIT_testAutocompleteFill() throws IOException {
         GraqlShellIT.testAutocompleteFill(graph.getKeyspace());
     }
 
     @Test
-    public void GraqlShellIT_14() throws IOException {
+    public void GraqlShellIT_testReasoner() throws IOException {
         GraqlShellIT.testReasoner(graph.getKeyspace());
     }
 
     @Test
-    public void GraqlShellIT_15() throws IOException {
+    public void GraqlShellIT_testInvalidQuery() throws IOException {
         GraqlShellIT.testInvalidQuery(graph.getKeyspace());
     }
 
     @Test
-    public void GraqlShellIT_16() throws IOException {
+    public void GraqlShellIT_testComputeCount() throws IOException {
         GraqlShellIT.testComputeCount(graph.getKeyspace());
     }
 
     @Test
-    public void GraqlShellIT_17() throws IOException {
+    public void GraqlShellIT_testRollback() throws IOException {
         GraqlShellIT.testRollback(graph.getKeyspace());
     }
 
     @Test
-    public void GraqlShellIT_18() throws IOException {
+    public void GraqlShellIT_testErrorWhenEngineNotRunning() throws IOException {
         GraqlShellIT.testErrorWhenEngineNotRunning(graph.getKeyspace());
     }
 
     @Test
-    public void GraqlShellIT_19() throws IOException {
+    public void GraqlShellIT_fuzzTest() throws IOException {
         GraqlShellIT.fuzzTest(graph.getKeyspace());
     }
 
     @Test
-    public void GraqlShellIT_20() throws IOException {
+    public void GraqlShellIT_testLargeQuery() throws IOException {
         GraqlShellIT.testLargeQuery(graph.getKeyspace());
     }
 }
