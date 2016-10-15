6ddbb4b19009896d16b2b83f87dd3d0d67e47912
Alexandra Orth <orth.alexandra@gmail.com>
2016-10-15 01:01:01 +0100

    Setting mindmaps dir when using migration scripts (#414)

--- mindmaps-dist/src/bin/migration.sh
+++ mindmaps-dist/src/bin/migration.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 [[ $(readlink $0) ]] && path=$(readlink $0) || path=$0
 
+SCRIPTPATH=`cd "$(dirname "$0")" && pwd -P`
+
 if [ $1 == "csv" ]
 then
   echo "csv migration starting"
-  java -cp "`dirname $path`/../lib/*" io.mindmaps.migration.csv.Main ${1+"$@"}
+  java -cp "`dirname $path`/../lib/*" -Dmindmaps.dir=$SCRIPTPATH io.mindmaps.migration.csv.Main ${1+"$@"}
 elif [ $1 == "sql" ]
 then
   echo "sql migration starting"
-  java -cp "`dirname $path`/../lib/*" io.mindmaps.migration.sql.Main ${1+"$@"}
+  java -cp "`dirname $path`/../lib/*" -Dmindmaps.dir=$SCRIPTPATH io.mindmaps.migration.sql.Main ${1+"$@"}
 elif [ $1 == "json" ]
 then
   echo "json migration starting"
-  java -cp "`dirname $path`/../lib/*" io.mindmaps.migration.json.Main ${1+"$@"}
+  java -cp "`dirname $path`/../lib/*" -Dmindmaps.dir=$SCRIPTPATH io.mindmaps.migration.json.Main ${1+"$@"}
 elif [ $1 == "owl" ]
 then
   echo "owl migration starting"
-  java -cp "`dirname $path`/../lib/*" io.mindmaps.migration.owl.Main ${1+"$@"}
+  java -cp "`dirname $path`/../lib/*" -Dmindmaps.dir=$SCRIPTPATH io.mindmaps.migration.owl.Main ${1+"$@"}
 else
   echo "usage: ./migration.sh {owl, sql, csv} <params>"
 fi
