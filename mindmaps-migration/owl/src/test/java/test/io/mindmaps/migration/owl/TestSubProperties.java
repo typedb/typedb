ff3d10377e2b5a1758e6fd9e7e6cbeca15cf9a0e
aelred <aelred717@gmail.com>
2016-10-17 16:45:35 +0100

    Remove System.exit from last OWL migration test (#421)

--- mindmaps-migration/owl/src/test/java/test/io/mindmaps/migration/owl/TestSubProperties.java
+++ mindmaps-migration/owl/src/test/java/test/io/mindmaps/migration/owl/TestSubProperties.java
@@ -1,34 +1,28 @@
 package test.io.mindmaps.migration.owl;
 
-import java.util.Map;
-import java.util.Set;
-
+import io.mindmaps.exception.MindmapsValidationException;
+import io.mindmaps.graql.Graql;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.Assert;
-import org.semanticweb.HermiT.Reasoner;
 import org.semanticweb.HermiT.Configuration;
+import org.semanticweb.HermiT.Reasoner;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLNamedIndividual;
 import org.semanticweb.owlapi.model.OWLOntology;
 
-import io.mindmaps.graql.Graql;
+import java.util.Map;
+import java.util.Set;
 
 public class TestSubProperties extends TestOwlMindMapsBase {
 	private IRI baseIri = IRI.create("http://www.workingontologist.org/Examples/Chapter3/shakespeare.owl");
 	private OWLOntology shakespeare = null;
 	
 	@Before
-	public void loadShakespeare() {
-        try {
-            shakespeare = loadOntologyFromResource("/io/mindmaps/migration/owl/samples/shakespeare.owl");               
-            migrator.ontology(shakespeare).graph(graph).migrate();
-            migrator.graph().commit();
-        }
-        catch (Throwable t) {
-            t.printStackTrace(System.err);
-            System.exit(-1);
-        }		
+	public void loadShakespeare() throws MindmapsValidationException {
+        shakespeare = loadOntologyFromResource("/io/mindmaps/migration/owl/samples/shakespeare.owl");
+        migrator.ontology(shakespeare).graph(graph).migrate();
+        migrator.graph().commit();
 	}
 	
     @Test
