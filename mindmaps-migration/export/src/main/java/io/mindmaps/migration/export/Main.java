9cde530932223ffa6b023e0ea0cb9f16ea813c7d
Alexandra Orth <alexandra@mindmaps.io>
2016-10-21 10:39:28 +0100

    removing unused imports

--- mindmaps-migration/export/src/main/java/io/mindmaps/migration/export/Main.java
+++ mindmaps-migration/export/src/main/java/io/mindmaps/migration/export/Main.java
@@ -20,10 +20,8 @@ package io.mindmaps.migration.export;
 import io.mindmaps.MindmapsGraph;
 import io.mindmaps.migration.base.io.MigrationCLI;
 
-import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.io.Writer;
 
