efb31ee18f98768e716177c64cf0cf1a357cd5ad
Sheldon <sheldon@mindmaps.io>
2016-10-28 18:03:23 +0100

    rough logic in place for statistics test

--- mindmaps-test/src/test/java/io/mindmaps/test/graql/analytics/ScalingTestIT.java
+++ mindmaps-test/src/test/java/io/mindmaps/test/graql/analytics/ScalingTestIT.java
@@ -28,6 +28,7 @@ import io.mindmaps.concept.RelationType;
 import io.mindmaps.concept.RoleType;
 import io.mindmaps.engine.loader.DistributedLoader;
 import io.mindmaps.exception.MindmapsValidationException;
+import io.mindmaps.graph.internal.AbstractMindmapsGraph;
 import io.mindmaps.graql.internal.analytics.Analytics;
 import io.mindmaps.test.AbstractScalingTest;
 import org.apache.commons.csv.CSVFormat;
@@ -44,6 +45,7 @@ import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -72,10 +74,10 @@ public class ScalingTestIT extends AbstractScalingTest {
     private MindmapsGraphFactory factory;
 
     // test parameters
-    int NUM_SUPER_NODES = 1; // the number of supernodes to generate in the test graph
-    int MAX_SIZE = 10; // the maximum number of non super nodes to add to the test graph
-    int NUM_DIVS = 2; // the number of divisions of the MAX_SIZE to use in the scaling test
-    int REPEAT = 1; // the number of times to repeat at each size for average runtimes
+    int NUM_SUPER_NODES = 10; // the number of supernodes to generate in the test graph
+    int MAX_SIZE = 10000; // the maximum number of non super nodes to add to the test graph
+    int NUM_DIVS = 4; // the number of divisions of the MAX_SIZE to use in the scaling test
+    int REPEAT = 3; // the number of times to repeat at each size for average runtimes
     int MAX_WORKERS = Runtime.getRuntime().availableProcessors(); // the maximum number of workers that spark should use
     int WORKER_DIVS = 4; // the number of divisions of MAX_WORKERS to use for testing
 
@@ -116,14 +118,11 @@ public class ScalingTestIT extends AbstractScalingTest {
         CSVPrinter printer = CSVFormat.DEFAULT.withHeader(headers.toArray(new String[0])).print(out);
 
         PrintWriter writer = null;
-
         try {
-            writer = new PrintWriter("timingsCount.txt", "UTF-8");
+            writer = new PrintWriter("countITProgress.txt", "UTF-8");
         } catch (FileNotFoundException | UnsupportedEncodingException e) {
             e.printStackTrace();
         }
-
-        Map<Long, Long> scaleToAverageTimeCount = new HashMap<>();
 
         // Insert super nodes into graph
         simpleOntology(keyspace);
@@ -133,6 +132,7 @@ public class ScalingTestIT extends AbstractScalingTest {
         int previousGraphSize = 0;
         for (int graphSize : graphSizes) {
             writer.println("current scale - super " + NUM_SUPER_NODES + " - nodes " + graphSize);
+            Long conceptCount = Long.valueOf(NUM_SUPER_NODES * (graphSize + 1) + graphSize);
             printer.print(String.valueOf(graphSize));
 
             writer.println("start generate graph " + System.currentTimeMillis() / 1000L + "s");
@@ -147,9 +147,6 @@ public class ScalingTestIT extends AbstractScalingTest {
                 Analytics computer = new AnalyticsMock(keyspace, new HashSet<>(), new HashSet<>(), workerNumber);
 
                 Long countTime = 0L;
-                Long startTime = 0L;
-                Long stopTime = 0L;
-                Long conceptCount = 0L;
 
                 MindmapsGraph graph = Mindmaps.factory(Mindmaps.DEFAULT_URI, keyspace).getGraph();
 
@@ -157,12 +154,12 @@ public class ScalingTestIT extends AbstractScalingTest {
                     writer.println("gremlin count is: " + graph.getTinkerTraversal().count().next());
                     writer.println("repeat number: " + i);
                     writer.flush();
-                    startTime = System.currentTimeMillis();
-                    conceptCount = computer.count();
-                    assertEquals(Long.valueOf(NUM_SUPER_NODES * (graphSize + 1) + graphSize), conceptCount);
-                    writer.println("count: " + conceptCount);
+                    Long startTime = System.currentTimeMillis();
+                    Long count = computer.count();
+                    assertEquals(conceptCount, count);
+                    writer.println("count: " + count);
                     writer.flush();
-                    stopTime = System.currentTimeMillis();
+                    Long stopTime = System.currentTimeMillis();
                     countTime += stopTime - startTime;
                     writer.println("count time: " + countTime / ((i + 1) * 1000));
                 }
@@ -170,122 +167,135 @@ public class ScalingTestIT extends AbstractScalingTest {
                 countTime /= REPEAT * 1000;
                 writer.println("time to count: " + countTime);
                 printer.print(String.valueOf(countTime));
-                scaleToAverageTimeCount.put(conceptCount, countTime);
             }
             printer.println();
             printer.flush();
         }
 
-        writer.println("counts: " + scaleToAverageTimeCount);
-
         writer.flush();
         writer.close();
+
         printer.flush();
         printer.close();
     }
 
     @Test
-    public void persistConstantIncreasingLoadIT() throws InterruptedException, MindmapsValidationException, ExecutionException {
+    public void persistConstantIncreasingLoadIT() throws InterruptedException, MindmapsValidationException, ExecutionException, IOException {
+        Appendable outWrite = new PrintWriter("persistConstantIncreasingLoadITWrite.txt","UTF-8");
+        Appendable outMutate = new PrintWriter("persistConstantIncreasingLoadITMutate.txt","UTF-8");
+        List<String> headers = new ArrayList<>();
+        headers.add("Size");
+        headers.addAll(workerNumbers.stream().map(String::valueOf).collect(Collectors.toList()));
+        CSVPrinter printerWrite = CSVFormat.DEFAULT.withHeader(headers.toArray(new String[0])).print(outWrite);
+        CSVPrinter printerMutate = CSVFormat.DEFAULT.withHeader(headers.toArray(new String[0])).print(outMutate);
 
         PrintWriter writer = null;
-
         try {
-            writer = new PrintWriter("timingsPersist.txt", "UTF-8");
+            writer = new PrintWriter("persistConstantIncreasingLoadITProgress.txt", "UTF-8");
         } catch (FileNotFoundException | UnsupportedEncodingException e) {
             e.printStackTrace();
         }
 
-        Long startTime = 0L;
-        Long stopTime = 0L;
-        Map<Long, Long> scaleToAverageTimeDegreeAndPersistWrite = new HashMap<>();
-        Map<Long, Long> scaleToAverageTimeDegreeAndPersistMutate = new HashMap<>();
-        Long conceptCount = 0L;
-
         for (int graphSize : graphSizes) {
-            Long degreeAndPersistTimeWrite = 0L;
-            Long degreeAndPersistTimeMutate = 0L;
+            Long conceptCount = Long.valueOf(graphSize) * 3 / 2;
+            printerWrite.print(String.valueOf(conceptCount));
+            printerMutate.print(String.valueOf(conceptCount));
+            for (int workerNumber : workerNumbers) {
+                Long degreeAndPersistTimeWrite = 0L;
+                Long degreeAndPersistTimeMutate = 0L;
 
-            // repeat persist
-            for (int i=0;i<REPEAT;i++) {
-                writer.println("repeat number: "+i);
-                String CURRENT_KEYSPACE = keyspace + String.valueOf(graphSize) + String.valueOf(i);
-                simpleOntology(CURRENT_KEYSPACE);
+                // repeat persist
+                for (int i = 0; i < REPEAT; i++) {
+                    writer.println("repeat number: " + i);
+                    String CURRENT_KEYSPACE = keyspace
+                            + "g" + String.valueOf(graphSize)
+                            + "w" + String.valueOf(workerNumber)
+                            + "r" + String.valueOf(i);
+                    simpleOntology(CURRENT_KEYSPACE);
 
-                // construct graph
-                writer.println("start generate graph " + System.currentTimeMillis()/1000L + "s");
-                writer.flush();
-                addNodes(CURRENT_KEYSPACE, 0, graphSize);
-                writer.println("stop generate graph " + System.currentTimeMillis()/1000L + "s");
+                    // construct graph
+                    writer.println("start generate graph " + System.currentTimeMillis() / 1000L + "s");
+                    writer.flush();
+                    addNodes(CURRENT_KEYSPACE, 0, graphSize);
+                    writer.println("stop generate graph " + System.currentTimeMillis() / 1000L + "s");
 
-                MindmapsGraph graph = Mindmaps.factory(Mindmaps.DEFAULT_URI, CURRENT_KEYSPACE).getGraph();
-                writer.println("gremlin count is: " + graph.getTinkerTraversal().count().next());
+                    MindmapsGraph graph = Mindmaps.factory(Mindmaps.DEFAULT_URI, CURRENT_KEYSPACE).getGraph();
+                    writer.println("gremlin count is: " + graph.getTinkerTraversal().count().next());
 
-                Analytics computer = new Analytics(CURRENT_KEYSPACE, new HashSet<>(), new HashSet<>());
+                    Analytics computer = new AnalyticsMock(CURRENT_KEYSPACE, new HashSet<>(), new HashSet<>(), workerNumber);
 
-                writer.println("persist degree");
-                writer.flush();
-                startTime = System.currentTimeMillis();
-                computer.degreesAndPersist();
-                stopTime = System.currentTimeMillis();
-                degreeAndPersistTimeWrite+=stopTime-startTime;
-                writer.println("persist time: " + degreeAndPersistTimeWrite / ((i + 1) * 1000));
+                    writer.println("persist degree");
+                    writer.flush();
+                    Long startTime = System.currentTimeMillis();
+                    computer.degreesAndPersist();
+                    Long stopTime = System.currentTimeMillis();
+                    degreeAndPersistTimeWrite += stopTime - startTime;
+                    writer.println("persist time: " + degreeAndPersistTimeWrite / ((i + 1) * 1000));
 
-                // mutate graph
-                writer.println("start mutate graph " + System.currentTimeMillis()/1000L + "s");
-                writer.flush();
+                    // mutate graph
+                    writer.println("start mutate graph " + System.currentTimeMillis() / 1000L + "s");
+                    writer.flush();
 
-                // add edges to force mutation
-                addEdges(CURRENT_KEYSPACE, graphSize);
+                    // add edges to force mutation
+                    addEdges(CURRENT_KEYSPACE, graphSize);
 
-                writer.println("stop mutate graph " + System.currentTimeMillis() / 1000L + "s");
+                    writer.println("stop mutate graph " + System.currentTimeMillis() / 1000L + "s");
 
-                graph = Mindmaps.factory(Mindmaps.DEFAULT_URI,CURRENT_KEYSPACE).getGraph();
-                writer.println("gremlin count is: " + graph.getTinkerTraversal().count().next());
+                    graph = Mindmaps.factory(Mindmaps.DEFAULT_URI, CURRENT_KEYSPACE).getGraph();
+                    writer.println("gremlin count is: " + graph.getTinkerTraversal().count().next());
 
-                writer.println("mutate degree");
-                computer = new Analytics(CURRENT_KEYSPACE, new HashSet<>(), new HashSet<>());
-                writer.flush();
-                startTime = System.currentTimeMillis();
-                computer.degreesAndPersist();
-                stopTime = System.currentTimeMillis();
-                degreeAndPersistTimeMutate+=stopTime-startTime;
-                writer.println("mutate time: " + degreeAndPersistTimeMutate / ((i + 1) * 1000));
+                    writer.println("mutate degree");
+                    computer = new AnalyticsMock(CURRENT_KEYSPACE, new HashSet<>(), new HashSet<>(), workerNumber);
+                    writer.flush();
+                    startTime = System.currentTimeMillis();
+                    computer.degreesAndPersist();
+                    stopTime = System.currentTimeMillis();
+                    degreeAndPersistTimeMutate += stopTime - startTime;
+                    writer.println("mutate time: " + degreeAndPersistTimeMutate / ((i + 1) * 1000));
 
-                writer.println("start clean graph" + System.currentTimeMillis()/1000L + "s");
-                graph.clear();
-                writer.println("stop clean graph" + System.currentTimeMillis()/1000L + "s");
+                    writer.println("start clean graph" + System.currentTimeMillis() / 1000L + "s");
+                    graph.clear();
+                    writer.println("stop clean graph" + System.currentTimeMillis() / 1000L + "s");
+                }
+
+                degreeAndPersistTimeWrite /= REPEAT * 1000;
+                degreeAndPersistTimeMutate /= REPEAT * 1000;
+                writer.println("time to degreesAndPersistWrite: " + degreeAndPersistTimeWrite);
+                writer.println("time to degreesAndPersistMutate: " + degreeAndPersistTimeMutate);
+
+                printerWrite.print(String.valueOf(degreeAndPersistTimeWrite));
+                printerMutate.print(String.valueOf(degreeAndPersistTimeMutate));
             }
-
-            conceptCount = Long.valueOf(graphSize)*3/2;
-            degreeAndPersistTimeWrite /= REPEAT*1000;
-            degreeAndPersistTimeMutate /= REPEAT*1000;
-            writer.println("time to degreesAndPersistWrite: " + degreeAndPersistTimeWrite);
-            writer.println("time to degreesAndPersistMutate: " + degreeAndPersistTimeMutate);
-            scaleToAverageTimeDegreeAndPersistWrite.put(conceptCount,degreeAndPersistTimeWrite);
-            scaleToAverageTimeDegreeAndPersistMutate.put(conceptCount,degreeAndPersistTimeMutate);
+            printerWrite.println();
+            printerWrite.flush();
+            printerMutate.println();
+            printerMutate.flush();
         }
 
-        writer.println("degreesAndPersistWrite: " + scaleToAverageTimeDegreeAndPersistWrite);
-        writer.println("degreesAndPersistMutate: " + scaleToAverageTimeDegreeAndPersistMutate);
+        printerWrite.close();
+        printerMutate.close();
 
         writer.flush();
         writer.close();
     }
 
     @Test
-    public void testLargeDegreeMutationResultsInReadableGraphIT() throws MindmapsValidationException, InterruptedException, ExecutionException {
+    public void testLargeDegreeMutationResultsInReadableGraphIT() throws Exception {
 
         simpleOntology(keyspace);
 
         // construct graph
         addNodes(keyspace, 0, MAX_SIZE);
 
+        //TODO: Get rid of this close. We should be refreshing the graph in the factory when switching between normal and batch
+        ((AbstractMindmapsGraph) factory.getGraph()).getTinkerPopGraph().close();
+
         Analytics computer = new Analytics(keyspace, new HashSet<>(), new HashSet<>());
 
         computer.degreesAndPersist();
 
         // assert mutated degrees are as expected
-        MindmapsGraph graph = Mindmaps.factory(Mindmaps.DEFAULT_URI, keyspace).getGraph();
+        MindmapsGraph graph = factory.getGraph();
         EntityType thing = graph.getEntityType("thing");
         Collection<Entity> things = thing.instances();
 
@@ -294,17 +304,20 @@ public class ScalingTestIT extends AbstractScalingTest {
             assertEquals(1L, thisThing.resources().size());
             assertEquals(0L, thisThing.resources().iterator().next().getValue());
         });
-        graph.close();
+
 
         // add edges to force mutation
         addEdges(keyspace, MAX_SIZE);
+
+        //TODO: Get rid of this close. We should be refreshing the graph in the factory when switching between normal and batch
+        ((AbstractMindmapsGraph) factory.getGraph()).getTinkerPopGraph().close();
 
         computer = new Analytics(keyspace, new HashSet<>(), new HashSet<>());
 
         computer.degreesAndPersist();
 
         // assert mutated degrees are as expected
-        graph = Mindmaps.factory(Mindmaps.DEFAULT_URI, keyspace).getGraph();
+        graph = factory.getGraph();
         thing = graph.getEntityType("thing");
         things = thing.instances();
 
@@ -322,6 +335,75 @@ public class ScalingTestIT extends AbstractScalingTest {
             assertEquals(2L, thisRelation.resources().iterator().next().getValue());
         });
 
+    }
+
+    /**
+     * This test creates a graph of S*N*2 entities in total where: S is the total number of steps and N is the number of
+     * entities per step. Each entity has a single numeric resource attached with the values chosen so that each size
+     * of graph results in different values for the min, max, mean, sum, std. The median value always remains the same
+     * but because the graph is growing, this in itself is a good test of median. The values follow the pattern:
+     *
+     * STEP g=1:         5 6 14 16
+     * STEP g=2:     3 4 5 6 14 16 18 20
+     * STEP g=3: 1 2 3 4 5 6 14 16 18 20 22 24
+     *
+     * for S = 3, N = 2.
+     *
+     * To generate the sequence there are two recursive formulae, one for the lower half of values v_m, another for the
+     * upper half V_m:
+     *
+     * v_m+1 = v_m - 1, v_1 = S*N
+     * V_m+1 = V_m + 2, V_1 = (S*N + 1)*2
+     *
+     * The sum of these values at STEP g is given by the formula:
+     *
+     * sum(g) = g*N(1/2 + g*N/2 + 3*S*N + 1)
+     */
+    @Test
+    public void testStatisticsWithConstantDegree() throws IOException, MindmapsValidationException {
+        Appendable outSum = new PrintWriter("testStatisticsWithConstantDegreeSum.txt","UTF-8");
+        List<String> headers = new ArrayList<>();
+        headers.add("Size");
+        headers.addAll(workerNumbers.stream().map(String::valueOf).collect(Collectors.toList()));
+        CSVPrinter printerSum = CSVFormat.DEFAULT.withHeader(headers.toArray(new String[0])).print(outSum);
+
+        // create the ontology
+        simpleOntology(keyspace);
+        new Analytics(keyspace, new HashSet<>(), new HashSet<>());
+
+        DistributedLoader distributedLoader = new DistributedLoader(keyspace,
+                Arrays.asList(HOST_NAME));
+        distributedLoader.setThreadsNumber(30);
+        distributedLoader.setPollingFrequency(1000);
+        distributedLoader.setBatchSize(100);
+
+        int totalSteps = 3;
+        int nodesPerStep = 2;
+        int v_m = totalSteps*nodesPerStep;
+        int V_m = 2*(totalSteps*nodesPerStep+1);
+        for (int g=1; g<totalSteps+1; g++) {
+            printerSum.print(2*g*nodesPerStep);
+
+            // load data
+            for (int m=1; m<nodesPerStep+1; m++) {
+                distributedLoader.addToQueue(var().isa("thing").has("degree",v_m));
+                distributedLoader.addToQueue(var().isa("thing").has("degree",V_m));
+                v_m--;
+                V_m+=2;
+            }
+            distributedLoader.waitToFinish();
+
+            // check the sum is correct
+            assertEquals((long) g*nodesPerStep*(1+g*nodesPerStep+6*totalSteps*nodesPerStep+2)/2
+                    ,new Analytics(keyspace, new HashSet<String>(), Collections.singleton("degree")).sum().get());
+
+            printerSum.println();
+            printerSum.flush();
+        }
+
+        printerSum.flush();
+        printerSum.close();
+        factory.getGraph().clear();
     }
 
     private void addNodes(String keyspace, int startRange, int endRange) throws MindmapsValidationException, InterruptedException {
