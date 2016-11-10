9e86235a8eeae3b0b657154d2109eccd8ca74e21
Alexandra Orth <alexandra@mindmaps.io>
2016-11-10 16:21:07 +0000

    Fix tests

--- mindmaps-graph/src/main/java/io/mindmaps/example/PokemonGraphFactory.java
+++ mindmaps-graph/src/main/java/io/mindmaps/example/PokemonGraphFactory.java
@@ -123,6 +123,14 @@ public class PokemonGraphFactory{
         description = graph.putResourceType("description", ResourceType.DataType.STRING);
         height = graph.putResourceType("height", ResourceType.DataType.LONG);
         weight = graph.putResourceType("weight", ResourceType.DataType.DOUBLE);
+
+        pokemon.hasResource(name);
+        pokemon.hasResource(pokedexNo);
+        pokemon.hasResource(description);
+        pokemon.hasResource(height);
+        pokemon.hasResource(weight);
+
+        pokemonType.hasResource(name);
     }
 
     private static void buildInstances(MindmapsGraph graph) {
