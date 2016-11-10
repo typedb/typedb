8d049e32327da8ccbe95c84df260e4b9dc9d4233
Alexandra Orth <alexandra@mindmaps.io>
2016-11-10 15:31:38 +0000

    Pokemon factory use has-resource

--- mindmaps-graph/src/main/java/io/mindmaps/example/PokemonGraphFactory.java
+++ mindmaps-graph/src/main/java/io/mindmaps/example/PokemonGraphFactory.java
@@ -189,18 +189,7 @@ public class PokemonGraphFactory{
 
     private static <T> void addResource(MindmapsGraph graph, Entity entity, T s, ResourceType<T> type) {
         Resource<T> resource = graph.putResource(s, type);
-
-        RoleType owner = graph.putRoleType("has-" + type.getId() + "-owner");
-        RoleType value = graph.putRoleType("has-" + type.getId() + "-value");
-        RelationType relationType = graph.putRelationType("has-" + type.getId())
-                .hasRole(owner).hasRole(value);
-
-        entity.type().playsRole(owner);
-        type.playsRole(value);
-
-        graph.addRelation(relationType)
-                .putRolePlayer(owner, entity)
-                .putRolePlayer(value, resource);
+        entity.hasResource(resource);
     }
 
     private static void putTypes(MindmapsGraph graph, Entity pokemon, Entity... entities) {
