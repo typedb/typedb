cf2e7b9d0c8519b8da0f44b80503776318060021
Filipe Peliz Pinto Teixeira <fppintoteixeira@gmail.com>
2016-11-11 09:28:26 +0000

    Some cleanup

--- grakn-dashboard/src/js/EngineClient.js
+++ grakn-dashboard/src/js/EngineClient.js
@@ -1,25 +1,25 @@
 /*
- * MindmapsDB - A Distributed Semantic Database
- * Copyright (C) 2016  Mindmaps Research Ltd
+ * Grakn - A Distributed Semantic Database
+ * Copyright (C) 2016  Grakn Labs
  *
- * MindmapsDB is free software: you can redistribute it and/or modify
+ * Grakn is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * MindmapsDB is distributed in the hope that it will be useful,
+ * Grakn is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with MindmapsDB. If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ * along with Grakn. If not, see <http://www.gnu.org/licenses/gpl.txt>.
  */
 
 "use strict";
 
 /*
- * REST API client for MindmapsDB Engine.
+ * REST API client for Grakn Engine.
  */
 export default class EngineClient {
     constructor() {
