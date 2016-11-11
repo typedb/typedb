864af36087aed6084e82ef23a7ee0992cdef2db7
Filipe Peliz Pinto Teixeira <fppintoteixeira@gmail.com>
2016-11-11 08:24:04 +0000

    Update copyright

--- mindmaps-test/src/test/java/ai/grakn/test/graql/reasoner/inference/WineInferenceTest.java
+++ mindmaps-test/src/test/java/ai/grakn/test/graql/reasoner/inference/WineInferenceTest.java
@@ -1,19 +1,19 @@
 /*
- * MindmapsDB - A Distributed Semantic Database
- * Copyright (C) 2016  Mindmaps Research Ltd
+ * Grakn - A Distributed Semantic Database
+ * Copyright (C) 2016  Grakn Labs
  *
- * MindmapsDB is free software: you can redistribute it and/or modify
+ * Grakn is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * MindmapsDB is distributed in the hope that it will be useful,
+ * Grakn is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with MindmapsDB. If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ * along with Grakn. If not, see <http://www.gnu.org/licenses/gpl.txt>.
  */
 
 package ai.grakn.test.graql.reasoner.inference;
