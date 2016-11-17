bf82015c3932e9a7e26821b9230b4c86ac16de51
Kasper Piskorski <kasper@mindmaps.io>
2016-11-17 17:08:12 +0000

    update predicate fix

--- grakn-graql/src/main/java/ai/grakn/graql/internal/reasoner/rule/InferenceRule.java
+++ grakn-graql/src/main/java/ai/grakn/graql/internal/reasoner/rule/InferenceRule.java
@@ -84,13 +84,14 @@ public class InferenceRule {
                     .filter(type -> !body.containsEquivalentAtom(type))
                     .collect(Collectors.toSet());
             Set<Predicate> predicates = new HashSet<>();
-            //predicates obtained from binaries
+            //predicates obtained from types
             types.stream().map(type -> (Binary) type)
                     .filter(type -> type.getPredicate() != null)
                     .map(Binary::getPredicate)
                     .forEach(predicates::add);
             //direct predicates
             predicates.addAll(parentAtom.getIdPredicates());
+            predicates.addAll(parentAtom.getValuePredicates());
 
             head.addAtomConstraints(predicates);
             body.addAtomConstraints(predicates);
