4b136c8b0a18d275d0078cadfcd3a29deb3e4bcc
Thanh C Pham <thanh@mindmaps.io>
2016-12-06 14:14:02 +0000

    Removing more stray dirnames
    Replace with GRAKN_HOME

--- grakn-dist/src/bin/graql.sh
+++ grakn-dist/src/bin/graql.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-[[ $(readlink $0) ]] && path=$(readlink $0) || path=$0
-java -cp "`dirname $path`/../lib/*" ai.grakn.graql.GraqlShell ${1+"$@"}
+
+if [ -z "${GRAKN_HOME}" ]; then
+    [[ $(readlink $0) ]] && path=$(readlink $0) || path=$0
+    GRAKN_BIN=$(cd "$(dirname "${path}")" && pwd -P)
+    GRAKN_HOME=$(cd "${GRAKN_BIN}"/.. && pwd -P)
+fi
+
+java -cp "${GRAKN_HOME}/lib/*" ai.grakn.graql.GraqlShell ${1+"$@"}
