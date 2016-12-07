1ebc6a7109a66dab5696150b2342f43d9fa545a8
Thanh Pham <burukuru@gmail.com>
2016-12-07 03:41:31 +0000

    Splitting out engine/cassandra start scripts (#755)
    * Splitting out engine/cassandra start scripts
    
    Start Grakn Engine and Cassandra independently.
    Set GRAKN_HOME and remove stray dirnames.
    This should work with symlinks to the scripts/dirs.
    Define GRAKN_HOME manually if starting still fails with symlinks.
    
    * Removing more stray dirnames
    
    Replace with GRAKN_HOME

--- grakn-dist/src/bin/migration.sh
+++ grakn-dist/src/bin/migration.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
-[[ $(readlink $0) ]] && path=$(readlink $0) || path=$0
+if [ -z "${GRAKN_HOME}" ]; then
+    [[ $(readlink $0) ]] && path=$(readlink $0) || path=$0
+    GRAKN_BIN=$(cd "$(dirname "${path}")" && pwd -P)
+    GRAKN_HOME=$(cd "${GRAKN_BIN}"/.. && pwd -P)
+fi
 
-CONCATCLASSPATH=$CLASSPATH":`dirname $path`/../lib/*"
-SCRIPTPATH=`cd "$(dirname "$0")" && pwd -P`
-
+CONCATCLASSPATH=$CLASSPATH":"${GRAKN_HOME}/lib/*"
 
 if [ $1 == "csv" ]
 then
-  java -cp ${CONCATCLASSPATH} -Dgrakn.dir=${SCRIPTPATH} ai.grakn.migration.csv.Main ${1+"$@"}
+  java -cp ${CONCATCLASSPATH} -Dgrakn.dir="${GRAKN_HOME}/bin" ai.grakn.migration.csv.Main ${1+"$@"}
 elif [ $1 == "json" ]
 then
-  java -cp ${CONCATCLASSPATH} -Dgrakn.dir=${SCRIPTPATH} ai.grakn.migration.json.Main ${1+"$@"}
+  java -cp ${CONCATCLASSPATH} -Dgrakn.dir="${GRAKN_HOME}/bin" ai.grakn.migration.json.Main ${1+"$@"}
 elif [ $1 == "owl" ]
 then
-  java -cp ${CONCATCLASSPATH} -Dgrakn.dir=${SCRIPTPATH} ai.grakn.migration.owl.Main ${1+"$@"}
+  java -cp ${CONCATCLASSPATH} -Dgrakn.dir="${GRAKN_HOME}/bin" ai.grakn.migration.owl.Main ${1+"$@"}
 elif [ $1 == "export" ]
 then
-  java -cp ${CONCATCLASSPATH} -Dgrakn.dir=${SCRIPTPATH} ai.grakn.migration.export.Main ${1+"$@"}
+  java -cp ${CONCATCLASSPATH} -Dgrakn.dir="${GRAKN_HOME}/bin" ai.grakn.migration.export.Main ${1+"$@"}
 elif [ $1 == "sql" ]
 then
-  java -cp ${CONCATCLASSPATH} -Dgrakn.dir=${SCRIPTPATH} ai.grakn.migration.sql.Main ${1+"$@"}
+  java -cp ${CONCATCLASSPATH} -Dgrakn.dir="${GRAKN_HOME}/bin" ai.grakn.migration.sql.Main ${1+"$@"}
 else
   echo "usage: ./migration.sh {owl, csv, json, export, sql} <params>"
 fi
