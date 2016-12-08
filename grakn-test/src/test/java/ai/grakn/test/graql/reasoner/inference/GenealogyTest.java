a46a86c88c7a45b7f11dd232d7688c8d9ec2fce1
Kasper Piskorski <kasper@mindmaps.io>
2016-12-08 17:38:02 +0000

    remove time

--- grakn-test/src/test/java/ai/grakn/test/graql/reasoner/inference/GenealogyTest.java
+++ grakn-test/src/test/java/ai/grakn/test/graql/reasoner/inference/GenealogyTest.java
@@ -37,7 +37,6 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
-import static ai.grakn.graql.internal.reasoner.Utility.printAnswers;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -279,11 +278,9 @@ public class GenealogyTest extends AbstractEngineTest{
 
     @Test
     public void testCousins() {
-        long startTime = System.currentTimeMillis();
         String queryString = "match ($x, $y) isa cousins;";
         MatchQuery query = new Query(queryString, graph);
         QueryAnswers answers = new QueryAnswers(Sets.newHashSet(reasoner.resolveToQuery(query)));
-        System.out.println("computed in: " + (System.currentTimeMillis() - startTime));
         assertTrue(!answers.isEmpty());
         assertTrue(!hasDuplicates(answers));
         assertEquals(answers, Sets.newHashSet(qb.<MatchQuery>parse(queryString)));
