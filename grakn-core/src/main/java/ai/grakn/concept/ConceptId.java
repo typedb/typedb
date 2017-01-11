efc6ba29c30ec84b192735a5ea820f941836526c
Filipe Peliz Pinto Teixeira <fppintoteixeira@gmail.com>
2017-01-11 10:58:07 +0000

    ConceptID base implementation

---
+++ grakn-core/src/main/java/ai/grakn/concept/ConceptId.java
@@ -0,0 +1,49 @@
+package ai.grakn.concept;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <p>
+ *     A Concept Id
+ * </p>
+ *
+ * <p>
+ *     A class which represents an id of any {@link Concept} in the {@link ai.grakn.GraknGraph}.
+ *     Also contains a static method for producing concept IDs from Strings.
+ * </p>
+ *
+ * @author fppt
+ */
+public class ConceptId {
+    private static Map<String, ConceptId> conceptIds = new HashMap();
+
+    private String conceptId;
+
+    private ConceptId(String conceptId){
+        this.conceptId = conceptId;
+    }
+
+    public String getValue(){
+        return conceptId;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        return object instanceof ConceptId && ((ConceptId) object).getValue().equals(conceptId);
+    }
+
+    @Override
+    public String toString(){
+        return conceptId;
+    }
+
+    /**
+     *
+     * @param value The string which potentially represents a Concept
+     * @return The matching concept ID
+     */
+    public static ConceptId of(String value){
+        return conceptIds.computeIfAbsent(value, ConceptId::new);
+    }
+}
