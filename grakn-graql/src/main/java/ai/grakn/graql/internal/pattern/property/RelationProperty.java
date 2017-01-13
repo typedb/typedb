e71f04471a57d78146a8de5b75af576fd50e2aa7
Felix Chapman <aelred717@gmail.com>
2017-01-13 12:32:53 +0000

    Fix some PMD suggestions

--- grakn-graql/src/main/java/ai/grakn/graql/internal/pattern/property/RelationProperty.java
+++ grakn-graql/src/main/java/ai/grakn/graql/internal/pattern/property/RelationProperty.java
@@ -27,12 +27,12 @@ import ai.grakn.concept.RoleType;
 import ai.grakn.concept.Type;
 import ai.grakn.graql.Graql;
 import ai.grakn.graql.Var;
+import ai.grakn.graql.VarName;
 import ai.grakn.graql.admin.Atomic;
 import ai.grakn.graql.admin.ReasonerQuery;
 import ai.grakn.graql.admin.RelationPlayer;
 import ai.grakn.graql.admin.UniqueVarProperty;
 import ai.grakn.graql.admin.VarAdmin;
-import ai.grakn.graql.VarName;
 import ai.grakn.graql.internal.gremlin.EquivalentFragmentSet;
 import ai.grakn.graql.internal.gremlin.ShortcutTraversal;
 import ai.grakn.graql.internal.pattern.Patterns;
@@ -49,7 +49,6 @@ import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static ai.grakn.graql.internal.gremlin.fragment.Fragments.distinctCasting;
@@ -291,7 +290,7 @@ public class RelationProperty extends AbstractVarProperty implements UniqueVarPr
     @Override
     public Atomic mapToAtom(VarAdmin var, Set<VarAdmin> vars, ReasonerQuery parent) {
             Var relVar = var.isUserDefinedName()? Graql.var(var.getVarName()) : Graql.var();
-            Set<RelationPlayer> relationPlayers = this.getRelationPlayers().collect(Collectors.toSet());
+            Set<RelationPlayer> relationPlayers = this.getRelationPlayers().collect(toSet());
             relationPlayers.forEach(rp -> {
                 VarAdmin role = rp.getRoleType().orElse(null);
                 VarAdmin rolePlayer = rp.getRolePlayer();
