938a4954148ff52d614530a8d674ad6e98d43003
Thanh C Pham <thanh@grakn.ai>
2017-01-21 00:49:43 +0000

    Use project.parent.basedir. Clean up bash script.
    put bash script in line with the move to grakn-dist

--- grakn-dist/src/test/bash/bash-startup-test.sh
+++ grakn-dist/src/test/bash/bash-startup-test.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
 BASEDIR=$(dirname "$0")
-GRAKN_DIST_DIR=$BASEDIR/../../../../grakn-dist/
-GRAKN_TEST_TARGET=$BASEDIR/../../../target/
-GRAKN_TEST_TMP=$GRAKN_TEST_TARGET/grakn/
-GRAKN_TEST_BIN=${GRAKN_TEST_TMP}/bin/
+GRAKN_DIST_TARGET=$BASEDIR/../../../target/
+GRAKN_DIST_TMP=$GRAKN_DIST_TARGET/grakn-bash-test/
+GRAKN_DIST_BIN=${GRAKN_DIST_TMP}/bin/
 
 startEngine(){
   echo "Starting!"
-  "${GRAKN_TEST_BIN}/grakn.sh" start
+  "${GRAKN_DIST_BIN}/grakn.sh" start
   sleep 10
-  echo "exit" | "${GRAKN_TEST_BIN}"/graql.sh # Waiting for Engine
+  echo "exit" | "${GRAKN_DIST_BIN}"/graql.sh # Waiting for Engine
 }
 
 loadData(){
   echo "Inserting data!"
-  "${GRAKN_TEST_BIN}"/graql.sh < insert-data.gql
+  "${GRAKN_DIST_BIN}"/graql.sh < insert-data.gql
 }
 
 oneTimeSetUp() {
   set -e
-  package=$(ls $GRAKN_DIST_DIR/target/grakn-dist-*.tar.gz | head -1)
-  cp "$package" $GRAKN_TEST_TARGET/
-  mkdir -p ${GRAKN_TEST_TMP}
-  tar -xf $GRAKN_TEST_TARGET/$(basename ${package}) -C ${GRAKN_TEST_TMP} --strip-components=1
+  package=$(ls $GRAKN_DIST_TARGET/grakn-dist-*.tar.gz | head -1)
+  mkdir -p ${GRAKN_DIST_TMP}
+  tar -xf $GRAKN_DIST_TARGET/$(basename ${package}) -C ${GRAKN_DIST_TMP} --strip-components=1
 
   startEngine
   loadData
@@ -31,23 +29,23 @@ oneTimeSetUp() {
 }
 
 oneTimeTearDown() {
-  echo "y" | "${GRAKN_TEST_BIN}"/grakn.sh clean
-  "${GRAKN_TEST_BIN}"/grakn.sh stop
+  echo "y" | "${GRAKN_DIST_BIN}"/grakn.sh clean
+  "${GRAKN_DIST_BIN}"/grakn.sh stop
 }
 
 testPersonCount()
 {
-  PERSON_COUNT=$(cat query-data.gql | "${GRAKN_TEST_BIN}"/graql.sh | grep -v '>>>' | grep person | wc -l)
+  PERSON_COUNT=$(cat query-data.gql | "${GRAKN_DIST_BIN}"/graql.sh | grep -v '>>>' | grep person | wc -l)
   echo Persons found $PERSON_COUNT
   assertEquals 4 $PERSON_COUNT
 }
 
 testMarriageCount()
 {
-  MARRIAGE_COUNT=$(cat query-marriage.gql | "${GRAKN_TEST_BIN}"/graql.sh |  grep -v '>>>' | grep person | wc -l)
+  MARRIAGE_COUNT=$(cat query-marriage.gql | "${GRAKN_DIST_BIN}"/graql.sh |  grep -v '>>>' | grep person | wc -l)
   echo Marriages found $MARRIAGE_COUNT
   assertEquals 1 $MARRIAGE_COUNT
 }
 
 # Source shunit2
-source "$GRAKN_TEST_TARGET"/lib/shunit2-master/source/2.1/src/shunit2
+source "$GRAKN_DIST_TARGET"/lib/shunit2-master/source/2.1/src/shunit2
