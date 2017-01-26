1fd371579f25839b219804fbdb679a2d91d24433
Michelangelo Bucci <mikonapoli@users.noreply.github.com>
2017-01-26 16:31:53 +0000

    Test Refactorings (#989)
    * Remove mvn-compiler plugin from migration Pom
    
    Redundant as already in the main pom
    
    * Remove compiler plugin from owl-migration
    
    * Add failsafe plugin version to main pom
    
    * Add surefire plugin to main POM
    
    * Parallelise build on Travis
    
    * Remove Philosopher Test from grakn-dist
    
    * Add Philosopher IntegrationTest (Should be acceptance test)
    
    * Remove Redundant Test from grakn-test
    
    * Move Template tests out of grakn-test
    
    * Have Travis run UnitTests and IntegrationTests in parallel
    
    * Fix extra *
    
    * Fix GraqlShellIT output
    
    * Ignore failing IT

--- grakn-graql/src/test/java/ai/grakn/graql/template/TemplateParserTest.java
+++ grakn-graql/src/test/java/ai/grakn/graql/template/TemplateParserTest.java
@@ -16,7 +16,7 @@
  * along with Grakn. If not, see <http://www.gnu.org/licenses/gpl.txt>.
  */
 
-package ai.grakn.test.graql.template;
+package ai.grakn.graql.template;
 
 import ai.grakn.exception.GraqlTemplateParsingException;
 import ai.grakn.graql.Graql;
