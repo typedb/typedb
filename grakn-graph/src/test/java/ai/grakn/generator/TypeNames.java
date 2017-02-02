e5749ece1b498f0b08a1801eb73c1e129aacc3ff
Felix Chapman <aelred717@gmail.com>
2017-02-02 12:08:53 +0000

    Introduce quick check with some generators

---
+++ grakn-graph/src/test/java/ai/grakn/generator/TypeNames.java
@@ -0,0 +1,37 @@
+/*
+ * Grakn - A Distributed Semantic Database
+ * Copyright (C) 2016  Grakn Labs Limited
+ *
+ * Grakn is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Grakn is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Grakn. If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ *
+ */
+
+package ai.grakn.generator;
+
+import ai.grakn.concept.TypeName;
+import com.pholser.junit.quickcheck.generator.GenerationStatus;
+import com.pholser.junit.quickcheck.generator.Generator;
+import com.pholser.junit.quickcheck.random.SourceOfRandomness;
+
+public class TypeNames extends Generator<TypeName> {
+
+    public TypeNames() {
+        super(TypeName.class);
+    }
+
+    @Override
+    public TypeName generate(SourceOfRandomness random, GenerationStatus status) {
+        return TypeName.of(gen().type(String.class).generate(random, status));
+    }
+}
