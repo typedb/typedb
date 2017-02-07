751e44b7a7bc7161b9a6f21da6fc1c1022da5a75
Marco Scoppetta <marco@grakn.ai>
2017-02-07 18:14:59 +0000

    New dashboard:
        - JS files' structure changed and a lot of refactoring
        - Removed almost all dependencies from themes or frameworks
        - New dashboard structure built from scratch using CSS3 flexbox
        - Almost everything is new

---
+++ grakn-dashboard/src/components/notFoundPage.vue
@@ -0,0 +1,47 @@
+<!--
+Grakn - A Distributed Semantic Database
+Copyright (C) 2016  Grakn Labs Limited
+
+Grakn is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Grakn is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Grakn. If not, see <http://www.gnu.org/licenses/gpl.txt>.
+-->
+
+<template>
+<section class="wrapper">
+    <!-- Main content-->
+    <section class="content">
+        <div class="content-404">
+          <div class="inside">
+            PAGE NOT FOUND - 404 Go back
+            <router-link tag="a" to="/">home.</router-link>
+          </div>
+        </div>
+    </section>
+    <!-- End main content-->
+
+</section>
+</template>
+
+<style>
+.content-404 {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+}
+
+</style>
+
+<script>
+
+</script>
