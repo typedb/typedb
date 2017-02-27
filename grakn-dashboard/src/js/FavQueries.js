f3ab6d5ab53a0e785f18924820b30ca9efc4d97a
Marco Scoppetta <marco@grakn.ai>
2017-02-27 17:25:42 -0500

    Saved queries are now scoped
    Saved queries in dashboard are now associated to a specific keyspace,
    and they will not be accessible when a different keyspace is selected.

--- grakn-dashboard/src/js/FavQueries.js
+++ grakn-dashboard/src/js/FavQueries.js
@@ -1,17 +1,29 @@
+import User from './User';
+
 // Default constant values
 const QUERIES_LS_KEY = 'fav_queries';
+
 
 export default {
   getFavQueries() {
     const queries = localStorage.getItem(QUERIES_LS_KEY);
+    const currentKeyspace = User.getCurrentKeySpace();
+
     if (queries === null) {
-      localStorage.setItem(QUERIES_LS_KEY, '{}');
+      localStorage.setItem(QUERIES_LS_KEY, JSON.stringify({ [currentKeyspace]: {} }));
       return {};
     }
-    return JSON.parse(queries);
+
+    const queriesObject = JSON.parse(queries);
+    // If there is not object associated to the current keyspace we return empty object
+    if (!(currentKeyspace in queriesObject)) {
+      return {};
+    }
+    return queriesObject[currentKeyspace];
   },
   addFavQuery(queryName, queryValue) {
     const queries = this.getFavQueries();
+
     queries[queryName] = queryValue;
     this.setFavQueries(queries);
   },
@@ -20,7 +32,9 @@ export default {
     delete queries[queryName];
     this.setFavQueries(queries);
   },
-  setFavQueries(queriesObject) {
-    localStorage.setItem(QUERIES_LS_KEY, JSON.stringify(queriesObject));
+  setFavQueries(queriesParam) {
+    const queries = JSON.parse(localStorage.getItem(QUERIES_LS_KEY));
+    Object.assign(queries, { [User.getCurrentKeySpace()]: queriesParam });
+    localStorage.setItem(QUERIES_LS_KEY, JSON.stringify(queries));
   },
 };
