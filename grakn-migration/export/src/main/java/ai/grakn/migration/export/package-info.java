288f8ccc986b8043f206ab07ac23e748a0f49507
Jo Stichbury <jo@mindmaps.io>
2017-03-01 12:12:10 +0000

    Add package descriptions for ai.grakn.migration packages

---
+++ grakn-migration/export/src/main/java/ai/grakn/migration/export/package-info.java
@@ -0,0 +1,23 @@
+/*
+ * Grakn - A Distributed Semantic Database
+ * Copyright (C) 2016  Grakn Labs Limited
+ *
+ * Grakn is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Grakn is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Grakn. If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ */
+
+/**
+ *  Implements export of data and ontology from a Grakn graph into Graql statements.
+ */
+
+package ai.grakn.migration.export;
