2e87fed6f04c6003f36a90603cc344dd2d357f36
Filipe Peliz Pinto Teixeira <fppintoteixeira@gmail.com>
2017-03-06 11:59:58 +0000

    More cleanups of roleplayers

--- grakn-graql/src/main/java/ai/grakn/graql/internal/pattern/property/HasResourceProperty.java
+++ grakn-graql/src/main/java/ai/grakn/graql/internal/pattern/property/HasResourceProperty.java
@@ -154,7 +154,7 @@ public class HasResourceProperty extends AbstractVarProperty implements NamedPro
     }
 
     private boolean testPredicate(Optional<ValuePredicateAdmin> optPredicate, Relation relation, RoleType resourceRole) {
-        Object value = relation.rolePlayers().get(resourceRole).asResource().getValue();
+        Object value = relation.newRolePlayers(resourceRole).iterator().next().asResource().getValue();
 
         return optPredicate
                 .flatMap(ValuePredicateAdmin::getPredicate)
