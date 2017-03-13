13cc098959c72d0be5d3fccca5260d299b70e000
aelred <aelred717@gmail.com>
2017-03-13 14:34:10 +0000

    Ignore randomly failing `GraqlShellIT` (#1268)

--- grakn-test/src/test/java/ai/grakn/test/graql/shell/GraqlShellIT.java
+++ grakn-test/src/test/java/ai/grakn/test/graql/shell/GraqlShellIT.java
@@ -62,6 +62,11 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.junit.Assume.assumeFalse;
 
+/*
+These tests are failing for some reason. The websocket appears to close itself mid-test. Possibly the graphs are not being
+cleared correctly, or engine is not starting/stopping correctly.
+ */
+@Ignore // TODO: Fix this test
 public class GraqlShellIT {
 
     @ClassRule
