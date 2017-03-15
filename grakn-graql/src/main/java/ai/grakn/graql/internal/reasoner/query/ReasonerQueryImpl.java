b0baa765dbc7359038026d1e66371f257a2544a4
Kasper Piskorski <kasper@mindmaps.io>
2017-03-15 13:41:18 +0000

    remove commented code

--- grakn-graql/src/main/java/ai/grakn/graql/internal/reasoner/query/ReasonerQueryImpl.java
+++ grakn-graql/src/main/java/ai/grakn/graql/internal/reasoner/query/ReasonerQueryImpl.java
@@ -459,14 +459,8 @@ public class ReasonerQueryImpl implements ReasonerQuery {
             childAtomicQuery = qit.next();
             Set<VarName> joinVars = Sets.intersection(joinedVars, childAtomicQuery.getVarNames());
             Stream<Answer> localSubs = childAtomicQuery.answerStream(subGoals, cache, dCache, materialise, false);
+            //TODO bring back the inverse join
             join = join(join, localSubs, ImmutableSet.copyOf(joinVars));
-            /*
-            join = joinWithInverse(
-                    join,
-                    localSubs,
-                    cache.getInverseAnswerMap(childAtomicQuery, joinVars),
-                    ImmutableSet.copyOf(joinVars));
-                    */
             joinedVars.addAll(childAtomicQuery.getVarNames());
         }
         return join;
@@ -488,14 +482,8 @@ public class ReasonerQueryImpl implements ReasonerQuery {
             for(ReasonerAtomicQuery qj : queries){
                 if ( qj != qi ){
                     Set<VarName> joinVars = Sets.intersection(joinedVars, qj.getVarNames());
+                    //TODO bring back the inverse join
                     subs = join(subs, cache.getAnswerStream(qj), ImmutableSet.copyOf(joinVars));
-                    /*
-                    subs = joinWithInverse(
-                            subs,
-                            cache.getAnswerStream(qj),
-                            cache.getInverseAnswerMap(qj, joinVars),
-                            ImmutableSet.copyOf(joinVars));
-                            */
                     joinedVars.addAll(qj.getVarNames());
                 }
             }
