cc726445fbd95d9e06316b9c9495f5df29440d4e
Filipe Peliz Pinto Teixeira <fppintoteixeira@gmail.com>
2017-03-21 17:33:48 +0000

    Minor correction

--- grakn-core/src/main/java/ai/grakn/util/ErrorMessage.java
+++ grakn-core/src/main/java/ai/grakn/util/ErrorMessage.java
@@ -57,7 +57,7 @@ public enum ErrorMessage {
     GRAPH_PERMANENTLY_CLOSED("The Graph for keyspace [%s] is closed. Use the factory to get a new graph."),
     TRANSACTIONS_OPEN("Cannot close graph [%s] connecting to keyspace [%s] because there are [%s] open transactions"),
     LOCKING_EXCEPTION("Internal locking exception. Please clear the transaction and try again."),
-    DUPLICATE_RULES("Rule [%s] already has patterns with lhs [%s] and rhs [%s]. Creating another rule with the same pattern is prohibited"),
+    DUPLICATE_RULES("Rule [%s] already has patterns with lhs [%s] and rhs [%s]. Creating another rule with the same patterns is prohibited"),
 
     //--------------------------------------------- Validation Errors
     VALIDATION("A structural validation error has occurred. Please correct the [`%s`] errors found. \n"),
