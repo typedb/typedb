9e12c192df30ecc2fd43258cddb639518bdffe9b
Alexandra Orth <alexandra@mindmaps.io>
2017-04-03 17:42:20 +0100

    Fix broken test

--- grakn-core/src/main/java/ai/grakn/exception/GraknEngineServerException.java
+++ grakn-core/src/main/java/ai/grakn/exception/GraknEngineServerException.java
@@ -32,8 +32,8 @@ public class GraknEngineServerException extends RuntimeException {
     final int status;
 
     public GraknEngineServerException(int status, ErrorMessage message, String... args) {
-        super(message.getMessage(new Object[]{args}));
-        log(status, message.getMessage(new Object[]{args}));
+        super(message.getMessage(args));
+        log(status, message.getMessage(args));
         this.status = status;
     }
 
