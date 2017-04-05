8aad19bb71ff5161584536e4e74f7ed30303b389
Filipe Peliz Pinto Teixeira <fppintoteixeira@gmail.com>
2017-04-05 12:01:00 +0100

    Add liscence header

--- grakn-engine/src/main/java/ai/grakn/engine/cache/EngineCacheAbstract.java
+++ grakn-engine/src/main/java/ai/grakn/engine/cache/EngineCacheAbstract.java
@@ -1,3 +1,21 @@
+/*
+ * Grakn - A Distributed Semantic Database
+ * Copyright (C) 2016  Grakn Labs Limited
+ *
+ * Grakn is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Grakn is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Grakn. If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ */
+
 package ai.grakn.engine.cache;
 
 import ai.grakn.concept.ConceptId;
@@ -7,7 +25,18 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
 
-public abstract class EngineCacheAbstract implements ConceptCache {
+/**
+ * <p>
+ *     Engine's internal Concept ID cache
+ * </p>
+ *
+ * <p>
+ *     This stores the common implementation details between {@link EngineCacheDistributed} and {@link EngineCacheStandAlone}
+ * </p>
+ *
+ * @author fppt
+ */
+abstract class EngineCacheAbstract implements ConceptCache {
     private final AtomicLong lastTimeModified;
 
     EngineCacheAbstract(){
