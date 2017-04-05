d6cebc5abffc692597ed4cb42049313856035541
Alexandra Orth <alexandra@mindmaps.io>
2017-04-05 10:45:04 +0100

    Created LockProvider class

---
+++ grakn-engine/src/main/java/ai/grakn/engine/lock/LockProvider.java
@@ -0,0 +1,45 @@
+/*
+ * Grakn - A Distributed Semantic Database
+ * Copyright (C) 2016  Grakn Labs Limited
+ *
+ * Grakn is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Grakn is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Grakn. If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ */
+
+package ai.grakn.engine.lock;
+
+import java.util.concurrent.locks.Lock;
+
+/**
+ * <p>
+ *     This class provides the correct {@link Lock} based on how it was initialised.
+ * </p>
+ *
+ * @author alexandraorth
+ */
+public class LockProvider {
+
+    private static Lock lock = null;
+
+    public static void init(Lock providedLock){
+        if(lock != null){
+            throw new RuntimeException("Lock class has already been initialised with another lock.");
+        }
+
+        lock = providedLock;
+    }
+
+    public static Lock getLock(){
+        return lock;
+    }
+}
