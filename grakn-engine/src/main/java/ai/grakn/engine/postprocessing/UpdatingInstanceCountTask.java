34618788e53208af5042312d7a8aeb535079e0d8
Filipe Peliz Pinto Teixeira <fppintoteixeira@gmail.com>
2017-04-06 16:48:04 +0100

    Make checkstyle happy

--- grakn-engine/src/main/java/ai/grakn/engine/postprocessing/UpdatingInstanceCountTask.java
+++ grakn-engine/src/main/java/ai/grakn/engine/postprocessing/UpdatingInstanceCountTask.java
@@ -1,3 +1,21 @@
+/*
+ * Grakn - A Distributed Semantic Database
+ * Copyright (C) 2016  Grakn Labs Limited
+ *
+ * Grakn is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Grakn is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Grakn. If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ */
+
 package ai.grakn.engine.postprocessing;
 
 import ai.grakn.engine.tasks.BackgroundTask;
@@ -6,6 +24,17 @@ import mjson.Json;
 
 import java.util.function.Consumer;
 
+/**
+ * <p>
+ *     Task that controls when types are updated with their new instance counts
+ * </p>
+ *
+ * <p>
+ *     This task begins only if enough time has passed (configurable) since the last time a job was added.
+ * </p>
+ *
+ * @author fppt
+ */
 public class UpdatingInstanceCountTask implements BackgroundTask {
     @Override
     public boolean start(Consumer<TaskCheckpoint> saveCheckpoint, Json configuration) {
