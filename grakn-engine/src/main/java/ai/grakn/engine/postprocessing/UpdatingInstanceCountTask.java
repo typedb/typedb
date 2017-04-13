8c32ea4a218801775553ed458098a03263ef5804
Alexandra Orth <alexandra@mindmaps.io>
2017-04-13 15:41:21 +0100

    Counting task is non-recurring, Lock indefinite

--- grakn-engine/src/main/java/ai/grakn/engine/postprocessing/UpdatingInstanceCountTask.java
+++ grakn-engine/src/main/java/ai/grakn/engine/postprocessing/UpdatingInstanceCountTask.java
@@ -72,7 +72,7 @@ public class UpdatingInstanceCountTask extends LockingBackgroundTask {
 
         updateCountsOnKeySpace(keyspace, instanceMap);
 
-        return false;
+        return true;
     }
 
     private void updateCountsOnKeySpace(String keyspace, Map<TypeLabel, Long> jobs){
