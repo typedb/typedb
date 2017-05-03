7e9dd3ebc44c42b4726b94722864bddd5f8b8baa
Filipe Peliz Pinto Teixeira <fppintoteixeira@gmail.com>
2017-05-03 10:55:46 +0100

    Fix issue where session was not actually closing the main connection to the graph

--- grakn-graph/src/main/java/ai/grakn/factory/GraknSessionImpl.java
+++ grakn-graph/src/main/java/ai/grakn/factory/GraknSessionImpl.java
@@ -109,8 +109,8 @@ public class GraknSessionImpl implements GraknSession {
 
         //Close the main graph connections
         try {
-            if(graph != null && !graph.isClosed()) ((AbstractGraknGraph) graph).getTinkerPopGraph().close();
-            if(graphBatch != null && !graphBatch.isClosed()) ((AbstractGraknGraph) graphBatch).getTinkerPopGraph().close();
+            if(graph != null) ((AbstractGraknGraph) graph).getTinkerPopGraph().close();
+            if(graphBatch != null) ((AbstractGraknGraph) graphBatch).getTinkerPopGraph().close();
         } catch (Exception e) {
             throw new GraphRuntimeException("Could not close graph.", e);
         }
