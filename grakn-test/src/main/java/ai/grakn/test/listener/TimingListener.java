b56363bb9aa202cf0fec933742df3c9b3bbf50f5
Borislav Iordanov <borislav.iordanov@gmail.com>
2017-05-08 13:41:36 -0400

    timing listener moved to grakn-test module and surefire plugin moved there as well

--- grakn-test/src/main/java/ai/grakn/test/listener/TimingListener.java
+++ grakn-test/src/main/java/ai/grakn/test/listener/TimingListener.java
@@ -1,3 +1,20 @@
+/*
+ * Grakn - A Distributed Semantic Database
+ * Copyright (C) 2016  Grakn Labs Limited
+ *
+ * Grakn is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Grakn is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Grakn. If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ */
 package ai.grakn.test.listener;
 
 import static java.nio.file.StandardOpenOption.APPEND;
@@ -11,25 +28,26 @@ import java.util.Collections;
 import org.junit.runner.Description;
 import org.junit.runner.notification.RunListener;
 
+/**
+ * <p>
+ * A junit listener around each executed test that collects the time it takes to run the test and
+ * logs into a file. The file name where time is logged can be specified in a
+ * <code>grakn.test.timerecord.file</code> system property. 
+ * </p>
+ * 
+ * @author borislav
+ *
+ */
 public class TimingListener extends RunListener {
     
-    private long startTime;
-    private String test;
-    
-    public static void main(String [] argv)
-    {
-        try {
-            Files.write(Paths.get("/Users/borislav/delme.txt"), Collections.singleton("Hello"), APPEND, CREATE);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
+    private long startTime = 0;
+    private String test = "";
     
     private void writeTime(String test, long time) {
         String timeLogFile = System.getProperty("grakn.test.timerecord.file");
-        if (timeLogFile == null)
+        if (timeLogFile == null) {
             timeLogFile = "grakn-test-timings.log";
+        }
         try {
             Files.write(Paths.get(timeLogFile), Collections.singleton(test + "," + time), APPEND, CREATE);
             System.out.println("WROTE to file " + Paths.get(timeLogFile));
@@ -50,8 +68,9 @@ public class TimingListener extends RunListener {
 
     public void testFinished(Description description) throws Exception {        
         String finishedTest = description.getClassName() + "." + description.getMethodName();
-        if (!test.equals(finishedTest))
-            throw new RuntimeException("Test started " + test + " different from test finished " + finishedTest);            
+        if (!test.equals(finishedTest)) {
+            throw new RuntimeException("Test started " + test + " different from test finished " + finishedTest);
+        }
         writeTime(test, (System.currentTimeMillis() - startTime));
     }
 }
