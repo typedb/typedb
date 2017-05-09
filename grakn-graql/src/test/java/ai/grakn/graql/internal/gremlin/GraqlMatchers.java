f3382bbc47d26a12b339eb69bbffd6d0d9b99102
Felix Chapman <aelred717@gmail.com>
2017-05-09 10:13:23 +0100

    Add javadoc for `GraqlMatchers`

--- grakn-graql/src/test/java/ai/grakn/graql/internal/gremlin/GraqlMatchers.java
+++ grakn-graql/src/test/java/ai/grakn/graql/internal/gremlin/GraqlMatchers.java
@@ -23,15 +23,33 @@ import org.hamcrest.Description;
 import org.hamcrest.FeatureMatcher;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeDiagnosingMatcher;
+import org.junit.Assert;
 
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 /**
+ * Provides {@link Matcher} implementations used by Graql tests in calls to {@link Assert#assertThat}.
+ *
  * @author Felix Chapman
  */
 public class GraqlMatchers {
 
+    /**
+     * Create a {@link Matcher} that checks that something satisfies the given {@link Predicate}.
+     * <p>
+     * Useful for describing more complicated {@link Matcher} classes, or for one-off tests.
+     * <p>
+     * Example:
+     * <pre>
+     * Matcher isEven = satisfies(x -> x % 2 == 0);
+     * assertThat(10, isEven);
+     * </pre>
+     *
+     * @param predicate the predicate to test against
+     * @param <T> the type of the object being tested
+     * @return a {@link Matcher} that checks that something satisfies the given {@link Predicate}
+     */
     public static <T> Matcher<T> satisfies(Predicate<T> predicate) {
         return new TypeSafeDiagnosingMatcher<T>() {
             @Override
@@ -46,6 +64,24 @@ public class GraqlMatchers {
         };
     }
 
+    /**
+     * Create a {@link Matcher} that extracts a feature from an object, then applies a {@link Matcher} to that feature.
+     * <p>
+     * This is useful when there is a field or method on an object that must be tested against.
+     * <p>
+     * Example:
+     * <pre>
+     * Matcher stringReprMentionsGraql = feature(containsString("Graql"), "contains graql", Object::toString);
+     * assertThat(Lists.newArrayList("1", "2", "Graql"), stringReprMentionsGraql);
+     * </pre>
+     *
+     * @param subMatcher a {@link Matcher} to apply to the extracted feature
+     * @param name the name of the resulting matcher
+     * @param extractor a {@link Function} to extract a feature from the the object
+     * @param <T> the type of the object being tested
+     * @param <U> the type of the extracted feature
+     * @return a {@link Matcher} that extracts a feature from an object, then applies a {@link Matcher} to that feature
+     */
     public static <T, U> Matcher<T> feature(Matcher<? super U> subMatcher, String name, Function<T, U> extractor) {
         return new FeatureMatcher<T, U>(subMatcher, name, name) {
 
