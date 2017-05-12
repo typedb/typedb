80857e01ef2936c2f3233058fc4ac816a2b4d6d3
Alexandra Orth <alexandra@mindmaps.io>
2017-05-12 10:27:18 +0100

    Concat Macro test

---
+++ grakn-graql/src/test/java/ai/grakn/graql/internal/template/macro/ConcatMacroTest.java
@@ -0,0 +1,85 @@
+/*
+ * Grakn - A Distributed Semantic Database
+ * Copyright (C) 2016  Grakn Labs Limited
+ *
+ * Grakn is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Grakn is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Grakn. If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ */
+
+package ai.grakn.graql.internal.template.macro;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import java.util.Collections;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static ai.grakn.graql.internal.template.MacroTest.assertParseEquals;
+import static org.junit.Assert.assertEquals;
+
+public class ConcatMacroTest {
+
+    private final ConcatMacro concatMacro = new ConcatMacro();
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void applyConcatMacroToNoArguments_ExceptionIsThrown(){
+        exception.expect(IllegalArgumentException.class);
+
+        concatMacro.apply(Collections.emptyList());
+    }
+
+    @Test
+    public void applyConcatMacroThanOneArgument_ExceptionIsThrown(){
+        exception.expect(IllegalArgumentException.class);
+
+        concatMacro.apply(ImmutableList.of("true"));
+    }
+
+    @Test
+    public void applyConcatMacroToTwoOrMoreArguments_ResultIsNonNull(){
+        exception.expect(IllegalArgumentException.class);
+
+        concatMacro.apply(ImmutableList.of("true"));
+    }
+
+    @Test
+    public void applyConcatMacroToString_StringsAreConcatenated(){
+        String result = concatMacro.apply(ImmutableList.of("this", " ", "that"));
+
+        assertEquals("this that", result);
+    }
+
+    @Test
+    public void applyConcatMacroToNumbers_NumbersAreConcatenated(){
+        String result = concatMacro.apply(ImmutableList.of(4, 5));
+
+        assertEquals("45", result);
+    }
+
+    @Test
+    public void whenUsingConcatMacroInTemplate_ResultIsAsExpected(){
+        String template = "insert $x val @concat(<value1>, <value2>);";
+        String expected = "insert $x0 val \"thing5\";";
+
+        ImmutableMap<String, Object> data = ImmutableMap.of(
+                "value1", "thing",
+                "value2", 5
+        );
+
+        assertParseEquals(template, data, expected);
+    }
+}
