748a6762ef5e790f1191813a16dfbb99a1b01ccb
Kasper Piskorski <kasper@mindmaps.io>
2017-05-12 17:41:01 +0100

    remove unused import

--- grakn-graql/src/main/java/ai/grakn/graql/internal/query/QueryAnswer.java
+++ grakn-graql/src/main/java/ai/grakn/graql/internal/query/QueryAnswer.java
@@ -28,7 +28,6 @@ import ai.grakn.graql.internal.reasoner.explanation.Explanation;
 import com.google.common.collect.Sets;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.BiConsumer;
