23cb38348c2e7951953c9da8f54e052032a6ac39
Felix Chapman <aelred717@gmail.com>
2017-05-15 16:37:50 +0100

    Bonus fix! Make "close error" button sit on the right of the box

--- grakn-dashboard/src/components/graqlEditor/messagePanel.vue
+++ grakn-dashboard/src/components/graqlEditor/messagePanel.vue
@@ -19,7 +19,7 @@ along with Grakn. If not, see <http://www.gnu.org/licenses/gpl.txt>.
 <template>
 <transition name="slideInDown" appear>
     <div class="z-depth-1 message-panel" v-if="showMessagePanel">
-        <div style="word-break: break-all">{{message}}</div><div><button @click="$emit('close-message')" class="btn">X</button></div>
+        <div style="word-break: break-all">{{message}}</div><button @click="$emit('close-message')" class="btn">X</button>
     </div>
 </transition>
 </template>
@@ -42,7 +42,8 @@ along with Grakn. If not, see <http://www.gnu.org/licenses/gpl.txt>.
 }
 
 button{
-  margin: 0px;
+  margin-left: auto;
+  margin-right: 0px;
   padding: 1px 3px;
   background-color: #cb0032;
 }
