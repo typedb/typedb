2962e4ad1957b2f9e8763b530bdd6a7c19346343
Filipe Peliz Pinto Teixeira <fppintoteixeira@gmail.com>
2017-05-23 11:45:56 +0100

    Add and improve failing tests

--- grakn-graph/src/test/java/ai/grakn/graph/internal/TxCacheTest.java
+++ grakn-graph/src/test/java/ai/grakn/graph/internal/TxCacheTest.java
@@ -25,10 +25,14 @@ import ai.grakn.concept.EntityType;
 import ai.grakn.concept.Relation;
 import ai.grakn.concept.RelationType;
 import ai.grakn.concept.Resource;
+import ai.grakn.concept.ResourceType;
 import ai.grakn.concept.RoleType;
 import ai.grakn.concept.RuleType;
-import ai.grakn.concept.ResourceType;
+import ai.grakn.concept.Type;
+import org.hamcrest.Matcher;
 import org.junit.Test;
+
+import java.util.function.Function;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.containsInAnyOrder;
@@ -203,12 +207,60 @@ public class TxCacheTest extends GraphTestBase{
         EntityType e3 = graknGraph.putEntityType("e3");
         graknGraph.commit();
 
+        //Check everything is okay
         graknGraph = (AbstractGraknGraph<?>) graknSession.open(GraknTxType.WRITE);
-        assertEquals(e1, e2.superType());
+        assertTxBoundConceptMatches(e2, Type::superType, is(e1));
 
         //Mutate Super Type
         e2.superType(e3);
-        assertEquals(e3, e2.superType());
-        assertEquals(e3, graknGraph.getTxCache().getCachedType(e2.getLabel()).superType());
+        assertTxBoundConceptMatches(e2, Type::superType, is(e3));
+    }
+
+    @Test
+    public void whenMutatingRoleTypesOfTypeCreatedInAnotherTransaction_EnsureTransactionBoundConceptsAreMutated(){
+        RoleType rol1 = graknGraph.putRoleType("role1");
+        RoleType rol2 = graknGraph.putRoleType("role2");
+        EntityType e1 = graknGraph.putEntityType("e1").plays(rol1).plays(rol2);
+        EntityType e2 = graknGraph.putEntityType("e2");
+        RelationType rel = graknGraph.putRelationType("rel").relates(rol1).relates(rol2);
+        graknGraph.commit();
+
+        //Check concepts match what is in transaction cache
+        graknGraph = (AbstractGraknGraph<?>) graknSession.open(GraknTxType.WRITE);
+        assertTxBoundConceptMatches(e1, Type::plays, containsInAnyOrder(rol1, rol2));
+        assertTxBoundConceptMatches(rel, RelationType::relates, containsInAnyOrder(rol1, rol2));
+        assertTxBoundConceptMatches(rol1, RoleType::playedByTypes, containsInAnyOrder(e1));
+        assertTxBoundConceptMatches(rol2, RoleType::playedByTypes, containsInAnyOrder(e1));
+        assertTxBoundConceptMatches(rol1, RoleType::relationTypes, containsInAnyOrder(rel));
+        assertTxBoundConceptMatches(rol2, RoleType::relationTypes, containsInAnyOrder(rel));
+
+        //Role Type 1 and 2 played by e2 now
+        e2.plays(rol1);
+        e2.plays(rol2);
+        assertTxBoundConceptMatches(rol1, RoleType::playedByTypes, containsInAnyOrder(e1, e2));
+        assertTxBoundConceptMatches(rol2, RoleType::playedByTypes, containsInAnyOrder(e1, e2));
+
+        //e1 no longer plays role 1
+        e1.deletePlays(rol1);
+        assertTxBoundConceptMatches(rol1, RoleType::playedByTypes, containsInAnyOrder(e2));
+        assertTxBoundConceptMatches(rol2, RoleType::playedByTypes, containsInAnyOrder(e1, e2));
+
+        //Role 2 no longer part of relation type
+        rel.deleteRelates(rol2);
+        assertTxBoundConceptMatches(rol2, RoleType::relationTypes, empty());
+        assertTxBoundConceptMatches(rel, RelationType::relates, containsInAnyOrder(rol1));
+    }
+
+    /**
+     * Helper method which will check that the cache and the provided type have the same expected values.
+     *
+     * @param type The type to check against as well as retreive from the concept cache
+     * @param resultSupplier The result of executing some operation on the type
+     * @param expectedMatch The expected result of the above operation
+     */
+    @SuppressWarnings("unchecked")
+    private <T extends Type> void assertTxBoundConceptMatches(T type, Function<T, Object> resultSupplier, Matcher expectedMatch){
+        assertThat(resultSupplier.apply(type), expectedMatch);
+        assertThat(resultSupplier.apply(graknGraph.getTxCache().getCachedType(type.getLabel())), expectedMatch);
     }
 }
