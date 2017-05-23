7eed04ae6aa5ffb8aa297c08fee881264b2a7938
Alexandra Orth <alexandra@mindmaps.io>
2017-05-23 11:22:13 +0100

    Remove servicemix apache commons csv exclusion from script

--- grakn-dist/src/bin/migration.sh
+++ grakn-dist/src/bin/migration.sh
@@ -5,33 +5,29 @@ if [ -z "${GRAKN_HOME}" ]; then
     GRAKN_HOME=$(cd "${GRAKN_BIN}"/.. && pwd -P)
 fi
 
-# Define CLASSPATH, exclude commons-csv because of a dependency clash when using migration
+# Define CLASSPATH
 for jar in "${GRAKN_HOME}"/lib/*.jar; do
-    if [[ $jar != *org.apache.servicemix.bundles.commons-csv* ]] ; then
-        CLASSPATH="$CLASSPATH:$jar"
-    fi
+   CLASSPATH="$CLASSPATH:$jar"
 done
-
-CONCATCLASSPATH=$CLASSPATH
 
 if [ "$1" == "csv" ]
 then
-  java -cp ${CONCATCLASSPATH} -Dlogback.configurationFile="${GRAKN_HOME}/conf/main/logback.xml" -Dgrakn.log.dirs="${GRAKN_HOME}/logs" -Dgrakn.dir="${GRAKN_HOME}/bin" ai.grakn.migration.csv.CSVMigrator ${1+"$@"}
+  java -cp ${CLASSPATH} -Dlogback.configurationFile="${GRAKN_HOME}/conf/main/logback.xml" -Dgrakn.log.dirs="${GRAKN_HOME}/logs" -Dgrakn.dir="${GRAKN_HOME}/bin" ai.grakn.migration.csv.CSVMigrator ${1+"$@"}
 elif [ "$1" == "json" ]
 then
-  java -cp ${CONCATCLASSPATH} -Dlogback.configurationFile="${GRAKN_HOME}/conf/main/logback.xml" -Dgrakn.log.dirs="${GRAKN_HOME}/logs" -Dgrakn.dir="${GRAKN_HOME}/bin" ai.grakn.migration.json.JsonMigrator ${1+"$@"}
+  java -cp ${CLASSPATH} -Dlogback.configurationFile="${GRAKN_HOME}/conf/main/logback.xml" -Dgrakn.log.dirs="${GRAKN_HOME}/logs" -Dgrakn.dir="${GRAKN_HOME}/bin" ai.grakn.migration.json.JsonMigrator ${1+"$@"}
 elif [ "$1" == "owl" ]
 then
-  java -cp ${CONCATCLASSPATH} -Dlogback.configurationFile="${GRAKN_HOME}/conf/main/logback.xml" -Dgrakn.log.dirs="${GRAKN_HOME}/logs" -Dgrakn.dir="${GRAKN_HOME}/bin" ai.grakn.migration.owl.Main ${1+"$@"}
+  java -cp ${CLASSPATH} -Dlogback.configurationFile="${GRAKN_HOME}/conf/main/logback.xml" -Dgrakn.log.dirs="${GRAKN_HOME}/logs" -Dgrakn.dir="${GRAKN_HOME}/bin" ai.grakn.migration.owl.Main ${1+"$@"}
 elif [ "$1" == "export" ]
 then
-  java -cp ${CONCATCLASSPATH} -Dlogback.configurationFile="${GRAKN_HOME}/conf/main/logback.xml" -Dgrakn.log.dirs="${GRAKN_HOME}/logs" -Dgrakn.dir="${GRAKN_HOME}/bin" ai.grakn.migration.export.Main ${1+"$@"}
+  java -cp ${CLASSPATH} -Dlogback.configurationFile="${GRAKN_HOME}/conf/main/logback.xml" -Dgrakn.log.dirs="${GRAKN_HOME}/logs" -Dgrakn.dir="${GRAKN_HOME}/bin" ai.grakn.migration.export.Main ${1+"$@"}
 elif [ "$1" == "sql" ]
 then
-  java -cp ${CONCATCLASSPATH} -Dlogback.configurationFile="${GRAKN_HOME}/conf/main/logback.xml" -Dgrakn.log.dirs="${GRAKN_HOME}/logs" -Dgrakn.dir="${GRAKN_HOME}/bin" ai.grakn.migration.sql.SQLMigrator ${1+"$@"}
+  java -cp ${CLASSPATH} -Dlogback.configurationFile="${GRAKN_HOME}/conf/main/logback.xml" -Dgrakn.log.dirs="${GRAKN_HOME}/logs" -Dgrakn.dir="${GRAKN_HOME}/bin" ai.grakn.migration.sql.SQLMigrator ${1+"$@"}
 elif [ "$1" == "xml" ]
 then
-  java -cp ${CONCATCLASSPATH} -Dlogback.configurationFile="${GRAKN_HOME}/conf/main/logback.xml" -Dgrakn.log.dirs="${GRAKN_HOME}/logs" -Dgrakn.dir="${GRAKN_HOME}/bin" ai.grakn.migration.xml.XmlMigrator ${1+"$@"}
+  java -cp ${CLASSPATH} -Dlogback.configurationFile="${GRAKN_HOME}/conf/main/logback.xml" -Dgrakn.log.dirs="${GRAKN_HOME}/logs" -Dgrakn.dir="${GRAKN_HOME}/bin" ai.grakn.migration.xml.XmlMigrator ${1+"$@"}
 else
   echo "usage: ./migration.sh {owl, csv, json, export, sql, xml} <params>"
 fi
