0d7275b86f3d9c999b9b1a8d69d81de7ee26bd44
Filipe Peliz Pinto Teixeira <fppintoteixeira@gmail.com>
2017-06-19 12:46:39 +0100

    Move PathGraph

--- grakn-test/src/test/java/ai/grakn/test/graql/reasoner/inference/RecursiveInferenceTest.java
+++ grakn-test/src/test/java/ai/grakn/test/graql/reasoner/inference/RecursiveInferenceTest.java
@@ -24,7 +24,7 @@ import ai.grakn.graphs.DiagonalGraph;
 import ai.grakn.graphs.MatrixGraph;
 import ai.grakn.graphs.MatrixGraphII;
 import ai.grakn.graphs.NguyenGraph;
-import ai.grakn.graphs.PathGraph;
+import ai.grakn.test.graphs.PathGraph;
 import ai.grakn.test.graphs.PathGraphII;
 import ai.grakn.test.graphs.PathGraphSymmetric;
 import ai.grakn.test.graphs.TailRecursionGraph;
