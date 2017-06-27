1d60d57bd5ff85cda4a396e8ff7f2f181a33b07c
Kasper Piskorski <kasper@mindmaps.io>
2017-06-27 15:26:07 +0100

    remove unused lines

--- grakn-graql/src/main/java/ai/grakn/graql/internal/reasoner/query/ReasonerQueryImpl.java
+++ grakn-graql/src/main/java/ai/grakn/graql/internal/reasoner/query/ReasonerQueryImpl.java
@@ -168,8 +168,6 @@ public class ReasonerQueryImpl implements ReasonerQuery {
         Set<Atom> inferredAtoms = inferrableAtoms.stream().map(Atom::inferTypes).collect(Collectors.toSet());
         inferrableAtoms.forEach(this::removeAtomic);
         inferredAtoms.forEach(this::addAtomic);
-        //Sets.difference(inferrableAtoms, inferredAtoms).forEach(this::removeAtomic);
-        //Sets.difference(inferredAtoms, inferrableAtoms).forEach(this::addAtomic);
     }
 
     public GraknGraph graph() {
