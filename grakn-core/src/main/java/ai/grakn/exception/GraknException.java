5d9afa08647c9181c4f43f61311326255c783322
Felix Chapman <aelred717@gmail.com>
2017-07-11 12:12:54 +0100

    Add `equals` and `hashCode` for `GraknException`

--- grakn-core/src/main/java/ai/grakn/exception/GraknException.java
+++ grakn-core/src/main/java/ai/grakn/exception/GraknException.java
@@ -38,4 +38,19 @@ public class GraknException extends RuntimeException {
     protected GraknException(String error, Exception e){
         super(error, e);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GraqlQueryException that = (GraqlQueryException) o;
+
+        return getMessage().equals(that.getMessage());
+    }
+
+    @Override
+    public int hashCode() {
+        return getMessage().hashCode();
+    }
 }
