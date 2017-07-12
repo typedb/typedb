da09304625e956240332d0b83c2d42346eaf6e49
Filipe Peliz Pinto Teixeira <fppintoteixeira@gmail.com>
2017-07-12 18:13:43 +0100

    Make test fail

--- grakn-graph/src/test/java/ai/grakn/graph/internal/ResourceTest.java
+++ grakn-graph/src/test/java/ai/grakn/graph/internal/ResourceTest.java
@@ -139,9 +139,10 @@ public class ResourceTest extends GraphTestBase{
 
         entity.resource(resource);
 
-        RelationStructure relationStructure = RelationImpl.from(entity.relations().iterator().next()).structure();
+        RelationStructure relationStructure = RelationImpl.from(Iterables.getOnlyElement(entity.relations())).structure();
         assertThat(relationStructure, instanceOf(RelationEdge.class));
         assertTrue("Edge Relation id not starting with [" + Schema.PREFIX_EDGE + "]",relationStructure.getId().getValue().startsWith(Schema.PREFIX_EDGE));
+        assertEquals(entity, resource.owner());
     }
 
     @Test
