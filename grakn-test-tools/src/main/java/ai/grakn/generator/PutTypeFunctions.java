909cb26d77599a01acdd31d36f81ee7b94f51649
Felix Chapman <aelred717@gmail.com>
2017-07-19 17:59:50 +0100

    Fix javadoc

--- grakn-test-tools/src/main/java/ai/grakn/generator/PutTypeFunctions.java
+++ grakn-test-tools/src/main/java/ai/grakn/generator/PutTypeFunctions.java
@@ -28,7 +28,7 @@ import com.google.common.collect.ImmutableList;
 import java.util.function.BiFunction;
 
 /**
- * Generator that produces {@link GraknGraph} methods that put an ontology concept in the graph, given {@link Label}.
+ * Generator that produces {@link GraknGraph} methods that put a type in the graph, given {@link Label}.
  *
  * @author Felix Chapman
  */
