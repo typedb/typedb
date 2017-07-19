406d87ba9f486dc29a8cd39c8f15e375d51f8f04
lolski <soulmagic13@gmail.com>
2017-07-19 22:07:53 +0700

    adding a few tests

---
+++ grakn-engine/src/test/java/ai/grakn/engine/supervision/ProcessSupervisionTest.java
@@ -0,0 +1,79 @@
+package ai.grakn.engine.supervision;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.spy;
+
+public class ProcessSupervisionTest {
+  @Test
+  public void shouldNotStartCassandraIfAlreadyRunning() {
+
+  }
+
+  @Test
+  public void shouldStartCassandraIfNoProcessFound() {
+
+  }
+
+  @Test
+  public void shouldStopCassandraIfRunning() {
+
+  }
+
+  @Test
+  public void shouldStartCassandraIfBinaryFound() {
+    // TODO
+  }
+
+  @Test
+  public void shouldThrowIfBinaryNotFound() {
+    // TODO
+  }
+
+  @Test
+  public void shouldStopCassandra() {
+    // TODO
+  }
+
+  @Test
+  public void shouldReturnTrueSinceCassandraIsRunning() {
+    ProcessSupervision supervision = new ProcessSupervision();
+    ProcessSupervision supervisionSpy = spy(supervision);
+
+    when(supervisionSpy.fileExists(anyString())).thenReturn(true);
+    when(supervisionSpy.psP(anyInt())).thenReturn(0);
+
+    assertEquals(supervisionSpy.isCassandraRunning(), true);
+  }
+
+  @Test
+  public void shouldReturnFalseSinceCassandraIsNotRunning() {
+    ProcessSupervision supervision = new ProcessSupervision();
+    ProcessSupervision supervisionSpy = spy(supervision);
+
+    when(supervisionSpy.fileExists(anyString())).thenReturn(false); // simulate file not found
+    when(supervisionSpy.psP(anyInt())).thenReturn(0);
+
+    assertEquals(supervisionSpy.isCassandraRunning(), false);
+  }
+
+  public void shouldThrowIfIncorrectPid() {
+    try {
+      ProcessSupervision supervision = new ProcessSupervision();
+      ProcessSupervision supervisionSpy = spy(supervision);
+
+      when(supervisionSpy.fileExists(anyString())).thenReturn(false);
+      when(supervisionSpy.psP(anyInt())).thenReturn(1); // simulate ps -p unsuccessful return value (due to incorrect PID)
+
+      supervisionSpy.isCassandraRunning();
+
+      assertEquals(true, false); // deliberately return false
+    } catch (RuntimeException e) {
+      assertEquals(true, true); // deliberately return true
+    }
+  }
+}
