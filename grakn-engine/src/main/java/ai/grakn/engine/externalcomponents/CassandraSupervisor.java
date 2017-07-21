5df49ae933754ccac17da47348f5686217d607bb
lolski <soulmagic13@gmail.com>
2017-07-21 19:34:59 +0700

    removing dependencies to grakn-cassandra script (TODO: don't hardcode pid file)

--- grakn-engine/src/main/java/ai/grakn/engine/externalcomponents/CassandraSupervisor.java
+++ grakn-engine/src/main/java/ai/grakn/engine/externalcomponents/CassandraSupervisor.java
@@ -41,7 +41,7 @@ public class CassandraSupervisor {
     public CassandraSupervisor(OperatingSystemCalls osCalls, String baseWorkDir) {
         this.osCalls = osCalls;
 
-        this.cassandraStartCmd = baseWorkDir + "bin/grakn-cassandra.sh start";
+        this.cassandraStartCmd = baseWorkDir + "bin/cassandra -p /tmp/grakn-cassandra.pid";
         this.cassandraStopCmd = baseWorkDir + "bin/grakn-cassandra.sh stop";
         this.nodeToolCheckRunningCmd = baseWorkDir + "bin/nodetool statusthrift 2>/dev/null | tr -d '\\n\\r'";
     }
@@ -85,12 +85,27 @@ public class CassandraSupervisor {
         waitForCassandraStarted();
     }
 
-    public void stop() throws IOException, InterruptedException {
+    public void stop2() throws IOException, InterruptedException {
         Process stopCassandra = osCalls.exec(new String[]{ "sh", "-c", cassandraStopCmd });
         int status = stopCassandra.waitFor();
         System.out.println("== " + cassandraStopCmd + " / " + status + " ==");
         if (status != 0) throw new RuntimeException("unable to stop cassandra - " + cassandraStopCmd);
         waitForCassandraStopped();
+    }
+
+    public void stop() throws IOException, InterruptedException {
+        if (osCalls.fileExists("/tmp/grakn-cassandra.pid")) {
+            int pid = osCalls.catPidFile("/tmp/grakn-cassandra.pid");
+            boolean processRunning = osCalls.psP(pid) == 0;
+            if (processRunning) {
+                // process found, stop it
+                Process kill = Runtime.getRuntime().exec(new String[]{"sh", "-c", "kill " + pid});
+                int status = kill.waitFor();
+                if (status != 0) {
+                    throw new RuntimeException("unable to stop cassandra - " + cassandraStopCmd);
+                }
+            }
+        }
     }
 
     private void waitForCassandraStarted() throws IOException, InterruptedException {
--- grakn-engine/src/main/java/ai/grakn/engine/externalcomponents/OperatingSystemCalls.java
+++ grakn-engine/src/main/java/ai/grakn/engine/externalcomponents/OperatingSystemCalls.java
@@ -23,6 +23,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.stream.Collectors;
 
 /**
@@ -33,19 +35,35 @@ import java.util.stream.Collectors;
 
 
 public class OperatingSystemCalls {
-  public String readStdoutFromProcess(Process process) throws IOException {
-    try (BufferedReader catStdout =
-             new BufferedReader(new InputStreamReader(process.getInputStream(), StandardCharsets.UTF_8))) {
-      return catStdout.lines().collect(Collectors.joining());
+    public boolean fileExists(String path) {
+        return Files.exists(Paths.get(path));
     }
-  }
 
-  public Process exec(String[] args) throws IOException {
-    return Runtime.getRuntime().exec(args);
-  }
+    public int psP(int pid) throws IOException, InterruptedException {
+        Process ps = Runtime.getRuntime().exec(new String[]{"sh", "-c", " ps -p " + pid});
 
-  public boolean isMac() {
-    String OS = System.getProperty("os.name").toLowerCase();
-    return OS.contains("mac");
-  }
+        return ps.waitFor();
+    }
+
+    public int catPidFile(String file) throws IOException {
+        Process catProcess = Runtime.getRuntime().exec(new String[]{"sh", "-c", "cat " + file});
+        String lines = readStdoutFromProcess(catProcess);
+        return Integer.parseInt(lines);
+    }
+
+    public String readStdoutFromProcess(Process process) throws IOException {
+        try (BufferedReader catStdout =
+                 new BufferedReader(new InputStreamReader(process.getInputStream(), StandardCharsets.UTF_8))) {
+            return catStdout.lines().collect(Collectors.joining());
+        }
+    }
+
+    public Process exec(String[] args) throws IOException {
+        return Runtime.getRuntime().exec(args);
+    }
+
+    public boolean isMac() {
+        String OS = System.getProperty("os.name").toLowerCase();
+        return OS.contains("mac");
+    }
 }
