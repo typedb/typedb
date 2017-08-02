2e9caa9c0749f338f28de2494d98119382b35cd0
Sheldon <sheldon@mindmaps.io>
2017-08-02 12:12:18 +0100

    fix pmd violation

--- grakn-test/test-snb/src/main/java/ai/grakn/GraknQueryHandlers.java
+++ grakn-test/test-snb/src/main/java/ai/grakn/GraknQueryHandlers.java
@@ -45,7 +45,6 @@ import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
