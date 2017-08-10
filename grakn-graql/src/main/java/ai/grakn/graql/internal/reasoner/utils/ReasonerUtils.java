6828deece6ee841762159757f665c6ff7bc14df6
Kasper Piskorski <kasper@mindmaps.io>
2017-08-10 11:59:58 +0100

    make pmd happy

--- grakn-graql/src/main/java/ai/grakn/graql/internal/reasoner/utils/ReasonerUtils.java
+++ grakn-graql/src/main/java/ai/grakn/graql/internal/reasoner/utils/ReasonerUtils.java
@@ -46,7 +46,6 @@ import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Sets;
-import ai.grakn.graql.internal.reasoner.utils.Pair;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
