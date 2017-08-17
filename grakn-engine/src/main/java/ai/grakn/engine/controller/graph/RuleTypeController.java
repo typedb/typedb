a90d3f84a95b806ce31aaae84caa5ee2706f8675
lolski <soulmagic13@gmail.com>
2017-08-17 22:25:02 +0700

    adding rule type

--- grakn-engine/src/main/java/ai/grakn/engine/controller/graph/RuleTypeController.java
+++ grakn-engine/src/main/java/ai/grakn/engine/controller/graph/RuleTypeController.java
@@ -1,3 +1,20 @@
+/*
+ * Grakn - A Distributed Semantic Database
+ * Copyright (C) 2016  Grakn Labs Limited
+ *
+ * Grakn is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Grakn is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Grakn. If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ */
 package ai.grakn.engine.controller.graph;
 
 import ai.grakn.GraknGraph;
@@ -20,6 +37,14 @@ import static ai.grakn.engine.controller.util.Requests.mandatoryBody;
 import static ai.grakn.engine.controller.util.Requests.mandatoryPathParameter;
 import static ai.grakn.engine.controller.util.Requests.mandatoryQueryParameter;
 import static ai.grakn.util.REST.Request.KEYSPACE;
+
+/**
+ * <p>
+ *     Endpoints for Graph API
+ * </p>
+ *
+ * @author Ganeshwara Herawan Hananda
+ */
 
 public class RuleTypeController {
     private final EngineGraknGraphFactory factory;
--- grakn-engine/src/test/java/ai/grakn/engine/controller/graph/RuleControllerTest.java
+++ grakn-engine/src/test/java/ai/grakn/engine/controller/graph/RuleControllerTest.java
@@ -1,3 +1,21 @@
+/*
+ * Grakn - A Distributed Semantic Database
+ * Copyright (C) 2016  Grakn Labs Limited
+ *
+ * Grakn is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Grakn is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Grakn. If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ */
+
 package ai.grakn.engine.controller.graph;
 
 import ai.grakn.GraknGraph;
@@ -58,13 +76,13 @@ public class RuleControllerTest {
     public void getRuleFromMovieGraphShouldExecuteSuccessfully() {
         Response response = with()
             .queryParam(KEYSPACE, mockGraph.getKeyspace())
-            .get("/graph/ruleType/production-with-cluster");
+            .get("/graph/ruleType/a-rule-type");
 
         Map<String, Object> responseBody = Json.read(response.body().asString()).asMap();
 
         assertThat(response.statusCode(), equalTo(200));
         assertThat(responseBody.get("conceptId"), notNullValue());
-        assertThat(responseBody.get("ruleTypeLabel"), equalTo("production-with-cluster"));
+        assertThat(responseBody.get("ruleTypeLabel"), equalTo("a-rule-type"));
     }
 
     @Test
