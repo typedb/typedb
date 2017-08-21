e7acaf1d145bfbb4cdb0d09870ae03166dba49f2
Filipe Peliz Pinto Teixeira <fppintoteixeira@gmail.com>
2017-08-21 08:21:20 +0100

    Fix test

--- grakn-graql/src/test/java/ai/grakn/graql/internal/query/AutocompleteTest.java
+++ grakn-graql/src/test/java/ai/grakn/graql/internal/query/AutocompleteTest.java
@@ -71,7 +71,7 @@ public class AutocompleteTest {
     public void whenAutocompletingWithinWord_AppropriateCandidatesAreFound() {
         String queryString = "match $x has title";
         Autocomplete autocomplete = Autocomplete.create(types, queryString, 11);
-        assertThat(autocomplete.getCandidates(), hasItem("has"));
+        assertThat(autocomplete.getCandidates(), hasItem(" "));//Nothing returned because only "has" was found
         assertThat(autocomplete.getCandidates(), not(hasItem("delete")));
         assertEquals(9, autocomplete.getCursorPosition());
     }
