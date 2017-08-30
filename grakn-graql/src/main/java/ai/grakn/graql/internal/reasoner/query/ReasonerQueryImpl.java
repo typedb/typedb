275bae60a62104dec316fc3b6e90669d47e79559
Kasper Piskorski <muydeemer@o2.pl>
2017-08-30 06:58:00 +0100

    add neq complement state (#2036)

--- grakn-graql/src/main/java/ai/grakn/graql/internal/reasoner/query/ReasonerQueryImpl.java
+++ grakn-graql/src/main/java/ai/grakn/graql/internal/reasoner/query/ReasonerQueryImpl.java
@@ -383,7 +383,10 @@ public class ReasonerQueryImpl implements ReasonerQuery {
         return this;
     }
 
-    public boolean hasFullSubstitution(){
+    /**
+     * @return true if this query is a ground query
+     */
+    public boolean isGround(){
         return getSubstitution().keySet().containsAll(getVarNames());
     }
 
