ca9d1c31acb1dbf40fa60d17e74b40b8220a786d
Felix Chapman <aelred717@gmail.com>
2017-09-07 09:52:07 +0100

    Reformat validate.sh

--- grakn-test/test-snb/src/main/bash/validate.sh
+++ grakn-test/test-snb/src/main/bash/validate.sh
@@ -7,7 +7,14 @@ BRANCH_NAME=$1
 LDBC_CONNECTOR=${WORKSPACE}/grakn-test/test-snb/target/test-snb-${BRANCH_NAME}-jar-with-dependencies.jar
 
 # execute validation
-java -cp ${LDBC_DRIVER}:${LDBC_CONNECTOR} com.ldbc.driver.Client -db ai.grakn.GraknDb -P ${LDBC_VALIDATION_CONFIG} -vdb ${CSV_DATA}/validation_params.csv -p ldbc.snb.interactive.parameters_dir ${CSV_DATA} -p ai.grakn.uri ${ENGINE} -p ai.grakn.keyspace ${KEYSPACE}
+java \
+    -cp ${LDBC_DRIVER}:${LDBC_CONNECTOR} com.ldbc.driver.Client \
+    -db ai.grakn.GraknDb \
+    -P ${LDBC_VALIDATION_CONFIG} \
+    -vdb ${CSV_DATA}/validation_params.csv \
+    -p ldbc.snb.interactive.parameters_dir ${CSV_DATA} \
+    -p ai.grakn.uri ${ENGINE} \
+    -p ai.grakn.keyspace ${KEYSPACE}
 
 # check for errors from Grakn
 check-errors.sh fail
