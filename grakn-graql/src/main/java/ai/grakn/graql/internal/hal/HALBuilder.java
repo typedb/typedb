1527809485372591c79e2d4eabdfdfd6784268ac
Marco Scoppetta <marco@grakn.ai>
2017-09-13 09:51:06 +0100

    Update to new syntax (#2089)
    * Updated HALBuilder and Dashboard to latest Graql syntax
    
    * Trim query before inserting limit and offset. Updated regex to recognise get as keyword
    
    * Fixed test

--- grakn-graql/src/main/java/ai/grakn/graql/internal/hal/HALBuilder.java
+++ grakn-graql/src/main/java/ai/grakn/graql/internal/hal/HALBuilder.java
@@ -179,7 +179,7 @@ public class HALBuilder {
         String varsWithIds = stringBuilderVarsWithIds.toString();
         String parenthesis = stringBuilderParenthesis.deleteCharAt(stringBuilderParenthesis.length() - 1).append(')').toString();
 
-        String withoutUrl = String.format(ASSERTION_URL, keyspace, varsWithIds, "", parenthesis, isaString, "", limit);
+        String withoutUrl = String.format(ASSERTION_URL, keyspace, varsWithIds, "", parenthesis, isaString, "get;", limit);
 
         String URL = REST.WebPath.Dashboard.EXPLAIN;
 
@@ -227,9 +227,9 @@ public class HALBuilder {
         String parenthesis = stringBuilderParenthesis.deleteCharAt(stringBuilderParenthesis.length() - 1).append(')').toString();
 
         String dollarR = (isInferred) ? "" : "$r";
-        String selectR = (isInferred) ? "" : "select $r;";
+        String getR = (isInferred) ? "get;" : "get $r;";
 
-        String withoutUrl = String.format(ASSERTION_URL, keyspace, varsWithIds, dollarR, parenthesis, isaString, selectR, limit);
+        String withoutUrl = String.format(ASSERTION_URL, keyspace, varsWithIds, dollarR, parenthesis, isaString, getR, limit);
 
         String URL = (isInferred) ? REST.WebPath.Dashboard.EXPLAIN : REST.WebPath.KB.GRAQL;
 
