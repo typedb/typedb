d1f7391b3f38e3e0b6456759abebba6ff36a7d82
lolski <soulmagic13@gmail.com>
2017-09-14 19:24:29 +0700

    errors in Client class are now logged to file

--- grakn-client/src/main/java/ai/grakn/client/Client.java
+++ grakn-client/src/main/java/ai/grakn/client/Client.java
@@ -24,6 +24,8 @@ import ai.grakn.util.REST;
 import mjson.Json;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ResponseHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -46,6 +48,8 @@ import static java.util.stream.Collectors.joining;
  */
 public class Client {
 
+    private static final Logger LOG = LoggerFactory.getLogger(Client.class);
+
     final ResponseHandler<Json> asJsonHandler = response -> {
         try(BufferedReader reader = new BufferedReader(
                 new InputStreamReader(response.getEntity().getContent(), StandardCharsets.UTF_8))){
@@ -61,7 +65,13 @@ public class Client {
     };
 
     public static void main(String[] args) throws IOException {
-        int result = checkServerRunning();
+        int result;
+        try {
+            result = checkServerRunning();
+        } catch (Exception e) {
+            LOG.error("An exception has occurred", e);
+            throw e;
+        }
         System.exit(result);
     }
 
@@ -70,7 +80,7 @@ public class Client {
 
         if (confPath == null) {
             String msg = "System property `" + GraknSystemProperty.CONFIGURATION_FILE.key() + "` has not been set";
-            System.err.println(msg);
+            LOG.error(msg);
             return 2;
         }
 
@@ -78,13 +88,16 @@ public class Client {
         try (FileInputStream stream = new FileInputStream(confPath)) {
             properties.load(stream);
         } catch (FileNotFoundException e) {
-            System.err.println("Could not find config file at `" + confPath + "`");
+            LOG.error("Could not find config file at `" + confPath + "`");
             return 2;
         }
 
-        if (serverIsRunning(properties.getProperty("server.host") + ":" + properties.getProperty("server.port"))) {
+        String serverUri = properties.getProperty("server.host") + ":" + properties.getProperty("server.port");
+        if (serverIsRunning(serverUri)) {
+            LOG.info("Server " + serverUri + " is running");
             return 0;
         } else {
+            LOG.info("Server " + serverUri + " is not running");
             return 1;
         }
     }
@@ -103,9 +116,11 @@ public class Client {
 
             InputStream inputStream = connection.getInputStream();
             if (inputStream.available() == 0) {
+                LOG.error("input stream is not available");
                 return false;
             }
         } catch (IOException e) {
+            LOG.error("An exception has occurred", e);
             return false;
         }
         return true;
