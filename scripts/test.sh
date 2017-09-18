272b2f959bce98b0623d24f2ecac25aeaad5cec0
Felix Chapman <aelred717@gmail.com>
2017-09-18 17:21:09 +0100

    Fix script typo

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-mvn test --projects "$1" -Dtest=**/*$2* -DfailIfNoTests=false jacoco:report
+mvn test --projects "$1" -Dtest="**/*$2*" -DfailIfNoTests=false jacoco:report
 bash <(curl -s https://codecov.io/bash)
