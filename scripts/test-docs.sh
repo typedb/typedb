d67c196cb86af8aa8b155c75ac197284d5c05591
Felix Chapman <aelred717@gmail.com>
2017-09-19 10:56:59 +0100

    Make sure scripts fail fast

--- scripts/test-docs.sh
+++ scripts/test-docs.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 cd docs
 
 rake dependencies
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+set -e
+
 mvn test --projects "$1" -Dtest="**/*$2*" -DfailIfNoTests=false jacoco:report
 bash <(curl -s https://codecov.io/bash)
