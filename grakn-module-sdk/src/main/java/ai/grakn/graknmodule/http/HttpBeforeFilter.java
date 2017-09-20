1f03e4bf56b0cf729942be2eaea95cc4537ca030
lolski <soulmagic13@gmail.com>
2017-09-20 22:01:37 +0700

    adding descriptions

--- grakn-module-sdk/src/main/java/ai/grakn/graknmodule/http/HttpBeforeFilter.java
+++ grakn-module-sdk/src/main/java/ai/grakn/graknmodule/http/HttpBeforeFilter.java
@@ -1,5 +1,30 @@
 package ai.grakn.graknmodule.http;
 
+/*
+ * Grakn - A Distributed Semantic Database
+ * Copyright (C) 2016  Grakn Labs Limited
+ *
+ * Grakn is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Grakn is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Grakn. If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ *
+ */
+
+/**
+ * A class representing an HTTP endpoint before filter, supported by a {@link ai.grakn.graknmodule.GraknModule}
+ *
+ * @author Ganeshwara Herawan Hananda
+ */
+
 public interface HttpBeforeFilter {
     String getUrlPattern();
 
