ec9f35a82271e9a30b740e71bb98472c33ffbec5
Felix Chapman <aelred717@gmail.com>
2017-09-27 10:34:36 +0100

    Allow changing schema concept label using the ID

--- grakn-graql/src/test/java/ai/grakn/graql/internal/query/DefineQueryTest.java
+++ grakn-graql/src/test/java/ai/grakn/graql/internal/query/DefineQueryTest.java
@@ -22,6 +22,7 @@ package ai.grakn.graql.internal.query;
 import ai.grakn.concept.AttributeType;
 import ai.grakn.concept.ConceptId;
 import ai.grakn.concept.EntityType;
+import ai.grakn.concept.Label;
 import ai.grakn.exception.GraqlQueryException;
 import ai.grakn.graql.DefineQuery;
 import ai.grakn.graql.Graql;
@@ -408,6 +409,16 @@ public class DefineQueryTest {
         ));
 
         qb.define(var().id(id).has("title", "Bob")).execute();
+    }
+
+    @Test
+    public void whenSpecifyingLabelOfAnExistingConcept_LabelIsChanged() {
+        EntityType movie = movies.tx().getEntityType("movie");
+        Label newLabel = Label.of("film");
+
+        qb.define(label(newLabel).id(movie.getId())).execute();
+
+        assertEquals(newLabel, movie.getLabel());
     }
 
     @Test
