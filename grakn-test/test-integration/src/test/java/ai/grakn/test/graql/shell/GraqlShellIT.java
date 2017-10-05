8653d21a95216d08948c550ecfc919870a40cabd
Filipe Peliz Pinto Teixeira <fppintoteixeira@gmail.com>
2017-10-05 11:24:27 +0100

    Disable logging which was causing shell tests to fail

--- grakn-test/test-integration/src/test/java/ai/grakn/test/graql/shell/GraqlShellIT.java
+++ grakn-test/test-integration/src/test/java/ai/grakn/test/graql/shell/GraqlShellIT.java
@@ -71,8 +71,7 @@ import static org.junit.Assume.assumeFalse;
 public class GraqlShellIT {
 
     @ClassRule
-    public static final DistributionContext dist = DistributionContext.startInMemoryEngineProcess().inheritIO(true);
-
+    public static final DistributionContext dist = DistributionContext.startInMemoryEngineProcess().inheritIO(false);
     private static InputStream trueIn;
     private static PrintStream trueOut;
     private static PrintStream trueErr;
