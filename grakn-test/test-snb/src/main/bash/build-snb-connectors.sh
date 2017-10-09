f39ee645c8f09945ab38d25d52e543119971f3ce
Felix Chapman <aelred717@gmail.com>
2017-10-09 14:10:08 +0100

    Revert "Build all dependencies of SNB"
    This reverts commit 7345cb9c9869d444c78a016fc9ae4f86c8321fa5.

--- grakn-test/test-snb/src/main/bash/build-snb-connectors.sh
+++ grakn-test/test-snb/src/main/bash/build-snb-connectors.sh
@@ -2,4 +2,6 @@
 
 source env.sh
 
-mvn clean package --projects grakn-test/test-snb --also-make assembly:single --batch-mode -Dmaven.repo.local=${WORKSPACE}/maven -DskipTests -Dcheckstyle.skip=true -Dfindbugs.skip=true -Dpmd.skip=true
+pushd grakn-test/test-snb
+mvn clean package assembly:single --batch-mode -Dmaven.repo.local=${WORKSPACE}/maven -DskipTests -Dcheckstyle.skip=true -Dfindbugs.skip=true -Dpmd.skip=true
+popd
