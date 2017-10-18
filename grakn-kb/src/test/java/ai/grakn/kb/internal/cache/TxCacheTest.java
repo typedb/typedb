1a73e83f8d1c8c8ef6b9e489768f7a8bcb558b61
Filipe Peliz Pinto Teixeira <fppintoteixeira@gmail.com>
2017-10-18 15:58:26 +0100

    Fix tests

--- grakn-kb/src/test/java/ai/grakn/kb/internal/cache/TxCacheTest.java
+++ grakn-kb/src/test/java/ai/grakn/kb/internal/cache/TxCacheTest.java
@@ -33,9 +33,6 @@ import ai.grakn.kb.internal.GraknTxAbstract;
 import ai.grakn.kb.internal.TxTestBase;
 import ai.grakn.kb.internal.concept.RelationshipImpl;
 import ai.grakn.kb.internal.structure.Casting;
-import ai.grakn.util.REST;
-import ai.grakn.util.Schema;
-import mjson.Json;
 import org.hamcrest.Matcher;
 import org.junit.Test;
 
@@ -138,34 +135,6 @@ public class TxCacheTest extends TxTestBase {
 
         i1.delete();
         assertThat(tx.txCache().getModifiedThings(), is(empty()));
-    }
-
-    @Test
-    public void whenNoOp_EnsureLogWellFormed() {
-        Json expected = Json.read("{\"" + REST.Request.COMMIT_LOG_FIXING +
-                "\":{\"" + Schema.BaseType.ATTRIBUTE.name() + "\":{}},\"" +
-                REST.Request.COMMIT_LOG_COUNTING + "\":[]}");
-        assertEquals("Unexpected graph logs", expected, tx.commitLog().getFormattedLog());
-    }
-
-    @Test
-    public void whenAddingEntities_EnsureLogIsFilledAfterCommit() {
-        EntityType entityType = tx.putEntityType("My Type");
-        entityType.addEntity();
-        entityType.addEntity();
-        Json emptyLog = Json.read("{\"" + REST.Request.COMMIT_LOG_FIXING +
-                "\":{\"" + Schema.BaseType.ATTRIBUTE.name() + "\":{}},\"" +
-                REST.Request.COMMIT_LOG_COUNTING + "\":[]}");
-        assertEquals("Logs are not empty", emptyLog, tx.commitLog().getFormattedLog());
-
-        tx.commit();
-
-        Json filledLog = Json.read("{\"" + REST.Request.COMMIT_LOG_FIXING +
-                "\":{\"" + Schema.BaseType.ATTRIBUTE.name() +
-                "\":{}},\"" + REST.Request.COMMIT_LOG_COUNTING  +
-                "\":[{\"" + REST.Request.COMMIT_LOG_CONCEPT_ID +
-                "\":\"" + entityType.getId() + "\",\"" + REST.Request.COMMIT_LOG_SHARDING_COUNT + "\":2}]}");
-        assertEquals("Logs are empty", filledLog, tx.commitLog().getFormattedLog());
     }
 
     @Test
