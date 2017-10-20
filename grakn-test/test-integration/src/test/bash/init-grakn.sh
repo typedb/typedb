94189ed513f1ec5ef5861fba99562fb57ad88e4f
Felix Chapman <aelred717@gmail.com>
2017-10-20 14:09:26 +0100

    Change logs on jenkins back to INFO

--- grakn-test/test-integration/src/test/bash/init-grakn.sh
+++ grakn-test/test-integration/src/test/bash/init-grakn.sh
@@ -8,8 +8,4 @@ mkdir ${PACKAGE}
 
 tar -xf grakn-dist/target/grakn-dist*.tar.gz --strip=1 -C ${PACKAGE}
 
-# set DEBUG log level
-# TODO: support this in a more elegant way (command line arg?)
-sed -i'' -e 's/log.level=INFO/log.level=DEBUG/g' "${PACKAGE}/conf/grakn.properties"
-
 grakn server start
