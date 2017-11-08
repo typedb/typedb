148abed48db0c10d9c2642108fa74fb2bac4a9fe
Felix Chapman <aelred717@gmail.com>
2017-11-08 12:48:15 +0000

    make something work

--- grakn-test/test-snb/src/main/bash/build-snb-connectors.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-source env.sh
-
-mvn clean package --also-make --projects grakn-test/test-snb --batch-mode -Dmaven.repo.local=${WORKSPACE}/maven -DskipTests -Dcheckstyle.skip=true -Dfindbugs.skip=true -Dpmd.skip=true
--- grakn-test/test-snb/src/main/bash/validate.sh
+++ grakn-test/test-snb/src/main/bash/validate.sh
@@ -2,21 +2,18 @@
 
 source snb-env.sh
 
-build-snb-connectors.sh
-
-LDBC_CONNECTOR=${WORKSPACE}/grakn-test/test-snb/target/test-snb-*-jar-with-dependencies.jar
-
 LDBC_VALIDATION_CONFIG=${WORKSPACE}/grakn-test/test-snb/src/main/bash/readwrite_grakn--ldbc_driver_config--db_validation.properties
 
 # execute validation
-java \
-    -classpath ${LDBC_CONNECTOR} com.ldbc.driver.Client \
-    -db ai.grakn.GraknDb \
-    -P ${LDBC_VALIDATION_CONFIG} \
-    -vdb ${CSV_DATA}/validation_params.csv \
-    -p ldbc.snb.interactive.parameters_dir ${CSV_DATA} \
-    -p ai.grakn.uri ${ENGINE} \
+mvn install -DskipTests --also-make --projects grakn-test/test-snb
+mvn exec:java --projects grakn-test/test-snb -Dexec.mainClass=com.ldbc.driver.Client -Dexec.args="
+    -db ai.grakn.GraknDb
+    -P ${LDBC_VALIDATION_CONFIG}
+    -vdb ${CSV_DATA}/validation_params.csv
+    -p ldbc.snb.interactive.parameters_dir ${CSV_DATA}
+    -p ai.grakn.uri ${ENGINE}
     -p ai.grakn.keyspace ${KEYSPACE}
+"
 
 # check for errors from Grakn
 check-errors.sh fail
