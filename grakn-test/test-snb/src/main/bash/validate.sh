0d7cb576e2d45190c2e7d9db3c65b95c03026b73
Felix Chapman <aelred717@gmail.com>
2017-11-08 15:05:36 +0000

    Add back some stuff I thought was unimportant

--- grakn-test/test-snb/src/main/bash/validate.sh
+++ grakn-test/test-snb/src/main/bash/validate.sh
@@ -7,11 +7,11 @@ LDBC_VALIDATION_CONFIG=${WORKSPACE}/grakn-test/test-snb/src/main/bash/readwrite_
 # execute validation
 mvn install -DskipTests --also-make --projects grakn-test/test-snb
 mvn exec:java --projects grakn-test/test-snb -Dexec.mainClass=com.ldbc.driver.Client -Dexec.args="
-    -db ai.grakn.GraknDb
-    -P ${LDBC_VALIDATION_CONFIG}
-    -vdb ${CSV_DATA}/validation_params.csv
-    -p ldbc.snb.interactive.parameters_dir ${CSV_DATA}
-    -p ai.grakn.uri ${ENGINE}
+    -db ai.grakn.GraknDb \
+    -P ${LDBC_VALIDATION_CONFIG} \
+    -vdb ${CSV_DATA}/validation_params.csv \
+    -p ldbc.snb.interactive.parameters_dir ${CSV_DATA} \
+    -p ai.grakn.uri ${ENGINE} \
     -p ai.grakn.keyspace ${KEYSPACE}
 "
 
