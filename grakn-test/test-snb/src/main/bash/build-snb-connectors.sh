314a58e662ff9e02edaa3fde4e935dc070094b95
Felix Chapman <aelred717@gmail.com>
2017-11-09 10:24:09 +0000

    Fix dependency issue with SNB (#2344)
    * 🐘
    
    * Clean up how validation starts and fix dependency
    
    * Explain why I'm doing a dumb thing
    
    * Remove duplicate dependency
    
    * make something work
    
    * Add back some stuff I thought was unimportant
    
    * Fix final(?) issue with SNB dependencies
    
    * Put dependency back
    
    * Revert "🐘"
    
    This reverts commit d6d6d3179911b6fcecbfb71a1bff53523e259621.

--- grakn-test/test-snb/src/main/bash/build-snb-connectors.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-source env.sh
-
-pushd grakn-test/test-snb
-mvn clean package assembly:single --batch-mode -Dmaven.repo.local=${WORKSPACE}/maven -DskipTests -Dcheckstyle.skip=true -Dfindbugs.skip=true -Dpmd.skip=true
-popd
