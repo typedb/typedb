70d39384e116eb762728c9f59823ee4191aa579f
Felix Chapman <felix@grakn.ai>
2017-11-16 16:08:12 +0000

    Fix script

--- grakn-test/test-snb/src/main/bash/validate.sh
+++ grakn-test/test-snb/src/main/bash/validate.sh
@@ -4,7 +4,7 @@ source snb-env.sh
 
 LDBC_VALIDATION_CONFIG=${WORKSPACE}/grakn-test/test-snb/src/main/bash/readwrite_grakn--ldbc_driver_config--db_validation.properties
 
-graql console -e -k ${KEYSPACE} 'compute count;'
+graql console -k ${KEYSPACE} -e 'compute count;'
 
 # execute validation
 mvn install -DskipTests --also-make --projects grakn-test/test-snb
@@ -19,7 +19,7 @@ mvn exec:java --projects grakn-test/test-snb \
     -p ai.grakn.uri ${ENGINE} \
     -p ai.grakn.keyspace ${KEYSPACE}"
 
-graql console -e -k ${KEYSPACE} 'compute count;'
+graql console -k ${KEYSPACE} -e 'compute count;'
 
 # check for errors from LDBC
 FAILURES=$(cat ${CSV_DATA}/validation_params-failed-actual.json)
