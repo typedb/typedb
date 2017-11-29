83b7f42bb0699e356ed9349a064d7a82751d1923
Felix Chapman <felix@grakn.ai>
2017-11-29 10:25:17 +0000

    Remove unnecessary script

--- grakn-test/test-snb/src/main/bash/load.sh
+++ grakn-test/test-snb/src/main/bash/load.sh
@@ -5,6 +5,6 @@ source env.sh
 # this a bash trick to specify the default value
 args=${@:-arch validate}
 
-download-snb.sh
+wget https://github.com/ldbc/ldbc_snb_interactive_validation/raw/master/neo4j/readwrite_neo4j--validation_set.tar.gz
 load-SNB.sh ${args}
 measure-size.sh
