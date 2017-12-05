a504ab7d67e59d0d2abf92ea6ad69c741bf88175
Filipe Peliz Pinto Teixeira <fppintoteixeira@gmail.com>
2017-12-05 16:40:21 +0200

    Log when retrying transaction (#2463)
    * 🐘
    
    * Retry listener
    
    * Fix PMD issue
    
    * Odd api
    
    * bye bye

--- grakn-engine/src/main/java/ai/grakn/engine/controller/GraqlController.java
+++ grakn-engine/src/main/java/ai/grakn/engine/controller/GraqlController.java
@@ -40,7 +40,9 @@ import ai.grakn.graql.internal.printer.Printers;
 import ai.grakn.util.REST;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.Timer;
+import com.github.rholder.retry.Attempt;
 import com.github.rholder.retry.RetryException;
+import com.github.rholder.retry.RetryListener;
 import com.github.rholder.retry.Retryer;
 import com.github.rholder.retry.RetryerBuilder;
 import com.github.rholder.retry.StopStrategies;
@@ -91,6 +93,7 @@ import static java.lang.Boolean.parseBoolean;
  */
 public class GraqlController {
     private static final Logger LOG = LoggerFactory.getLogger(GraqlController.class);
+    private static final RetryLogger retryLogger = new RetryLogger();
     private static final int MAX_RETRY = 10;
     private final EngineGraknTxFactory factory;
     private final Service spark;
@@ -176,6 +179,7 @@ public class GraqlController {
         try {
             Retryer<Object> retryer = RetryerBuilder.newBuilder()
                 .retryIfExceptionOfType(TemporaryWriteException.class)
+                .withRetryListener(retryLogger)
                 .withWaitStrategy(WaitStrategies.exponentialWait(100, 5, TimeUnit.MINUTES))
                 .withStopStrategy(StopStrategies.stopAfterAttempt(MAX_RETRY))
                 .build();
@@ -187,6 +191,15 @@ public class GraqlController {
                 throw (RuntimeException) cause;
             } else {
                 throw e;
+            }
+        }
+    }
+
+    private static class RetryLogger implements RetryListener{
+        @Override
+        public <V> void onRetry(Attempt<V> attempt) {
+            if(attempt.hasException()) {
+                LOG.warn("Retrying transaction after {" + attempt.getAttemptNumber() + "} attempts due to exception {" + attempt.getExceptionCause().getMessage() + "}");
             }
         }
     }
