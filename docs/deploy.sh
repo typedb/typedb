1f34205b890613d8428dbddca1b8704760eb5c13
Harjyot Singh <harjyot7@gmail.com>
2017-12-05 01:59:56 +0000

    Deploy script updated

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -9,7 +9,7 @@ git add -A
 git commit -m "Site update"
 echo "Pushing to Heroku"
 cd ../
-git subtree push --prefix docs https://git.heroku.com/docs-grakn.git master
+git push https://git.heroku.com/docs-grakn.git `git subtree split --prefix docs docs-deploy-temp`:master --force
 echo "Deleting temporary branch"
 git checkout docs-redesign
 git branch -D docs-deploy-temp
