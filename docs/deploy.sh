561fac60b96ee8402de642d503af54c6dc588f2f
Harjyot Singh <harjyot7@gmail.com>
2017-12-05 17:46:55 +0000

    Updated deploy script

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-echo "Checking out temporary branch"
-git checkout -b docs-deploy-temp
+echo "Checking out temporary branch from stable"
+git checkout -b docs-deploy-temp stable
 echo "Building _site"
 rake build
 echo "Staging new files"
@@ -9,7 +8,7 @@ git add -A
 git commit -m "Site update"
 echo "Pushing to Heroku"
 cd ../
-git push https://git.heroku.com/docs-grakn.git `git subtree split --prefix docs docs-deploy-temp`:master --force
+git push git@heroku.com:docs-grakn.git `git subtree split --prefix docs docs-deploy-temp`:master --force
 echo "Deleting temporary branch"
-git checkout docs-redesign
+git checkout stable
 git branch -D docs-deploy-temp
