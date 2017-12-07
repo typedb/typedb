c2d058843133fa4365ea5dad65a96fc5752adb93
Kasper Piskorski <kasper@mindmaps.io>
2017-12-07 19:07:29 +0000

    resolve conflicts

--- grakn-test/test-integration/src/test/java/ai/grakn/test/graql/reasoner/BenchmarkTests.java
+++ grakn-test/test-integration/src/test/java/ai/grakn/test/graql/reasoner/BenchmarkTests.java
@@ -29,34 +29,20 @@ import ai.grakn.client.GraknClient;
 import ai.grakn.concept.ConceptId;
 import ai.grakn.concept.RelationshipType;
 import ai.grakn.concept.Role;
-import ai.grakn.concept.Thing;
-import ai.grakn.graql.DefineQuery;
 import ai.grakn.graql.GetQuery;
 import ai.grakn.graql.Graql;
 import ai.grakn.graql.InsertQuery;
 import ai.grakn.graql.Pattern;
-import ai.grakn.graql.Query;
 import ai.grakn.graql.Var;
-import ai.grakn.graql.VarPattern;
 import ai.grakn.graql.admin.Answer;
-import ai.grakn.test.kbs.RandomLinearTransitivityKB;
 import ai.grakn.test.rule.EngineContext;
-import ai.grakn.test.rule.SampleKBContext;
-import ai.grakn.test.rule.SessionContext;
 import com.google.common.collect.HashMultimap;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Multimap;
 import com.google.common.io.Files;
 import java.io.File;
 import java.nio.charset.Charset;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
-import java.util.Set;
-import java.util.stream.Collectors;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
@@ -67,10 +53,6 @@ import static ai.grakn.graql.Graql.var;
 import static ai.grakn.util.SampleKBLoader.randomKeyspace;
 
 public class BenchmarkTests {
-
-    //Needed to start cass depending on profile
-    @ClassRule
-    public static final SessionContext sessionContext = SessionContext.create();
 
     private static final Logger LOG = LoggerFactory.getLogger(BenchmarkTests.class);
 
@@ -85,9 +67,7 @@ public class BenchmarkTests {
         this.session = Grakn.session(engine.uri(), keyspace);
     }
 
-    public void load(String fileName, int N){
-        GraknClient graknClient = new GraknClient(engine.uri());
-
+    private void loadOntology(String fileName, GraknSession session){
         try {
             File graqlFile = new File(GraknSystemProperty.PROJECT_RELATIVE_DIR.value() + "/grakn-test-tools/src/main/graql/" + fileName);
             String s = Files.toString(graqlFile, Charset.forName("UTF-8"));
@@ -96,27 +76,33 @@ public class BenchmarkTests {
             tx.commit();
             tx.close();
         } catch (Exception e){
-            System.out.println(e);
+            System.err.println(e);
         }
+    }
 
+    private void loadEntities(String entityLabel, int N, GraknClient graknClient){
         try(BatchExecutorClient loader = BatchExecutorClient.newBuilder().taskClient(graknClient).build()){
             for(int i = 0 ; i < N ;i++){
-                InsertQuery entityInsert = Graql.insert(var().asUserDefined().isa("a-entity"));
+                InsertQuery entityInsert = Graql.insert(var().asUserDefined().isa(entityLabel));
                 loader.add(entityInsert, keyspace).subscribe(System.out::println);
             }
         }
+    }
 
+    private void loadRelations(String entityLabel, String fromRoleLabel, String toRoleLabel, String relationLabel, int N,
+                               GraknSession session, GraknClient graknClient, Keyspace keyspace){
         try(BatchExecutorClient loader = BatchExecutorClient.newBuilder().taskClient(graknClient).build()) {
             GraknTx tx = session.open(GraknTxType.READ);
-            ConceptId[] aInstances = tx.graql().<GetQuery>parse("match $x isa a-entity; get;").execute().stream()
-                    .map(ans -> ans.get("x").getId())
+            Var entityVar = var().asUserDefined();
+            ConceptId[] instances = tx.graql().match(entityVar.isa(entityLabel)).get().execute().stream()
+                    .map(ans -> ans.get(entityVar).getId())
                     .toArray(ConceptId[]::new);
 
-            System.out.println(aInstances.length);
-            assert(aInstances.length == N);
-            Role Qfrom = tx.getRole("Q-from");
-            Role Qto = tx.getRole("Q-to");
-            RelationshipType Q = tx.getRelationshipType("Q");
+            System.out.println(instances.length);
+            assert(instances.length == N);
+            Role fromRole = tx.getRole(fromRoleLabel);
+            Role toRole = tx.getRole(toRoleLabel);
+            RelationshipType relationType = tx.getRelationshipType(relationLabel);
 
             Random rand = new Random();
             Multimap<Integer, Integer> assignmentMap = HashMultimap.create();
@@ -128,40 +114,94 @@ public class BenchmarkTests {
                 Var fromRolePlayer = Graql.var();
                 Var toRolePlayer = Graql.var();
                 Pattern relationInsert = Graql.var()
-                        .rel(Graql.label(Qfrom.getLabel()), fromRolePlayer)
-                        .rel(Graql.label(Qto.getLabel()), toRolePlayer)
-                        .isa(Graql.label(Q.getLabel()))
-                        .and(fromRolePlayer.asUserDefined().id(aInstances[from]))
-                        .and(toRolePlayer.asUserDefined().id(aInstances[to]));
+                        .rel(Graql.label(fromRole.getLabel()), fromRolePlayer)
+                        .rel(Graql.label(toRole.getLabel()), toRolePlayer)
+                        .isa(Graql.label(relationType.getLabel()))
+                        .and(fromRolePlayer.asUserDefined().id(instances[from]))
+                        .and(toRolePlayer.asUserDefined().id(instances[to]));
                 loader.add(Graql.insert(relationInsert.admin().varPatterns()), keyspace).subscribe(System.out::println);
             }
             tx.close();
         }
-
-
     }
+
+    private void loadJoinData(int N) {
+        final GraknClient graknClient = new GraknClient(engine.uri());
+        final int M = N/5;
+        loadOntology("multiJoin.gql", session);
+        loadEntities("genericEntity", M, graknClient);
+        loadRelations("genericEntity", "fromRole", "toRole", "C2", M, session, graknClient, keyspace);
+        loadRelations("genericEntity", "fromRole", "toRole", "C3", M, session, graknClient, keyspace);
+        loadRelations("genericEntity", "fromRole", "toRole", "C4", M, session, graknClient, keyspace);
+        loadRelations("genericEntity", "fromRole", "toRole", "D1", M, session, graknClient, keyspace);
+        loadRelations("genericEntity", "fromRole", "toRole", "D2", M, session, graknClient, keyspace);
+    }
+
+    private void loadTransitivityData(int N){
+        final GraknClient graknClient = new GraknClient(engine.uri());
+        loadOntology("linearTransitivity.gql", session);
+        loadEntities("a-entity", N, graknClient);
+        loadRelations("a-entity", "Q-from", "Q-to", "Q", N, session, graknClient, keyspace);
+    }
+
     /**
      * 2-rule transitive test with transitivity expressed in terms of two linear rules.
      * Data arranged randomly with N number of db relation instances.
      */
     @Test
     public void testRandomSetLinearTransitivity()  {
-        final int N = 10000;
+        final int N = 2000;
         final int limit = 100;
         LOG.debug(new Object(){}.getClass().getEnclosingMethod().getName());
-        load("linearTransitivity.gql", N);
+        loadTransitivityData(N);
 
         try(GraknTx tx = session.open(GraknTxType.READ)) {
 
             ConceptId entityId = tx.getEntityType("a-entity").instances().findFirst().get().getId();
-            String queryString = "match (P-from: $x, P-to: $y) isa P; get;";
-            String subbedQueryString = "match (P-from: $x, P-to: $y) isa P;" +
+            String queryPattern = "(P-from: $x, P-to: $y) isa P;";
+            String queryString = "match " + queryPattern + " get;";
+            String subbedQueryString = "match " +
+                    queryPattern +
                     "$x id '" + entityId.getValue() + "';" +
                     "get;";
-            String subbedQueryString2 = "match (P-from: $x, P-to: $y) isa P;" +
+            String subbedQueryString2 = "match " +
+                    queryPattern +
                     "$y id '" + entityId.getValue() + "';" +
                     "get;";
-            String limitedQueryString = "match (P-from: $x, P-to: $y) isa P;" +
+            String limitedQueryString = "match " +
+                    queryPattern +
+                    "limit " + limit + ";" +
+                    "get;";
+
+            executeQuery(queryString, tx, "full");
+            executeQuery(subbedQueryString, tx, "first argument bound");
+            executeQuery(subbedQueryString2, tx, "second argument bound");
+            executeQuery(limitedQueryString, tx, "limit " + limit);
+        }
+    }
+
+    @Test
+    public void testMultiJoin()  {
+        final int N = 10000;
+        final int limit = 100;
+        LOG.debug(new Object(){}.getClass().getEnclosingMethod().getName());
+        loadJoinData(N);
+
+        try(GraknTx tx = session.open(GraknTxType.READ)) {
+
+            ConceptId entityId = tx.getEntityType("genericEntity").instances().findFirst().get().getId();
+            String queryPattern = "(fromRole: $x, toRole: $y) isa A;";
+            String queryString = "match " + queryPattern + " get;";
+            String subbedQueryString = "match " +
+                    queryPattern +
+                    "$x id '" + entityId.getValue() + "';" +
+                    "get;";
+            String subbedQueryString2 = "match " +
+                    queryPattern +
+                    "$y id '" + entityId.getValue() + "';" +
+                    "get;";
+            String limitedQueryString = "match " +
+                    queryPattern +
                     "limit " + limit + ";" +
                     "get;";
 
