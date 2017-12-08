26e9dcacfbfc013d1d4c4391bd45e0cd11058eed
Felix Chapman <felix@grakn.ai>
2017-12-08 13:39:52 +0000

    Make `path` work like old path query again

--- grakn-graql/src/main/java/ai/grakn/graql/internal/query/analytics/PathQueryImpl.java
+++ grakn-graql/src/main/java/ai/grakn/graql/internal/query/analytics/PathQueryImpl.java
@@ -22,31 +22,16 @@ import ai.grakn.GraknTx;
 import ai.grakn.concept.Concept;
 import ai.grakn.concept.ConceptId;
 import ai.grakn.concept.Label;
-import ai.grakn.concept.LabelId;
-import ai.grakn.concept.Thing;
-import ai.grakn.exception.GraqlQueryException;
 import ai.grakn.graql.analytics.PathQuery;
-import ai.grakn.graql.internal.analytics.NoResultException;
-import ai.grakn.graql.internal.analytics.ShortestPathVertexProgram;
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.Multimap;
-import org.apache.tinkerpop.gremlin.process.computer.ComputerResult;
+import ai.grakn.graql.analytics.PathsQuery;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.Deque;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 
-import static ai.grakn.graql.internal.analytics.Utility.getResourceEdgeId;
 import static ai.grakn.graql.internal.util.StringConverter.idToString;
 
-class PathQueryImpl extends AbstractComputeQuery<List<List<Concept>>> implements PathQuery {
+class PathQueryImpl extends AbstractComputeQuery<Optional<List<Concept>>> implements PathQuery {
 
     private ConceptId sourceId = null;
     private ConceptId destinationId = null;
@@ -56,131 +41,10 @@ class PathQueryImpl extends AbstractComputeQuery<List<List<Concept>>> implements
     }
 
     @Override
-    public List<List<Concept>> execute() {
-        LOGGER.info("ShortestPathVertexProgram is called");
-        long startTime = System.currentTimeMillis();
-
-        if (sourceId == null) throw GraqlQueryException.noPathSource();
-        if (destinationId == null) throw GraqlQueryException.noPathDestination();
-        initSubGraph();
-        getAllSubTypes();
-
-        if (!verticesExistInSubgraph(sourceId, destinationId)) {
-            throw GraqlQueryException.instanceDoesNotExist();
-        }
-        if (sourceId.equals(destinationId)) {
-            return Collections.singletonList(Collections.singletonList(tx.get().getConcept(sourceId)));
-        }
-
-        ComputerResult result;
-        Set<LabelId> subLabelIds = convertLabelsToIds(subLabels);
-        try {
-            result = getGraphComputer().compute(
-                    new ShortestPathVertexProgram(sourceId, destinationId), null, subLabelIds);
-        } catch (NoResultException e) {
-            LOGGER.info("ShortestPathVertexProgram is done in " + (System.currentTimeMillis() - startTime) + " ms");
-            return Collections.emptyList();
-        }
-
-        Multimap<Concept, Concept> predecessorMapFromSource = getPredecessorMap(result);
-        List<List<Concept>> allPaths = getAllPaths(predecessorMapFromSource);
-        if (includeAttribute) { // this can be slow
-            return getExtendedPaths(allPaths);
-        }
-
-        LOGGER.info("Number of paths: " + allPaths.size());
-        LOGGER.info("ShortestPathVertexProgram is done in " + (System.currentTimeMillis() - startTime) + " ms");
-        return allPaths;
-    }
-
-    // If the sub graph contains attributes, we may need to add implicit relations to the paths
-    private List<List<Concept>> getExtendedPaths(List<List<Concept>> allPaths) {
-        List<List<Concept>> extendedPaths = new ArrayList<>();
-        for (List<Concept> currentPath : allPaths) {
-            boolean hasAttribute = currentPath.stream().anyMatch(Concept::isAttribute);
-            if (!hasAttribute) {
-                extendedPaths.add(currentPath);
-            }
-        }
-
-        // If there exist a path without attributes, we don't need to expand any path
-        // as paths contain attributes would be longer after implicit relations are added
-        int numExtensionAllowed = extendedPaths.isEmpty() ? Integer.MAX_VALUE : 0;
-        for (List<Concept> currentPath : allPaths) {
-            List<Concept> extendedPath = new ArrayList<>();
-            int numExtension = 0; // record the number of extensions needed for the current path
-            for (int j = 0; j < currentPath.size() - 1; j++) {
-                extendedPath.add(currentPath.get(j));
-                ConceptId resourceRelationId = getResourceEdgeId(tx.get(),
-                        currentPath.get(j).getId(), currentPath.get(j + 1).getId());
-                if (resourceRelationId != null) {
-                    numExtension++;
-                    if (numExtension > numExtensionAllowed) break;
-                    extendedPath.add(getConcept(resourceRelationId));
-                }
-            }
-            if (numExtension == numExtensionAllowed) {
-                extendedPath.add(currentPath.get(currentPath.size() - 1));
-                extendedPaths.add(extendedPath);
-            } else if (numExtension < numExtensionAllowed) {
-                extendedPath.add(currentPath.get(currentPath.size() - 1));
-                extendedPaths.clear(); // longer paths are discarded
-                extendedPaths.add(extendedPath);
-                // update the minimum number of extensions needed so all the paths have the same length
-                numExtensionAllowed = numExtension;
-            }
-        }
-        return extendedPaths;
-    }
-
-    private Multimap<Concept, Concept> getPredecessorMap(ComputerResult result) {
-        Map<String, Set<String>> predecessorMapFromSource =
-                result.memory().get(ShortestPathVertexProgram.PREDECESSORS_FROM_SOURCE);
-        Map<String, Set<String>> predecessorMapFromDestination =
-                result.memory().get(ShortestPathVertexProgram.PREDECESSORS_FROM_DESTINATION);
-
-        Multimap<Concept, Concept> predecessors = HashMultimap.create();
-        predecessorMapFromSource.forEach((id, idSet) -> idSet.forEach(id2 -> {
-            predecessors.put(getConcept(id), getConcept(id2));
-        }));
-        predecessorMapFromDestination.forEach((id, idSet) -> idSet.forEach(id2 -> {
-            predecessors.put(getConcept(id2), getConcept(id));
-        }));
-        return predecessors;
-    }
-
-    private List<List<Concept>> getAllPaths(Multimap<Concept, Concept> predecessorMapFromSource) {
-        List<List<Concept>> allPaths = new ArrayList<>();
-        List<Concept> firstPath = new ArrayList<>();
-        firstPath.add(getConcept(sourceId.getValue()));
-
-        Deque<List<Concept>> queue = new ArrayDeque<>();
-        queue.addLast(firstPath);
-        while (!queue.isEmpty()) {
-            List<Concept> currentPath = queue.pollFirst();
-            if (predecessorMapFromSource.containsKey(currentPath.get(currentPath.size() - 1))) {
-                Collection<Concept> successors = predecessorMapFromSource.get(currentPath.get(currentPath.size() - 1));
-                Iterator<Concept> iterator = successors.iterator();
-                for (int i = 0; i < successors.size() - 1; i++) {
-                    List<Concept> extendedPath = new ArrayList<>(currentPath);
-                    extendedPath.add(iterator.next());
-                    queue.addLast(extendedPath);
-                }
-                currentPath.add(iterator.next());
-                queue.addLast(currentPath);
-            } else {
-                allPaths.add(currentPath);
-            }
-        }
-        return allPaths;
-    }
-
-    private Thing getConcept(String conceptId) {
-        return tx.get().getConcept(ConceptId.of(conceptId));
-    }
-
-    private Thing getConcept(ConceptId conceptId) {
-        return tx.get().getConcept(conceptId);
+    public Optional<List<Concept>> execute() {
+        PathsQuery pathsQuery = new PathsQueryImpl(tx);
+        if (includeAttribute) pathsQuery = pathsQuery.includeAttribute();
+        return pathsQuery.from(sourceId).to(destinationId).execute().stream().findAny();
     }
 
     @Override
