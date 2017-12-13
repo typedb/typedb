70c958f158a645f0f48542a1668a97e3deb8d6aa
Harjyot Singh <harjyot7@gmail.com>
2017-12-13 02:33:27 +0000

    Future proofing URL changes, and making code less ugly

--- docs/deploy-server.js
+++ docs/deploy-server.js
@@ -8,7 +8,10 @@ const dist = path.join(__dirname, '_site');
 
 app.get('*', (req, res) => {
   let requestedResource = req.path;
-  if (requestedResource.indexOf('.') === -1 && requestedResource !== '/') {
+  if (requestedResource.match(/^\/(overview|academy|contributors)\/?$/)){
+    requestedResource = requestedResource.concat('/index.html');
+  }
+  else if (!requestedResource.match(/\.(svg|pdf|png|jpg|jpeg|ico|ttf|otf|woff|woff2|eot|css|js|html|json)$/) && requestedResource !== '/') {
     requestedResource = requestedResource.concat('.html');
   }
   res.sendFile(path.join(dist, requestedResource));
