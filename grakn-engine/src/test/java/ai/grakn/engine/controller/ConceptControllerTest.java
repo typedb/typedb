68af622e9001024275ad9584a5d43652a481e801
Filipe Peliz Pinto Teixeira <fppintoteixeira@gmail.com>
2017-12-13 12:19:41 +0000

    Add new tests targetting new endpoints

--- grakn-engine/src/test/java/ai/grakn/engine/controller/ConceptControllerTest.java
+++ grakn-engine/src/test/java/ai/grakn/engine/controller/ConceptControllerTest.java
@@ -84,8 +84,10 @@ public class ConceptControllerTest {
     private static RelationshipType relationshipTypeWrapper;
     private static Relationship relationshipWrapper;
     private static EntityType entityTypeWrapper;
+    private static EntityType entityTypeSubWrapper;
     private static Entity entityWrapper;
     private static AttributeType attributeTypeWrapper;
+    private static AttributeType attributeTypeKeyWrapper;
     private static Attribute attributeWrapper1;
     private static Attribute attributeWrapper2;
     private static Rule ruleWrapper;
@@ -115,8 +117,12 @@ public class ConceptControllerTest {
             ai.grakn.concept.Attribute attribute1 = attributeType.putAttribute("An attribute 1");
             ai.grakn.concept.Attribute attribute2 = attributeType.putAttribute("An attribute 2");
 
-            ai.grakn.concept.EntityType entityType = tx.putEntityType("My Special Entity Type").plays(role1).plays(role2).attribute(attributeType);
-            entity = entityType.addEntity().attribute(attribute1).attribute(attribute2);
+            ai.grakn.concept.AttributeType attributeTypeKey = tx.putAttributeType("My Key Attribute Type", ai.grakn.concept.AttributeType.DataType.STRING);
+            ai.grakn.concept.Attribute key = attributeTypeKey.putAttribute("An attribute Key 1");
+
+            ai.grakn.concept.EntityType entityType = tx.putEntityType("My Special Entity Type").plays(role1).plays(role2).attribute(attributeType).key(attributeTypeKey);
+            ai.grakn.concept.EntityType entityTypeSub = tx.putEntityType("My Special Sub Entity Type").sub(entityType);
+            entity = entityType.addEntity().attribute(attribute1).attribute(attribute2).attribute(key);
 
             ai.grakn.concept.RelationshipType relationshipType = tx.putRelationshipType("My Relationship Type").relates(role1).relates(role2);
             ai.grakn.concept.Relationship relationship = relationshipType.addRelationship().addRolePlayer(role1, entity).addRolePlayer(role2, entity);
@@ -133,9 +139,11 @@ public class ConceptControllerTest {
             relationshipWrapper = ConceptBuilder.build(relationship);
 
             entityTypeWrapper = ConceptBuilder.build(entityType);
+            entityTypeSubWrapper = ConceptBuilder.build(entityTypeSub);
             entityWrapper = ConceptBuilder.build(entity);
 
             attributeTypeWrapper = ConceptBuilder.build(attributeType);
+            attributeTypeKeyWrapper = ConceptBuilder.build(attributeTypeKey);
             attributeWrapper1 = ConceptBuilder.build(attribute1);
             attributeWrapper2 = ConceptBuilder.build(attribute2);
 
@@ -168,6 +176,48 @@ public class ConceptControllerTest {
         List<RolePlayer> relationships = new ObjectMapper().readValue(response.body().asString(), new TypeReference<List<RolePlayer>>(){});
         assertEquals(relationshipsExpected.size(), relationships.size());
         assertTrue(relationships.containsAll(relationshipsExpected));
+    }
+
+    @Test
+    public void whenGettingSubsOfSchemaConcept_EnsureSubsAreReturned() throws IOException {
+        Response response = RestAssured.when().get(entityTypeWrapper.subs().id());
+        assertEquals(SC_OK, response.getStatusCode());
+
+        List<EntityType> subs = new ObjectMapper().readValue(response.body().asString(), new TypeReference<List<EntityType>>(){});
+        assertEquals(1, subs.size());
+        assertTrue(subs.contains(entityTypeSubWrapper));
+    }
+
+    @Test
+    public void whenGettingPlaysOfType_EnsurePlaysAreReturned() throws IOException {
+        Response response = RestAssured.when().get(entityTypeWrapper.plays().id());
+        assertEquals(SC_OK, response.getStatusCode());
+
+        List<Role> plays = new ObjectMapper().readValue(response.body().asString(), new TypeReference<List<Role>>(){});
+        assertEquals(4, plays.size());
+        assertTrue(plays.contains(roleWrapper1));
+        assertTrue(plays.contains(roleWrapper2));
+    }
+
+    @Test
+    public void whenGettingAttributesOfType_EnsureAttributesAreReturned() throws IOException {
+        Response response = RestAssured.when().get(entityTypeWrapper.attributes().id());
+        assertEquals(SC_OK, response.getStatusCode());
+
+        List<AttributeType> attributeTypes = new ObjectMapper().readValue(response.body().asString(), new TypeReference<List<AttributeType>>(){});
+        assertEquals(2, attributeTypes.size());
+        assertTrue(attributeTypes.contains(attributeTypeWrapper));
+        assertTrue(attributeTypes.contains(attributeTypeKeyWrapper));
+    }
+
+    @Test
+    public void whenGettingKeysOfType_EnsureAttributesAreReturned()throws IOException {
+        Response response = RestAssured.when().get(entityTypeWrapper.keys().id());
+        assertEquals(SC_OK, response.getStatusCode());
+
+        List<AttributeType> keys = new ObjectMapper().readValue(response.body().asString(), new TypeReference<List<AttributeType>>(){});
+        assertEquals(1, keys.size());
+        assertTrue(keys.contains(attributeTypeKeyWrapper));
     }
 
     @Test
