d831159f63236dc6286121d5decd31323a6f5fb0
Harjyot Singh <harjyot7@gmail.com>
2017-12-14 02:54:20 +0000

    Handling 404

--- docs/deploy-server.js
+++ docs/deploy-server.js
@@ -22,7 +22,11 @@ app.get('*', (req, res) => {
   else if (!requestedResource.match(/\.(svg|pdf|png|jpg|jpeg|ico|ttf|otf|woff|woff2|eot|css|js|html|json|xml)$/) && requestedResource !== '/') {
     requestedResource = requestedResource.concat('.html');
   }
-  res.sendFile(path.join(dist, requestedResource));
+  res.sendFile(path.join(dist, requestedResource), (error) => {
+    if(error && error.statusCode === 404 && error.path.match(/\.html$/)) {
+      res.sendFile(path.join(dist, '/404.html'));
+    }
+  });
 });
 
 
