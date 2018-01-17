1a2858c82c5471d89008b41ad5d495ee5d9ca850
Felix Chapman <felix@grakn.ai>
2018-01-17 15:07:09 +0000

    Fix issue with printing map with `JacksonPrinter`

--- grakn-engine/src/main/java/ai/grakn/engine/printer/JacksonPrinter.java
+++ grakn-engine/src/main/java/ai/grakn/engine/printer/JacksonPrinter.java
@@ -80,7 +80,7 @@ public class JacksonPrinter implements Printer<Object>{
         Stream<Map.Entry> entries = map.<Map.Entry>entrySet().stream();
         return entries.collect(Collectors.toMap(
                 entry -> graqlString(inner, entry.getKey()),
-                entry -> graqlString(inner, entry.getKey())
+                entry -> graqlString(inner, entry.getValue())
         ));
     }
 
