acc7edfa11e56378da47e34fb6e770492fb54add
Filipe Peliz Pinto Teixeira <fppintoteixeira@gmail.com>
2018-02-01 13:43:01 +0000

    Dealing with code review stuff

--- grakn-graql/src/main/java/ai/grakn/graql/internal/query/analytics/AbstractCentralityQuery.java
+++ grakn-graql/src/main/java/ai/grakn/graql/internal/query/analytics/AbstractCentralityQuery.java
@@ -18,6 +18,7 @@
 
 package ai.grakn.graql.internal.query.analytics;
 
+import ai.grakn.API;
 import ai.grakn.concept.Label;
 import ai.grakn.graql.ComputeQuery;
 import ai.grakn.graql.internal.util.StringConverter;
@@ -62,8 +63,7 @@ abstract class AbstractCentralityQuery<V extends ComputeQuery<Map<Long, Set<Stri
         includeAttribute = true;
     }
 
-    //TODO: @Jason. This is only ever used by a test. Is it part of the public api? Why do we need this method?
-    @SuppressWarnings("unused")
+    @API
     public V of(String... ofTypeLabels) {
         return of(Arrays.stream(ofTypeLabels).map(Label::of).collect(Collectors.toSet()));
     }
