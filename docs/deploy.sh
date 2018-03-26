db86cf63e1acdb20d676d1c3b6fc479c6db732ee
Marco Scoppetta <marco@grakn.ai>
2018-03-26 12:22:19 +0100

    updated docs deploy script

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -4,11 +4,12 @@ git checkout -b docs-deploy-temp stable
 echo "Building _site"
 rake build
 echo "Staging new files"
-git add -A 
+git add -A
+git add --force -- _jekyll _site
 git commit -m "Site update"
 echo "Pushing to Heroku"
 cd ../
 git push git@heroku.com:dev-grakn.git `git subtree split --prefix docs docs-deploy-temp`:master --force
 echo "Deleting temporary branch"
-git checkout stable
+git checkout @{-1}
 git branch -D docs-deploy-temp
