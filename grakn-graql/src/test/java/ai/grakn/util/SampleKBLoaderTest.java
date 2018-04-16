7271b29580845f32af2692873fb65255b3931662
Ganeshwara Herawan Hananda <soulmagic13@gmail.com>
2018-04-16 15:45:27 +0100

    replacing the occurrences of GPL license URLs in the Java files with AGPL URLs

--- grakn-graql/src/test/java/ai/grakn/util/SampleKBLoaderTest.java
+++ grakn-graql/src/test/java/ai/grakn/util/SampleKBLoaderTest.java
@@ -13,7 +13,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Grakn. If not, see <http://www.gnu.org/licenses/gpl.txt>.
+ * along with Grakn. If not, see <http://www.gnu.org/licenses/agpl.txt>.
  */
 package ai.grakn.util;
 
