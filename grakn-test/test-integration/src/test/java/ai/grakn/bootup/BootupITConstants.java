95976f08b0c16d43279abe45111b4a6b46c4d4c9
Ganeshwara Herawan Hananda <soulmagic13@gmail.com>
2018-05-22 17:17:30 +0100

    added bootup test

---
+++ grakn-test/test-integration/src/test/java/ai/grakn/bootup/BootupITConstants.java
@@ -0,0 +1,46 @@
+package ai.grakn.bootup;
+
+import ai.grakn.util.GraknVersion;
+import org.junit.Assert;
+import org.zeroturnaround.exec.ProcessExecutor;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeoutException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class BootupITConstants {
+    // path of grakn zip files
+    public static final String ZIP_FILENAME = "grakn-dist-" + GraknVersion.VERSION + ".zip";
+    public static final Path GRAKN_BASE_DIRECTORY = Paths.get(System.getProperty("main.basedir"));
+    public static final Path GRAKN_TARGET_DIRECTORY = Paths.get(GRAKN_BASE_DIRECTORY.toString(), "grakn-dist", "target");
+    public static final Path ZIP_FULLPATH = Paths.get(GRAKN_TARGET_DIRECTORY.toString(), ZIP_FILENAME);
+    public static final Path GRAKN_UNZIPPED_DIRECTORY = Paths.get(GRAKN_TARGET_DIRECTORY.toString(), "grakn-dist-" + GraknVersion.VERSION);
+
+    public static void assertGraknRunning() {
+        assertThat(Paths.get("/tmp/grakn-engine.pid").toFile().exists(), equalTo(true));
+        assertThat(Paths.get("/tmp/grakn-queue.pid").toFile().exists(), equalTo(true));
+        assertThat(Paths.get("/tmp/grakn-storage.pid").toFile().exists(), equalTo(true));
+    }
+
+    public static void assertGraknStopped() {
+        assertThat(Paths.get("/tmp/grakn-engine.pid").toFile().exists(), equalTo(false));
+        assertThat(Paths.get("/tmp/grakn-queue.pid").toFile().exists(), equalTo(false));
+        assertThat(Paths.get("/tmp/grakn-storage.pid").toFile().exists(), equalTo(false));
+    }
+
+    public static void assertZipExists() {
+        if(!ZIP_FULLPATH.toFile().exists()) {
+            Assert.fail("Grakn distribution '" + ZIP_FULLPATH.toString() + "' could not be found. Please ensure it has been build (ie., run `mvn package`)");
+        }
+    }
+
+    public static void unzipGrakn() throws IOException, InterruptedException, TimeoutException {
+        new ProcessExecutor()
+                .directory(GRAKN_TARGET_DIRECTORY.toFile())
+                .command("unzip", ZIP_FULLPATH.toString()).execute();
+    }
+}
