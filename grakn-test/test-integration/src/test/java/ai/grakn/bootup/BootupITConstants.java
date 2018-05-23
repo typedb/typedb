83c66a6b10eaef7fd50b0624c48305a3d4e8f8e5
Ganeshwara Herawan Hananda <soulmagic13@gmail.com>
2018-05-23 14:25:26 +0100

    fix typo

--- grakn-test/test-integration/src/test/java/ai/grakn/bootup/BootupITConstants.java
+++ grakn-test/test-integration/src/test/java/ai/grakn/bootup/BootupITConstants.java
@@ -21,9 +21,9 @@ public class BootupITConstants {
     public static final Path GRAKN_UNZIPPED_DIRECTORY = Paths.get(GRAKN_TARGET_DIRECTORY.toString(), "grakn-dist-" + GraknVersion.VERSION);
 
     public static void assertGraknRunning() {
-        assertThat("assertKgmsRunning() failed because /tmp/grakn-engine.pid is not found", Paths.get("/tmp/grakn-engine.pid").toFile().exists(), equalTo(true));
-        assertThat("assertKgmsRunning() failed because /tmp/grakn-queue.pid is not found", Paths.get("/tmp/grakn-queue.pid").toFile().exists(), equalTo(true));
-        assertThat("assertKgmsRunning() failed because /tmp/grakn-storage.pid is not found", Paths.get("/tmp/grakn-storage.pid").toFile().exists(), equalTo(true));
+        assertThat("assertGraknRunning() failed because /tmp/grakn-engine.pid is not found", Paths.get("/tmp/grakn-engine.pid").toFile().exists(), equalTo(true));
+        assertThat("assertGraknRunning() failed because /tmp/grakn-queue.pid is not found", Paths.get("/tmp/grakn-queue.pid").toFile().exists(), equalTo(true));
+        assertThat("assertGraknRunning() failed because /tmp/grakn-storage.pid is not found", Paths.get("/tmp/grakn-storage.pid").toFile().exists(), equalTo(true));
     }
 
     public static void assertGraknStopped() {
