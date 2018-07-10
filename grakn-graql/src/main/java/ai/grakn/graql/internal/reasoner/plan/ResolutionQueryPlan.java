5f265c72f024ccc410f374056953ad68bdea5ad0
kasper-piskorski <muydeemer@o2.pl>
2018-07-10 16:42:42 +0100

    cleanup

--- grakn-graql/src/main/java/ai/grakn/graql/internal/reasoner/plan/ResolutionQueryPlan.java
+++ grakn-graql/src/main/java/ai/grakn/graql/internal/reasoner/plan/ResolutionQueryPlan.java
@@ -18,26 +18,16 @@
 
 package ai.grakn.graql.internal.reasoner.plan;
 
-import ai.grakn.graql.Var;
-import ai.grakn.graql.admin.ReasonerQuery;
 import ai.grakn.graql.internal.reasoner.atom.Atom;
 import ai.grakn.graql.internal.reasoner.query.ReasonerQueries;
-import ai.grakn.graql.internal.reasoner.query.ReasonerQueryEquivalence;
 import ai.grakn.graql.internal.reasoner.query.ReasonerQueryImpl;
 import ai.grakn.kb.internal.EmbeddedGraknTx;
-import com.google.common.base.Equivalence;
-import com.google.common.collect.HashMultimap;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Multimap;
-import com.google.common.collect.Sets;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 import java.util.Stack;
 import java.util.stream.Collectors;
 
@@ -100,20 +90,7 @@ public class ResolutionQueryPlan {
         return refine? refine(queries) : ImmutableList.copyOf(queries);
     }
 
-    private static Equivalence<ReasonerQuery> equality = ReasonerQueryEquivalence.Equality;
-
-    private static List<Equivalence.Wrapper<ReasonerQueryImpl>> prioritiseQueries(Collection<Equivalence.Wrapper<ReasonerQueryImpl>> queries){
-        return prioritise(
-                queries.stream()
-                        .map(Equivalence.Wrapper::get)
-                        .collect(Collectors.toList())
-        )
-                .stream()
-                .map(q -> equality.wrap(q))
-                .collect(Collectors.toList());
-    }
-
-    private static List<ReasonerQueryImpl> prioritise(Collection<ReasonerQueryImpl> queries){
+    private static List<ReasonerQueryImpl> prioritise(QueryCollectionBase queries){
         return queries.stream()
                 .sorted(Comparator.comparing(q -> !q.isAtomic()))
                 .sorted(Comparator.comparing(ReasonerQueryImpl::isRuleResolvable))
@@ -122,148 +99,26 @@ public class ResolutionQueryPlan {
     }
 
     private static ImmutableList<ReasonerQueryImpl> refine(List<ReasonerQueryImpl> qs){
-        return ImmutableList.copyOf(
-                refinePlan(qs.stream().map(equality::wrap).collect(Collectors.toList()))
-                        .stream()
-                        .map(Equivalence.Wrapper::get)
-                        .collect(Collectors.toList())
-        );
+        return ImmutableList.copyOf(refinePlan(new QueryList(qs)).toCollection());
     }
 
+    private static QueryList refinePlan(QueryList queries){
+        QueryList plan = new QueryList();
+        Stack<ReasonerQueryImpl> queryStack = new Stack<>();
 
-    private static boolean isQueryDisconnected(Equivalence.Wrapper<ReasonerQueryImpl> query, Collection<Equivalence.Wrapper<ReasonerQueryImpl>> queries){
-        return isQueryDisconnected(
-                query.get(),
-                queries.stream().filter(q -> !q.equals(query)).map(Equivalence.Wrapper::get).collect(Collectors.toList())
-        );
-    }
-
-    /**
-     * @param query of interest
-     * @param queries wrt which connectivity is to be determined
-     * @return true if query is disconnected wrt queries
-     */
-    private static boolean isQueryDisconnected(ReasonerQueryImpl query, Collection<ReasonerQueryImpl> queries){
-        return queries.stream()
-                .allMatch(q -> Sets.intersection(q.getVarNames(), query.getVarNames()).isEmpty());
-    }
-
-    private static boolean isQueryReachable(Equivalence.Wrapper<ReasonerQueryImpl> query,
-                                            Collection<Equivalence.Wrapper<ReasonerQueryImpl>> target,
-                                            Collection<Equivalence.Wrapper<ReasonerQueryImpl>> queries){
-        return isQueryReachable(
-                query.get(),
-                target.stream().map(Equivalence.Wrapper::get).collect(Collectors.toList()),
-                queries.stream().map(Equivalence.Wrapper::get).collect(Collectors.toList())
-        );
-    }
-
-    private static boolean isQueryReachable(ReasonerQueryImpl query,
-                                            Collection<ReasonerQueryImpl> target,
-                                            Collection<ReasonerQueryImpl> queries){
-        Set<Var> queryVars = getAllNeighbours(query, queries).stream()
-                .flatMap(q -> q.getVarNames().stream())
-                .collect(Collectors.toSet());
-        return target.stream()
-                .anyMatch(tq -> !Sets.intersection(tq.getVarNames(), queryVars).isEmpty());
-    }
-
-    private static Set<ReasonerQueryImpl> getAllNeighbours(ReasonerQueryImpl entryQuery, Collection<ReasonerQueryImpl> queries) {
-        Set<ReasonerQueryImpl> neighbours = new HashSet<>();
-        Set<Equivalence.Wrapper<ReasonerQueryImpl>> visitedQueries = new HashSet<>();
-        Stack<Equivalence.Wrapper<ReasonerQueryImpl>> queryStack = new Stack<>();
-
-        Multimap<ReasonerQueryImpl, ReasonerQueryImpl> neighbourMap = createNeighbourMap(queries);
-        neighbourMap.get(entryQuery).stream().map(q -> equality.wrap(q)).forEach(queryStack::push);
-        while (!queryStack.isEmpty()) {
-            Equivalence.Wrapper<ReasonerQueryImpl> wrappedQuery = queryStack.pop();
-            ReasonerQueryImpl query = wrappedQuery.get();
-            if (!visitedQueries.contains(wrappedQuery) && query != null) {
-                neighbourMap.get(query).stream()
-                        .peek(neighbours::add)
-                        .flatMap(q -> neighbourMap.get(q).stream())
-                        .map(equality::wrap)
-                        .filter(q -> !visitedQueries.contains(q))
-                        .filter(q -> !queryStack.contains(q))
-                        .forEach(queryStack::add);
-                visitedQueries.add(wrappedQuery);
-            }
-        }
-        return neighbours;
-    }
-
-    private static Set<ReasonerQueryImpl> getNeighbours(ReasonerQueryImpl query, Collection<ReasonerQueryImpl> queries){
-        Set<Var> vars = query.getVarNames();
-        return queries.stream()
-                .filter(q2 -> !Sets.intersection(vars, q2.getVarNames()).isEmpty())
-                .collect(Collectors.toSet());
-    }
-
-    private static Set<Equivalence.Wrapper<ReasonerQueryImpl>> getNeighbours(Equivalence.Wrapper<ReasonerQueryImpl> query, Collection<Equivalence.Wrapper<ReasonerQueryImpl>> queries){
-        return getNeighbours(
-                query.get(),
-                queries.stream().filter(q2 -> !query.equals(q2)).map(Equivalence.Wrapper::get).collect(Collectors.toList())
-                )
-                .stream()
-                .map(equality::wrap)
-                .collect(Collectors.toSet());
-    }
-
-    private static Multimap<ReasonerQueryImpl, ReasonerQueryImpl> createNeighbourMap(Collection<ReasonerQueryImpl> queries){
-        Multimap<ReasonerQueryImpl, ReasonerQueryImpl> neighbourMap = HashMultimap.create();
-        queries.forEach(q -> neighbourMap.putAll(q, getNeighbours(q, queries)));
-        return neighbourMap;
-    }
-
-    private static List<Equivalence.Wrapper<ReasonerQueryImpl>> refinePlan(List<Equivalence.Wrapper<ReasonerQueryImpl>> queries){
-        LinkedList<Equivalence.Wrapper<ReasonerQueryImpl>> plan = new LinkedList<>();
-        Stack<Equivalence.Wrapper<ReasonerQueryImpl>> queryStack = new Stack<>();
-
-        Multimap<Equivalence.Wrapper<ReasonerQueryImpl>, Equivalence.Wrapper<ReasonerQueryImpl>> neighbourMap = HashMultimap.create();
-
-        //determine connectivity
-        queries.forEach(q -> neighbourMap.putAll(q, getNeighbours(q, queries)));
-
-        Lists.reverse(prioritiseQueries(queries)).forEach(queryStack::push);
+        Lists.reverse(prioritise(queries)).forEach(queryStack::push);
         while(!plan.containsAll(queries)) {
-            if (queryStack.isEmpty()){
-                //backtrack
+            ReasonerQueryImpl query = queryStack.pop();
 
-                Equivalence.Wrapper<ReasonerQueryImpl> last = plan.remove();
-                Lists.reverse(prioritiseQueries(queries)).forEach(queryStack::push);
-                queryStack.remove(last);
-            }
-            Equivalence.Wrapper<ReasonerQueryImpl> query = queryStack.pop();
-
-            Set<Equivalence.Wrapper<ReasonerQueryImpl>> availableQueries = queries.stream()
-                    .filter(q -> !(plan.contains(q) || q.equals(query)))
-                    .collect(Collectors.toSet());
-
-            Set<Equivalence.Wrapper<ReasonerQueryImpl>> availableNeighbours = neighbourMap.get(query).stream()
-                    .filter(availableQueries::contains)
-                    .collect(Collectors.toSet());
-
-            Set<Var> subbedVars = plan.stream()
-                    .map(Equivalence.Wrapper::get)
-                    .flatMap(q -> q.getVarNames().stream())
-                    .collect(Collectors.toSet());
-            Set<Equivalence.Wrapper<ReasonerQueryImpl>> availableNeighboursFromSubs = availableQueries.stream()
-                    .map(Equivalence.Wrapper::get)
-                    .filter(q -> !Sets.intersection(q.getVarNames(), subbedVars).isEmpty())
-                    .map(q -> equality.wrap(q))
-                    .collect(Collectors.toSet());
-
-            //candidates
-            Set<Equivalence.Wrapper<ReasonerQueryImpl>> candidates = isQueryDisconnected(query, queries)?
-                    availableQueries :
-                    isQueryReachable(query, availableQueries, queries)? Sets.union(availableNeighbours, availableNeighboursFromSubs): availableQueries;
+            QuerySet candidates = queries.getCandidates(query, plan);
 
             if (!candidates.isEmpty() || queries.size() - plan.size() == 1){
                 plan.add(query);
-                Lists.reverse(prioritiseQueries(candidates)).forEach(queryStack::push);
+                Lists.reverse(prioritise(candidates)).forEach(queryStack::push);
             }
         }
 
         return plan;
     }
+
 }
