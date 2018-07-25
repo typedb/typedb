219c18e359496a1eb71135be9c646d2c32917ecc
Joshua Send <joshua@Joshuas-MacBook-Pro.local>
2018-07-25 19:26:34 +0100

    setting up for test pip package

---
+++ client-python/README.md
@@ -0,0 +1,8 @@
+# Python Grakn Client
+
+New in version 1.2:
+* gRCP connected
+* completely rewritte
+
+--- currently under development ---
+
---
+++ client-python/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='grakn',
+    packages=['grakn'],
+    version='1.2',
+    license='Apache-2.0',
+    description='A Python client for Grakn',
+    long_description=open('README.md').read(),
+    author='Grakn Labs',
+    author_email='community@grakn.ai',
+    url='https://github.com/flyingsilverfin/grakn/tree/client-python-dev',
+#    download_url='https://github.com/graknlabs/grakn-python/archive/v0.8.1.tar.gz',
+    keywords=['grakn', 'database', 'graph', 'knowledgebase', 'knowledge-engineering'],
+    python_requires='>=3.6.0',
+    install_requires=['grpcio']
+)
