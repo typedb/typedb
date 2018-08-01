abc1eba05cc5e8ade80d11bf9f078cb6ea95abf0
Ganeshwara Herawan Hananda <soulmagic13@gmail.com>
2018-08-01 18:05:55 +0100

    refactor

--- grakn-test/test-client-java/src/test/java/ai/grakn/client/ClientJavaE2E.java
+++ grakn-test/test-client-java/src/test/java/ai/grakn/client/ClientJavaE2E.java
@@ -15,6 +15,7 @@ import org.zeroturnaround.exec.ProcessExecutor;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 import static ai.grakn.client.ClientJavaE2EConstants.GRAKN_UNZIPPED_DIRECTORY;
@@ -34,29 +35,29 @@ import static org.hamcrest.Matchers.*;
  * @author Ganeshwara Herawan Hananda
  */
 public class ClientJavaE2E {
-    private static final SimpleURI graknHost = new SimpleURI("localhost", 48555);
-
-    private static ProcessExecutor commandExecutor = new ProcessExecutor()
-            .directory(GRAKN_UNZIPPED_DIRECTORY.toFile())
-            .redirectOutput(System.out)
-            .redirectError(System.err)
-            .readOutput(true);
-
     @BeforeClass
     public static void setup_prepareDistribution() throws IOException, InterruptedException, TimeoutException {
-        assertGraknStopped();
+        ProcessExecutor commandExecutor = new ProcessExecutor()
+                .directory(GRAKN_UNZIPPED_DIRECTORY.toFile())
+                .redirectOutput(System.out)
+                .redirectError(System.err)
+                .readOutput(true);
 
-        // unzip grakn
+        assertGraknStopped();
         assertZipExists();
         unzipGrakn();
-
-        // start grakn
         commandExecutor.command("./grakn", "server", "start").execute();
         assertGraknRunning();
     }
 
     @AfterClass
     public static void cleanup_cleanupDistribution() throws IOException, InterruptedException, TimeoutException {
+        ProcessExecutor commandExecutor = new ProcessExecutor()
+                .directory(GRAKN_UNZIPPED_DIRECTORY.toFile())
+                .redirectOutput(System.out)
+                .redirectError(System.err)
+                .readOutput(true);
+
         commandExecutor.command("./grakn", "server", "stop").execute();
         assertGraknStopped();
         FileUtils.deleteDirectory(GRAKN_UNZIPPED_DIRECTORY.toFile());
@@ -65,100 +66,91 @@ public class ClientJavaE2E {
     @Test
     public void clientJavaE2E() {
         // define a schema
-        Keyspace keyspace = Keyspace.of("grakn");
-        try (Grakn.Session session = Grakn.session(graknHost, keyspace)) {
-            try (Grakn.Transaction transaction = session.transaction(GraknTxType.WRITE)) {
-                transaction.graql().define(
-                        label("marriage").sub("relationship").relates("spouse"),
-                        label("parentship").sub("relationship").relates("parent").relates("child"),
-                        label("name").sub("attribute").datatype(AttributeType.DataType.STRING),
-                        label("person").sub("entity").has("name").plays("spouse").plays("child")
-                ).execute();
-                transaction.commit();
-            }
-        }
+        localGraknTx(tx -> {
+            tx.graql().define(
+                    label("marriage").sub("relationship").relates("spouse"),
+                    label("parentship").sub("relationship").relates("parent").relates("child"),
+                    label("name").sub("attribute").datatype(AttributeType.DataType.STRING),
+                    label("person").sub("entity").has("name").plays("spouse").plays("child")
+            ).execute();
+            tx.commit();
+        });
 
-        // assert schema
-        try (Grakn.Session session = Grakn.session(graknHost, keyspace)) {
-            try (Grakn.Transaction transaction = session.transaction(GraknTxType.READ)) {
-                List<String> definedSchema = transaction.graql().match(var("t").sub("thing")).get().execute().stream()
-                        .map(answer -> answer.get(var("t")).asType().label().getValue()).collect(Collectors.toList());
-                String[] correctSchema = new String[] { "thing", "entity", "relationship", "attribute",
-                        "person", "marriage", "parentship", "@has-name", "name" };
-                assertThat(definedSchema, hasItems(correctSchema));
-            }
-        }
+        // assert schema defined
+        localGraknTx(tx -> {
+            List<String> definedSchema = tx.graql().match(var("t").sub("thing")).get().execute().stream()
+                    .map(answer -> answer.get(var("t")).asType().label().getValue()).collect(Collectors.toList());
+            String[] correctSchema = new String[] { "thing", "entity", "relationship", "attribute",
+                    "person", "marriage", "parentship", "@has-name", "name" };
+            assertThat(definedSchema, hasItems(correctSchema));
+        });
 
         // insert
-        try (Grakn.Session session = Grakn.session(graknHost, keyspace)) {
-            try (Grakn.Transaction transaction = session.transaction(GraknTxType.WRITE)) {
-                String[] familyMembers = gatesFamilyNames();
-                transaction.graql().insert(
-                        var().isa("person").has("name").val(familyMembers[0]),
-                        var().isa("person").has("name").val(familyMembers[1]),
-                        var().isa("person").has("name").val(familyMembers[2]),
-                        var().isa("person").has("name").val(familyMembers[3]),
-                        var().isa("person").has("name").val(familyMembers[4])
-                ).execute();
-                transaction.commit();
-            }
-        }
+        localGraknTx(tx -> {
+            String[] familyMembers = gatesFamilyNames();
+            tx.graql().insert(
+                    var().isa("person").has("name").val(familyMembers[0]),
+                    var().isa("person").has("name").val(familyMembers[1]),
+                    var().isa("person").has("name").val(familyMembers[2]),
+                    var().isa("person").has("name").val(familyMembers[3]),
+                    var().isa("person").has("name").val(familyMembers[4])
+            ).execute();
+            tx.commit();
+        });
 
         // match get
-        try (Grakn.Session session = Grakn.session(graknHost, keyspace)) {
-            try (Grakn.Transaction transaction = session.transaction(GraknTxType.READ)) {
-                List<ConceptMap> people = transaction.graql().match(var("p").isa("person").has("name", var("n"))).get().execute();
-                List<String> insertedNames = people.stream().map(answer -> answer.get("n").asAttribute().value().toString()).collect(Collectors.toList());
-                assertThat(insertedNames, containsInAnyOrder(gatesFamilyNames()));
-            }
-        }
+        localGraknTx(tx -> {
+            List<ConceptMap> people = tx.graql().match(var("p").isa("person").has("name", var("n"))).get().execute();
+            List<String> insertedNames = people.stream().map(answer -> answer.get("n").asAttribute().value().toString()).collect(Collectors.toList());
+            assertThat(insertedNames, containsInAnyOrder(gatesFamilyNames()));
+        });
 
         // match insert
-        try (Grakn.Session session = Grakn.session(graknHost, keyspace)) {
-            try (Grakn.Transaction transaction = session.transaction(GraknTxType.WRITE)) {
-                List<ConceptMap> insertedMarriage = transaction.graql()
-                        .match(var("husband").isa("person").has("name").val("Bill Gates"),
-                                var("wife").isa("person").has("name").val("Melinda Gates"))
-                        .insert(var().isa("marriage").rel("spouse", var("husband"))
-                                .rel("spouse", var("wife"))).execute();
-                assertThat(insertedMarriage, hasSize(1));
-            }
-        }
+        localGraknTx(tx -> {
+            String[] familyMembers = gatesFamilyNames();
+            List<ConceptMap> insertedMarriage = tx.graql()
+                    .match(
+                            var("husband").isa("person").has("name").val(familyMembers[0]),
+                            var("wife").isa("person").has("name").val(familyMembers[1]))
+                    .insert(var().isa("marriage").rel("spouse", var("husband")).rel("spouse", var("wife")))
+                    .execute();
+            assertThat(insertedMarriage, hasSize(1));
+        });
 
         // match aggregate
-        try (Grakn.Session session = Grakn.session(graknHost, keyspace)) {
-            try (Grakn.Transaction transaction = session.transaction(GraknTxType.READ)) {
-                int aggregateCount = transaction.graql().match(var("p").isa("person")).aggregate(count()).execute().number().intValue();
-                assertThat(aggregateCount, equalTo(gatesFamilyNames().length));
-            }
-        }
+        localGraknTx(tx -> {
+            int aggregateCount = tx.graql().match(var("p").isa("person")).aggregate(count()).execute().number().intValue();
+            assertThat(aggregateCount, equalTo(gatesFamilyNames().length));
+        });
 
         // compute count
-        try (Grakn.Session session = Grakn.session(graknHost, keyspace)) {
-            try (Grakn.Transaction transaction = session.transaction(GraknTxType.READ)) {
-                int computeCount = transaction.graql().compute(GraqlSyntax.Compute.Method.COUNT).in("person")
-                        .execute().get(0).number().intValue();
-                assertThat(computeCount, equalTo(gatesFamilyNames().length));
-            }
-        }
+        localGraknTx(tx -> {
+            int computeCount = tx.graql().compute(GraqlSyntax.Compute.Method.COUNT).in("person")
+                    .execute().get(0).number().intValue();
+            assertThat(computeCount, equalTo(gatesFamilyNames().length));
+        });
 
         // match delete
-        try (Grakn.Session session = Grakn.session(graknHost, keyspace)) {
-            try (Grakn.Transaction transaction = session.transaction(GraknTxType.WRITE)) {
-                transaction.graql().match(var("m").isa("marriage")).delete(var("m")).execute();
-                List<ConceptMap> marriages = transaction.graql().match(var("m").isa("marriage")).get().execute();
-                assertThat(marriages, hasSize(0));
-            }
-        }
+        localGraknTx(tx -> {
+            tx.graql().match(var("m").isa("marriage")).delete(var("m")).execute();
+            List<ConceptMap> marriages = tx.graql().match(var("m").isa("marriage")).get().execute();
+            assertThat(marriages, hasSize(0));
+        });
     }
-
-//    /**
-//     * TODO: match sibling via rule
-//     */
-//
 
     private String[] gatesFamilyNames() {
         return new String[] { "Bill Gates", "Melinda Gates", "Jennifer Katharine Gates",
                 "Phoebe Adele Gates", "Rory John Gates" };
+    }
+
+    private void localGraknTx(Consumer<Grakn.Transaction> fn) {
+        SimpleURI graknHost = new SimpleURI("localhost", 48555);
+        Keyspace keyspace = Keyspace.of("grakn");
+
+        try (Grakn.Session session = Grakn.session(graknHost, keyspace)) {
+            try (Grakn.Transaction transaction = session.transaction(GraknTxType.WRITE)) {
+                fn.accept(transaction);
+            }
+        }
     }
 }
