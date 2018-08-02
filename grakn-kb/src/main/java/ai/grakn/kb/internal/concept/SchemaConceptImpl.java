9a8218feefb87efc811c5c83163b33e1dbf7d8e2
Haikal Pribadi <haikal@grakn.ai>
2018-08-02 11:46:28 +0100

    removed unused imports

--- grakn-kb/src/main/java/ai/grakn/kb/internal/concept/SchemaConceptImpl.java
+++ grakn-kb/src/main/java/ai/grakn/kb/internal/concept/SchemaConceptImpl.java
@@ -18,15 +18,14 @@
 
 package ai.grakn.kb.internal.concept;
 
-import ai.grakn.API;
 import ai.grakn.concept.Concept;
 import ai.grakn.concept.EntityType;
 import ai.grakn.concept.Label;
 import ai.grakn.concept.LabelId;
-import ai.grakn.concept.Rule;
-import ai.grakn.concept.SchemaConcept;
 import ai.grakn.concept.RelationshipType;
 import ai.grakn.concept.Role;
+import ai.grakn.concept.Rule;
+import ai.grakn.concept.SchemaConcept;
 import ai.grakn.exception.GraknTxOperationException;
 import ai.grakn.exception.PropertyNotUniqueException;
 import ai.grakn.kb.internal.cache.Cache;
