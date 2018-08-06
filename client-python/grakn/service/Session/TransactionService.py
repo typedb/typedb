73505ee3f1c4b0f98be44beb037029b75d2686b4
Joshua Send <joshua@Joshuas-MacBook-Pro.local>
2018-08-06 16:20:07 +0100

    ../.circleci

--- client-python/grakn/service/Session/TransactionService.py
+++ client-python/grakn/service/Session/TransactionService.py
@@ -1,9 +1,27 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
 import queue
 from typing import Type
 
 from grakn.service.Session.util.RequestBuilder import RequestBuilder
-# from src.service.Session.util.ResponseConverter import ResponseConverter
-import grakn.service.Session.util.ResponseConverter as ResponseConverter
+import grakn.service.Session.util.ResponseConverter as ResponseConverter # for circular import issue
 from grakn.service.Session.util import enums
 from grakn.exception.ClientError import ClientError
 
--- client-python/grakn/service/Session/util/ResponseConverter.py
+++ client-python/grakn/service/Session/util/ResponseConverter.py
@@ -1,10 +1,27 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
 import abc
 from grakn.service.Session.util import enums
 from grakn.service.Session.Concept import ConceptFactory
 from grakn.exception.ClientError import ClientError
 
-
-# TODO this file is a bit of a mess -- clean up
 
 class ResponseConverter(object):
     
