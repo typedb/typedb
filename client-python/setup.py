39ede1e28dfe876ea3ac139fb78311663736540c
Joshua Send <flyingsilverfin@users.noreply.github.com>
2018-08-07 01:28:13 +0100

    Client python dev (#2918)
    * initial transfer from grakn-python repository
    
    * in progress python GRPC
    
    * in progress to a single full execution through grpc
    
    * add vim swp files to gitignore
    
    * Functional coupel of RPC methods from the top all the way through GRPC and back
    
    * refactor to remove SessionService.py
    
    * remove extra file
    
    * fleshing out some of the driver functionality horizontally
    
    * in progress
    
    * almost completed totally untested drivergit add *.pygit add *.py
    
    * continued fleshing out of RequestBuilder; found insidious bug with protobuf file imports; missing keyspace and response converter mostly
    
    * refactor converters to be stateless
    
    * pre-import fixing checkpoint
    
    * working, untested state of python driver
    
    * fix typo bugs
    
    * add path manipulation, consolidate files
    
    * rename client_python to client-python
    
    * setting up for test pip package
    
    * basics for pubslihing on pip
    
    * update gitignore
    
    * modified for pip packaging
    
    * begin tests and corresponding modifications
    
    * testing and bugfixes in progress
    
    * more tests
    
    * finish writing tests and incrememtal bug fixes, documentation
    
    * fix bugs found during testing
    
    * add README doc based on Node driver's and fix tests and move code around
    
    * checkpoint rewriting for new answer format
    
    * new circleCI workflow, partially finished new Answer format
    
    * disable other tests in circleCI
    
    * fix indent circleCI config
    
    * add --user to make pip3 permissions ok
    
    * add --user to make pip3 permissions ok
    
    * ../.circleci/
    
    * final circleci fix, push some refactors in client python
    
    * specialize exceptions, new circle config
    
    * change to machine image and install pip over apt-get
    
    * documentation update
    
    * documentation update
    
    * finish tests for new Answer types
    
    * bla
    
    * hopefully final circle
    
    * new circlci
    
    * edit makefile and fix CI config
    
    * update circleCI
    
    * next circle attempt...
    
    * circleci
    
    * make sessions and transactions  able
    
    * new tests for  statements
    
    * ../.circleci
    
    * add license headers and new circle with __init__.py touch
    
    * fixed finally circle config
    
    * add test for with syntax and re-enable all the text
    
    (cherry picked from commit 337f9a778f3dd4e5ee626977815e3b46335a56ff)

---
+++ client-python/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, PEP420PackageFinder 
+
+pep420_package_finder = PEP420PackageFinder()
+
+setup(
+    name='grakn',
+    packages=pep420_package_finder.find('.', include=['grakn*']),
+    version='1.2.4.6',
+    license='Apache-2.0',
+    description='A Python client for Grakn',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author='Grakn Labs',
+    author_email='community@grakn.ai',
+    url='https://github.com/flyingsilverfin/grakn/tree/client-python-dev',
+#    download_url='https://github.com/graknlabs/grakn-python/archive/v0.8.1.tar.gz',
+    keywords=['grakn', 'database', 'graph', 'knowledgebase', 'knowledge-engineering'],
+    python_requires='>=3.4.0',
+    install_requires=['grpcio', 'protobuf']
+)
