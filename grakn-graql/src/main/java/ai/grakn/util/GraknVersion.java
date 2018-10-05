b059bc64adf6d25baf4e1979f82a9997f72d3ec1
Haikal Pribadi <haikal@grakn.ai>
2018-10-05 13:54:05 +0100

    replaced GraknVersion java template with with a real class

---
+++ grakn-graql/src/main/java/ai/grakn/util/GraknVersion.java
@@ -0,0 +1,10 @@
+package ai.grakn.util;
+
+/**
+ * Class for storing the Grakn Version.
+ *
+ * TODO: needs to be removed and replaced with server endpoint configured by bazel
+ */
+public class GraknVersion {
+    public static final String VERSION = "1.5.0-SNAPSHOT";
+}
