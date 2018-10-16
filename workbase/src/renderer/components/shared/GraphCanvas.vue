f05684edff517040c62e21c8297bae7fd03ed55e
syed <syed@grakn.ai>
2018-10-16 15:09:52 +0100

    pass object to INITIALIZE_VISUALISER action

--- workbase/src/renderer/components/shared/GraphCanvas.vue
+++ workbase/src/renderer/components/shared/GraphCanvas.vue
@@ -35,7 +35,7 @@ export default {
   mounted() {
     this.$nextTick(() => {
       const graphDiv = this.$refs.graph;
-      this.$store.dispatch(INITIALISE_VISUALISER, graphDiv, VisFacade);
+      this.$store.dispatch(INITIALISE_VISUALISER, { container: graphDiv, visFacade: VisFacade });
     });
   },
 };
