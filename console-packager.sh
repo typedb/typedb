ab95afeaf23e9b6bd805f5c391b2a59c6346343b
Ganeshwara Herawan Hananda <soulmagic13@gmail.com>
2018-10-24 20:55:29 +0100

    removed logback.xml from console

--- console-packager.sh
+++ console-packager.sh
@@ -5,24 +5,20 @@ echo "packaging the grakn-core-all distribution..."
 output="$1"
 script="$2"
 jar="$3"
-logback_xml="$4"
 
 # configurations
 base_dir="grakn-core-console"
-conf_dir="conf"
 services_dir="services"
 services_dir_lib="lib"
 
 # prepare directories and files
 echo "making directories..."
 mkdir -p "$base_dir"
-mkdir -p "$base_dir/$conf_dir"
 mkdir -p "$base_dir/$services_dir/$services_dir_lib"
 echo "the following directories have been created:"
 find $base_dir
 
 cp "$script" "$base_dir"
-cp "$logback_xml" "$base_dir/$conf_dir"
 cp "$jar" "$base_dir/$services_dir/$services_dir_lib"
 
 echo "the grakn-core-console distribution will contain the following files:"
