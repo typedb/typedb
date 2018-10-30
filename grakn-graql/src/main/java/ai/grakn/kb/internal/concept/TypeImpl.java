cb60122053b202f406806ba3be5ffc89f35ba7b8
Marco Scoppetta <marco@grakn.ai>
2018-10-30 16:13:51 +0000

    checkpoint

--- grakn-graql/src/main/java/ai/grakn/kb/internal/concept/TypeImpl.java
+++ grakn-graql/src/main/java/ai/grakn/kb/internal/concept/TypeImpl.java
@@ -40,7 +40,6 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.BiFunction;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
