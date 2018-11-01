d1960369eabfe057fc55e583e70678d257b14b36
Ganeshwara Herawan Hananda <soulmagic13@gmail.com>
2018-11-01 21:34:52 +0000

    wip - adding //console:deploy-github. TODO: actually deploy to github

--- console/distribution.sh
+++ console/distribution.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-set -e # exit immediately when there's a failure. add verbose flag '-x', (ie., 'set -xe') in order to debug the script
+set -x
 
-echo "========================================="
-echo "packaging the grakn-core-all distribution"
-echo "========================================="
 # 1. modules and configurations
 # inputs
 output="$1"
@@ -12,38 +9,18 @@ script="$2"
 jar="$3"
 
 # configurations
-base_dir="grakn-core-console"
+base_dir="$output"
 services_dir="services"
 services_dir_lib="lib"
-echo
 
 # 2. prepare directories and files
-echo "--- making directories ---"
 mkdir -p "$base_dir"
 mkdir -p "$base_dir/$services_dir/$services_dir_lib"
-echo "--- the following directories have been created ---"
 find $base_dir
-echo
 
 # 3. copying files into the respective locations
-echo "--- copying files into the respective locations ---"
 cp "$script" "$base_dir"
 cp "$jar" "$base_dir/$services_dir/$services_dir_lib"
-echo
 
 # 4. the grakn-core-server distribution will contain the following files
-echo "--- the grakn-core-console distribution will contain the following files ---"
 find $base_dir
-echo
-
-# 5. building zip
-echo "--- building zip: 'zip -r \"$output\" \"$base_dir\"' ---"
-zip -r "$output" "$base_dir"
-echo
-
-echo "====================================================="
-echo "the grakn-core-console distribution has been created!"
-echo "====================================================="
-
-# 6. cleanup
-rm -r "$base_dir"
