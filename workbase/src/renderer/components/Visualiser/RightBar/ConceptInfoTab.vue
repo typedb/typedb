a3633ea42918539d416e4e23daf22c738b348844
Syed Irtaza Raza <syed@grakn.ai>
2018-11-12 18:40:08 +0000

    Relationships panel (#4565)

--- workbase/src/renderer/components/Visualiser/RightBar/ConceptInfoTab.vue
+++ workbase/src/renderer/components/Visualiser/RightBar/ConceptInfoTab.vue
@@ -3,20 +3,23 @@
         <div class="tab-header noselect">Concept Info</div>
         <identity-panel :tabId="tabId"></identity-panel>
         <attributes-panel :tabId="tabId"></attributes-panel>
+        <relationships-panel :tabId="tabId"></relationships-panel>
     </div>
 </template>
 <script>
   import IdentityPanel from './ConceptInfoTab/IdentityPanel';
   import AttributesPanel from './ConceptInfoTab/AttributesPanel';
+  import RelationshipsPanel from './ConceptInfoTab/RelationshipsPanel';
+
 
   export default {
     name: 'ConceptInfoTab',
     props: ['tabId'],
-    components: { IdentityPanel, AttributesPanel },
+    components: { IdentityPanel, AttributesPanel, RelationshipsPanel },
   };
 </script>
 <style scoped>
     .node-tab-content {
-        width: 213.14px;
+        width: 214.14px;
     }
 </style>
