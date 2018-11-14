f3a425f9a55e5be1eb9008024449fc4e2eaf75a9
Marco Scoppetta <marco@grakn.ai>
2018-11-14 12:50:14 +0000

    remove unused inner class

--- grakn-engine/src/main/java/ai/grakn/engine/rpc/KeyspaceService.java
+++ grakn-engine/src/main/java/ai/grakn/engine/rpc/KeyspaceService.java
@@ -18,7 +18,6 @@
 
 package ai.grakn.engine.rpc;
 
-import ai.grakn.GraknTxType;
 import ai.grakn.Keyspace;
 import ai.grakn.engine.KeyspaceStore;
 import ai.grakn.rpc.proto.KeyspaceProto;
@@ -60,8 +59,7 @@ public class KeyspaceService extends KeyspaceServiceGrpc.KeyspaceServiceImplBase
     @Override
     public void delete(KeyspaceProto.Keyspace.Delete.Req request, StreamObserver<KeyspaceProto.Keyspace.Delete.Res> response) {
         try {
-            ServerOpenRequest.Arguments args = new ServerOpenRequest.Arguments(Keyspace.of(request.getName()), GraknTxType.WRITE);
-            keyspaceStore.deleteKeyspace(args.getKeyspace());
+            keyspaceStore.deleteKeyspace(Keyspace.of(request.getName()));
 
             response.onNext(KeyspaceProto.Keyspace.Delete.Res.getDefaultInstance());
             response.onCompleted();
