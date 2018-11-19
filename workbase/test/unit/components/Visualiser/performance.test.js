c13e1586f711790e70e1d8dd7faf972bc6695c14
syed <syed@grakn.ai>
2018-11-19 18:23:27 +0000

    update test scripts to allow correct grpc version

--- workbase/test/unit/components/Visualiser/performance.test.js
+++ workbase/test/unit/components/Visualiser/performance.test.js
@@ -1,68 +1,68 @@
-// import Vue from 'vue';
-// import Vuex from 'vuex';
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-// import { RUN_CURRENT_QUERY, CURRENT_KEYSPACE_CHANGED } from '@/components/shared/StoresActions';
+import { RUN_CURRENT_QUERY, CURRENT_KEYSPACE_CHANGED } from '@/components/shared/StoresActions';
 
-// import getters from '@/components/Visualiser/store/getters';
-// import mutations from '@/components/Visualiser/store/mutations';
-// import actions from '@/components/Visualiser/store/actions';
+import getters from '@/components/Visualiser/store/getters';
+import mutations from '@/components/Visualiser/store/mutations';
+import actions from '@/components/Visualiser/store/actions';
 
-// import store from '@/store';
+import store from '@/store';
 
-// import TabState from '@/components/Visualiser/store/tabState';
+import TabState from '@/components/Visualiser/store/tabState';
 
-// jest.mock('@/../Logger', () => ({ error: () => {} }));
+jest.mock('@/../Logger', () => ({ error: () => {} }));
 
-// jest.mock('@/components/shared/PersistentStorage', () => ({
-// }));
+jest.mock('@/components/shared/PersistentStorage', () => ({
+}));
 
-// jest.mock('@/components/EngineSettings', () => ({
-//   getEngineHost: () => '127.0.0.1',
-//   getEngineGrpcUri: () => '127.0.0.1:48555',
-// }));
+jest.mock('@/components/ServerSettings', () => ({
+  getServerHost: () => '127.0.0.1',
+  getServerUri: () => '127.0.0.1:48555',
+}));
 
-// jest.mock('@/components/Visualiser/RightBar/SettingsTab/QuerySettings', () => ({
-//   getRolePlayersStatus: () => true,
-//   getNeighboursLimit: () => 1,
-// }));
+jest.mock('@/components/Visualiser/RightBar/SettingsTab/QuerySettings', () => ({
+  getRolePlayersStatus: () => true,
+  getNeighboursLimit: () => 1,
+}));
 
-// jest.mock('@/components/Visualiser/RightBar/SettingsTab/DisplaySettings', () => ({
-//   getTypeLabels: () => [],
-// }));
+jest.mock('@/components/Visualiser/RightBar/SettingsTab/DisplaySettings', () => ({
+  getTypeLabels: () => [],
+}));
 
-// Vue.use(Vuex);
+Vue.use(Vuex);
 
-// jest.setTimeout(30000);
+jest.setTimeout(30000);
 
-// Array.prototype.flatMap = function flat(lambda) { return Array.prototype.concat.apply([], this.map(lambda)); };
+Array.prototype.flatMap = function flat(lambda) { return Array.prototype.concat.apply([], this.map(lambda)); };
 
-// beforeAll(async () => {
-//   store.registerModule('tab-0', { namespaced: true, getters, state: TabState.create(), mutations, actions });
+beforeAll(async () => {
+  store.registerModule('tab-0', { namespaced: true, getters, state: TabState.create(), mutations, actions });
 
-//   store.dispatch('initGrakn');
+  store.dispatch('initGrakn');
 
-//   const visFacade = {
-//     updateNode: jest.fn(),
-//     fitGraphToWindow: jest.fn(),
-//     addToCanvas: jest.fn(),
-//     resetCanvas: jest.fn(),
-//     getAllNodes: jest.fn().mockImplementation(() => [{ id: 1234, type: 'person' }]),
-//   };
+  const visFacade = {
+    updateNode: jest.fn(),
+    fitGraphToWindow: jest.fn(),
+    addToCanvas: jest.fn(),
+    resetCanvas: jest.fn(),
+    getAllNodes: jest.fn().mockImplementation(() => [{ id: 1234, type: 'person' }]),
+  };
 
-//   store.commit('tab-0/setVisFacade', visFacade);
+  store.commit('tab-0/setVisFacade', visFacade);
 
-//   store.dispatch(`tab-0/${CURRENT_KEYSPACE_CHANGED}`, 'gene');
-// });
+  store.dispatch(`tab-0/${CURRENT_KEYSPACE_CHANGED}`, 'gene');
+});
 
 describe('Run Query', () => {
   test('match $x isa person', async () => {
-    // store.commit('tab-0/setCurrentQuery', 'match $x isa person; limit 1; get;');
+    store.commit('tab-0/setCurrentQuery', 'match $x isa person; limit 1; get;');
 
-    // const t0 = performance.now();
-    // await store.dispatch(`tab-0/${RUN_CURRENT_QUERY}`);
-    // const t1 = performance.now();
+    const t0 = performance.now();
+    await store.dispatch(`tab-0/${RUN_CURRENT_QUERY}`);
+    const t1 = performance.now();
 
-    // expect((t1 - t0) < 100).toBe(true);
+    expect((t1 - t0) < 200).toBe(true);
   });
 });
 
