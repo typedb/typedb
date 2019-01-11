5c1dea0201a66057e89266f43bc21cfb92ee4cae
syed <syed@grakn.ai>
2019-01-11 19:25:11 +0500

    update test button to a loading button to wait for change of grakn running flag in the state

--- workbase/src/renderer/components/shared/Preferences.vue
+++ workbase/src/renderer/components/shared/Preferences.vue
@@ -16,7 +16,8 @@
                     <input class="input" v-model="serverHost">
                     <h1 class="connection-label port">Port:</h1>
                     <input class="input" type="number" v-model="serverPort">
-                    <button @click="testConnection" :class="connectionTest" class="btn test-btn" :disabled="(connectionTest !== 'Test')? true : false">{{connectionTest}}</button>
+                    <loading-button v-on:clicked="testConnection" :text="connectionTest" className="btn test-btn" :loading="connectionTest === 'testing'"></loading-button>
+                    <!-- <button @click="testConnection" :class="connectionTest" class="btn test-btn" :disabled="(connectionTest !== 'Test')? true : false">{{connectionTest}}</button> -->
                 </div>
             </div>
 
@@ -219,6 +220,7 @@ export default {
   },
   methods: {
     testConnection() {
+      this.connectionTest = 'testing';
       this.$store.dispatch('initGrakn');
     },
     addNewKeyspace() {
