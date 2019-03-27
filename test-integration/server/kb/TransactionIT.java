f28b8e8ee5b67321f07d8c262235a824d7ba8ba7
Marco Scoppetta <marco@grakn.ai>
2019-03-27 16:37:20 +0000

    Add test for concurrent queries in dataset with rules (#5067)
    Make sure Reasoner does not crash when concurrent clients are querying the same keyspace which contains rules.
    
    In Grakn 1.4.3 we would get java.lang.RuntimeException: java.util.ConcurrentModificationException
    
    Changes:
    * minor cleanup in TransactionIT
    
    * add test for concurrent match queries in dataset with rules

--- test-integration/server/kb/TransactionIT.java
+++ test-integration/server/kb/TransactionIT.java
@@ -19,7 +19,6 @@
 package grakn.core.server.kb;
 
 import grakn.core.common.exception.ErrorMessage;
-import grakn.core.concept.Label;
 import grakn.core.concept.thing.Attribute;
 import grakn.core.concept.thing.Entity;
 import grakn.core.concept.type.AttributeType;
@@ -27,6 +26,7 @@ import grakn.core.concept.type.EntityType;
 import grakn.core.concept.type.RelationType;
 import grakn.core.concept.type.Role;
 import grakn.core.concept.type.SchemaConcept;
+import grakn.core.concept.type.Type;
 import grakn.core.rule.GraknTestServer;
 import grakn.core.server.exception.InvalidKBException;
 import grakn.core.server.exception.TransactionException;
@@ -43,13 +43,11 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toSet;
 import static org.hamcrest.CoreMatchers.instanceOf;
@@ -98,9 +96,9 @@ public class TransactionIT {
     }
 
     @Test
-    public void whenGettingResourcesByValue_ReturnTheMatchingResources() {
+    public void whenGettingAttributesByValue_ReturnTheMatchingAttributes() {
         String targetValue = "Geralt";
-//        assertThat(tx.getAttributesByValue(targetValue), is(empty()));
+        assertTrue(tx.getAttributesByValue(targetValue).isEmpty());
 
         AttributeType<String> t1 = tx.putAttributeType("Parent 1", AttributeType.DataType.STRING);
         AttributeType<String> t2 = tx.putAttributeType("Parent 2", AttributeType.DataType.STRING);
@@ -109,7 +107,7 @@ public class TransactionIT {
         Attribute<String> r2 = t2.create(targetValue);
         t2.create("Dragon");
 
-//        assertThat(tx.getAttributesByValue(targetValue), containsInAnyOrder(r1, r2));
+        assertThat(tx.getAttributesByValue(targetValue), containsInAnyOrder(r1, r2));
     }
 
     @Test
@@ -179,7 +177,7 @@ public class TransactionIT {
     }
 
     @Test
-    public void attemptingToUseClosedTxFailingThenOpeningGraph_EnsureGraphIsUsable() throws InvalidKBException {
+    public void attemptingToUseClosedTxFailsThenOpeningNewTx_EnsureTxIsUsable() throws InvalidKBException {
         tx.close();
 
         boolean errorThrown = false;
@@ -198,7 +196,7 @@ public class TransactionIT {
 
 
     @Test
-    public void whenClosingAGraphWhichWasJustCommitted_DoNothing() {
+    public void whenClosingATxWhichWasJustCommitted_DoNothing() {
         tx.commit();
         assertTrue("Graph is still open after commit", tx.isClosed());
         tx.close();
@@ -206,7 +204,7 @@ public class TransactionIT {
     }
 
     @Test
-    public void whenCommittingAGraphWhichWasJustCommitted_DoNothing() {
+    public void whenCommittingATxWhichWasJustCommitted_DoNothing() {
         tx.commit();
         assertTrue("Graph is still open after commit", tx.isClosed());
         tx.commit();
@@ -255,7 +253,7 @@ public class TransactionIT {
     }
 
     @Test
-    public void whenOpeningDifferentTypesOfGraphsOnTheSameThread_Throw() {
+    public void whenOpeningDifferentTypesOfTransactionsOnTheSameThread_Throw() {
         String keyspace = tx.keyspace().name();
         failAtOpeningTx(session, true, keyspace);
         tx.close();
@@ -352,14 +350,15 @@ public class TransactionIT {
         assertEquals(2L, tx.getShardCount(entity));
     }
 
-//    @Test
-//    public void whenGettingSupsOfASchemaConcept_ResultIncludesMetaThing() {
-//        EntityType yes = tx.putEntityType("yes");
-//        EntityType entity = tx.getMetaEntityType();
-//        Type thing = tx.getMetaConcept();
-//        Set<SchemaConcept> no = (Set<SchemaConcept>) tx.sups(yes).collect(toSet());
-//        assertThat(no, containsInAnyOrder(yes, entity, thing));
-//        assertThat(tx.sups(entity).collect(toSet()), containsInAnyOrder(entity, thing));
-//        assertThat(tx.sups(thing).collect(toSet()), containsInAnyOrder(thing));
-//    }
+    @Test
+    public void whenGettingSupsOfASchemaConcept_ResultIncludesMetaThing() {
+        EntityType yes = tx.putEntityType("yes");
+        EntityType entity = tx.getMetaEntityType();
+        Type thing = tx.getMetaConcept();
+        Set<SchemaConcept> no = tx.sups(yes).collect(toSet());
+        assertThat(no, containsInAnyOrder(yes, entity, thing));
+        assertThat(tx.sups(entity).collect(toSet()), containsInAnyOrder(entity, thing));
+        assertThat(tx.sups(thing).collect(toSet()), containsInAnyOrder(thing));
+    }
+
 }
