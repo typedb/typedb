4497cf0bbaca0ee1eec191538cc7d520739b6823
kasper-piskorski <muydeemer@o2.pl>
2019-04-08 11:03:06 +0100

    fix GraknConsoleIT

--- console/test/GraknConsoleIT.java
+++ console/test/GraknConsoleIT.java
@@ -139,13 +139,14 @@ public class GraknConsoleIT {
         runConsoleSessionWithoutExpectingErrors("define bar-bar sub entity;\ncommit\n", "-k", "bar");
 
         String fooFooInFoo = runConsoleSessionWithoutExpectingErrors("match foo-foo sub entity; get; count;\n", "-k", "foo");
-        String fooFooInBar = runConsoleSessionWithoutExpectingErrors("match foo-foo sub entity; get; count;\n", "-k", "bar");
-        String barBarInFoo = runConsoleSessionWithoutExpectingErrors("match bar-bar sub entity; get; count;\n", "-k", "foo");
         String barBarInBar = runConsoleSessionWithoutExpectingErrors("match bar-bar sub entity; get; count;\n", "-k", "bar");
         assertThat(fooFooInFoo, containsString("1"));
-        assertThat(fooFooInBar, containsString("0"));
-        assertThat(barBarInFoo, containsString("0"));
         assertThat(barBarInBar, containsString("1"));
+
+        Response barBarInFoo = runConsoleSession("match bar-bar sub entity; get; count;\n", "-k", "foo");
+        Response fooFooInBar = runConsoleSession("match foo-foo sub entity; get; count;\n", "-k", "bar");
+        assertTrue(barBarInFoo.err().contains("label 'bar-bar' not found"));
+        assertTrue(fooFooInBar.err().contains("label 'foo-foo' not found"));
     }
 
     @Test
