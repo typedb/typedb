a8ca7c116e27f7d740e30b5c62cfae518e96f132
kasper-piskorski <muydeemer@o2.pl>
2019-08-12 14:18:37 +0100

    cleanup

--- server/src/server/session/cache/RuleCache.java
+++ server/src/server/session/cache/RuleCache.java
@@ -30,7 +30,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toSet;
