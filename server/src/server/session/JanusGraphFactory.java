8205f9803a82ed87b9d3359b40d297d42f6078cf
kasper-piskorski <muydeemer@o2.pl>
2019-09-02 22:59:33 +0100

    set to 60M

--- server/src/server/session/JanusGraphFactory.java
+++ server/src/server/session/JanusGraphFactory.java
@@ -101,7 +101,7 @@ final public class JanusGraphFactory {
         org.janusgraph.core.JanusGraphFactory.Builder builder = org.janusgraph.core.JanusGraphFactory.build()
                 .set(STORAGE_BACKEND, THRIFT_BACKEND)
                 .set(STORAGE_KEYSPACE, keyspace)
-                .set(STORAGE_FRAME_SIZE, 256);
+                .set(STORAGE_FRAME_SIZE, 60);
 
         //Load Passed in properties
         config.properties().forEach((key, value) -> {
