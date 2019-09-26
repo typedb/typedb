2f2a18c7c0b56460f3bb969ffc2f581cbf879a9d
Joshua Send <joshua@grakn.ai>
2019-09-26 15:19:14 +0100

    Implement Custom Lazy Stream Merging (#5449)
    ## What is the goal of this PR?
    Replace io.vavr (#5430) that was introduced to solve the lack of lazy `flatMap` that is mentioned in #5430. We then determined that conversion to the `io.vavr` Stream implementation is more lazy, but the conversion step itself does a single call to `.next()`. In practical terms, this means Grakn always precomputed the first answer on query, which is still not fully lazy.
    This led to the decision to restore prior behavior and solve the eagerness problem by implementing our own lazy stream merging solution.
    
    ## What are the changes implemented in this PR?
    * Implement a lazy stream merging solution that uses `stream.iterator()` conversions interally
    * Remove `io.vavr` as a dependency
    * tests for lazy stream merging

--- server/src/graql/executor/QueryExecutor.java
+++ server/src/graql/executor/QueryExecutor.java
@@ -38,6 +38,7 @@ import grakn.core.graql.gremlin.GraqlTraversal;
 import grakn.core.graql.gremlin.TraversalPlanner;
 import grakn.core.graql.reasoner.query.ReasonerQueries;
 import grakn.core.graql.reasoner.query.ReasonerQueryImpl;
+import grakn.core.graql.util.LazyMergingStream;
 import grakn.core.server.exception.GraknServerException;
 import grakn.core.server.session.TransactionOLTP;
 import graql.lang.Graql;
@@ -57,6 +58,13 @@ import graql.lang.query.MatchClause;
 import graql.lang.query.builder.Filterable;
 import graql.lang.statement.Statement;
 import graql.lang.statement.Variable;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
+import org.apache.tinkerpop.gremlin.structure.Edge;
+import org.apache.tinkerpop.gremlin.structure.Element;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -71,12 +79,6 @@ import java.util.function.Function;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
-import org.apache.tinkerpop.gremlin.structure.Edge;
-import org.apache.tinkerpop.gremlin.structure.Element;
-import org.apache.tinkerpop.gremlin.structure.Vertex;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.groupingBy;
@@ -107,26 +109,26 @@ public class QueryExecutor {
 
             if (!infer) {
 
-                // TODO: this is automatically fixed in Java 10 or OpenJDK 8u222, remove workaround if these conditions met
-                // workaround to deal with non-lazy Java 8 flatMap() functions
-                io.vavr.collection.Stream<Conjunction<Statement>> conjunctions =
-                        io.vavr.collection.Stream.ofAll(matchClause.getPatterns().getDisjunctiveNormalForm().getPatterns().stream());
-
-                answerStream = conjunctions
+                // TODO: lazy flatMap() is automatically fixed in Java 10 or OpenJDK 8u222, remove workaround if these conditions met
+                // custom workaround to deal with non-lazy Java 8 flatMap() functions is in LazyMergingStream
+                Stream<Conjunction<Statement>> conjunctions = matchClause.getPatterns().getDisjunctiveNormalForm().getPatterns().stream();
+                Stream<Stream<ConceptMap>> answerStreams = conjunctions
                         .map(p -> ReasonerQueries.create(p, transaction))
                         .map(ReasonerQueryImpl::getPattern)
-                        .flatMap(p -> io.vavr.collection.Stream.ofAll(traverse(p)))
-                        .toJavaStream();
+                        .map(p -> traverse(p));
+
+                LazyMergingStream<ConceptMap> mergedStreams = new LazyMergingStream<>(answerStreams);
+                return mergedStreams.flatStream();
 
             } else {
 
-                io.vavr.collection.Stream<Conjunction<Pattern>> conjunctions =
-                        io.vavr.collection.Stream.ofAll(matchClause.getPatterns().getNegationDNF().getPatterns().stream());
-
-                answerStream = conjunctions
+                Stream<Conjunction<Pattern>> conjunctions = matchClause.getPatterns().getNegationDNF().getPatterns().stream();
+                Stream<Stream<ConceptMap>> answerStreams = conjunctions
                         .map(p -> ReasonerQueries.resolvable(p, transaction).rewrite())
-                        .flatMap(q -> io.vavr.collection.Stream.ofAll(q.resolve()))
-                        .toJavaStream();
+                        .map(q -> q.resolve());
+
+                LazyMergingStream<ConceptMap> mergedStreams = new LazyMergingStream<>(answerStreams);
+                return mergedStreams.flatStream();
 
             }
         } catch (GraqlCheckedException e) {
---
+++ server/test/graql/util/LazyMergingStreamTest.java
@@ -0,0 +1,121 @@
+/*
+ * GRAKN.AI - THE KNOWLEDGE GRAPH
+ * Copyright (C) 2019 Grakn Labs Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package grakn.core.graql.util;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class LazyMergingStreamTest {
+
+    @Test
+    public void whenOuterStreamEmptyNoErrorThrown() {
+        Stream<Stream<Integer>> emptyStream = Stream.empty();
+        LazyMergingStream<Integer> mergingStream = new LazyMergingStream<>(emptyStream);
+        long count = mergingStream.flatStream().count();
+        assertEquals(0, count);
+    }
+
+    @Test
+    public void whenInnerStreamEmptyNoErrorIsThrown() {
+        Stream<Stream<Integer>> emptyStream = Stream.of(Stream.empty());
+        LazyMergingStream<Integer> mergingStream = new LazyMergingStream<>(emptyStream);
+        long count = mergingStream.flatStream().count();
+        assertEquals(0, count);
+    }
+
+    @Test
+    public void whenAllInnerStreamsEmptyNoErrorIsThrown() {
+        Stream<Stream<Integer>> emptyStream = Stream.of(Stream.empty(), Stream.empty(), Stream.empty());
+        LazyMergingStream<Integer> mergingStream = new LazyMergingStream<>(emptyStream);
+        long count = mergingStream.flatStream().count();
+        assertEquals(0, count);
+    }
+
+    @Test
+    public void whenoneInnerStreamsEmptyNoErrorIsThrown() {
+        Stream<Stream<Integer>> emptyStream = Stream.of(Stream.of(1,2), Stream.empty(), Stream.of(3,4));
+        LazyMergingStream<Integer> mergingStream = new LazyMergingStream<>(emptyStream);
+        long count = mergingStream.flatStream().count();
+        assertEquals(4, count);
+    }
+
+    @Test
+    public void outerStreamIsConsumedLazily() {
+        Stream<Stream<Integer>> mockStream = mock(Stream.class);
+        Stream<Integer> mockFirstInnerStream = mock(Stream.class);
+        Stream<Integer> mockSecondInnerStream = mock(Stream.class);
+
+        Iterator<Stream<Integer>> mockIterator = mock(Iterator.class);
+        when(mockStream.iterator()).thenReturn(mockIterator);
+        when(mockIterator.hasNext()).thenReturn(true).thenReturn(true).thenReturn(false);
+        when(mockIterator.next()).thenReturn(mockFirstInnerStream).thenReturn(mockSecondInnerStream);
+
+        when(mockFirstInnerStream.iterator()).thenReturn(Arrays.asList(1).iterator());
+        when(mockSecondInnerStream.iterator()).thenReturn(Arrays.asList(21).iterator());
+
+        LazyMergingStream<Integer> mergingStream = new LazyMergingStream<>(mockStream);
+        Stream<Integer> flatStream = mergingStream.flatStream();
+        // creating the stream should not call anything and be fully lazy
+        verify(mockStream, times(1)).iterator();
+        verify(mockIterator, times(0)).hasNext();
+        verify(mockIterator, times(0)).next();
+    }
+
+    @Test
+    public void innerStreamIsConsumedLazily() {
+        Stream<Integer> mockFirstInnerStream = mock(Stream.class);
+        Stream<Integer> mockSecondInnerStream = mock(Stream.class);
+
+        Stream<Stream<Integer>> outerStream = Stream.of(mockFirstInnerStream, mockSecondInnerStream);
+
+        Iterator<Integer> mockIntegerIterator = mock(Iterator.class);
+        when(mockIntegerIterator.hasNext()).thenReturn(true).thenReturn(true).thenReturn(false);
+        when(mockIntegerIterator.next()).thenReturn(1).thenReturn(2);
+        when(mockFirstInnerStream.iterator()).thenReturn(mockIntegerIterator);
+
+        Iterator<Integer> mockSecondIntegerIterator = mock(Iterator.class);
+        when(mockSecondIntegerIterator.hasNext()).thenReturn(true).thenReturn(true).thenReturn(true).thenReturn(false);
+        when(mockSecondIntegerIterator.next()).thenReturn(21).thenReturn(22).thenReturn(23);
+        when(mockSecondInnerStream.iterator()).thenReturn(mockSecondIntegerIterator);
+
+        LazyMergingStream<Integer> mergingStream = new LazyMergingStream<>(outerStream);
+        Stream<Integer> flatStream = mergingStream.flatStream();
+
+        List<Integer> numbers = flatStream.limit(3).collect(Collectors.toList());
+
+        verify(mockIntegerIterator, times(2)).next();
+        verify(mockIntegerIterator, times(3)).hasNext();
+
+        verify(mockSecondIntegerIterator, times(1)).next();
+        verify(mockSecondIntegerIterator, times(1)).hasNext();
+
+        assertEquals(Arrays.asList(1, 2, 21), numbers);
+    }
+}
