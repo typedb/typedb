f5b8e78803445eea3b8a63687f4570437ed31946
Joshua Send <joshuasend1@gmail.com>
2019-11-01 14:26:47 +0000

    Add extra explanation test

--- test-integration/graql/reasoner/query/ExplanationIT.java
+++ test-integration/graql/reasoner/query/ExplanationIT.java
@@ -67,22 +67,22 @@ public class ExplanationIT {
     private static Session explanationSession;
 
     @BeforeClass
-    public static void loadContext(){
+    public static void loadContext() {
         geoSession = server.sessionWithNewKeyspace();
         GeoGraph geoGraph = new GeoGraph(geoSession);
         geoGraph.load();
         explanationSession = server.sessionWithNewKeyspace();
-        loadFromFileAndCommit(resourcePath,"explanations.gql", explanationSession);
+        loadFromFileAndCommit(resourcePath, "explanations.gql", explanationSession);
     }
 
     @AfterClass
-    public static void closeSession(){
+    public static void closeSession() {
         geoSession.close();
         explanationSession.close();
     }
 
     @Test
-    public void whenExplainingNonRuleResolvableQuery_explanationsAreEmpty(){
+    public void whenExplainingNonRuleResolvableQuery_explanationsAreEmpty() {
         try (Transaction tx = geoSession.readTransaction()) {
             String queryString = "match $x isa city, has name $n; get;";
 
@@ -93,7 +93,7 @@ public class ExplanationIT {
     }
 
     @Test
-    public void whenQueryingWithReasoningOff_explanationsAreEmpty(){
+    public void whenQueryingWithReasoningOff_explanationsAreEmpty() {
         try (Transaction tx = geoSession.readTransaction()) {
             String queryString = "match $x isa city, has name $n; get;";
 
@@ -192,7 +192,7 @@ public class ExplanationIT {
     }
 
     @Test
-    public void whenExplainingAGroundQuery_explanationsAreCorrect(){
+    public void whenExplainingAGroundQuery_explanationsAreCorrect() {
         try (Transaction tx = geoSession.readTransaction()) {
             Concept polibuda = getConcept(tx, "name", "Warsaw-Polytechnics");
             Concept europe = getConcept(tx, "name", "Europe");
@@ -215,7 +215,7 @@ public class ExplanationIT {
     }
 
     @Test
-    public void whenExplainingConjunctiveQueryWithTwoIdPredicates_explanationsAreCorrect(){
+    public void whenExplainingConjunctiveQueryWithTwoIdPredicates_explanationsAreCorrect() {
         try (Transaction tx = geoSession.readTransaction()) {
             Concept polibuda = getConcept(tx, "name", "Warsaw-Polytechnics");
             Concept masovia = getConcept(tx, "name", "Masovia");
@@ -234,7 +234,7 @@ public class ExplanationIT {
     }
 
     @Test
-    public void whenExplainingConjunctions_explanationsAreCorrect(){
+    public void whenExplainingConjunctions_explanationsAreCorrect() {
         try (Transaction tx = explanationSession.readTransaction()) {
             String queryString = "match " +
                     "(role1: $x, role2: $w) isa inferredRelation;" +
@@ -248,7 +248,7 @@ public class ExplanationIT {
     }
 
     @Test
-    public void whenExplainingMixedAtomicQueries_explanationsAreCorrect(){
+    public void whenExplainingMixedAtomicQueries_explanationsAreCorrect() {
         try (Transaction tx = explanationSession.readTransaction()) {
             String queryString = "match " +
                     "$x has value 'high';" +
@@ -269,7 +269,7 @@ public class ExplanationIT {
     }
 
     @Test
-    public void whenExplainingEquivalentPartialQueries_explanationsAreCorrect(){
+    public void whenExplainingEquivalentPartialQueries_explanationsAreCorrect() {
         try (Transaction tx = explanationSession.writeTransaction()) {
             String queryString = "match $x isa same-tag-column-link; get;";
 
@@ -288,18 +288,18 @@ public class ExplanationIT {
 
     /**
      * Validates issue#3061 is fixed.
-     *
+     * <p>
      * The test illustrates the following issue. The `pair` and `has name` rules are mutually recursive - one fires the other.
      * As a result, when resolving the second top atom of the input conjunctive query the answer is already in the cache.
      * The answer contains a `RuleExplanation`.
      * However, the query to be checked in cache is:
-     *
+     * <p>
      * `$p isa pair;`,
-     *
+     * <p>
      * whereas the cache contains:
-     *
+     * <p>
      * `$p (prep: $prep, pobj: $pobj) isa pair;`,
-     *
+     * <p>
      * which are not equivalent.
      * Although the cache contained query will contain all answers to `$p isa pair;`, this is a special case.
      * If we had an instance of pair relation with only a single roleplayer, recognising a subsumption relation between
@@ -310,7 +310,7 @@ public class ExplanationIT {
     public void whenRulesAreMutuallyRecursive_explanationsAreRecognisedAsRuleOnes() {
         try (Session session = server.sessionWithNewKeyspace()) {
             loadFromFileAndCommit(resourcePath, "testSet30.gql", session);
-            for(int i = 0 ; i < 10 ; i++) {
+            for (int i = 0; i < 10; i++) {
                 try (Transaction tx = session.writeTransaction()) {
                     String queryString = "match $p isa pair, has name 'ff'; get;";
                     List<ConceptMap> answers = tx.execute(Graql.parse(queryString).asGet());
@@ -327,18 +327,21 @@ public class ExplanationIT {
         }
     }
 
-    private void testExplanation(Collection<ConceptMap> answers){
+    private void testExplanation(Collection<ConceptMap> answers) {
         answers.forEach(this::testExplanation);
     }
 
-    private void testExplanation(ConceptMap answer){
+    private void testExplanation(ConceptMap answer) {
         answerHasConsistentExplanations(answer);
         checkExplanationCompleteness(answer);
         checkAnswerConnectedness(answer);
+        if (answer.getPattern() != null) {
+            patternContainsIdPerVariable(answer.getPattern());
+        }
     }
 
     //ensures that each branch ends up with an lookup explanation
-    private void checkExplanationCompleteness(ConceptMap answer){
+    private void checkExplanationCompleteness(ConceptMap answer) {
         assertFalse("Non-lookup explanation misses children",
                 answer.explanations().stream()
                         .filter(e -> !e.isLookupExplanation())
@@ -346,7 +349,7 @@ public class ExplanationIT {
         );
     }
 
-    private void checkAnswerConnectedness(ConceptMap answer){
+    private void checkAnswerConnectedness(ConceptMap answer) {
         List<ConceptMap> answers = answer.explanation().getAnswers();
         answers.forEach(a -> {
             assertTrue("Disconnected answer in explanation",
@@ -357,7 +360,7 @@ public class ExplanationIT {
         });
     }
 
-    private void answerHasConsistentExplanations(ConceptMap answer){
+    private void answerHasConsistentExplanations(ConceptMap answer) {
         Set<ConceptMap> answers = answer.explanation().deductions().stream()
                 .filter(a -> !a.explanation().isJoinExplanation())
                 .collect(Collectors.toSet());
@@ -365,30 +368,30 @@ public class ExplanationIT {
         answers.forEach(a -> assertTrue("Answer has inconsistent explanations", explanationConsistentWithAnswer(a)));
     }
 
-    private static Concept getConcept(Transaction tx, String typeLabel, String val){
+    private static Concept getConcept(Transaction tx, String typeLabel, String val) {
         return tx.stream(Graql.match(var("x").has(typeLabel, val)).get("x"))
                 .map(ans -> ans.get("x")).findAny().orElse(null);
     }
 
-    private ConceptMap findAnswer(ConceptMap a, List<ConceptMap> list){
-        for(ConceptMap ans : list) {
+    private ConceptMap findAnswer(ConceptMap a, List<ConceptMap> list) {
+        for (ConceptMap ans : list) {
             if (ans.equals(a)) return ans;
         }
         return new ConceptMap();
     }
 
-    private Set<Explanation> getRuleExplanations(ConceptMap a){
+    private Set<Explanation> getRuleExplanations(ConceptMap a) {
         return a.explanations().stream().filter(Explanation::isRuleExplanation).collect(Collectors.toSet());
     }
 
-    private Set<Explanation> getLookupExplanations(ConceptMap a){
+    private Set<Explanation> getLookupExplanations(ConceptMap a) {
         return a.explanations().stream().filter(Explanation::isLookupExplanation).collect(Collectors.toSet());
     }
 
-    private boolean explanationConsistentWithAnswer(ConceptMap ans){
+    private boolean explanationConsistentWithAnswer(ConceptMap ans) {
         Pattern queryPattern = ans.getPattern();
         Map<Variable, String> varIds = new HashMap<>();
-        if (queryPattern != null){
+        if (queryPattern != null) {
             queryPattern.statements().stream()
                     .filter(statement -> statement.hasProperty(IdProperty.class))
                     .forEach(statement -> {
@@ -402,5 +405,13 @@ public class ExplanationIT {
             allVariableIdsMatch &= mapping.getValue().id().toString().equals(varIds.get(mapping.getKey()));
         }
         return allVariableIdsMatch;
+    }
+
+    private void patternContainsIdPerVariable(Pattern pattern) {
+        Set<Variable> variables = pattern.variables();
+        long variablesWithIds = pattern.statements().stream()
+                .filter(statement -> statement.hasProperty(IdProperty.class))
+                .count();
+        assertEquals(variables.size(), variablesWithIds);
     }
 }
