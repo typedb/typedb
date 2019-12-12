415d938de7fa6da3d86e6c0ee19b38e914336e85
Kasper Piskorski <muydeemer@o2.pl>
2019-12-12 15:18:45 +0000

    Bring back the previous LogInstanceCount behaviour (#5539)
    ## What is the goal of this PR?
    Revert the `getLogInstanceCount` function to previous behaviour as the query planner needs updating to implement the function in a more granular fashion.
    ## What are the changes implemented in this PR?
    - revert `getLogInstanceCount` to previous behaviour

--- graql/planning/TraversalPlanFactoryImpl.java
+++ graql/planning/TraversalPlanFactoryImpl.java
@@ -328,13 +328,19 @@ public class TraversalPlanFactoryImpl implements TraversalPlanFactory {
     private double getLogInstanceCount(Fragment fragment) {
         // set the weight of the node as a starting point based on log(number of this node)
         double logInstanceCount;
+
+        //this is to make sure we don't end up with log(0)
+        double shardLoadFactor = 0.25;
         if (fragment instanceof LabelFragment) {
             // only LabelFragment (corresponding to type vertices) can be sharded
             LabelFragment labelFragment = (LabelFragment)fragment;
             Label label = Iterators.getOnlyElement(labelFragment.labels().iterator());
-            long instanceCount = conceptManager.getSchemaConcept(label).subs()
+
+            //TODO: this manipulation is to retain the previous behaviour, we need to update the query planner
+            //to remove the sharding threshold dependency and make this more granular
+            double instanceCount = (conceptManager.getSchemaConcept(label).subs()
                     .mapToLong(schemaConcept -> keyspaceStatistics.count(conceptManager, schemaConcept.label()))
-                    .sum();
+                    .sum() / shardingThreshold + shardLoadFactor ) * shardingThreshold;
             logInstanceCount = Math.log(instanceCount);
         } else {
             logInstanceCount = -1D;
