b9e7504568e4d190b5688c552e314f9841c40519
Kasper Piskorski <muydeemer@o2.pl>
2020-02-18 16:29:15 +0000

    Introduce generated reasoner operational tests (#5576)
    ## What is the goal of this PR?
    Using our subsumption `operator` tool, we generate large number of query pattern pairs (currently a subset of all possibilities) such that each pair is in a subsumption relationship (one pattern generifies the other). Knowing that this property holds between the pairs, we can then test our query subsumption, query unification and query equivalence algorithms as their output is known based on the subsumption relationship.
    
    ## What are the changes implemented in this PR?
    We introduce an `Operator` facility that defines different generalisation operators to act on patterns - the generalisation operators are defined in such way that the output pattern subsumes the input pattern.
    
    Using the facility, we introduce a new `GenerativeOperationalIT` which generates query pairs and for each `(parent, child)` query pattern pair tests whether:
    - there exists a `RULE` unifier between the parent and the child
    - there exists a `SUBSUMPTIVE` unifier between the parent and the child (child subsumes parent)
    - there exists a `STRUCTURAL_SUBSUMPTIVE` unifier between the parent and the child

---
+++ test-integration/graql/reasoner/operator/RemoveRoleplayerOperator.java
@@ -0,0 +1,101 @@
+/*
+ * GRAKN.AI - THE KNOWLEDGE GRAPH
+ * Copyright (C) 2019 Grakn Labs Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+package grakn.core.graql.reasoner.operator;
+
+import com.google.common.collect.Sets;
+import graql.lang.Graql;
+import graql.lang.pattern.Pattern;
+import graql.lang.property.RelationProperty;
+import graql.lang.property.VarProperty;
+import graql.lang.statement.Statement;
+import graql.lang.statement.Variable;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toSet;
+
+/**
+ * Generates a set of generalised patterns by removing existing roleplayers.
+ * The set is computed from a Cartesian product of sets of statements each containing a single roleplayer removal
+ * - the set is computed in analogous fashion to RemoveSubstitutionOperator for substitution removal.
+ */
+public class RemoveRoleplayerOperator implements Operator {
+
+    @Override
+    public Stream<Pattern> apply(Pattern src, TypeContext ctx) {
+        if (!src.statements().stream().flatMap(s -> s.getProperties(RelationProperty.class)).findFirst().isPresent()){
+            return Stream.of(src);
+        }
+
+        List<Set<Statement>> transformedStatements = src.statements().stream()
+                .map(this::transformStatement)
+                .collect(Collectors.toList());
+        return Sets.cartesianProduct(transformedStatements).stream()
+                .map(Graql::and)
+                .filter(p -> !p.equals(src))
+                .map(p -> Graql.and(
+                        p.statements().stream()
+                                .filter(st -> !st.properties().isEmpty())
+                                .collect(toSet())
+                        )
+                );
+    }
+
+    private Set<Statement> transformStatement(Statement src){
+        Variable var = src.var();
+        RelationProperty relProperty = src.getProperty(RelationProperty.class).orElse(null);
+        if (relProperty == null) return Sets.newHashSet(src);
+
+        Set<Optional<RelationProperty>> transformedRelationsProps = transformRelationProperty(relProperty);
+
+        Set<Statement> transformedStatements = Sets.newHashSet(src);
+        transformedRelationsProps.stream()
+                .map(o -> {
+                    LinkedHashSet<VarProperty> properties = new LinkedHashSet<>(src.properties());
+                    properties.remove(relProperty);
+                    o.ifPresent(properties::add);
+                    return Statement.create(var, properties);
+                })
+                .forEach(transformedStatements::add);
+
+        return transformedStatements;
+    }
+
+    private Set<Optional<RelationProperty>> transformRelationProperty(RelationProperty prop){
+        List<Set<Optional<RelationProperty.RolePlayer>>> rPconfigurations = new ArrayList<>();
+
+        prop.relationPlayers().forEach(rp -> {
+            Set<Optional<RelationProperty.RolePlayer>> rps = Sets.newHashSet(Optional.of(rp));
+            rps.add(Optional.empty());
+            rPconfigurations.add(rps);
+        });
+
+        return Sets.cartesianProduct(rPconfigurations).stream()
+                .map(rpSet -> rpSet.stream().map(o -> o.orElse(null)).filter(Objects::nonNull).collect(toSet()))
+                .map(rpSet -> Optional.ofNullable(Utils.relationProperty(rpSet)))
+                .collect(toSet());
+    }
+}
