edff3bca6058b00458146c1a1251494ae032537d
kasper-piskorski <muydeemer@o2.pl>
2020-03-30 17:11:28 +0100

    fix checkstyle

--- graql/reasoner/ResolutionIterator.java
+++ graql/reasoner/ResolutionIterator.java
@@ -78,8 +78,7 @@ public class ResolutionIterator extends ReasonerQueryIterator {
 
             ResolutionState newState = state.generateChildState();
             if (newState != null) {
-                if (LOG.isDebugEnabled())
-                    logTree.addState(newState);
+                if (LOG.isDebugEnabled()) logTree.addState(newState);
 
                 if (!state.isAnswerState()) states.push(state);
                 states.push(newState);
