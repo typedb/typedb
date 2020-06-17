c3caeb7b626b41f0a0a8784af11840d682d9b17c
Adam Mitchell <adam@grakn.ai>
2020-06-17 15:55:01 +0100

    Merge changes from 1.7.2 (#5736)
    ## What is the goal of this PR?
    
    We made some changes in 1.7.2 to add GRPC back pressure in order to address some client issues with server overloading. Without back-pressure, excessive async client requests (possible in the new client releases) could cause the server to become overloaded and run out of memory, taking down the entire server. Since Grakn currently does not have automatic restart or recovery features, this is considered a serious issue so this fix was patched into 1.7.2 during the 1.8 development cycle and now needs to be merged into 1.8
    
    ## What are the changes implemented in this PR?
    
    - Merge change from 1.7.2: Add back-pressure by blocking grpc transaction service RequestObserver whilst server is still processing the last request.

--- server/rpc/SessionService.java
+++ server/rpc/SessionService.java
@@ -43,6 +43,7 @@ import graql.lang.Graql;
 import graql.lang.pattern.Pattern;
 import graql.lang.query.GraqlQuery;
 import io.grpc.Status;
+import io.grpc.StatusException;
 import io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -56,6 +57,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
@@ -187,12 +189,12 @@ public class SessionService extends SessionServiceGrpc.SessionServiceImplBase {
                 String rootId = metadata.get("traceRootId");
                 String parentId = metadata.get("traceParentId");
                 if (rootId != null && parentId != null) {
-                    submit(() -> handleRequest(request, GrablTracingThreadStatic.getGrablTracing()
+                    process(() -> handleRequest(request, GrablTracingThreadStatic.getGrablTracing()
                             .trace(UUID.fromString(rootId), UUID.fromString(parentId), "received")));
                     return;
                 }
             }
-            submit(() -> handleRequest(request));
+            process(() -> handleRequest(request));
         }
 
         @Override
@@ -322,8 +324,12 @@ public class SessionService extends SessionServiceGrpc.SessionServiceImplBase {
             }
         }
 
-        private void submit(Runnable runnable) {
-            threadExecutor.submit(runnable);
+        private void process(Runnable runnable) {
+            try {
+                threadExecutor.submit(runnable).get();
+            } catch (InterruptedException | ExecutionException ex) {
+                // Exception will have been handled already
+            }
         }
 
         private void open(Transaction.Open.Req request) {
