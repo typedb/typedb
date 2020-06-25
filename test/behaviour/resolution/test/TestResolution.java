9a5ab7f9f2530d8bde914b3d39795da0b5f7d978
James Fletcher <james@grakn.ai>
2020-06-25 15:16:29 +0100

    Add a test to ensure that the completeness test throws and `testQuery` throws once the same query is made more ambiguous

--- test/behaviour/resolution/test/TestResolution.java
+++ test/behaviour/resolution/test/TestResolution.java
@@ -163,6 +163,64 @@ public class TestResolution {
         resolution_test.close();
     }
 
+    @Test
+    public void testResolutionTestThrowsWhenRuleTriggersTooOftenAndResultCountIsIncorrect() {
+        Path schemaPath = Paths.get("test", "behaviour", "resolution", "test", "cases", "case1", "schema.gql").toAbsolutePath();
+        Path dataPath = Paths.get("test", "behaviour", "resolution", "test", "cases", "case1", "data.gql").toAbsolutePath();
+        GraqlGet inferenceQuery = Graql.parse("" +
+                "match $lh ($continent, " +
+                "$area) isa location-hierarchy; " +
+                "$continent isa continent; " +
+                "$area isa area; get;").asGet();
+
+
+        Session completionSession = graknTestServer.sessionWithNewKeyspace();
+        initialiseKeyspace(completionSession, schemaPath, dataPath);
+
+        Session testSession = graknTestServer.sessionWithNewKeyspace();
+        initialiseKeyspace(testSession, schemaPath, dataPath);
+
+        // Undefine a rule in the keyspace under test such that the expected facts will not be inferred
+        Transaction tx = testSession.transaction(Transaction.Type.WRITE);
+        tx.execute(Graql.undefine(Graql.type("location-hierarchy-transitivity").sub("rule")));
+        tx.execute(Graql.define(Graql.parsePattern("location-hierarchy-transitivity sub rule,\n" +
+                "when {\n" +
+                "  ($a, $b) isa location-hierarchy;\n" +
+                "  ($b, $c) isa location-hierarchy;\n" +
+                "  $a != $c;\n" +
+                "}, then {\n" +
+                "  (location-hierarchy_superior: $a, location-hierarchy_subordinate: $c) isa location-hierarchy;\n" +
+                "};").statements()));
+        tx.commit();
+
+        Resolution resolution_test = new Resolution(completionSession, testSession);
+
+        Exception testQueryThrows = null;
+        try {
+            resolution_test.testQuery(inferenceQuery);
+        } catch (Exception e) {
+            testQueryThrows = e;
+        }
+        assertNotNull(testQueryThrows);
+        assertThat(testQueryThrows, instanceOf(RuntimeException.class));
+
+        // In this case `testResolution()` could be correct or incorrect, since it could pick a correct path, or one
+        // that has the location-hierarchy backwards
+
+        // resolution_test.testResolution(inferenceQuery);
+
+        Exception testCompletenessThrows = null;
+        try {
+            resolution_test.testCompleteness();
+        } catch (Exception e) {
+            testCompletenessThrows = e;
+        }
+        assertNotNull(testCompletenessThrows);
+        assertThat(testCompletenessThrows, instanceOf(RuntimeException.class));
+
+        resolution_test.close();
+    }
+
 
     @Test
     public void testCase1HappyPath() {
