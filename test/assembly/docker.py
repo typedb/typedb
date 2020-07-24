bc1376d856b67456dccc3caf8e51f91f4ac3eba4
Max Vorobev <vmax0770@gmail.com>
2020-07-24 20:10:30 +0300

    Fix Docker test to be compatible with both Python 2 and Python 3 (#5849)
    ## What is the goal of this PR?
    
    Previously, it was only possible to run `test-assembly-docker` with `python` pointing to `python3`. In order to be compatible with both Python 2 and Python 3, test shouldn't use any new language features.
    
    ## What are the changes implemented in this PR?
    
    * Don't use `time.perf_counter`: it was introduced in 3.3
    * Don't use `raise Exception() from ex` syntax
    * Don't use `socket.create_connection` as context manager

--- test/assembly/docker.py
+++ test/assembly/docker.py
@@ -8,16 +8,16 @@ import time
 
 
 def wait_for_port(port, host='localhost', timeout=30.0):
-    start_time = time.perf_counter()
+    start_time = time.time()
     while True:
         try:
-            with socket.create_connection((host, port), timeout=timeout):
-                break
+            socket.create_connection((host, port), timeout=timeout)
+            return
         except OSError as ex:
             time.sleep(0.01)
-            if time.perf_counter() - start_time >= timeout:
+            if time.time() - start_time >= timeout:
                 raise TimeoutError('Waited too long for the port {} on host {} to start accepting '
-                                   'connections.'.format(port, host)) from ex
+                                   'connections.'.format(port, host))
 
 
 print('Building the image...')
