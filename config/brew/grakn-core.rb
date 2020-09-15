195648b34780dc17a6ca2fbc6c85ea33f5a1e1bf
Max Vorobev <vmax0770@gmail.com>
2020-09-15 16:11:56 +0300

    restructure brew code

--- config/brew/grakn-core.rb
+++ config/brew/grakn-core.rb
@@ -9,15 +9,19 @@ class GraknCore < Formula
   depends_on :java => "1.8"
 
   def install
+    # unpack Grakn Core distribution
     libexec.install Dir["*"]
+    # set up db directory
     dbpath = (var/name/"db/")
     dbpath.mkpath
+    rm_rf libexec/"server/db"
+    ln_s dbpath, libexec/"server/db"
+    # set up logs directory
     logpath = (var/name/"log/")
     logpath.mkpath
-    rm_rf libexec/"server/db"
     rm_rf libexec/"server/logs"
-    ln_s dbpath, libexec/"server/db"
     ln_s logpath, libexec/"server/logs"
+    # install grakn as system-wide command
     bin.install libexec/"grakn"
     bin.env_script_all_files(libexec, Language::Java.java_home_env("1.8"))
   end
