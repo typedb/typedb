e95a30fec3dfdabdab0d9e2a0301285efeb648a4
haikalpribadi <haikal@grakn.ai>
2020-12-01 09:40:34 +0000

    Prepare ProcedureEdge.execute() placeholder methods

--- traversal/procedure/ProcedureEdge.java
+++ traversal/procedure/ProcedureEdge.java
@@ -19,8 +19,10 @@
 package grakn.core.traversal.procedure;
 
 import grakn.core.common.exception.GraknException;
+import grakn.core.common.iterator.ResourceIterator;
 import grakn.core.common.parameters.Label;
 import grakn.core.graph.util.Encoding;
+import grakn.core.traversal.Traversal;
 import grakn.core.traversal.graph.TraversalEdge;
 import grakn.core.traversal.planner.PlannerEdge;
 import graql.lang.common.GraqlToken;
@@ -28,8 +30,10 @@ import graql.lang.common.GraqlToken;
 import java.util.Set;
 
 import static grakn.core.common.exception.ErrorMessage.Internal.UNRECOGNISED_VALUE;
+import static grakn.core.common.iterator.Iterators.iterate;
 import static grakn.core.graph.util.Encoding.Direction.Edge.BACKWARD;
 import static grakn.core.graph.util.Encoding.Direction.Edge.FORWARD;
+import static java.util.Collections.emptyIterator;
 
 abstract class ProcedureEdge<VERTEX_FROM extends ProcedureVertex<?>, VERTEX_TO extends ProcedureVertex<?>>
         extends TraversalEdge<VERTEX_FROM, VERTEX_TO> {
@@ -44,12 +48,12 @@ abstract class ProcedureEdge<VERTEX_FROM extends ProcedureVertex<?>, VERTEX_TO e
     }
 
     public static ProcedureEdge<?, ?> of(ProcedureVertex<?> from, ProcedureVertex<?> to, PlannerEdge.Directional<?, ?> plannerEdge) {
-        int ord = plannerEdge.orderNumber();
+        int order = plannerEdge.orderNumber();
         Encoding.Direction.Edge dir = plannerEdge.direction();
         if (plannerEdge.isEqual()) {
-            return new Equal(from, to, ord, dir);
+            return new Equal(from, to, order, dir);
         } else if (plannerEdge.isPredicate()) {
-            return new Predicate(from.asThing(), to.asThing(), ord, dir, plannerEdge.asPredicate().predicate());
+            return new Predicate(from.asThing(), to.asThing(), order, dir, plannerEdge.asPredicate().predicate());
         } else if (plannerEdge.isNative()) {
             PlannerEdge.Native.Directional<?, ?> edge = plannerEdge.asNative();
             return Native.of(from, to, edge);
@@ -58,10 +62,25 @@ abstract class ProcedureEdge<VERTEX_FROM extends ProcedureVertex<?>, VERTEX_TO e
         }
     }
 
+    abstract ResourceIterator<VERTEX_TO> execute(VERTEX_FROM from, Traversal.Parameters parameters);
+
+    public int order() {
+        return order;
+    }
+
+    public Encoding.Direction.Edge direction() {
+        return direction;
+    }
+
     static class Equal extends ProcedureEdge<ProcedureVertex<?>, ProcedureVertex<?>> {
 
         private Equal(ProcedureVertex<?> from, ProcedureVertex<?> to, int order, Encoding.Direction.Edge direction) {
             super(from, to, order, direction);
+        }
+
+        @Override
+        ResourceIterator<ProcedureVertex<?>> execute(ProcedureVertex<?> procedureVertex, Traversal.Parameters parameters) {
+            return iterate(emptyIterator()); // TODO
         }
     }
 
@@ -74,9 +93,14 @@ abstract class ProcedureEdge<VERTEX_FROM extends ProcedureVertex<?>, VERTEX_TO e
             super(from, to, order, direction);
             this.predicate = predicate;
         }
+
+        @Override
+        ResourceIterator<ProcedureVertex.Thing> execute(ProcedureVertex.Thing from, Traversal.Parameters parameters) {
+            return iterate(emptyIterator()); // TODO
+        }
     }
 
-    static class Native<VERTEX_NATIVE_FROM extends ProcedureVertex<?>, VERTEX_NATIVE_TO extends ProcedureVertex<?>>
+    static abstract class Native<VERTEX_NATIVE_FROM extends ProcedureVertex<?>, VERTEX_NATIVE_TO extends ProcedureVertex<?>>
             extends ProcedureEdge<VERTEX_NATIVE_FROM, VERTEX_NATIVE_TO> {
 
         private Native(VERTEX_NATIVE_FROM from, VERTEX_NATIVE_TO to, int order, Encoding.Direction.Edge direction) {
@@ -84,11 +108,12 @@ abstract class ProcedureEdge<VERTEX_FROM extends ProcedureVertex<?>, VERTEX_TO e
         }
 
         static Native<?, ?> of(ProcedureVertex<?> from, ProcedureVertex<?> to, PlannerEdge.Native.Directional<?, ?> edge) {
+            boolean isForward = edge.direction().isForward();
             if (edge.isIsa()) {
-                int ord = edge.orderNumber();
-                boolean trns = edge.asIsa().isTransitive();
-                if (edge.direction().isForward()) return new Isa.Forward(from.asThing(), to.asType(), ord, trns);
-                else return new Isa.Backward(from.asType(), to.asThing(), ord, trns);
+                int orderNumber = edge.orderNumber();
+                boolean isTransitive = edge.asIsa().isTransitive();
+                if (isForward) return new Isa.Forward(from.asThing(), to.asType(), orderNumber, isTransitive);
+                else return new Isa.Backward(from.asType(), to.asThing(), orderNumber, isTransitive);
             } else if (edge.isType()) {
                 return Native.Type.of(from.asType(), to.asType(), edge.asType());
             } else if (edge.isThing()) {
@@ -98,7 +123,7 @@ abstract class ProcedureEdge<VERTEX_FROM extends ProcedureVertex<?>, VERTEX_TO e
             }
         }
 
-        static class Isa<VERTEX_ISA_FROM extends ProcedureVertex<?>, VERTEX_ISA_TO extends ProcedureVertex<?>>
+        static abstract class Isa<VERTEX_ISA_FROM extends ProcedureVertex<?>, VERTEX_ISA_TO extends ProcedureVertex<?>>
                 extends Native<VERTEX_ISA_FROM, VERTEX_ISA_TO> {
 
             private final boolean isTransitive;
@@ -113,6 +138,11 @@ abstract class ProcedureEdge<VERTEX_FROM extends ProcedureVertex<?>, VERTEX_TO e
                 private Forward(ProcedureVertex.Thing thing, ProcedureVertex.Type type, int order, boolean isTransitive) {
                     super(thing, type, order, FORWARD, isTransitive);
                 }
+
+                @Override
+                ResourceIterator<ProcedureVertex.Type> execute(ProcedureVertex.Thing thing, Traversal.Parameters parameters) {
+                    return iterate(emptyIterator()); // TODO
+                }
             }
 
             static class Backward extends Isa<ProcedureVertex.Type, ProcedureVertex.Thing> {
@@ -120,10 +150,15 @@ abstract class ProcedureEdge<VERTEX_FROM extends ProcedureVertex<?>, VERTEX_TO e
                 private Backward(ProcedureVertex.Type type, ProcedureVertex.Thing thing, int order, boolean isTransitive) {
                     super(type, thing, order, BACKWARD, isTransitive);
                 }
+
+                @Override
+                ResourceIterator<ProcedureVertex.Thing> execute(ProcedureVertex.Type type, Traversal.Parameters parameters) {
+                    return iterate(emptyIterator()); // TODO
+                }
             }
         }
 
-        static class Type extends Native<ProcedureVertex.Type, ProcedureVertex.Type> {
+        static abstract class Type extends Native<ProcedureVertex.Type, ProcedureVertex.Type> {
 
             private final boolean isTransitive;
 
@@ -165,12 +200,22 @@ abstract class ProcedureEdge<VERTEX_FROM extends ProcedureVertex<?>, VERTEX_TO e
                     private Forward(ProcedureVertex.Type from, ProcedureVertex.Type to, int order, boolean isTransitive) {
                         super(from, to, order, FORWARD, isTransitive);
                     }
+
+                    @Override
+                    ResourceIterator<ProcedureVertex.Type> execute(ProcedureVertex.Type type, Traversal.Parameters parameters) {
+                        return iterate(emptyIterator()); // TODO
+                    }
                 }
 
                 static class Backward extends Sub {
 
                     private Backward(ProcedureVertex.Type from, ProcedureVertex.Type to, int order, boolean isTransitive) {
                         super(from, to, order, BACKWARD, isTransitive);
+                    }
+
+                    @Override
+                    ResourceIterator<ProcedureVertex.Type> execute(ProcedureVertex.Type type, Traversal.Parameters parameters) {
+                        return iterate(emptyIterator()); // TODO
                     }
                 }
             }
@@ -189,12 +234,22 @@ abstract class ProcedureEdge<VERTEX_FROM extends ProcedureVertex<?>, VERTEX_TO e
                     private Forward(ProcedureVertex.Type from, ProcedureVertex.Type to, int order, boolean isKey) {
                         super(from, to, order, FORWARD, isKey);
                     }
+
+                    @Override
+                    ResourceIterator<ProcedureVertex.Type> execute(ProcedureVertex.Type type, Traversal.Parameters parameters) {
+                        return iterate(emptyIterator()); // TODO
+                    }
                 }
 
                 static class Backward extends Owns {
 
                     private Backward(ProcedureVertex.Type from, ProcedureVertex.Type to, int order, boolean isKey) {
                         super(from, to, order, BACKWARD, isKey);
+                    }
+
+                    @Override
+                    ResourceIterator<ProcedureVertex.Type> execute(ProcedureVertex.Type type, Traversal.Parameters parameters) {
+                        return iterate(emptyIterator()); // TODO
                     }
                 }
             }
@@ -210,12 +265,22 @@ abstract class ProcedureEdge<VERTEX_FROM extends ProcedureVertex<?>, VERTEX_TO e
                     private Forward(ProcedureVertex.Type from, ProcedureVertex.Type to, int order) {
                         super(from, to, order, FORWARD);
                     }
+
+                    @Override
+                    ResourceIterator<ProcedureVertex.Type> execute(ProcedureVertex.Type type, Traversal.Parameters parameters) {
+                        return iterate(emptyIterator()); // TODO
+                    }
                 }
 
                 static class Backward extends Plays {
 
                     private Backward(ProcedureVertex.Type from, ProcedureVertex.Type to, int order) {
                         super(from, to, order, BACKWARD);
+                    }
+
+                    @Override
+                    ResourceIterator<ProcedureVertex.Type> execute(ProcedureVertex.Type type, Traversal.Parameters parameters) {
+                        return iterate(emptyIterator()); // TODO
                     }
                 }
             }
@@ -231,6 +296,11 @@ abstract class ProcedureEdge<VERTEX_FROM extends ProcedureVertex<?>, VERTEX_TO e
                     private Forward(ProcedureVertex.Type from, ProcedureVertex.Type to, int order) {
                         super(from, to, order, FORWARD);
                     }
+
+                    @Override
+                    ResourceIterator<ProcedureVertex.Type> execute(ProcedureVertex.Type type, Traversal.Parameters parameters) {
+                        return iterate(emptyIterator()); // TODO
+                    }
                 }
 
                 static class Backward extends Relates {
@@ -238,11 +308,16 @@ abstract class ProcedureEdge<VERTEX_FROM extends ProcedureVertex<?>, VERTEX_TO e
                     private Backward(ProcedureVertex.Type from, ProcedureVertex.Type to, int order) {
                         super(from, to, order, BACKWARD);
                     }
+
+                    @Override
+                    ResourceIterator<ProcedureVertex.Type> execute(ProcedureVertex.Type type, Traversal.Parameters parameters) {
+                        return iterate(emptyIterator()); // TODO
+                    }
                 }
             }
         }
 
-        static class Thing extends Native<ProcedureVertex.Thing, ProcedureVertex.Thing> {
+        static abstract class Thing extends Native<ProcedureVertex.Thing, ProcedureVertex.Thing> {
 
             private Thing(ProcedureVertex.Thing from, ProcedureVertex.Thing to, int order, Encoding.Direction.Edge direction) {
                 super(from, to, order, direction);
@@ -281,12 +356,22 @@ abstract class ProcedureEdge<VERTEX_FROM extends ProcedureVertex<?>, VERTEX_TO e
                     private Forward(ProcedureVertex.Thing from, ProcedureVertex.Thing to, int order) {
                         super(from, to, order, FORWARD);
                     }
+
+                    @Override
+                    ResourceIterator<ProcedureVertex.Thing> execute(ProcedureVertex.Thing thing, Traversal.Parameters parameters) {
+                        return iterate(emptyIterator()); // TODO
+                    }
                 }
 
                 static class Backward extends Has {
 
                     private Backward(ProcedureVertex.Thing from, ProcedureVertex.Thing to, int order) {
                         super(from, to, order, BACKWARD);
+                    }
+
+                    @Override
+                    ResourceIterator<ProcedureVertex.Thing> execute(ProcedureVertex.Thing thing, Traversal.Parameters parameters) {
+                        return iterate(emptyIterator()); // TODO
                     }
                 }
             }
@@ -302,12 +387,22 @@ abstract class ProcedureEdge<VERTEX_FROM extends ProcedureVertex<?>, VERTEX_TO e
                     private Forward(ProcedureVertex.Thing from, ProcedureVertex.Thing to, int order) {
                         super(from, to, order, FORWARD);
                     }
+
+                    @Override
+                    ResourceIterator<ProcedureVertex.Thing> execute(ProcedureVertex.Thing thing, Traversal.Parameters parameters) {
+                        return iterate(emptyIterator()); // TODO
+                    }
                 }
 
                 static class Backward extends Playing {
 
                     private Backward(ProcedureVertex.Thing from, ProcedureVertex.Thing to, int order) {
                         super(from, to, order, BACKWARD);
+                    }
+
+                    @Override
+                    ResourceIterator<ProcedureVertex.Thing> execute(ProcedureVertex.Thing thing, Traversal.Parameters parameters) {
+                        return iterate(emptyIterator()); // TODO
                     }
                 }
             }
@@ -323,12 +418,22 @@ abstract class ProcedureEdge<VERTEX_FROM extends ProcedureVertex<?>, VERTEX_TO e
                     private Forward(ProcedureVertex.Thing from, ProcedureVertex.Thing to, int order) {
                         super(from, to, order, FORWARD);
                     }
+
+                    @Override
+                    ResourceIterator<ProcedureVertex.Thing> execute(ProcedureVertex.Thing thing, Traversal.Parameters parameters) {
+                        return iterate(emptyIterator()); // TODO
+                    }
                 }
 
                 static class Backward extends Relating {
 
                     private Backward(ProcedureVertex.Thing from, ProcedureVertex.Thing to, int order) {
                         super(from, to, order, BACKWARD);
+                    }
+
+                    @Override
+                    ResourceIterator<ProcedureVertex.Thing> execute(ProcedureVertex.Thing thing, Traversal.Parameters parameters) {
+                        return iterate(emptyIterator()); // TODO
                     }
                 }
             }
@@ -347,12 +452,22 @@ abstract class ProcedureEdge<VERTEX_FROM extends ProcedureVertex<?>, VERTEX_TO e
                     private Forward(ProcedureVertex.Thing from, ProcedureVertex.Thing to, int order, Set<Label> roleTypes) {
                         super(from, to, order, FORWARD, roleTypes);
                     }
+
+                    @Override
+                    ResourceIterator<ProcedureVertex.Thing> execute(ProcedureVertex.Thing thing, Traversal.Parameters parameters) {
+                        return iterate(emptyIterator()); // TODO
+                    }
                 }
 
                 static class Backward extends RolePlayer {
 
                     private Backward(ProcedureVertex.Thing from, ProcedureVertex.Thing to, int order, Set<Label> roleTypes) {
                         super(from, to, order, BACKWARD, roleTypes);
+                    }
+
+                    @Override
+                    ResourceIterator<ProcedureVertex.Thing> execute(ProcedureVertex.Thing thing, Traversal.Parameters parameters) {
+                        return iterate(emptyIterator()); // TODO
                     }
                 }
             }
