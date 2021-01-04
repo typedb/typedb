8b12d70fa217237479a9fc9cedc2d248b547c03e
Ganeshwara Hananda <ganeshwara@grakn.ai>
2021-01-04 19:18:26 +0000

    Implement match aggregate, match group, match group aggregate

--- server/rpc/util/ResponseBuilder.java
+++ server/rpc/util/ResponseBuilder.java
@@ -22,6 +22,7 @@ import grakn.core.common.exception.ErrorMessage;
 import grakn.core.common.exception.GraknException;
 import grakn.core.concept.answer.AnswerGroup;
 import grakn.core.concept.answer.ConceptMap;
+import grakn.core.concept.answer.Numeric;
 import grakn.core.concept.thing.Attribute;
 import grakn.core.concept.thing.Entity;
 import grakn.core.concept.thing.Relation;
@@ -36,6 +37,7 @@ import grakn.core.logic.Rule;
 import grakn.protocol.AnswerProto;
 import grakn.protocol.ConceptProto;
 import grakn.protocol.LogicProto;
+import grakn.protocol.QueryProto;
 import grakn.protocol.TransactionProto;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
@@ -47,6 +49,7 @@ import static grakn.common.util.Objects.className;
 import static grakn.core.common.exception.ErrorMessage.Internal.ILLEGAL_STATE;
 import static grakn.core.common.exception.ErrorMessage.Server.BAD_VALUE_TYPE;
 import static grakn.core.common.exception.ErrorMessage.Server.UNKNOWN_ANSWER_TYPE;
+import static java.util.stream.Collectors.toList;
 
 public class ResponseBuilder {
 
@@ -262,29 +265,29 @@ public class ResponseBuilder {
      */
     public static class Answer {
 
-        public static AnswerProto.Answer answer(Object object) {
-            final AnswerProto.Answer.Builder answer = AnswerProto.Answer.newBuilder();
-
-            if (object instanceof AnswerGroup) {
-                answer.setAnswerGroup(answerGroup((AnswerGroup<?>) object));
-            } else if (object instanceof ConceptMap) {
-                answer.setConceptMap(conceptMap((ConceptMap) object));
-            } else if (object instanceof Number) {
-                answer.setNumber(number((Number) object));
-            } else {
-                throw GraknException.of(UNKNOWN_ANSWER_TYPE, className(object.getClass()));
-            }
-
-            return answer.build();
-        }
-
-        public static AnswerProto.AnswerGroup answerGroup(AnswerGroup<?> answer) {
-            final AnswerProto.AnswerGroup.Builder answerGroupProto = AnswerProto.AnswerGroup.newBuilder()
-                    .setOwner(ResponseBuilder.Concept.concept(answer.owner()))
-                    .addAllAnswers(answer.answers().stream().map(Answer::answer).collect(Collectors.toList()));
-
-            return answerGroupProto.build();
-        }
+//        public static AnswerProto.Answer answer(Object object) {
+//            final AnswerProto.Answer.Builder answer = AnswerProto.Answer.newBuilder();
+//
+//            if (object instanceof AnswerGroup) {
+//                answer.setAnswerGroup(answerGroup((AnswerGroup<?>) object));
+//            } else if (object instanceof ConceptMap) {
+//                answer.setConceptMap(conceptMap((ConceptMap) object));
+//            } else if (object instanceof Number) {
+//                answer.setNumber(number((Number) object));
+//            } else {
+//                throw GraknException.of(UNKNOWN_ANSWER_TYPE, className(object.getClass()));
+//            }
+//
+//            return answer.build();
+//        }
+//
+//        public static AnswerProto.AnswerGroup answerGroup(AnswerGroup<?> answer) {
+//            final AnswerProto.AnswerGroup.Builder answerGroupProto = AnswerProto.AnswerGroup.newBuilder()
+//                    .setOwner(ResponseBuilder.Concept.concept(answer.owner()))
+//                    .addAllAnswers(answer.answers().stream().map(Answer::answer).collect(Collectors.toList()));
+//
+//            return answerGroupProto.build();
+//        }
 
         public static AnswerProto.ConceptMap conceptMap(ConceptMap answer) {
             final AnswerProto.ConceptMap.Builder conceptMapProto = AnswerProto.ConceptMap.newBuilder();
@@ -307,8 +310,21 @@ public class ResponseBuilder {
             return conceptMapProto.build();
         }
 
+        public static AnswerProto.ConceptMapGroup conceptMapGroup(AnswerGroup<ConceptMap> answer) {
+            return AnswerProto.ConceptMapGroup.newBuilder()
+                    .setOwner(ResponseBuilder.Concept.concept(answer.owner()))
+                    .addAllConceptMaps(answer.answers().stream().map(ResponseBuilder.Answer::conceptMap).collect(toList()))
+                    .build();
+        }
+
         public static AnswerProto.Number number(Number number) {
             return AnswerProto.Number.newBuilder().setValue(number.toString()).build();
+        }
+
+        public static AnswerProto.NumberGroup numberGroup(AnswerGroup<Numeric> a) {
+            return AnswerProto.NumberGroup.newBuilder().setOwner(ResponseBuilder.Concept.concept(a.owner()))
+                    .addAllNumbers(a.answers().stream().map(n -> ResponseBuilder.Answer.number(n.number())).collect(toList()))
+                    .build();
         }
     }
 }
