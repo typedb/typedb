08e518551be84b255ef1465d3bff82e770207ded
James Fletcher <james@grakn.ai>
2021-01-21 12:23:27 +0000

    Throw IllegalState.

--- reasoner/resolution/ResolverManager.java
+++ reasoner/resolution/ResolverManager.java
@@ -243,7 +243,7 @@ public class ResolverManager {
                     add(concludable.get());
                     continue;
                 }
-                assert false;
+                throw GraknException.of(ILLEGAL_STATE);
             }
         }
 
