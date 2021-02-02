bbcf14300c1118c20bc6dc54ef20fd49431a0e7a
Rui Liu <33423526+lriuui0x0@users.noreply.github.com>
2021-02-02 16:45:40 +0000

    Verified transaction is open before accessing RocksDB (#6125)
    ## What is the goal of this PR?
    
    Protect access to RocksDB operations when the transaction is closed, which causes the server to crash.
    
    ## What are the changes implemented in this PR?
    
    - Convert assertions into if throws exception checks, as assertions are not running in production mode.
    - This is only a partial solution, as they're still race conditions that cause this kind of access-after-close to happen.

--- rocks/RocksStorage.java
+++ rocks/RocksStorage.java
@@ -46,6 +46,7 @@ import java.util.function.BiFunction;
 import static grakn.core.common.collection.Bytes.bytesHavePrefix;
 import static grakn.core.common.exception.ErrorMessage.Internal.ILLEGAL_OPERATION;
 import static grakn.core.common.exception.ErrorMessage.Internal.ILLEGAL_STATE;
+import static grakn.core.common.exception.ErrorMessage.Transaction.TRANSACTION_CLOSED;
 import static grakn.core.common.exception.ErrorMessage.Transaction.TRANSACTION_DATA_READ_VIOLATION;
 import static grakn.core.common.exception.ErrorMessage.Transaction.TRANSACTION_SCHEMA_READ_VIOLATION;
 
@@ -200,7 +201,7 @@ public abstract class RocksStorage implements Storage {
 
         @Override
         public byte[] get(byte[] key) {
-            assert isOpen();
+            if (!isOpen()) throw GraknException.of(TRANSACTION_CLOSED);
             try {
                 if (!isReadOnly) readWriteLock.readLock().lock();
                 return storageTransaction.get(readOptions, key);
@@ -227,7 +228,7 @@ public abstract class RocksStorage implements Storage {
 
         @Override
         public void delete(byte[] key) {
-            assert isOpen() && transaction.isOpen();
+            if (!isOpen() || !transaction.isOpen()) throw GraknException.of(TRANSACTION_CLOSED);
             if (isReadOnly) {
                 if (transaction.isSchema()) throw exception(TRANSACTION_SCHEMA_READ_VIOLATION);
                 else if (transaction.isData()) throw exception(TRANSACTION_DATA_READ_VIOLATION);
@@ -245,7 +246,7 @@ public abstract class RocksStorage implements Storage {
 
         @Override
         public <G> ResourceIterator<G> iterate(byte[] key, BiFunction<byte[], byte[], G> constructor) {
-            assert isOpen();
+            if (!isOpen()) throw GraknException.of(TRANSACTION_CLOSED);
             RocksIterator<G> iterator = new RocksIterator<>(this, key, constructor);
             iterators.add(iterator);
             return iterator;
