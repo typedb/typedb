eef6de2c993bd482273a8d54fd4f307fbcc970d6
joshua <joshuasend@gmail.com>
2021-02-18 12:59:34 +0000

    synchronize all methods in RocksIterator

--- rocks/RocksIterator.java
+++ rocks/RocksIterator.java
@@ -21,7 +21,6 @@ package grakn.core.rocks;
 import grakn.core.common.iterator.AbstractResourceIterator;
 
 import java.util.NoSuchElementException;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.BiFunction;
 
 import static grakn.core.common.collection.Bytes.bytesHavePrefix;
@@ -30,7 +29,6 @@ public final class RocksIterator<T> extends AbstractResourceIterator<T> implemen
 
     private final byte[] prefix;
     private final RocksStorage storage;
-    private final AtomicBoolean isOpen;
     private final BiFunction<byte[], byte[], T> constructor;
     private org.rocksdb.RocksIterator internalRocksIterator;
     private State state;
@@ -42,43 +40,42 @@ public final class RocksIterator<T> extends AbstractResourceIterator<T> implemen
         this.storage = storage;
         this.prefix = prefix;
         this.constructor = constructor;
-
-        isOpen = new AtomicBoolean(true);
         state = State.INIT;
     }
 
-    private void initalise() {
-        this.internalRocksIterator = storage.getInternalRocksIterator();
-        this.internalRocksIterator.seek(prefix);
-    }
-
-    private boolean fetchAndCheck() {
-        byte[] key;
-        if (!internalRocksIterator.isValid() || !bytesHavePrefix(key = internalRocksIterator.key(), prefix)) {
-            state = State.COMPLETED;
-            recycle();
-            return false;
-        }
-
-        next = constructor.apply(key, internalRocksIterator.value());
-        synchronized (this) {
-            if (state != State.COMPLETED) {
-                internalRocksIterator.next();
-                state = State.FETCHED;
-                return true;
-            } else{
+    private synchronized boolean initialise() {
+        if (state != State.COMPLETED) {
+            this.internalRocksIterator = storage.getInternalRocksIterator();
+            this.internalRocksIterator.seek(prefix);
+            if (!internalRocksIterator.isValid()) System.out.println("INVALID ITERATOR");
+            state = State.EMPTY;
+            byte[] key;
+            if (!internalRocksIterator.isValid() || !bytesHavePrefix(key = internalRocksIterator.key(), prefix)) {
+                recycle();
                 return false;
             }
+            next = constructor.apply(key, internalRocksIterator.value());
+            state = State.FETCHED;
+            return true;
+        } else {
+            return false;
         }
     }
 
-    public final T peek() {
+    public synchronized final T peek() {
         if (!hasNext()) throw new NoSuchElementException();
         return next;
     }
 
     @Override
-    public final boolean hasNext() {
+    public synchronized final T next() {
+        if (!hasNext()) throw new NoSuchElementException();
+        state = State.EMPTY;
+        return next;
+    }
+
+    @Override
+    public synchronized final boolean hasNext() {
         switch (state) {
             case COMPLETED:
                 return false;
@@ -87,18 +84,26 @@ public final class RocksIterator<T> extends AbstractResourceIterator<T> implemen
             case EMPTY:
                 return fetchAndCheck();
             case INIT:
-                initalise();
-                return fetchAndCheck();
+                return initialise();
             default: // This should never be reached
                 return false;
         }
     }
 
-    @Override
-    public final T next() {
-        if (!hasNext()) throw new NoSuchElementException();
-        state = State.EMPTY;
-        return next;
+    private synchronized boolean fetchAndCheck() {
+        if (state != State.COMPLETED) {
+            internalRocksIterator.next();
+            byte[] key;
+            if (!internalRocksIterator.isValid() || !bytesHavePrefix(key = internalRocksIterator.key(), prefix)) {
+                recycle();
+                return false;
+            }
+            next = constructor.apply(key, internalRocksIterator.value());
+            state = State.FETCHED;
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
@@ -107,13 +112,11 @@ public final class RocksIterator<T> extends AbstractResourceIterator<T> implemen
     }
 
     @Override
-    public void close() {
-        if (isOpen.compareAndSet(true, false)) {
-            synchronized (this) {
-                if (state != State.INIT) storage.recycle(internalRocksIterator);
-                state = State.COMPLETED;
-                storage.remove(this);
-            }
+    public synchronized void close() {
+        if (state != State.COMPLETED) {
+            if (state != State.INIT) storage.recycle(internalRocksIterator);
+            state = State.COMPLETED;
+            storage.remove(this);
         }
     }
 }
