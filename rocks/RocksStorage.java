dc3f174808708ace3abec7078fa4a28c3ce96f86
Alex Walker <alex@grakn.ai>
2021-02-23 16:41:06 +0000

    Comment on comment

--- rocks/RocksStorage.java
+++ rocks/RocksStorage.java
@@ -181,7 +181,7 @@ public abstract class RocksStorage implements Storage {
 
         @Override
         public <G> ResourceIterator<G> iterate(byte[] key, BiFunction<byte[], byte[], G> constructor) {
-//            assert isOpen();
+//            assert isOpen(); // TODO: verify why this was an assertion rather than an exception
             if (!isOpen()) throw GraknException.of(TRANSACTION_CLOSED);
             RocksIterator<G> iterator = new RocksIterator<>(this, key, constructor);
             iterators.add(iterator);
