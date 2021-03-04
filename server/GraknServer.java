14fa782c3e33b705b4d85be12ee783e1b4287f89
haikalpribadi <haikal@grakn.ai>
2021-03-04 16:11:26 +0000

    Rename 'main' thread pool to 'service' and reduce the size

--- server/GraknServer.java
+++ server/GraknServer.java
@@ -104,7 +104,7 @@ public class GraknServer implements AutoCloseable {
     private Server rpcServer() {
         assert Executors.isInitialised();
         return NettyServerBuilder.forPort(command.port())
-                .executor(Executors.main())
+                .executor(Executors.service())
                 .workerEventLoopGroup(Executors.network())
                 .bossEventLoopGroup(Executors.network())
                 .maxConnectionIdle(1, TimeUnit.HOURS) // TODO: why 1 hour?
