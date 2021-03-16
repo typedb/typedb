3d04a2b761be80fc111ff285bc2fac66bd4951a6
Ganeshwara Hananda <ganeshwara@grakn.ai>
2021-03-16 20:16:36 +0000

    Fix checkstyle errors

--- server/common/Util.java
+++ server/common/Util.java
@@ -1,7 +1,24 @@
+/*
+ * Copyright (C) 2021 Grakn Labs
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 package grakn.core.server.common;
 
 import grakn.core.common.exception.GraknException;
-import grakn.core.server.GraknServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import picocli.CommandLine;
