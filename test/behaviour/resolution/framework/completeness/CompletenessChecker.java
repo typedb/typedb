2426d1d209fc21a68788e9ea5018504de16fadf9
James Fletcher <james@grakn.ai>
2021-06-24 11:11:31 +0100

    Validate the bounds used to execute the Conclusion with the real reasoner. Refactoring.

--- test/behaviour/resolution/framework/completeness/CompletenessChecker.java
+++ test/behaviour/resolution/framework/completeness/CompletenessChecker.java
@@ -5,11 +5,9 @@ import com.vaticle.typedb.core.common.exception.TypeDBException;
 import com.vaticle.typedb.core.common.parameters.Arguments;
 import com.vaticle.typedb.core.common.parameters.Context;
 import com.vaticle.typedb.core.common.parameters.Options;
-import com.vaticle.typedb.core.concept.Concept;
 import com.vaticle.typedb.core.concept.answer.ConceptMap;
 import com.vaticle.typedb.core.logic.Rule;
 import com.vaticle.typedb.core.logic.resolvable.Concludable;
-import com.vaticle.typedb.core.logic.resolvable.Unifier;
 import com.vaticle.typedb.core.logic.resolvable.Unifier.Requirements.Instance;
 import com.vaticle.typedb.core.pattern.Conjunction;
 import com.vaticle.typedb.core.pattern.Disjunction;
@@ -17,10 +15,11 @@ import com.vaticle.typedb.core.rocks.RocksSession;
 import com.vaticle.typedb.core.rocks.RocksTransaction;
 import com.vaticle.typedb.core.test.behaviour.resolution.framework.common.Exceptions;
 import com.vaticle.typedb.core.test.behaviour.resolution.framework.reference.Reasoner;
+import com.vaticle.typedb.core.traversal.common.Identifier;
 import com.vaticle.typeql.lang.query.TypeQLMatch;
 
 import java.util.Collections;
-import java.util.Map;
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
@@ -55,73 +54,48 @@ public class CompletenessChecker {
                     new Context.Query(referenceTx.context(), new Options.Query()),
                     filterRetrievableVars(conjunction.identifiers())
             ).forEachRemaining(answer -> {
-                checkAnswer(answer, conjunction);
+                checkConjunctionConcludables(answer, conjunction);
             });
         });
     }
 
-    private void checkAnswer(ConceptMap answer, Conjunction conjunction) {
+    private void checkConjunctionConcludables(ConceptMap answer, Conjunction conjunction) {
         Concludable.create(conjunction).forEach(concludable -> {
-            Map<Rule, Set<Unifier>> applicableRules = concludable.applicableRules(referenceTx.concepts(),
-                                                                                  referenceTx.logic());
-            applicableRules.forEach(((rule, unifiers) -> unifiers.forEach(unifier -> {
-                Optional<Pair<ConceptMap, Instance>> unified = unifier.unify(answer.filter(concludable.retrieves()));
-                if (unified.isPresent()) {
-                    ConceptMap conclusionAnswer = unified.get().first();
-                    ConceptMap conditionAnswer = referenceReasoner.ruleRecorderMap().get(rule.getLabel())
-                            .inferencesByConclusion().get(conclusionAnswer);
-                    // Make sure that the unifier is valid for the particular answer we have
-                    if (conditionAnswer != null) {
-                        checkAnswer(conditionAnswer, rule.when());
-                        checkConclusion(rule.conclusion(), conclusionAnswer);
-                    }
-                }
-            })));
+            concludable
+                    .applicableRules(referenceTx.concepts(), referenceTx.logic())
+                    .forEach(((rule, unifiers) -> unifiers.forEach(unifier -> {
+                        Optional<Pair<ConceptMap, Instance>> unified =
+                                unifier.unify(answer.filter(concludable.retrieves()));
+                        if (unified.isPresent()) {
+                            ConceptMap conclusionAnswer = unified.get().first();
+                            ConceptMap conditionAnswer = referenceReasoner.ruleRecorderMap().get(rule.getLabel())
+                                    .inferencesByConclusion().get(conclusionAnswer);
+                            // Make sure that the unifier is valid for the particular answer we have
+                            if (conditionAnswer != null) {
+                                checkConjunctionConcludables(conditionAnswer, rule.when());
+                                checkConclusion(rule.conclusion(), conclusionAnswer);
+                            }
+                        }
+                    })));
         });
-    }
-
-    private static boolean containsInferredConcept(ConceptMap answer) {
-        for (Concept concept : answer.concepts().values()) {
-            if (concept.isThing() && concept.asThing().isInferred()) return true;
-        }
-        return false;
     }
 
     private void checkConclusion(Rule.Conclusion conclusion, ConceptMap conclusionAnswer) {
-        // TODO: Do we need to assert somewhere that there should only be one rule used to find a particular concludable answer across all of its explanations?
+        // TODO: Do we need to assert somewhere that there should only be one rule used to find a particular
+        //  concludable answer across all of its explanations?
+        ConceptMap conclusionBounds = removeGeneratingBound(conclusion, conclusionAnswer);
+        validateFullyBound(conclusion, conclusionBounds);
+        Conjunction conclusionWithIIDs = constrainByIIDs(conclusion, conclusionAnswer);
+        validateInference(conclusion, conclusionWithIIDs);
+    }
 
-
-
-        // TODO: Realised this method should deal with a Conclusion not a Concludable. The unification is already done so we can work with the rule conclusion with its known structure.
-        if (conclusion.isRelation()) {
-            // TODO if bounds leave more than one variable unbound then this is unsupported
-            iterate(conclusionAnswer.concepts()).filter((id, c) -> conclusion.generating().isPresent() && conclusion.generating().get().id().equals(id))
-            conclusion.asRelation().relation().players()
-        } else if (conclusion.isHas()) {
-        } else if (conclusion.isVariableHas()) {
-        } else if (conclusion.isExplicitHas()) {
-        } else if (conclusion.isValue()) {
-        } else if (conclusion.isIsa()) {
-
-        }
-        conclusion.
-
-        Conjunction constrainedByIIDs = conclusion.pattern().clone();
-        conclusionAnswer.concepts().forEach((identifier, concept) -> {
-            if (concept.isThing()) {
-                assert constrainedByIIDs.variable(identifier).isThing();
-                // Make sure this isn't the generating variable, we want to leave that unconstrained.
-                if (!(conclusion.generating().isPresent() && conclusion.generating().get().id().equals(identifier))) {
-                    constrainedByIIDs.variable(identifier).asThing().iid(concept.asThing().getIID());
-                }
-            }
-        });
-
-        Disjunction concludableQuery = new Disjunction(Collections.singletonList(constrainedByIIDs));
+    private void validateInference(Rule.Conclusion conclusion, Conjunction conclusionWithIIDs) {
+        // Validate that this conclusion can be reached using the production reasoner
+        Disjunction concludableQuery = new Disjunction(Collections.singletonList(conclusionWithIIDs));
         try (RocksTransaction tx = session.transaction(Arguments.Transaction.Type.READ,
                                                        new Options.Transaction().infer(true))) {
             int numAnswers = tx.reasoner().executeReasoner(concludableQuery,
-                                                           conclusion.retrieves(),
+                                                           conclusion.retrievableIds(),
                                                            new Context.Query(tx.context(), new Options.Query())).toList().size();
             if (numAnswers == 0) {
                 throw new Exceptions.CompletenessException("Expected an answer which is not present.");
@@ -129,6 +103,35 @@ public class CompletenessChecker {
                 throw TypeDBException.of(ILLEGAL_STATE);
             }
         }
+    }
+
+    private static Conjunction constrainByIIDs(Rule.Conclusion conclusion, ConceptMap conclusionBounds) {
+        Conjunction constrainedByIIDs = conclusion.conjunction().clone();
+        conclusionBounds.concepts().forEach((identifier, concept) -> {
+            if (concept.isThing()) {
+                assert constrainedByIIDs.variable(identifier).isThing();
+                // Make sure this isn't the generating variable, we want to leave that unconstrained.
+                constrainedByIIDs.variable(identifier).asThing().iid(concept.asThing().getIID());
+            }
+        });
+        return constrainedByIIDs;
+    }
+
+    private static ConceptMap removeGeneratingBound(Rule.Conclusion conclusion, ConceptMap conclusionBounds) {
+        // Remove the bound for the variable that the conclusion may generate
+        Set<Identifier.Variable.Retrievable> nonGenerating = new HashSet<>(conclusion.retrievableIds());
+        if (conclusion.generating().isPresent()) nonGenerating.remove(conclusion.generating().get().id());
+        return conclusionBounds.filter(nonGenerating);
+    }
+
+    private static void validateFullyBound(Rule.Conclusion conclusion, ConceptMap conclusionAnswer) {
+        // If any variable is unbound then throw
+        iterate(conclusion.retrievableIds()).forEachRemaining(v -> {
+            if (conclusionAnswer.concepts().get(v) == null) {
+                throw new UnsupportedOperationException("Completion testing does not yet support non-generated " +
+                                                                "inferred concepts in rule conclusions.");
+            }
+        });
     }
 
 }
