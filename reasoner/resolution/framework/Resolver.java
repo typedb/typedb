5ef8389bc1f0a09debffb9b5e08833e5cede65f0
James Fletcher <james@grakn.ai>
2021-08-26 17:34:42 +0100

    Missing import

--- reasoner/resolution/framework/Resolver.java
+++ reasoner/resolution/framework/Resolver.java
@@ -39,6 +39,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
