typedb/typedb

commit 3e0696257972ad76d61031a8e09378efc8b65fd5

Rearchitect Server parameters implementation (#6512)
## What is the goal of this PR?

The architecture of the **server parameters** implementation maps to the conceptual model more closely.  This is achieved by first identifying the main types of parameters (namely **CLI subcommands**, **CLI options**, and **TypeDB config**) and dividing the classes according to them.

Additionally, for each aspect above, the domain-agnostic values and parsers are separated from the domain-specific ones. This puts the focus on the "what" (ie., what parameters are we declaring), rather than the "how" (ie., how exactly are we declaring a particular parameter).

## What are the changes implemented in this PR?

**Before**:

The conceptual model of "server parameters" were not rigorously identified:

- The classes for handling server parameters were put in the `//server/common` package, mixed with other classes.
- The responsibility of the classes were not clear:
  - The names were inconsistent. For example, the subcommand values and parsers are represented by a single class named `Command`.
  - Classes that weren't supposed to depend on one another often do. For example, the Yaml parser class depends on the CLI subcommand/option classes. This is not good since conceptually Yaml parser shouldn't know anything about the CLI aspects.

**After**:

We identified the conceptual model rigorously, and redesign the classes to map to them:

1. Move parameters classes into `//server/parameters`.
2. Split domain-specific and domain-agnostic values and parsers:
    - Domain-specific:
        - `server` subcommand, (for running TypeDB Server). it supports the following options
            - auxillary options such as `--help`, `--version`, or `--debug`
            - config overrides options such as `--server.addres=localhost:5000`
        - `server import` subcommand (for importing data)
        - `server export` subcommand (for exporting data)
        - TypeDB config

server/BUILD
@@ -26,6 +26,12 @@ load("@stackb_rules_proto//java:java_grpc_compile.bzl", "java_grpc_compile")
 
 package(default_visibility = ["//visibility:private",])
 
+filegroup(
+    name = "config",
+    srcs = ["parameters/config.yml"],
+    visibility = ["//visibility:public"],
+)
+
 genrule(
     name = "version-src",
     srcs = [
@@ -93,7 +99,7 @@ java_binary(
     main_class = "com.vaticle.typedb.core.server.TypeDBServer",
     runtime_deps = [":server-mac"],
     tags = ["maven_coordinates=com.vaticle.typedb:typedb-core-server-bin:{pom_version}"],
-    data = ["//server/conf:typedb"],
+    data = ["//server:config"],
 )
 
 java_binary( # TODO: this target should not be needed if :server-deps-mac can depend directly on :server-mac (lib)
@@ -108,7 +114,7 @@ java_binary(
     main_class = "com.vaticle.typedb.core.server.TypeDBServer",
     runtime_deps = [":server-linux"],
     tags = ["maven_coordinates=com.vaticle.typedb:typedb-core-server-bin:{pom_version}"],
-    data = ["//server/conf:typedb"],
+    data = ["//server:config"],
 )
 
 java_binary( # TODO: this target should not be needed if :server-deps-linux can depend directly on :server-linux (lib)
@@ -123,7 +129,7 @@ java_binary(
     main_class = "com.vaticle.typedb.core.server.TypeDBServer",
     runtime_deps = [":server-windows"],
     tags = ["maven_coordinates=com.vaticle.typedb:typedb-core-server-bin:{pom_version}"],
-    data = ["//server/conf:typedb"],
+    data = ["//server:config"],
 )
 
 java_binary( # TODO: this target should not be needed if :server-deps-windows can depend directly on :server-windows (lib)
@@ -179,19 +185,25 @@ assemble_deps_common = [
 ]
 
 assemble_files = {
-    "//server/conf:typedb": "server/conf/typedb.yml",
+    "//server:config": "server/conf/config.yml",
     "//server/resources:logo": "server/resources/typedb-ascii.txt",
     "//:LICENSE": "LICENSE",
 }
 
+empty_directories = [
+    "server/data"
+]
+
 permissions = {
-    "server/conf/typedb.yml" : "0755",
+    "server/conf/config.yml" : "0755",
+    "server/data" : "0755",
 }
 
 assemble_targz(
     name = "assemble-linux-targz",
     targets = ["server-deps-linux", "@vaticle_typedb_common//binary:assemble-bash-targz"],
     additional_files = assemble_files,
+    empty_directories = empty_directories,
     permissions = permissions,
     output_filename = "typedb-server-linux",
     visibility = ["//:__pkg__", "//test:__subpackages__"]
@@ -201,6 +213,7 @@ assemble_zip(
     name = "assemble-mac-zip",
     targets = assemble_deps_common + ["server-deps-mac", "@vaticle_typedb_common//binary:assemble-bash-targz"],
     additional_files = assemble_files,
+    empty_directories = empty_directories,
     permissions = permissions,
     output_filename = "typedb-server-mac",
     visibility = ["//:__pkg__", "//test:__subpackages__"]
@@ -210,6 +223,7 @@ assemble_zip(
     name = "assemble-windows-zip",
     targets = ["server-deps-windows", "@vaticle_typedb_common//binary:assemble-bat-targz"],
     additional_files = assemble_files,
+    empty_directories = empty_directories,
     permissions = permissions,
     output_filename = "typedb-server-windows",
     visibility = ["//:__pkg__", "//test:__subpackages__"]

server/parameters/ServerSubcommandParser.java
@@ -1,0 +1,161 @@
+/*
+ * Copyright (C) 2021 Vaticle
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.vaticle.typedb.core.server.parameters;
+
+import com.vaticle.typedb.core.common.exception.TypeDBException;
+import com.vaticle.typedb.core.server.parameters.util.Help;
+import com.vaticle.typedb.core.server.parameters.util.SubcommandParser;
+import com.vaticle.typedb.core.server.parameters.util.Option;
+import com.vaticle.typedb.core.server.parameters.util.OptionParser;
+
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static com.vaticle.typedb.common.collection.Collections.list;
+import static com.vaticle.typedb.common.collection.Collections.set;
+import static com.vaticle.typedb.core.common.exception.ErrorMessage.Server.CLI_OPTION_REQUIRED;
+import static com.vaticle.typedb.core.common.exception.ErrorMessage.Server.CLI_OPTION_UNRECOGNISED;
+import static com.vaticle.typedb.core.common.iterator.Iterators.iterate;
+
+public class ServerSubcommandParser {
+
+    private static void validateRequiredOptions(Set<OptionParser> requiredParsers, Set<Option> options) {
+        requiredParsers.forEach(parser -> {
+            if (iterate(options).noneMatch(option -> option.name().equals(parser.name()))) {
+                throw TypeDBException.of(CLI_OPTION_REQUIRED, parser.help());
+            }
+        });
+    }
+
+    private static void validateUnrecognisedOptions(Set<OptionParser> recognisedParsers, Set<Option> options) {
+        recognisedParsers.forEach(parser -> {
+            if (iterate(options).noneMatch(option -> option.name().equals(parser.name()))) {
+                throw TypeDBException.of(CLI_OPTION_UNRECOGNISED, parser);
+            }
+        });
+    }
+
+    public static class Server extends SubcommandParser<ServerSubcommand.Server> {
+
+        private static final OptionParser.Flag debug = new OptionParser.Flag("debug", "Run server in debug mode.");
+        private static final OptionParser.Flag help = new OptionParser.Flag("help", "Print help menu.");
+        private static final OptionParser.Flag version = new OptionParser.Flag("version", "Print version number.");
+        private static final OptionParser.Path configPath =
+                new OptionParser.Path("config", "Path to TypeDB YAML configuration file.");
+        private static final Set<OptionParser> auxiliaryParsers = set(debug, help, version, configPath);
+        private static final String[] tokens = new String[]{};
+        private static final String description = "Run TypeDB server";
+
+        private final ConfigParser configParser;
+
+        public Server(ConfigParser configParser) {
+            super(tokens, description);
+            this.configParser = configParser;
+        }
+
+        @Override
+        protected ServerSubcommand.Server parse(Set<Option> options) {
+            Set<Option> auxOptions = findAuxiliaryOptions(options);
+            Set<Option> configOptions = excludeOptions(options, auxOptions);
+            Optional<Path> configPath = Server.configPath.parse(auxOptions);
+            Config config = configPath
+                    .map(path -> ConfigFactory.config(path, configOptions, configParser))
+                    .orElse(ConfigFactory.config(configOptions, configParser));
+            return new ServerSubcommand.Server(debug.parse(auxOptions), help.parse(auxOptions),
+                    version.parse(auxOptions), config);
+        }
+
+        private Set<Option> findAuxiliaryOptions(Set<Option> options) {
+            return iterate(options)
+                    .filter(opt -> iterate(auxiliaryParsers).anyMatch(auxParser -> auxParser.name.equals(opt.name())))
+                    .toSet();
+        }
+
+        private Set<Option> excludeOptions(Set<Option> options, Set<Option> exclude) {
+            return iterate(options).filter(opt -> !exclude.contains(opt)).toSet();
+        }
+
+        @Override
+        public List<Help> helpList() {
+            List<Help> aux = list(help.help(), version.help(), debug.help(), configPath.help());
+            return list(aux, configParser.helpList(""));
+        }
+    }
+
+    public static class Import extends SubcommandParser<ServerSubcommand.Import> {
+
+        public static final String[] tokens = new String[]{"import"};
+        public static final String description = "Run TypeDB import.";
+
+        private static final OptionParser.String database =
+                new OptionParser.String("database", "Database to import into.");
+        private static final OptionParser.Path filePath =
+                new OptionParser.Path("file", "Path to data file to import (.typedb format).");
+        private static final OptionParser.Int port = new OptionParser.Int("port", "TypeDB's GRPC port.");
+        private static final Set<OptionParser> parsers = set(database, filePath, port);
+
+        public Import() {
+            super(tokens, description);
+        }
+
+        @Override
+        protected ServerSubcommand.Import parse(Set<Option> options) {
+            validateRequiredOptions(parsers, options);
+            validateUnrecognisedOptions(parsers, options);
+            return new ServerSubcommand.Import(database.parse(options).get(), filePath.parse(options).get(),
+                    port.parse(options).get());
+        }
+
+        @Override
+        public List<Help> helpList() {
+            return list(database.help(), filePath.help(), port.help());
+        }
+    }
+
+    public static class Export extends SubcommandParser<ServerSubcommand.Export> {
+
+        public static final String[] tokens = new String[]{"export"};
+        public static final String description = "Run TypeDB export.";
+
+        private static final OptionParser.String database =
+                new OptionParser.String("database", "Database to export.");
+        private static final OptionParser.Path filePath =
+                new OptionParser.Path("file", "Path to data file to export to.");
+        private static final OptionParser.Int port = new OptionParser.Int("port", "TypeDB's GRPC port.");
+        private static final Set<OptionParser> parsers = set(database, filePath, port);
+
+        public Export() {
+            super(tokens, description);
+        }
+
+        @Override
+        protected ServerSubcommand.Export parse(Set<Option> options) {
+            validateRequiredOptions(parsers, options);
+            validateUnrecognisedOptions(parsers, options);
+            return new ServerSubcommand.Export(database.parse(options).get(), filePath.parse(options).get(),
+                    port.parse(options).get());
+        }
+
+        @Override
+        public List<Help> helpList() {
+            return list(database.help(), filePath.help(), port.help());
+        }
+    }
+}

BUILD
@@ -53,13 +53,18 @@ native_java_libraries(
 )
 
 assemble_files = {
-    "//server/conf:typedb": "server/conf/typedb.yml",
+    "//server:config": "server/conf/config.yml",
     "//server/resources:logo": "server/resources/typedb-ascii.txt",
     "//:LICENSE": "LICENSE",
 }
 
+empty_directories = [
+    "server/data"
+]
+
 permissions = {
-    "server/conf/typedb.yml" : "0755",
+    "server/conf/config.yml" : "0755",
+    "server/data" : "0755",
 }
 
 artifact_repackage(
@@ -73,6 +78,7 @@ assemble_targz(
     name = "assemble-linux-targz",
     targets = ["//server:server-deps-linux", ":console-artifact-jars", "@vaticle_typedb_common//binary:assemble-bash-targz"],
     additional_files = assemble_files,
+    empty_directories = empty_directories,
     permissions = permissions,
     output_filename = "typedb-all-linux",
 )
@@ -81,6 +87,7 @@ assemble_zip(
     name = "assemble-mac-zip",
     targets = ["//server:server-deps-mac", "//server:server-deps-prod", ":console-artifact-jars", "@vaticle_typedb_common//binary:assemble-bash-targz"],
     additional_files = assemble_files,
+    empty_directories = empty_directories,
     permissions = permissions,
     output_filename = "typedb-all-mac",
 )
@@ -89,6 +96,7 @@ assemble_zip(
     name = "assemble-windows-zip",
     targets = ["//server:server-deps-windows", ":console-artifact-jars", "@vaticle_typedb_common//binary:assemble-bat-targz"],
     additional_files = assemble_files,
+    empty_directories = empty_directories,
     permissions = permissions,
     output_filename = "typedb-all-windows",
 )

server/conf/BUILD
@@ -1,31 +1,0 @@
-#
-# Copyright (C) 2021 Vaticle
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
-
-package(default_visibility = ["//server:__subpackages__", "//:__pkg__"])
-
-load("@vaticle_dependencies//tool/checkstyle:rules.bzl", "checkstyle_test")
-
-filegroup(
-    name = "typedb",
-    srcs = ["typedb.yml"],
-)
-
-checkstyle_test(
-    name = "checkstyle",
-    include = glob(["*"]),
-    license_type = "agpl",
-)

server/test/BUILD
@@ -29,7 +29,7 @@ package(default_visibility = ["//visibility:private",])
 host_compatible_java_test(
     name = "test-configuration",
     srcs = [
-        "ConfigurationTest.java",
+        "ConfigTest.java",
     ],
     native_libraries_deps = [
         "//common:common",
@@ -43,7 +43,7 @@ host_compatible_java_test(
         "@vaticle_typedb_common//:common",
     ],
     data = [
-        "//server/conf:typedb",
+        "//server:config",
         ":configurations"
     ],
 )

migrator/MigratorClient.java
@@ -24,6 +24,7 @@ import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
 
+import java.nio.file.Path;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.CountDownLatch;
@@ -40,10 +41,10 @@ public class MigratorClient {
         stub = MigratorGrpc.newStub(channel);
     }
 
-    public boolean importData(String database, String filename) {
+    public boolean importData(String database, Path file) {
         MigratorProto.Import.Req req = MigratorProto.Import.Req.newBuilder()
                 .setDatabase(database)
-                .setFilename(filename)
+                .setFilename(file.toAbsolutePath().toString())
                 .build();
         ResponseObserver.Import streamObserver = new ResponseObserver.Import(new ProgressPrinter.Import());
         stub.importData(req, streamObserver);
@@ -51,10 +52,10 @@ public class MigratorClient {
         return streamObserver.success();
     }
 
-    public boolean exportData(String database, String filename) {
+    public boolean exportData(String database, Path file) {
         MigratorProto.Export.Req req = MigratorProto.Export.Req.newBuilder()
                 .setDatabase(database)
-                .setFilename(filename)
+                .setFilename(file.toAbsolutePath().toString())
                 .build();
         ResponseObserver.Export streamObserver = new ResponseObserver.Export(new ProgressPrinter.Export());
         stub.exportData(req, streamObserver);

commit 19c6d8761ceeb39519ad082a078bbbb19920608a

Fix bazel server binary by preparing required directories (#6520)
## What is the goal of this PR?

We address the requirement of pre-creating directory structures before running the server binary via bazel, which was previously broken. The new requirements were caused by #6512 in which we expect the configuration file and data directory to exist at server bootup. 

## What are the changes implemented in this PR?

* Use a bazel `genrule` to pre-create the data directory, and copy the configuration file into the expected location before executing the server binary
* Fix a bug in loading of the configuration using the `ConfigFactory`, which should only load the default configuration file lazily if there is no configuration path given otherwise

server/BUILD
@@ -94,12 +94,20 @@ native_java_libraries(
     visibility = ["//visibility:public"],
 )
 
+genrule(
+    name = "prepare-server-directories",
+    srcs = [":config"],
+    # TODO: can we find a way to create a directory without including an empty file in the outs?
+    cmd = "cp $(location :config) $(@D)/conf/ && touch $(@D)/data/empty",
+    outs = ["conf/config.yml", "data/empty"]
+)
+
 java_binary(
     name = "server-bin-mac",
     main_class = "com.vaticle.typedb.core.server.TypeDBServer",
     runtime_deps = [":server-mac"],
     tags = ["maven_coordinates=com.vaticle.typedb:typedb-core-server-bin:{pom_version}"],
-    data = ["//server:config"],
+    data = [":prepare-server-directories"]
 )
 
 java_binary( # TODO: this target should not be needed if :server-deps-mac can depend directly on :server-mac (lib)
@@ -114,7 +122,7 @@ java_binary(
     main_class = "com.vaticle.typedb.core.server.TypeDBServer",
     runtime_deps = [":server-linux"],
     tags = ["maven_coordinates=com.vaticle.typedb:typedb-core-server-bin:{pom_version}"],
-    data = ["//server:config"],
+    data = [":prepare-server-directories"]
 )
 
 java_binary( # TODO: this target should not be needed if :server-deps-linux can depend directly on :server-linux (lib)
@@ -129,7 +137,7 @@ java_binary(
     main_class = "com.vaticle.typedb.core.server.TypeDBServer",
     runtime_deps = [":server-windows"],
     tags = ["maven_coordinates=com.vaticle.typedb:typedb-core-server-bin:{pom_version}"],
-    data = ["//server:config"],
+    data = [":prepare-server-directories"]
 )
 
 java_binary( # TODO: this target should not be needed if :server-deps-windows can depend directly on :server-windows (lib)

server/parameters/ServerSubcommandParser.java
@@ -77,7 +77,7 @@ public class ServerSubcommandParser {
             Optional<Path> configPath = Server.configPath.parse(auxOptions);
             Config config = configPath
                     .map(path -> ConfigFactory.config(path, configOptions, configParser))
-                    .orElse(ConfigFactory.config(configOptions, configParser));
+                    .orElseGet(() -> ConfigFactory.config(configOptions, configParser));
             return new ServerSubcommand.Server(debug.parse(auxOptions), help.parse(auxOptions),
                     version.parse(auxOptions), config);
         }