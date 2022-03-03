9ab7669bd2771002f89f436c3cf4340fcfdd2159
joshua <joshuasend1@gmail.com>
2022-03-03 16:42:19 +0000

    Add more comprehensive tests

--- test/integration/statistics/StatisticsTest.java
+++ test/integration/statistics/StatisticsTest.java
@@ -59,6 +59,7 @@ public class StatisticsTest {
                         "person sub entity, owns name, plays friendship:friend, plays employment:employee;" +
                         "friendship sub relation, relates friend;" +
                         "name sub attribute, value string;" +
+                        "nickname sub attribute, value string, owns nickname;" +
                         "company sub entity, owns address @key, owns name, plays employment:employer;" +
                         "employment sub relation, relates employer, relates employee;" +
                         "address sub attribute, value string;").asDefine());
@@ -73,14 +74,14 @@ public class StatisticsTest {
     }
 
     @Test
-    public void nonconcurrent_statistics() {
+    public void nonconcurrent_insert_delete() {
         int batches = 10;
         try (CoreSession session = databaseMgr.session(database, Arguments.Session.Type.DATA)) {
             for (int i = 0; i < batches; i++) {
                 try (CoreTransaction txn = session.transaction(Arguments.Transaction.Type.WRITE)) {
                     txn.query().insert(TypeQL.parseQuery("insert " +
                             "$x isa person, has name 'name-" + i + "';" +
-                            "$clone isa person, has name 'name-" + i + "';" +
+                            "$clone isa person, has name 'name-" + i + "', has name 'clone-" + i + "';" +
                             "$y isa person, has name 'name-" + (i + batches) + "';" +
                             "(friend: $x, friend: $y) isa friendship;" +
                             "$c isa company, has name 'company-name-" + i + "', has address 'Margaret Street Nr. " + i + "';" +
@@ -93,8 +94,8 @@ public class StatisticsTest {
                 ThingGraph.Statistics statistics = txn.graphMgr.data().stats();
 
                 assertEquals(batches * 3, statistics.thingVertexCount(Label.of("person")));
-                assertEquals(batches * 3, statistics.hasEdgeCount(Label.of("person"), Label.of("name")));
-                assertEquals(batches * 3, statistics.thingVertexCount(Label.of("name")));
+                assertEquals(batches * 4, statistics.hasEdgeCount(Label.of("person"), Label.of("name")));
+                assertEquals(batches * 4, statistics.thingVertexCount(Label.of("name")));
                 assertEquals(batches, statistics.thingVertexCount(Label.of("friendship")));
                 assertEquals(batches * 2, statistics.thingVertexCount(Label.of("friend", "friendship")));
                 assertEquals(batches, statistics.thingVertexCount(Label.of("company")));
@@ -102,17 +103,17 @@ public class StatisticsTest {
                 assertEquals(batches, statistics.thingVertexCount(Label.of("address")));
                 assertEquals(batches, statistics.hasEdgeCount(Label.of("company"), Label.of("address")));
                 assertEquals(batches, statistics.thingVertexCount(Label.of("employment")));
-                assertEquals(batches, statistics.thingVertexCount(Label.of("employee", "employment")));
                 assertEquals(batches, statistics.thingVertexCount(Label.of("employer", "employment")));
-                assertEquals(batches * 10, statistics.thingVertexTransitiveCount(txn.graphMgr.schema().getType(Label.of("thing"))));
+                assertEquals(batches, statistics.thingVertexCount(Label.of("employee", "employment")));
+                assertEquals(batches * 11, statistics.thingVertexTransitiveCount(txn.graphMgr.schema().getType(Label.of("thing"))));
                 assertEquals(batches * 4, statistics.thingVertexTransitiveCount(txn.graphMgr.schema().getType(Label.of("role", "relation"))));
             }
 
             try (CoreTransaction txn = session.transaction(Arguments.Transaction.Type.WRITE)) {
                 txn.query().delete(TypeQL.parseQuery("match " +
-                        "$x isa person, has name $n; $n 'name-1';" +
-                        "$c isa company, has name $a; $a 'company-name-1';" +
-                        "$r (employee: $x, employer: $c) isa employment;" +
+                        "$x isa person, has name $n; $n 'name-1'; not { $x has name 'clone-1';};" +
+                        "$c isa company, has name $a; $a 'company-name-9';" +
+                        "$r (employer: $c) isa employment;" +
                         "delete " +
                         "$x isa person; $n isa name;" +
                         "$c has $a;" +
@@ -124,8 +125,8 @@ public class StatisticsTest {
 
                 assertEquals(batches * 3 - 1, statistics.thingVertexCount(Label.of("person")));
                 // deleting 'name-1' should remove it from first person and the clone
-                assertEquals(batches * 3 - 2, statistics.hasEdgeCount(Label.of("person"), Label.of("name")));
-                assertEquals(batches * 3 - 1, statistics.thingVertexCount(Label.of("name")));
+                assertEquals(batches * 4 - 2, statistics.hasEdgeCount(Label.of("person"), Label.of("name")));
+                assertEquals(batches * 4 - 1, statistics.thingVertexCount(Label.of("name")));
                 assertEquals(batches, statistics.thingVertexCount(Label.of("friendship")));
                 assertEquals(batches * 2 - 1, statistics.thingVertexCount(Label.of("friend", "friendship")));
                 assertEquals(batches, statistics.thingVertexCount(Label.of("company")));
@@ -133,10 +134,12 @@ public class StatisticsTest {
                 assertEquals(batches, statistics.thingVertexCount(Label.of("address")));
                 assertEquals(batches, statistics.hasEdgeCount(Label.of("company"), Label.of("address")));
                 assertEquals(batches, statistics.thingVertexCount(Label.of("employment")));
-                assertEquals(batches, statistics.thingVertexCount(Label.of("employee", "employment")));
+                // deleted one role explicitly, and another by deleting a person
                 assertEquals(batches - 1, statistics.thingVertexCount(Label.of("employer", "employment")));
-                assertEquals(batches * 10 - 2, statistics.thingVertexTransitiveCount(txn.graphMgr.schema().getType(Label.of("thing"))));
-                assertEquals(batches * 4 - 2, statistics.thingVertexTransitiveCount(txn.graphMgr.schema().getType(Label.of("role", "relation"))));
+                assertEquals(batches - 1, statistics.thingVertexCount(Label.of("employee", "employment")));
+                assertEquals(batches * 11 - 2, statistics.thingVertexTransitiveCount(txn.graphMgr.schema().getType(Label.of("thing"))));
+                // deleted one friendship:friend, one employment:employee, one employment:employer
+                assertEquals(batches * 4 - 3, statistics.thingVertexTransitiveCount(txn.graphMgr.schema().getType(Label.of("role", "relation"))));
             }
         }
     }
@@ -197,11 +200,98 @@ public class StatisticsTest {
         }
     }
 
-    // TODO deletes
+    @Test
+    public void concurrent_circular_ownerships_are_corrected() throws InterruptedException {
+        int batches = 10;
+        try (CoreSession session = databaseMgr.session(database, Arguments.Session.Type.DATA)) {
+            try (CoreTransaction txn = session.transaction(Arguments.Transaction.Type.WRITE)) {
+                txn.query().insert(TypeQL.parseQuery("insert $n 'Johnny' isa nickname, has nickname $n;").asInsert());
+                txn.commit();
+            }
+            try (CoreTransaction txn = session.transaction(Arguments.Transaction.Type.WRITE)) {
+                ThingGraph.Statistics statistics = txn.graphMgr.data().stats();
+                assertEquals(1, statistics.thingVertexCount(Label.of("nickname")));
+                assertEquals(1, statistics.hasEdgeCount(Label.of("nickname"), Label.of("nickname")));
+            }
+            List<CoreTransaction> transactions = new ArrayList<>();
+            for (int i = 0; i < batches; i++) transactions.add(session.transaction(Arguments.Transaction.Type.WRITE));
+            for (int i = 0; i < batches; i++) {
+                CoreTransaction txn = transactions.get(i);
+                txn.query().insert(TypeQL.parseQuery("match " +
+                        "$a isa nickname;" +
+                        "insert" +
+                        "$a has $a;"
+                ));
+                txn.commit();
+            }
 
-    // TODO attribute on attribute
+            // TODO force statistics to update
+            Thread.sleep(1000);
 
-    // TODO test insert, delete in same tx, then commit
+            try (CoreTransaction txn = session.transaction(Arguments.Transaction.Type.WRITE)) {
+                ThingGraph.Statistics statistics = txn.graphMgr.data().stats();
+                assertEquals(1, statistics.thingVertexCount(Label.of("nickname")));
+                assertEquals(1, statistics.hasEdgeCount(Label.of("nickname"), Label.of("nickname")));
+            }
+        }
+    }
+
+    @Test
+    public void insert_and_delete_in_same_transaction() {
+        try (CoreSession session = databaseMgr.session(database, Arguments.Session.Type.DATA)) {
+            // starting with no data
+            try (CoreTransaction txn = session.transaction(Arguments.Transaction.Type.WRITE)) {
+                ThingGraph.Statistics statistics = txn.graphMgr.data().stats();
+
+                txn.query().insert(TypeQL.parseQuery("insert " +
+                        "$x isa person, has name 'John';" +
+                        "$clone isa person, has name 'John', has name 'Clone';" +
+                        "$y isa person, has name 'Alice';" +
+                        "(friend: $x, friend: $y) isa friendship;" +
+                        "$c isa company, has name 'Vaticle', has address 'Margaret Street Nr. 1';" +
+                        "(employee: $x, employer: $c) isa employment;"));
+
+                assertEquals(3, statistics.thingVertexCount(Label.of("person")));
+                assertEquals(4, statistics.hasEdgeCount(Label.of("person"), Label.of("name")));
+                assertEquals(4, statistics.thingVertexCount(Label.of("name")));
+                assertEquals(1, statistics.thingVertexCount(Label.of("friendship")));
+                assertEquals(2, statistics.thingVertexCount(Label.of("friend", "friendship")));
+                assertEquals(1, statistics.thingVertexCount(Label.of("company")));
+                assertEquals(1, statistics.hasEdgeCount(Label.of("company"), Label.of("name")));
+                assertEquals(1, statistics.thingVertexCount(Label.of("address")));
+                assertEquals(1, statistics.hasEdgeCount(Label.of("company"), Label.of("address")));
+                assertEquals(1, statistics.thingVertexCount(Label.of("employment")));
+                assertEquals(1, statistics.thingVertexCount(Label.of("employer", "employment")));
+                assertEquals(1, statistics.thingVertexCount(Label.of("employee", "employment")));
+                assertEquals(11, statistics.thingVertexTransitiveCount(txn.graphMgr.schema().getType(Label.of("thing"))));
+                assertEquals(4, statistics.thingVertexTransitiveCount(txn.graphMgr.schema().getType(Label.of("role", "relation"))));
+
+                txn.query().delete(TypeQL.parseQuery("match " +
+                        "$x isa person, has name $n; $n 'John'; not { $x has name 'Clone';};" +
+                        "$clone isa person, has name $n, has name $cn; $cn 'Clone';" +
+                        "delete " +
+                        "$x isa person; $n isa name; " +
+                        "$clone has $cn;"));
+                assertEquals(2, statistics.thingVertexCount(Label.of("person")));
+                assertEquals(1, statistics.hasEdgeCount(Label.of("person"), Label.of("name")));
+                assertEquals(3, statistics.thingVertexCount(Label.of("name")));
+                assertEquals(1, statistics.thingVertexCount(Label.of("friendship")));
+                assertEquals(1, statistics.thingVertexCount(Label.of("friend", "friendship")));
+                assertEquals(1, statistics.thingVertexCount(Label.of("employment")));
+                assertEquals(1, statistics.thingVertexCount(Label.of("employer", "employment")));
+                assertEquals(0, statistics.thingVertexCount(Label.of("employee", "employment")));
+                assertEquals(9, statistics.thingVertexTransitiveCount(txn.graphMgr.schema().getType(Label.of("thing"))));
+                assertEquals(2, statistics.thingVertexTransitiveCount(txn.graphMgr.schema().getType(Label.of("role", "relation"))));
+
+                txn.query().insert(TypeQL.parseQuery("match " +
+                        "$clone isa person; not { $clone has name $n; };" +
+                        "insert " +
+                        "$clone has name 'Clone';"));
+                assertEquals(2, statistics.hasEdgeCount(Label.of("person"), Label.of("name")));
+                assertEquals(3, statistics.thingVertexCount(Label.of("name")));
+            }
+        }
+    }
 
     @Test
     public void reboot_counts_correct() {
@@ -209,85 +299,4 @@ public class StatisticsTest {
         // TODO
 
     }
-
-//    @Test
-//    public void test_statistics() throws IOException {
-//        Util.resetDirectory(dataDir);
-//        try (CoreDatabaseManager databaseMgr = CoreDatabaseManager.open(options)) {
-//            databaseMgr.create(database);
-//            loadSchema(databaseMgr);
-//            int personCount = 1000;
-//            Set<Long> ages = new HashSet<>();
-//            Random random = new Random(0);
-//            insertPersonAndAges(databaseMgr, personCount, ages, random);
-//            assertStatistics(databaseMgr, personCount, ages);
-//            updateAges(databaseMgr, ages);
-//            assertStatistics(databaseMgr, personCount, ages);
-//        }
-//    }
-//
-//    private void updateAges(CoreDatabaseManager databaseMgr, Set<Long> ages) {
-//        try (CoreSession session = databaseMgr.session(database, Arguments.Session.Type.DATA)) {
-//            try (CoreTransaction tx = session.transaction(Arguments.Transaction.Type.WRITE)) {
-//                TypeQLQuery query = TypeQL.parseQuery("match $x isa person, has age $y;");
-//                List<ConceptMap> conceptMaps = tx.query().match(query.asMatch()).toList();
-//                conceptMaps.forEach(cm -> {
-//                    Attribute.Long attribute = cm.get("y").asAttribute().asLong();
-//                    cm.get("x").asEntity().unsetHas(attribute);
-//                    long newAge = attribute.getValue() + 1;
-//                    Attribute.Long newAttribute = attribute.getType().asLong().put(newAge);
-//                    cm.get("x").asEntity().setHas(newAttribute);
-//                    cm.get("x").asEntity().setHas(newAttribute);
-//                    ages.add(newAge);
-//                });
-//                tx.commit();
-//            }
-//        }
-//    }
-//
-//    private void insertPersonAndAges(CoreDatabaseManager databaseMgr, int personCount, Set<Long> ages, Random random) {
-//        try (CoreSession session = databaseMgr.session(database, Arguments.Session.Type.DATA)) {
-//            try (CoreTransaction tx = session.transaction(Arguments.Transaction.Type.WRITE)) {
-//                for (int i = 0; i < personCount; i++) {
-//                    long age = random.nextInt(personCount);
-//                    ages.add(age);
-//                    TypeQLQuery query = TypeQL.parseQuery("insert $x isa person, has age " + age + ";").asInsert();
-//                    tx.query().insert(query.asInsert());
-//                }
-//                tx.commit();
-//            }
-//        }
-//    }
-//
-//    private void assertStatistics(CoreDatabaseManager databaseMgr, int personCount, Set<Long> ages) {
-//        waitForStatisticsCounter();
-//        try (CoreSession session = databaseMgr.session(database, Arguments.Session.Type.DATA)) {
-//            try (CoreTransaction tx = session.transaction(Arguments.Transaction.Type.READ)) {
-//                assertEquals(personCount, tx.graphMgr.data().stats().thingVertexCount(Label.of("person")));
-//                assertEquals(ages.size(), tx.graphMgr.data().stats().thingVertexCount(Label.of("age")));
-//                assertEquals(personCount, tx.graphMgr.data().stats().hasEdgeCount(Label.of("person"), Label.of("age")));
-//            }
-//        }
-//    }
-//
-//    private void loadSchema(CoreDatabaseManager databaseMgr) {
-//        try (TypeDB.Session session = databaseMgr.session(database, Arguments.Session.Type.SCHEMA)) {
-//            try (TypeDB.Transaction tx = session.transaction(Arguments.Transaction.Type.WRITE)) {
-//                TypeQLQuery query = TypeQL.parseQuery("define " +
-//                        "person sub entity, owns age; " +
-//                        "age sub attribute, value long; " +
-//                        "");
-//                tx.query().define(query.asDefine());
-//                tx.commit();
-//            }
-//        }
-//    }
-//
-//    private void waitForStatisticsCounter() {
-//        try {
-//            Thread.sleep(1000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//    }
 }
