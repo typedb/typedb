88e4451de6e869794416629627d9b4a573d301d8
joshua <joshuasend1@gmail.com>
2022-08-03 17:34:36 +0100

    fix state check

--- common/iterator/sorted/DistinctSortedIterator.java
+++ common/iterator/sorted/DistinctSortedIterator.java
@@ -127,7 +127,7 @@ public class DistinctSortedIterator<T extends Comparable<? super T>, ORDER exten
 
         @Override
         public void forward(T target) {
-            if (state != State.INIT && !order.isValidNext(last, target)) throw TypeDBException.of(ILLEGAL_ARGUMENT);
+            if (last != null && !order.isValidNext(last, target)) throw TypeDBException.of(ILLEGAL_ARGUMENT);
             iterator.forward(target);
             if (state == State.FETCHED) state = State.EMPTY;
         }
