266334fcd9dc3eb763df3aaab1f384e1ce1c9735
joshua <joshuasend1@gmail.com>
2022-09-30 18:17:06 +0100

    update headers

--- encoding/key/KeyGenerator.java
+++ encoding/key/KeyGenerator.java
@@ -13,6 +13,7 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
  */
 
 package com.vaticle.typedb.core.encoding.key;
