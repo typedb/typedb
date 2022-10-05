70866e09b4b3adb7739ac652a466879e7e832602
joshua <joshuasend1@gmail.com>
2022-10-05 10:49:20 +0100

    fix error message

--- common/exception/ErrorMessage.java
+++ common/exception/ErrorMessage.java
@@ -310,11 +310,11 @@ public abstract class ErrorMessage extends com.vaticle.typedb.common.exception.E
         public static final ThingRead SORT_ATTRIBUTE_NOT_COMPARABLE =
                 new ThingRead(7, "The variable '%s' cannot be used to sort, as it may represent incomparable types '%s' and '%s'.");
         public static final ThingRead VALUES_NOT_COMPARABLE =
-                new ThingRead(7, "The value '%s' of value type: '%s' cannot be compared to '%s' of value type '%s'.");
+                new ThingRead(8, "The value '%s' of value type: '%s' cannot be compared to '%s' of value type '%s'.");
         public static final ThingRead AGGREGATE_ATTRIBUTE_NOT_NUMBER =
-                new ThingRead(8, "The variable '%s' cannot be used to calculate aggregate functions, as it contains non-numeric values.");
+                new ThingRead(9, "The variable '%s' cannot be used to calculate aggregate functions, as it contains non-numeric values.");
         public static final ThingRead NUMERIC_IS_NOT_NUMBER =
-                new ThingRead(9, "The numeric value is not a valid number (NaN).");
+                new ThingRead(10, "The numeric value is not a valid number (NaN).");
 
         private static final String codePrefix = "THR";
         private static final String messagePrefix = "Invalid Thing Read";
