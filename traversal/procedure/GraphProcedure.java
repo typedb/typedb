06eef03c4146737bc906a70ec39c869dbba25835
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2022-10-07 14:42:17 +0100

    Clean up imports

--- traversal/procedure/GraphProcedure.java
+++ traversal/procedure/GraphProcedure.java
@@ -42,7 +42,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
