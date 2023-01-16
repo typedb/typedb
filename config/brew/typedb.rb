0a04283e6b40978d9a6bb95c8b8354187afc327d
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2023-01-16 16:41:15 +0000

    fix log directory name

--- config/brew/typedb.rb
+++ config/brew/typedb.rb
@@ -29,7 +29,7 @@ class Typedb < Formula
     libexec.install Dir["*"]
     mkdir_p var/"typedb/data"
     inreplace libexec/"server/conf/config.yml", "server/data", var/"typedb/data"
-    mkdir_p var/"log/typedb"
+    mkdir_p var/"typedb/logs"
     inreplace libexec/"server/conf/config.yml", "server/logs", var/"typedb/logs"
     bin.install libexec / "typedb"
     bin.env_script_all_files(libexec, Language::Java.java_home_env)
