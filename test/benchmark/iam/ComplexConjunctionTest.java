b80159d81a62b5dd1e42c24a89e3cc7b9d56ccbe
Krishnan Govindraj <krishnan@vaticle.com>
2023-04-24 11:18:38 +0100

    Add multi-starting point test

--- test/benchmark/iam/ComplexConjunctionTest.java
+++ test/benchmark/iam/ComplexConjunctionTest.java
@@ -47,6 +47,7 @@ public class ComplexConjunctionTest extends ReasonerBenchmarkSuite {
 
     @Test
     public void testCheckPermission() {
+        // Simple, but needs to pick the right plan.
         String query = "match\n" +
                 "$p isa person, has email \"douglas.schmidt@vaticle.com\";\n" +
                 "$f isa file, has path \"root/engineering/typedb-studio/src/README.md\";\n" +
@@ -54,6 +55,25 @@ public class ComplexConjunctionTest extends ReasonerBenchmarkSuite {
                 "$a (object: $f, action: $o) isa access;\n" +
                 "$pe (subject: $p, access: $a) isa permission, has validity true;";
         Benchmark benchmark = new Benchmark("check-permission", query, 1, 3);
+        runBenchmark(benchmark);
+        benchmark.assertAnswerCountCorrect();
+    }
+
+    @Test
+    public void testSegregationViolationMultipleStartingPoints() {
+        // Stresses resolvable ordering planner by having many candidate orderings
+        String query = "match\n" +
+                "   $a1 isa action, has name \"submit pull request\";\n" +
+                "   $a2 isa action, has name \"approve pull request\";\n" +
+                "   $s isa subject, has email \"genevieve.gallegos@vaticle.com\";\n" +
+                "   $parent isa directory, has path \"root/engineering\";\n" +
+                "   $policy (action: $a1, action: $a2) isa segregation-policy;\n" +
+                "   (collection: $parent, member:$o) isa collection-membership;\n" +
+                "   $ac1(object: $o, action: $a1) isa access;\n" +
+                "   $ac2(object: $o, action: $a2) isa access;\n" +
+                "   $p1(subject: $s, access: $ac1) isa permission;\n" +
+                "   $p2(subject: $s, access: $ac2) isa permission;\n";
+        Benchmark benchmark = new Benchmark("many-starting-points-segregation-violation", query, 2, 3);
         runBenchmark(benchmark);
         benchmark.assertAnswerCountCorrect();
     }
