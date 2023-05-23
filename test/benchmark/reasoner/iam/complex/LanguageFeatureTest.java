fb80ce3c5468483d978d642876e5cab967c893b8
Krishnan Govindraj <krishnan@vaticle.com>
2023-05-23 14:10:13 +0100

    Fix typo in automation.yml for language-feature(s)-test

--- test/benchmark/reasoner/iam/complex/LanguageFeatureTest.java
+++ test/benchmark/reasoner/iam/complex/LanguageFeatureTest.java
@@ -47,7 +47,7 @@ public class LanguageFeatureTest {
         benchmarker.setUp();
         benchmarker.loadSchema(COMMON_RESOURCE_DIR.resolve("types.tql"));
         benchmarker.loadSchema(COMMON_RESOURCE_DIR.resolve("rules.tql"));
-        benchmarker.loadSchema(RESOURCE_DIRECTORY.resolve("language-features-test.tql"));
+        benchmarker.loadSchema(RESOURCE_DIRECTORY.resolve("language-feature-test.tql"));
         benchmarker.importData(COMMON_RESOURCE_DIR.resolve("data.typedb"));
         benchmarker.warmUp();
     }
