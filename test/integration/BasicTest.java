186860aa71235ae8df322253e51efb7316c858f0
joshua <joshua@vaticle.com>
2023-08-21 12:43:59 +0100

    Reduce concurrent query and transaction testing from 100 to 50

--- test/integration/BasicTest.java
+++ test/integration/BasicTest.java
@@ -134,7 +134,7 @@ public class BasicTest {
 
     @Test
     public void write_types_concurrently_repeatedly() throws IOException, InterruptedException {
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 50; i++) {
             System.out.println(i + " ---- ");
             write_types_concurrently();
         }
@@ -414,7 +414,7 @@ public class BasicTest {
 
     @Test
     public void write_attributes_successfully_repeatedly() throws IOException {
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 50; i++) {
             System.out.println(i + " ---- ");
             write_attributes_successfully();
         }
@@ -489,7 +489,7 @@ public class BasicTest {
 
     @Test
     public void write_different_attributes_in_parallel_successfully_repeatedly() throws IOException {
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 50; i++) {
             System.out.println(i + " ---- ");
             write_different_attributes_in_parallel_successfully();
         }
@@ -611,7 +611,7 @@ public class BasicTest {
 
     @Test
     public void write_identical_attributes_in_parallel_successfully_repeatedly() throws IOException {
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 50; i++) {
             System.out.println(i + " ---- ");
             write_identical_attributes_in_parallel_successfully();
         }
@@ -713,7 +713,7 @@ public class BasicTest {
 
     @Test
     public void write_and_delete_attributes_concurrently_repeatedly() throws IOException {
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 50; i++) {
             System.out.println(i + " ---- ");
             write_and_delete_attributes_concurrently();
         }
