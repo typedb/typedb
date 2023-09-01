f87a2ccb6945f0dc3858904bf2adf870ba358d4b
joshua <joshua@vaticle.com>
2023-09-01 14:31:25 +0100

    Add runtime type checking

--- common/exception/ErrorMessage.java
+++ common/exception/ErrorMessage.java
@@ -413,37 +413,37 @@ public abstract class ErrorMessage extends com.vaticle.typedb.common.exception.E
         public static final ThingWrite INVALID_DELETE_HAS =
                 new ThingWrite(20, "Invalid attempt to delete attribute ownership. The thing '%s' does not have attribute '%s'.");
         public static final ThingWrite HAS_TYPE_MISMATCH =
-                new ThingWrite(20, "The instance of type '%s' cannot own instances of attribute type '%s'.");
+                new ThingWrite(21, "The instance of type '%s' cannot own instances of attribute type '%s'.");
         public static final ThingWrite ILLEGAL_IS_CONSTRAINT =
-                new ThingWrite(21, "The 'is' constraint, e.g. used in '%s', is not accepted in an insert/delete query.");
+                new ThingWrite(22, "The 'is' constraint, e.g. used in '%s', is not accepted in an insert/delete query.");
         public static final ThingWrite ATTRIBUTE_VALUE_TOO_MANY =
-                new ThingWrite(22, "Unable to insert attribute '%s' of type '%s' with more than one value operations.");
+                new ThingWrite(23, "Unable to insert attribute '%s' of type '%s' with more than one value operations.");
         public static final ThingWrite ATTRIBUTE_VALUE_MISSING =
-                new ThingWrite(23, "Unable to insert attribute '%s' of type '%s' without a value assigned to the variable.");
+                new ThingWrite(24, "Unable to insert attribute '%s' of type '%s' without a value assigned to the variable.");
         public static final ThingWrite INSERT_RELATION_CONSTRAINT_TOO_MANY =
-                new ThingWrite(24, "Unable to insert relation '%s' as it has more than one relation tuple describing the role players.");
+                new ThingWrite(25, "Unable to insert relation '%s' as it has more than one relation tuple describing the role players.");
         public static final ThingWrite RELATION_CONSTRAINT_MISSING =
-                new ThingWrite(25, "Unable to insert relation '%s' as it is missing the relation tuple describing the role players.");
+                new ThingWrite(26, "Unable to insert relation '%s' as it is missing the relation tuple describing the role players.");
         public static final ThingWrite ROLE_TYPE_AMBIGUOUS =
-                new ThingWrite(26, "Unable to add role player '%s' to the relation, as there are more than one possible role type it could play.");
+                new ThingWrite(27, "Unable to add role player '%s' to the relation, as there are more than one possible role type it could play.");
         public static final ThingWrite ROLE_TYPE_MISSING =
-                new ThingWrite(27, "Unable to add role player '%s' to the relation, as there is no provided or inferrable role type.");
+                new ThingWrite(28, "Unable to add role player '%s' to the relation, as there is no provided or inferrable role type.");
         public static final ThingWrite ROLE_TYPE_MISMATCH =
-                new ThingWrite(28, "The type '%s' cannot be used as a role type.");
+                new ThingWrite(29, "The type '%s' cannot be used as a role type.");
         public static final ThingWrite PLAYING_TYPE_MISMATCH =
-                new ThingWrite(29, "The instance of type '%s' cannot play the role type '%s'.");
+                new ThingWrite(30, "The instance of type '%s' cannot play the role type '%s'.");
         public static final ThingWrite RELATING_TYPE_MISMATCH =
-                new ThingWrite(30, "The relation instance of type '%s' cannot relate the role type '%s'.");
+                new ThingWrite(31, "The relation instance of type '%s' cannot relate the role type '%s'.");
         public static final ThingWrite MAX_INSTANCE_REACHED =
-                new ThingWrite(31, "The maximum number of instances for type '%s' has been reached: '%s'");
+                new ThingWrite(32, "The maximum number of instances for type '%s' has been reached: '%s'");
         public static final ThingWrite DELETE_RELATION_CONSTRAINT_TOO_MANY =
-                new ThingWrite(32, "Could not perform delete of role players due to multiple relation constraints being present for relation '%s'.");
+                new ThingWrite(33, "Could not perform delete of role players due to multiple relation constraints being present for relation '%s'.");
         public static final ThingWrite DELETE_ROLEPLAYER_NOT_PRESENT =
-                new ThingWrite(33, "Could not delete roleplayer '%s' as relation '%s' does not relate it.");
+                new ThingWrite(34, "Could not delete roleplayer '%s' as relation '%s' does not relate it.");
         public static final ThingWrite ILLEGAL_VALUE_VARIABLE_IN_DELETE =
-                new ThingWrite(34, "Illegal value variable '%s' found in delete query. Value variables may not be used in delete queries.");
+                new ThingWrite(35, "Illegal value variable '%s' found in delete query. Value variables may not be used in delete queries.");
         public static final ThingWrite ILLEGAL_VALUE_CONSTRAINT_IN_INSERT =
-                new ThingWrite(35, "Illegal value constraint found in insert query on variable '%s'. Value variables are only permitted to specify attribute values.");
+                new ThingWrite(36, "Illegal value constraint found in insert query on variable '%s'. Value variables are only permitted to specify attribute values.");
 
         private static final String codePrefix = "THW";
         private static final String messagePrefix = "Invalid Thing Write";
