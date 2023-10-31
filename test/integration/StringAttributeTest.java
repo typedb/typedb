192dbfd66368b9f6f5dd39a7a196cde1eb5afd51
joshua <joshua@vaticle.com>
2023-10-31 11:34:38 +0000

    Fix String attribute test

--- test/integration/StringAttributeTest.java
+++ test/integration/StringAttributeTest.java
@@ -105,7 +105,7 @@ public class StringAttributeTest {
                         // String escaped = generated.replace("\\", "\\\\").replace("\"", "\\\"");
                         if (!(generated.contains("\\") || generated.contains("\""))) {
                             Optional<? extends ConceptMap> ans = txn.query().get(TypeQL.parseQuery(
-                                    "match $a \"" + generated + "\" isa string-value;").asGet()).first();
+                                    "match $a \"" + generated + "\" isa string-value; get;").asGet()).first();
                             assertTrue(ans.isPresent());
                             assertEquals(generated, ans.get().getConcept("a").asAttribute().asString().getValue());
                         }
