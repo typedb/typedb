1bc008dc14f6b9289c7243999525dbc107ac090f
joshua <joshua@vaticle.com>
2024-01-04 14:07:27 +0000

    Fix checkstyle

--- common/diagnostics/Diagnostics.java
+++ common/diagnostics/Diagnostics.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Vaticle
+ * Copyright (C) 2022 Vaticle
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -13,6 +13,7 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
  */
 
 package com.vaticle.typedb.core.common.diagnostics;
