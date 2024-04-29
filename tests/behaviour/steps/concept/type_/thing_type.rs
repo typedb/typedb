9e514717f1d3db1cc3c4d6c73bf589f5e8a307a0
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2024-04-29 12:17:36 +0100

    Missing license headers

--- tests/behaviour/steps/concept/type_/thing_type.rs
+++ tests/behaviour/steps/concept/type_/thing_type.rs
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 use cucumber::gherkin::Step;
 use macro_rules_attribute::apply;
 use crate::{generic_step, tx_as_read, tx_as_write, tx_as_schema, concept, Context, params::{Boolean, MayError, ContainsOrDoesnt, RootLabel, Label}, transaction_context::{ActiveTransaction}, util};
