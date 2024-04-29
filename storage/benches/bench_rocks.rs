e9741b3ae6e996c629328598be615f288a870bfa
Krishnan Govindraj <krishnan@vaticle.com>
2024-04-29 18:26:29 +0100

    Summary

--- storage/benches/bench_rocks.rs
+++ storage/benches/bench_rocks.rs
@@ -42,27 +42,37 @@ pub struct BenchmarkResult {
 
 impl BenchmarkResult {
     fn print_report(&self, runner: &BenchmarkRunner) {
-        println!("-- Report for benchmark threads = {}, batches={}, batch_size={} ---", runner.n_threads, runner.n_batches, runner.batch_size);
+        println!("-- Report for benchmark ---");
+        println!("threads = {}, batches={}, batch_size={} ---", runner.n_threads, runner.n_batches, runner.batch_size);
         println!("key-size: {KEY_SIZE}; value_size: {VALUE_SIZE}");
-        println!("Total time (ns): {}", self.total_time.as_nanos());
         println!("Batch timings (ns):");
+        println!("- - - - - - - -");
         self.batch_timings.iter().enumerate().for_each(|(batch_id, time)| {
             println!("{:8}: {:12}", batch_id, time.as_nanos());
         });
+        println!("- - - - - - - -");
+
+        let n_keys: usize = runner.n_batches * runner.batch_size;
+        let data_size_mb : f64 = ((n_keys * (KEY_SIZE + VALUE_SIZE)) as f64) / ((1024 * 1024) as f64) ;
+        println!("Summary:");
+        println!("Total time: {:12} ns; total_keys: {:10}; data_size: {:8} MB\nrate: {:.2} keys/s = {:.2} MB/s ",
+                 self.total_time.as_nanos(), n_keys, data_size_mb,
+                 n_keys as f64 / self.total_time.as_secs_f64(), data_size_mb / self.total_time.as_secs_f64(),
+        );
     }
 }
 
 pub struct BenchmarkRunner {
     n_threads: u16,
     n_batches: usize,
-    batch_size: u64,
+    batch_size: usize,
 }
 
 impl BenchmarkRunner {
     const VALUE_EMPTY :[u8;0] = [];
     fn run(&self, database_arc: &impl RocksDatabase) -> BenchmarkResult {
         debug_assert_eq!(1, N_DATABASES, "I've not bothered implementing multiple databases");
-        let batch_timings: Vec<RwLock<Duration>> = (0..self.n_batches).into_iter().map({|_| RwLock::new(Duration::from_secs(0)) }).collect();
+        let batch_timings: Vec<RwLock<Duration>> = (0..self.n_batches).into_iter().map(|_| RwLock::new(Duration::from_secs(0))).collect();
         let batch_counter = AtomicUsize::new(0);
         let benchmark_start_instant = Instant::now();
         thread::scope(|s| {
@@ -82,7 +92,7 @@ impl BenchmarkRunner {
                         write_batch.commit().unwrap();
                         let mut duration_for_batch = batch_timings.get(batch_number).unwrap().write().unwrap();
                         *duration_for_batch = batch_start_instant.elapsed();
-                        println!("Thread completed batch {}", batch_number)
+                        // println!("Thread completed batch {}", batch_number)
                     }
                 });
             }
@@ -118,7 +128,7 @@ fn main() {
     let benchmarker = BenchmarkRunner {
         n_threads: get_arg_as::<u16>(&args, "threads").unwrap(),
         n_batches: get_arg_as::<usize>(&args, "batches").unwrap(),
-        batch_size: get_arg_as::<u64>(&args, "batch_size").unwrap()
+        batch_size: get_arg_as::<usize>(&args, "batch_size").unwrap()
     };
     let report = benchmarker.run(&database);
     println!("Done");
