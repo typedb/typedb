4037e20c6cb7ea611beeafba6c22f269a8fcfcc4
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2024-05-24 12:03:15 +0100

    extract keyspace raw iterator module into file

--- storage/keyspace/iterator.rs
+++ storage/keyspace/iterator.rs
@@ -14,100 +14,16 @@ use lending_iterator::{
 use logger::result::ResultExt;
 use speedb::DB;
 
-use super::keyspace::{Keyspace, KeyspaceError};
+use super::{
+    keyspace::{Keyspace, KeyspaceError},
+    raw_iterator,
+};
 use crate::key_range::{KeyRange, RangeEnd};
-
-mod raw {
-    use std::{cmp::Ordering, mem::transmute};
-
-    use lending_iterator::{LendingIterator, Seekable};
-    use speedb::DBRawIterator;
-
-    type KeyValue<'a> = Result<(&'a [u8], &'a [u8]), speedb::Error>;
-
-    /// SAFETY NOTE: `'static` here represents that the `DBIterator` owns the data.
-    /// The item's lifetime is in fact invalidated when `iterator` is advanced.
-    pub(super) struct DBIterator {
-        iterator: DBRawIterator<'static>,
-        item: Option<KeyValue<'static>>,
-    }
-
-    impl DBIterator {
-        pub(super) fn new_from(mut iterator: DBRawIterator<'static>, start: &[u8]) -> Self {
-            iterator.seek(start);
-            let item = iterator.valid().then(|| unsafe { transmute(iterator.item()) });
-            Self { item: Ok(item).transpose(), iterator }
-        }
-
-        fn peek(&mut self) -> Option<&<Self as LendingIterator>::Item<'_>> {
-            match self.next() {
-                Some(item) => {
-                    self.item = Some(unsafe { 
-                        // SAFETY: the stored item is only accessible while mutably borrowing this iterator.
-                        // When the underlying iterator is advanced, the stored item is discarded.
-                        transmute::<KeyValue<'_>, KeyValue<'static>>(item)
-                    });
-                    self.item.as_ref()
-                }
-                None => None,
-            }
-        }
-    }
-
-    impl LendingIterator for DBIterator {
-        type Item<'a> = Result<(&'a [u8], &'a [u8]), speedb::Error>
-        where
-            Self: 'a;
-
-        fn next(&mut self) -> Option<Self::Item<'_>> {
-            if self.item.is_some() {
-                self.item.take()
-            } else if self.iterator.valid() {
-                self.iterator.next();
-                self.iterator.item().map(Ok)
-            } else if self.iterator.status().is_err() {
-                Some(Err(self.iterator.status().err().unwrap().clone()))
-            } else {
-                None
-            }
-        }
-    }
-
-    impl Seekable<[u8]> for DBIterator {
-        fn seek(&mut self, key: &[u8]) {
-            if let Some(item) = self.peek() {
-                match compare_key(item, key) {
-                    Ordering::Less => {
-                        self.item.take();
-                        self.iterator.seek(key);
-                    }
-                    Ordering::Equal => (),
-                    Ordering::Greater => {
-                        // TODO: seeking backward could be a no-op or an error or illegal state??
-                    }
-                }
-            }
-        }
-
-        fn compare_key(&self, item: &Self::Item<'_>, key: &[u8]) -> Ordering {
-            compare_key(item, key)
-        }
-    }
-
-    pub(super) fn compare_key<E>(item: &Result<(&[u8], &[u8]), E>, key: &[u8]) -> Ordering {
-        if let Ok(item) = item {
-            let (peek, _) = item;
-            peek.cmp(&key)
-        } else {
-            Ordering::Equal
-        }
-    }
-}
 
 pub struct KeyspaceRangeIterator {
     iterator: Peekable<
         SeekableMap<
-            TakeWhile<raw::DBIterator, Box<dyn FnMut(&Result<(&[u8], &[u8]), speedb::Error>) -> bool>>,
+            TakeWhile<raw_iterator::DBIterator, Box<dyn FnMut(&Result<(&[u8], &[u8]), speedb::Error>) -> bool>>,
             Box<
                 dyn for<'a> Fn(
                     Result<(&'a [u8], &'a [u8]), speedb::Error>,
@@ -129,7 +45,8 @@ impl KeyspaceRangeIterator {
         // read_opts.set_prefix_same_as_start(true);
         let read_opts = keyspace.new_read_options();
         let kv_storage: &'static DB = unsafe { std::mem::transmute(&keyspace.kv_storage) };
-        let iterator = raw::DBIterator::new_from(kv_storage.raw_iterator_opt(read_opts), range.start().bytes());
+        let iterator =
+            raw_iterator::DBIterator::new_from(kv_storage.raw_iterator_opt(read_opts), range.start().bytes());
         let start = range.start().to_array();
         let range = match range.end() {
             RangeEnd::SameAsStart => KeyRange::new_within(start, range.fixed_width()),
@@ -146,7 +63,7 @@ impl KeyspaceRangeIterator {
                 Err(_) => true,
             }) as Box<_>)
             .map(error_mapper(keyspace_name))
-            .into_seekable(identity as _, raw::compare_key as _);
+            .into_seekable(identity as _, raw_iterator::compare_key as _);
 
         KeyspaceRangeIterator { iterator: Peekable::new(range_iterator) }
     }
