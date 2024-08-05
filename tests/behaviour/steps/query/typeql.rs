e65675a6a5167b00daf9a5a9924c25a34b5515ec
Krishnan Govindraj <krishnan@vaticle.com>
2024-08-05 17:15:46 +0100

    Bump typedb behaviour

--- tests/behaviour/steps/query/typeql.rs
+++ tests/behaviour/steps/query/typeql.rs
@@ -4,7 +4,12 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+use std::{collections::HashMap, sync::Arc};
+
+use compiler::inference::annotated_functions::AnnotatedCommittedFunctions;
 use cucumber::gherkin::Step;
+use executor::{batch::Row, insert_executor::InsertExecutor};
+use ir::program::{function_signature::HashMapFunctionIndex, program::Program};
 use macro_rules_attribute::apply;
 use query::query_manager::QueryManager;
 
@@ -28,9 +33,42 @@ async fn typeql_define(context: &mut Context, may_error: MayError, step: &Step) 
 #[apply(generic_step)]
 #[step(expr = r"typeql insert{may_error}")]
 async fn typeql_insert(context: &mut Context, may_error: MayError, step: &Step) {
-    let typeql_insert = step.docstring.as_ref().unwrap().as_str();
     with_write_tx!(context, |tx| {
-        println!("TypeQL insert is ignored");
+        let typeql_insert = typeql::parse_query(step.docstring.as_ref().unwrap().as_str())
+            .unwrap()
+            .into_pipeline()
+            .stages
+            .pop()
+            .unwrap()
+            .into_insert();
+        let block =
+            ir::translation::insert::translate_insert(&HashMapFunctionIndex::empty(), &typeql_insert).unwrap().finish();
+        let annotated_program = compiler::inference::type_inference::infer_types(
+            Program::new(block, vec![]),
+            &tx.snapshot,
+            &tx.type_manager,
+            Arc::new(AnnotatedCommittedFunctions::new(vec![].into_boxed_slice(), vec![].into_boxed_slice())),
+        )
+        .unwrap();
+        let insert_plan = compiler::planner::insert_planner::build_insert_plan(
+            &HashMap::new(),
+            annotated_program.get_entry_annotations(),
+            annotated_program.get_entry().conjunction().constraints(),
+        )
+        .unwrap();
+
+        println!("{:?}", &insert_plan.instructions);
+        insert_plan.debug_info.iter().for_each(|(k, v)| {
+            println!("{:?} -> {:?}", k, annotated_program.get_entry().context().get_variables_named().get(v))
+        });
+        let mut executor = InsertExecutor::new(insert_plan);
+        executor::insert_executor::execute(
+            &mut tx.snapshot,
+            &tx.thing_manager,
+            &mut executor,
+            &Row::new(vec![].as_mut_slice(), &mut 1),
+        )
+        .unwrap();
     });
 }
 
@@ -53,4 +91,10 @@ async fn uniquely_identify_answer_concepts(context: &mut Context, step: &Step) {
         // Can't read_tx because execute always takes a mut snapshot
         println!("Uniquely identify is ignored!")
     });
+}
+
+#[apply(generic_step)]
+#[step(expr = r"set time-zone is: {word}\/{word}")] // TODO: Maybe make time-zone a param
+async fn set_timezone_is(context: &mut Context, step: &Step) {
+    println!("Set timezone is ignored!")
 }
