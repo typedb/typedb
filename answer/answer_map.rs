fa642929eda6a5095f483d6abd2ca9222e3b4ad4
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2024-08-23 14:44:51 +0100

    AnswerMap iterator

--- answer/answer_map.rs
+++ answer/answer_map.rs
@@ -4,7 +4,7 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-use std::collections::HashMap;
+use std::collections::{hash_map, HashMap};
 
 use crate::{variable::Variable, variable_value::VariableValue};
 
@@ -12,4 +12,20 @@ use crate::{variable::Variable, variable_value::VariableValue};
 // TODO: what about optionality and lists
 pub struct AnswerMap<'a> {
     map: HashMap<Variable, VariableValue<'a>>,
+}
+
+impl<'a> IntoIterator for AnswerMap<'a> {
+    type Item = (Variable, VariableValue<'a>);
+    type IntoIter = hash_map::IntoIter<Variable, VariableValue<'a>>;
+    fn into_iter(self) -> Self::IntoIter {
+        self.map.into_iter()
+    }
+}
+
+impl<'a, 'b> IntoIterator for &'b AnswerMap<'a> {
+    type Item = (&'b Variable, &'b VariableValue<'a>);
+    type IntoIter = hash_map::Iter<'b, Variable, VariableValue<'a>>;
+    fn into_iter(self) -> Self::IntoIter {
+        self.map.iter()
+    }
 }
