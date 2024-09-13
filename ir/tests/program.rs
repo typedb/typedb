89f07cf18118a9417268cce26fcd510c574e9fc1
joshua <joshua@vaticle.com>
2024-09-13 10:15:36 +0100

    Fix build with new function IR arguments

--- ir/tests/program.rs
+++ ir/tests/program.rs
@@ -96,7 +96,7 @@ fn build_program_with_functions() {
     );
     conjunction
         .constraints_mut()
-        .add_function_binding(vec![var_count, var_mean], &function_signature, vec![var_person])
+        .add_function_binding(vec![var_count, var_mean], &function_signature, vec![var_person], "test_fn")
         .unwrap();
     let block = builder.finish();
     println!("{}", block.conjunction());
