ac8d414231859b16a7329c9ec6eb73035363b5ab
Krishnan Govindraj <krishnan@vaticle.com>
2024-10-18 17:30:02 +0100

    Fix up tests to build, but they don't pass

--- executor/tests/execute_comparison_check.rs
+++ executor/tests/execute_comparison_check.rs
@@ -21,6 +21,7 @@ use compiler::{
     },
     ExecutorVariable, VariablePosition,
 };
+use compiler::executable::match_::planner::function_plan::ExecutableFunctionRegistry;
 use concept::type_::{annotation::AnnotationIndependent, attribute_type::AttributeTypeAnnotation};
 use encoding::value::{label::Label, value::Value, value_type::ValueType};
 use executor::{
@@ -150,7 +151,7 @@ fn attribute_equality() {
 
     // Executor
     let snapshot = Arc::new(storage.clone().open_snapshot_read());
-    let executor = MatchExecutor::new(&executable, &snapshot, &thing_manager, MaybeOwnedRow::empty()).unwrap();
+    let executor = MatchExecutor::new(&executable, &snapshot, &thing_manager, MaybeOwnedRow::empty(), &ExecutableFunctionRegistry::empty()).unwrap();
 
     let context = ExecutionContext::new(snapshot, thing_manager, Arc::default());
     let iterator = executor.into_iterator(context, ExecutionInterrupt::new_uninterruptible());
