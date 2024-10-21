12487d90a5bed48eb2c0f47375819f9aa9738ecc
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2024-10-21 17:18:58 +0100

    0-width batch is only full when multiplicities run out
    This shouldn't happen in the first place if deduplicated

--- executor/batch.rs
+++ executor/batch.rs
@@ -53,7 +53,7 @@ impl FixedBatch {
     }
 
     pub(crate) fn is_full(&self) -> bool {
-        (self.entries * self.width) as usize == self.data.len()
+        self.entries < FIXED_BATCH_ROWS_MAX || self.width > 0 && (self.entries * self.width) as usize == self.data.len()
     }
 
     pub(crate) fn get_row(&self, index: u32) -> MaybeOwnedRow<'_> {
