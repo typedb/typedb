e2eff910e0e41f9e71ae652c8d9efb640fe452ce
Ganeshwara Hananda <ganesh@vaticle.com>
2024-11-13 14:53:35 +0000

    Refactor

--- system/repositories.rs
+++ system/repositories.rs
@@ -1,64 +1,54 @@
-use crate::concepts::{Credential, User};
-use database::transaction::{TransactionRead, TransactionWrite};
-use storage::durability_client::WALClient;
+pub mod user_repository {
+    use crate::concepts::User;
+    use database::transaction::{TransactionRead, TransactionWrite};
+    use storage::durability_client::WALClient;
 
-#[derive(Debug)]
-pub struct UserRepository {}
-
-impl UserRepository {
-    pub fn new() -> Self {
-        Self {}
-    }
-
-    pub fn list(&self, tx: &TransactionRead<WALClient>) -> Vec<User> {
+    pub fn list(tx: &TransactionRead<WALClient>) -> Vec<User> {
         println!("listing users");
         todo!()
     }
 
-    pub fn get(&self, tx: &TransactionRead<WALClient>, username: &str) -> Option<User> {
+    pub fn get(tx: &TransactionRead<WALClient>, username: &str) -> Option<User> {
         println!("getting user {}", username);
         todo!()
     }
 
-    pub fn create(&self, tx: &TransactionWrite<WALClient>, user: &User) {
+    pub fn create(tx: &TransactionWrite<WALClient>, user: &User) {
         println!("creating user {:?}", user);
         todo!()
     }
 
     pub fn update(
-        &self, tx: &TransactionWrite<WALClient>, username: &str, update: &User
+        tx: &TransactionWrite<WALClient>, username: &str, update: &User
     ) {
         todo!()
     }
 
-    pub fn delete(&self, tx: &TransactionWrite<WALClient>, username: &str) {
+    pub fn delete(tx: &TransactionWrite<WALClient>, username: &str) {
         println!("deleting user {}", username);
         todo!()
     }
 }
 
-#[derive(Debug)]
-pub struct CredentialRepository {}
-
-impl CredentialRepository {
-    pub fn new() -> Self {
-        Self {}
-    }
+pub mod credential_repository {
+    use crate::concepts::Credential;
+    use database::transaction::TransactionWrite;
+    use storage::durability_client::WALClient;
 
     pub fn create(
-        &self, tx: &TransactionWrite<WALClient>, username: &str, credential: &Credential
+        tx: &TransactionWrite<WALClient>, username: &str, credential: &Credential
     ) {
         println!("creating credential for user {}: {:?}", username, credential);
         todo!()
     }
 
     pub fn update(
-        &self, tx: &TransactionWrite<WALClient>, username: &str, credential: &Credential
+        tx: &TransactionWrite<WALClient>, username: &str, credential: &Credential
     ) {
         todo!()
     }
 
-    pub fn delete_by_user(&self, tx: &&TransactionWrite<WALClient>, username: &str) -> Vec<Credential> {
+    pub fn delete_by_user(tx: &&TransactionWrite<WALClient>, username: &str) -> Vec<Credential> {
         println!("deleting credentials for user {}", username);
         todo!()
     }
--- user/user_manager.rs
+++ user/user_manager.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 use system::concepts::{Credential, User};
-use system::repositories::{CredentialRepository, UserRepository};
+use system::repositories::{credential_repository, user_repository};
 use system::transaction_manager::TransactionManager;
 use crate::errors::{UserCreateError, UserDeleteError};
 use database::Database;
@@ -8,29 +8,25 @@ use storage::durability_client::WALClient;
 
 #[derive(Debug)]
 pub struct UserManager {
-    transaction_manager: TransactionManager,
-    user_repository: UserRepository,
-    credential_repository: CredentialRepository
+    transaction_manager: TransactionManager
 }
 
 impl UserManager {
     pub fn new(system_db: Arc<Database<WALClient>>) -> Self {
         UserManager {
             transaction_manager: TransactionManager::new(system_db.clone()),
-            user_repository: UserRepository::new(),
-            credential_repository: CredentialRepository::new()
         }
     }
 
     pub fn all(&self) -> Vec<User> {
         self.transaction_manager.read_transaction(|tx| {
-            self.user_repository.list(&tx)
+            user_repository::list(&tx)
         })
     }
 
     pub fn get(&self, name: &str) -> Option<User> {
         self.transaction_manager.read_transaction(|tx| {
-            self.user_repository.get(&tx, name)
+            user_repository::get(&tx, name)
         })
     }
 
@@ -40,15 +36,15 @@ impl UserManager {
 
     pub fn create(&self, user: &User, credential: &Credential) -> Result<(), UserCreateError> {
         self.transaction_manager.write_transaction(|tx| {
-            self.user_repository.create(&tx, &user);
-            self.credential_repository.create(&tx, &user.name, credential);
+            user_repository::create(&tx, &user);
+            credential_repository::create(&tx, &user.name, credential);
         }).map_err(|e| UserCreateError::Unexpected {})
     }
 
     pub fn delete(&self, username: &str) -> Result<(), UserDeleteError> {
         self.transaction_manager.write_transaction(|tx| {
-            self.user_repository.delete(&tx, username);
-            self.credential_repository.delete_by_user(&tx, username);
+            user_repository::delete(&tx, username);
+            credential_repository::delete_by_user(&tx, username);
         }).map_err(|_| UserDeleteError::Unexpected {})
     }
 }
