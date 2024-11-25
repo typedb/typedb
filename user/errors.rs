9aed2abf349a9ed471351f821e3a8fddc083a1c5
Ganeshwara Hananda <ganesh@vaticle.com>
2024-11-25 19:13:11 +0000

    Improve error message

--- user/errors.rs
+++ user/errors.rs
@@ -2,18 +2,18 @@ use error::typedb_error;
 
 typedb_error!(
     pub UserCreateError(component = "User create", prefix = "USC") {
-        Unexpected(1, "TODO: unexpected error"),
+        Unexpected(1, "An unexpected error has occurred in the process of creating a new user"),
     }
 );
 
 typedb_error!(
     pub UserUpdateError(component = "User update", prefix = "USC") {
-        Unexpected(1, "TODO: unexpected error"),
+        Unexpected(1, "An unexpected error has occurred in the process of updating a user"),
     }
 );
 
 typedb_error!(
     pub UserDeleteError(component = "User delete", prefix = "USC") {
-        Unexpected(1, "TODO: unexpected error"),
+        Unexpected(1, "An unexpected error has occurred in the process of deleting a user"),
     }
 );
