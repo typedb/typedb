b3e8591e7453bd04953791f4c4ef8f1e2771d751
Ganeshwara Hananda <ganesh@vaticle.com>
2024-11-27 20:18:08 +0000

    In-flight encryption

--- server/parameters/config.rs
+++ server/parameters/config.rs
@@ -28,14 +28,33 @@ impl Config {
             .map(|path| path.parent().unwrap().to_path_buf())
             .unwrap_or(std::env::current_dir().unwrap());
         Self {
-            server: ServerConfig { address: SocketAddr::from_str("0.0.0.0:1729").unwrap() },
+            server: ServerConfig {
+                address: SocketAddr::from_str("0.0.0.0:1729").unwrap(),
+                encryption: EncryptionConfig::disabled()
+            },
+            storage: StorageConfig { data: typedb_dir_or_current.join(PathBuf::from_str("server/data").unwrap()) },
+        }
+    }
+
+    pub fn new_with_encryption_config(encryption_config: EncryptionConfig) -> Self {
+        let typedb_dir_or_current = std::env::current_exe()
+            .map(|path| path.parent().unwrap().to_path_buf())
+            .unwrap_or(std::env::current_dir().unwrap());
+        Self {
+            server: ServerConfig {
+                address: SocketAddr::from_str("0.0.0.0:1729").unwrap(),
+                encryption: encryption_config
+            },
             storage: StorageConfig { data: typedb_dir_or_current.join(PathBuf::from_str("server/data").unwrap()) },
         }
     }
 
     pub fn new_with_data_directory(data_directory: &Path) -> Self {
         Self {
-            server: ServerConfig { address: SocketAddr::from_str("0.0.0.0:1729").unwrap() },
+            server: ServerConfig {
+                address: SocketAddr::from_str("0.0.0.0:1729").unwrap(),
+                encryption: EncryptionConfig::disabled()
+            },
             storage: StorageConfig { data: data_directory.to_owned() },
         }
     }
@@ -44,6 +63,29 @@ impl Config {
 #[derive(Debug)]
 pub(crate) struct ServerConfig {
     pub(crate) address: SocketAddr,
+    pub(crate) encryption: EncryptionConfig,
+}
+
+#[derive(Debug)]
+pub struct EncryptionConfig {
+    pub enabled: bool,
+    pub cert: Option<PathBuf>,
+    pub cert_key: Option<PathBuf>,
+    pub root_ca: Option<PathBuf>,
+}
+
+impl EncryptionConfig {
+    pub fn disabled() -> Self {
+        Self::new(false, None, None, None)
+    }
+
+    pub fn new(enabled: bool,
+        cert: Option<PathBuf>,
+        cert_key: Option<PathBuf>,
+        root_ca: Option<PathBuf>
+    ) -> Self {
+        Self { enabled, cert, cert_key, root_ca }
+    }
 }
 
 #[derive(Debug)]
--- server/parameters/mod.rs
+++ server/parameters/mod.rs
@@ -5,3 +5,4 @@
  */
 
 pub mod config;
+pub mod cli;
