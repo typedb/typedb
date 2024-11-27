2dcf5c9eff4d542045dd7467fc6d5eedafaf30ec
Ganeshwara Hananda <ganesh@vaticle.com>
2024-11-27 18:17:50 +0000

    TLS configuration - wip

--- server/parameters/config.rs
+++ server/parameters/config.rs
@@ -28,14 +28,20 @@ impl Config {
             .map(|path| path.parent().unwrap().to_path_buf())
             .unwrap_or(std::env::current_dir().unwrap());
         Self {
-            server: ServerConfig { address: SocketAddr::from_str("0.0.0.0:1729").unwrap() },
+            server: ServerConfig {
+                address: SocketAddr::from_str("0.0.0.0:1729").unwrap(),
+                encryption: EncryptionConfig::disabled()
+            },
             storage: StorageConfig { data: typedb_dir_or_current.join(PathBuf::from_str("server/data").unwrap()) },
         }
     }
 
     pub fn new_with_data_directory(data_directory: &Path) -> Self {
         Self {
-            server: ServerConfig { address: SocketAddr::from_str("0.0.0.0:1729").unwrap() },
+            server: ServerConfig {
+                address: SocketAddr::from_str("0.0.0.0:1729").unwrap(),
+                encryption: EncryptionConfig::disabled()
+            },
             storage: StorageConfig { data: data_directory.to_owned() },
         }
     }
@@ -44,6 +50,45 @@ impl Config {
 #[derive(Debug)]
 pub(crate) struct ServerConfig {
     pub(crate) address: SocketAddr,
+    pub(crate) encryption: EncryptionConfig,
+}
+
+
+#[derive(Debug)]
+pub(crate) struct EncryptionConfig {
+    pub(crate) enabled: bool,
+    pub(crate) cert: Option<PathBuf>,
+    pub(crate) cert_key: Option<PathBuf>,
+    pub(crate) root_ca: Option<PathBuf>,
+}
+
+impl EncryptionConfig {
+    pub(crate) fn disabled() -> Self {
+        Self {
+            enabled: false,
+            cert: None,
+            cert_key: None,
+            root_ca: None,
+        }
+    }
+
+    pub(crate) fn enabled(cert: PathBuf, cert_key: PathBuf) -> Self {
+        Self {
+            enabled: true,
+            cert: Some(cert),
+            cert_key: Some(cert_key),
+            root_ca: None,
+        }
+    }
+
+    pub(crate) fn enabled_with_custom_ca(cert: PathBuf, cert_key: PathBuf, root_ca: PathBuf) -> Self {
+        Self {
+            enabled: true,
+            cert: Some(cert),
+            cert_key: Some(cert_key),
+            root_ca: Some(root_ca),
+        }
+    }
 }
 
 #[derive(Debug)]
--- server/typedb.rs
+++ server/typedb.rs
@@ -15,6 +15,7 @@ use system::{
 use user::{initialise_default_user, user_manager::UserManager};
 
 use crate::{authenticator::Authenticator, parameters::config::Config, service::typedb_service::TypeDBService};
+use crate::parameters::config::EncryptionConfig;
 
 #[derive(Debug)]
 pub struct Server {
@@ -54,24 +55,28 @@ impl Server {
         let service = typedb_protocol::type_db_server::TypeDbServer::new(self.typedb_service.take().unwrap());
         println!("Ready!");
         let authenticator = Arc::new(Authenticator::new(self.user_manager.clone()));
-        tonic::transport::Server::builder()
-            .tls_config(Self::tls_config())?
-            .http2_keepalive_interval(Some(GRPC_CONNECTION_KEEPALIVE))
+        Self::create_tonic_server(&self.config.server.encryption)
             .layer(tonic::service::interceptor(move |req| authenticator.authenticate(req)))
             .add_service(service)
             .serve(self.config.server.address)
             .await
     }
 
-    fn tls_config() -> ServerTlsConfig {
-        let data_dir = std::path::PathBuf::from_iter(["tls-3"]);
-        let cert = std::fs::read_to_string(data_dir.join("cert.pem")).unwrap();
-        let key = std::fs::read_to_string(data_dir.join("cert.key")).unwrap();
-        let root_ca = std::fs::read_to_string(data_dir.join("root-ca.pem")).unwrap();
-        ServerTlsConfig::new()
-            .client_ca_root(Certificate::from_pem(root_ca))
-            .client_auth_optional(true)
-            .identity(Identity::from_pem(cert, key))
+    fn create_tonic_server(encryption_config: &EncryptionConfig) -> tonic::transport::Server {
+        let mut tonic_server = tonic::transport::Server::builder();
+        if encryption_config.enabled {
+            let cert = fs::read_to_string(encryption_config.cert.clone().unwrap()).unwrap();
+            let cert_key = fs::read_to_string(encryption_config.cert_key.clone().unwrap()).unwrap();
+            let mut tls_config = ServerTlsConfig::new().identity(Identity::from_pem(cert, cert_key));
+            if encryption_config.root_ca.is_some() {
+                let root_ca = fs::read_to_string(encryption_config.root_ca.clone().unwrap()).unwrap();
+                tls_config = tls_config
+                    .client_ca_root(Certificate::from_pem(root_ca))
+                    .client_auth_optional(true)
+            }
+            tonic_server = tonic_server.tls_config(tls_config).unwrap();
+        }
+        tonic_server.http2_keepalive_interval(Some(GRPC_CONNECTION_KEEPALIVE))
     }
 
     fn create_storage_directory(storage_directory: &PathBuf) -> Result<(), ServerOpenError> {
