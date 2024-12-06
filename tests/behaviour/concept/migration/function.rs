50e054d84f4579d954d9f5817e0d8d7fb6d52023
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2024-12-06 15:43:48 +0000

    fmt

--- tests/behaviour/concept/migration/function.rs
+++ tests/behaviour/concept/migration/function.rs
@@ -14,7 +14,7 @@ async fn test() {
     // @typedb_behaviour is stored in a directory that is a sibling to
     // the working directory.
     #[cfg(feature = "bazel")]
-    let path ="../typedb_behaviour/concept/migration/function.feature";
+    let path = "../typedb_behaviour/concept/migration/function.feature";
 
     #[cfg(not(feature = "bazel"))]
     let path = "bazel-typedb/external/typedb_behaviour/concept/migration/function.feature";
