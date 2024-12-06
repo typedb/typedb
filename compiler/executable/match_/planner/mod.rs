599ae7687c0a5788d5d565b9ff672ce6828640e6
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2024-12-06 09:43:36 +0000

    undo debug outlining

--- compiler/executable/match_/planner/mod.rs
+++ compiler/executable/match_/planner/mod.rs
@@ -50,7 +50,7 @@ pub fn compile(
     let assigned_identities =
         input_variables.iter().map(|(&var, &position)| (var, ExecutorVariable::RowPosition(position))).collect();
 
-    let plan = plan_conjunction(
+    plan_conjunction(
         conjunction,
         block_context,
         input_variables,
@@ -60,9 +60,7 @@ pub fn compile(
         statistics,
     )
     .lower(input_variables.keys().copied(), selected_variables.to_vec(), &assigned_identities, variable_registry)
-    .finish(variable_registry);
-
-    plan
+    .finish(variable_registry)
 }
 
 #[derive(Debug)]
