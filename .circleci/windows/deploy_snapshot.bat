5ad5a9b701c1587a3cda19655ca8ab1097366e6e
Krishnan Govindraj <krishnan@typedb.com>
2025-01-10 17:53:47 +0530

    WIP: Towards windows build through cargo

--- .circleci/windows/deploy_snapshot.bat
+++ .circleci/windows/deploy_snapshot.bat
@@ -3,8 +3,8 @@ REM This Source Code Form is subject to the terms of the Mozilla Public
 REM License, v. 2.0. If a copy of the MPL was not distributed with this
 REM file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-REM install dependencies needed for build
-git apply .circleci\windows\git.patch
-choco install .circleci\windows\dependencies.config  --limit-output --yes --no-progress
-
+REM build file
+cargo build --profile=release
+copy target\release\typedb_server_bin.exe  .\
+git
 
