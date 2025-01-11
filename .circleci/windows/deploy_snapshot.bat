899c002e803779d5756e35a66790fe4131234573
Krishnan Govindraj <krishnan@typedb.com>
2025-01-11 12:18:31 +0530

    Do we need refreshenv?

--- .circleci/windows/deploy_snapshot.bat
+++ .circleci/windows/deploy_snapshot.bat
@@ -3,6 +3,10 @@ REM This Source Code Form is subject to the terms of the Mozilla Public
 REM License, v. 2.0. If a copy of the MPL was not distributed with this
 REM file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+REM needs to be called such that software installed
+REM by Chocolatey in prepare.bat is accessible
+CALL refreshenv
+
 REM build file
 cargo build --profile=release
 copy target\release\typedb_server_bin.exe  .\
