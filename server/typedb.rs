50c3896f45007ce4547d68244e856d8f07f5f565
Georgii Novoselov <georgii@typedb.com>
2025-01-24 14:40:41 +0000

    Fix codestyle issues after review

--- server/typedb.rs
+++ server/typedb.rs
@@ -165,7 +165,8 @@ impl Server {
     }
 
     fn create_tonic_server(encryption_config: &EncryptionConfig) -> Result<tonic::transport::Server, ServerOpenError> {
-        let mut tonic_server = Self::configure_server_encryption(tonic::transport::Server::builder(), encryption_config)?;
+        let mut tonic_server =
+            Self::configure_server_encryption(tonic::transport::Server::builder(), encryption_config)?;
         Ok(tonic_server.http2_keepalive_interval(Some(GRPC_CONNECTION_KEEPALIVE)))
     }
 
@@ -177,30 +178,28 @@ impl Server {
             return Ok(server);
         }
 
-        let cert_path = encryption_config.cert.clone().ok_or_else(|| ServerOpenError::MissingTLSCertificate {})?;
-        let cert = fs::read_to_string(cert_path.clone()).map_err(|source| ServerOpenError::CouldNotReadTLSCertificate {
-            path: cert_path.to_string_lossy().to_string(),
+        let cert_path = encryption_config.cert.as_ref().ok_or_else(|| ServerOpenError::MissingTLSCertificate {})?;
+        let cert = fs::read_to_string(cert_path).map_err(|source| ServerOpenError::CouldNotReadTLSCertificate {
+            path: cert_path.display().to_string(),
             source: Arc::new(source),
         })?;
         let cert_key_path =
-            encryption_config.cert_key.clone().ok_or_else(|| ServerOpenError::MissingTLSCertificateKey {})?;
+            encryption_config.cert_key.as_ref().ok_or_else(|| ServerOpenError::MissingTLSCertificateKey {})?;
         let cert_key =
-            fs::read_to_string(cert_key_path.clone()).map_err(|source| ServerOpenError::CouldNotReadTLSCertificateKey {
-                path: cert_key_path.to_string_lossy().to_string(),
+            fs::read_to_string(cert_key_path).map_err(|source| ServerOpenError::CouldNotReadTLSCertificateKey {
+                path: cert_key_path.display().to_string(),
                 source: Arc::new(source),
             })?;
         let mut tls_config = ServerTlsConfig::new().identity(Identity::from_pem(cert, cert_key));
 
         if let Some(root_ca_path) = &encryption_config.root_ca {
             let root_ca = fs::read_to_string(root_ca_path).map_err(|source| ServerOpenError::CouldNotReadRootCA {
-                path: root_ca_path.to_string_lossy().to_string(),
+                path: root_ca_path.display().to_string(),
                 source: Arc::new(source),
             })?;
             tls_config = tls_config.client_ca_root(Certificate::from_pem(root_ca)).client_auth_optional(true);
         }
-        Ok(server
-            .tls_config(tls_config)
-            .map_err(|source| ServerOpenError::TLSConfigError { source: Arc::new(source) })?)
+        server.tls_config(tls_config).map_err(|source| ServerOpenError::TLSConfigError { source: Arc::new(source) })
     }
 
     fn create_storage_directory(storage_directory: &Path) -> Result<(), ServerOpenError> {
