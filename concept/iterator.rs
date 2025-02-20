fe8cab2c8eb25b9fcb80d0c8a39501fbff9e8000
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2025-02-20 17:34:12 +0000

    fix macro

--- concept/iterator.rs
+++ concept/iterator.rs
@@ -129,6 +129,7 @@ macro_rules! edge_iterator {
         impl Iterator for $name {
             type Item = Result<$mapped_type, Box<$crate::error::ConceptReadError>>;
             fn next(&mut self) -> Option<Self::Item> {
+                use ::lending_iterator::LendingIterator;
                 use $crate::error::ConceptReadError::SnapshotIterate;
                 self.snapshot_iterator.as_mut()?.next().map(|result| {
                     result
