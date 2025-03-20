f05e4d114c91bc99446332002ab69f836dc2c213
Ganeshwara Hananda <ganesh@vaticle.com>
2025-03-20 11:59:47 +0000

    Move methods from main to Server::open

--- main.rs
+++ main.rs
@@ -7,26 +7,24 @@
 #![deny(unused_must_use)]
 #![deny(elided_lifetimes_in_paths)]
 
-use std::{path::PathBuf, str::FromStr};
-
 use clap::Parser;
 use logger::initialise_logging_global;
 use resource::constants::server::{DISTRIBUTION, SENTRY_REPORTING_URI, VERSION};
 use sentry::ClientInitGuard as SentryGuard;
 use server::parameters::{
     cli::CLIArgs,
-    config::{Config, DiagnosticsConfig, EncryptionConfig, ServerConfig},
+    config::ServerConfig,
 };
 
 fn main() {
     setup_abort_on_panic();
     initialise_logging_global();
 
-    let cli_args = server::parameters::cli::CLIArgs::parse();
+    let args = CLIArgs::parse();
+    let config = args.to_config();
 
     let deployment_id = None;
-    let is_error_reporting_enabled = cli_args.diagnostics_reporting_errors;
-    let config = get_configuration(cli_args);
+    let is_error_reporting_enabled = args.diagnostics_reporting_errors;
     let _error_reporting_guard = setup_error_reporting(config.server_config(), is_error_reporting_enabled);
 
     tokio::runtime::Builder::new_multi_thread().enable_all().build().expect("Expected a main tokio runtime").block_on(
@@ -39,28 +37,6 @@ fn main() {
             }
         },
     );
-}
-
-fn get_configuration(cli_args: CLIArgs) -> Config {
-    let encryption_config = EncryptionConfig {
-        enabled: cli_args.server_encryption_enabled,
-        cert: cli_args.server_encryption_cert.map(|path| PathBuf::from_str(path.as_str()).unwrap()),
-        cert_key: cli_args.server_encryption_cert_key.map(|path| PathBuf::from_str(path.as_str()).unwrap()),
-        root_ca: cli_args.server_encryption_root_ca.map(|path| PathBuf::from_str(path.as_str()).unwrap()),
-    };
-    let diagnostics_config = DiagnosticsConfig {
-        is_monitoring_enabled: cli_args.diagnostics_monitoring_enable,
-        monitoring_port: cli_args.diagnostics_monitoring_port,
-        is_reporting_enabled: cli_args.diagnostics_reporting_metrics,
-    };
-    let data_dir = cli_args.storage_data.map(|dir| PathBuf::from_str(dir.as_str()).unwrap());
-    Config::customised(
-        cli_args.server_address,
-        Some(encryption_config),
-        Some(diagnostics_config),
-        data_dir,
-        cli_args.development_mode_enabled,
-    )
 }
 
 fn setup_abort_on_panic() {
