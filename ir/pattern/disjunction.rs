0f33ac414f835844f6917e3778b43de70b44e27a
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2025-03-20 12:47:51 +0000

    remove dbg

--- ir/pattern/disjunction.rs
+++ ir/pattern/disjunction.rs
@@ -47,9 +47,7 @@ impl Disjunction {
     }
 
     pub fn required_inputs(&self, block_context: &BlockContext) -> impl Iterator<Item = Variable> + '_ {
-        dbg!(self.variable_dependency(block_context))
-            .into_iter()
-            .filter_map(|(v, mode)| mode.is_required().then_some(v))
+        self.variable_dependency(block_context).into_iter().filter_map(|(v, mode)| mode.is_required().then_some(v))
     }
 
     pub(crate) fn variable_dependency(
