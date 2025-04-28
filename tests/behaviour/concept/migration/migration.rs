53a24512dc9e36b8d0ec7ab1fb04deaf87a065ac
Krishnan Govindraj <krishnan@typedb.com>
2025-04-28 15:41:02 +0530

    rename lib.rs -> main.rs; rename test methods so we can resolve

--- tests/behaviour/concept/migration/migration.rs
+++ tests/behaviour/concept/migration/migration.rs
@@ -7,7 +7,7 @@
 use steps::Context;
 
 #[tokio::test]
-async fn test() {
+async fn test_migration() {
     // Bazel specific path: when running the test in bazel, the external data from
     // @typedb_behaviour is stored in a directory that is a sibling to
     // the working directory.
