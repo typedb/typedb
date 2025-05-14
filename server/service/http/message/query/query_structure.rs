d51ac41c348a7a51ee7ff696ef3e61e0e4d19ecf
Krishnan Govindraj <krishnan@typedb.com>
2025-05-14 17:04:21 +0200

    Replay changes from grpc branch + rename kind to tag

--- server/service/http/message/query/query_structure.rs
+++ server/service/http/message/query/query_structure.rs
@@ -61,53 +61,101 @@ pub struct QueryStructureResponse {
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct QueryStructureBranchResponse {
-    edges: Vec<QueryStructureEdgeResponse>,
+    constraints: Vec<QueryStructureConstraintResponse>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase", tag = "kind", content = "param")]
-pub enum QueryStructureEdgeTypeResponse {
-    Isa,
-    Has,
-    Links(QueryStructureVertexResponse),
+#[serde(rename_all = "camelCase", tag = "tag")]
+pub enum QueryStructureConstraint {
+    Isa {
+        instance: QueryStructureVertexResponse,
+        r#type: QueryStructureVertexResponse,
+        exactness: QueryStructureConstraintExactness,
+    },
+    Has {
+        owner: QueryStructureVertexResponse,
+        attribute: QueryStructureVertexResponse,
+        exactness: QueryStructureConstraintExactness,
+    },
+    Links {
+        relation: QueryStructureVertexResponse,
+        player: QueryStructureVertexResponse,
+        role: QueryStructureVertexResponse,
+        exactness: QueryStructureConstraintExactness,
+    },
 
-    Sub,
-    Owns,
-    Relates,
-    Plays,
+    Sub {
+        subtype: QueryStructureVertexResponse,
+        supertype: QueryStructureVertexResponse,
+        exactness: QueryStructureConstraintExactness,
+    },
+    Owns {
+        owner: QueryStructureVertexResponse,
+        attribute: QueryStructureVertexResponse,
+        exactness: QueryStructureConstraintExactness,
+    },
+    Relates {
+        relation: QueryStructureVertexResponse,
+        role: QueryStructureVertexResponse,
+        exactness: QueryStructureConstraintExactness,
+    },
+    Plays {
+        player: QueryStructureVertexResponse,
+        role: QueryStructureVertexResponse,
+        exactness: QueryStructureConstraintExactness,
+    },
 
-    IsaExact,
-    SubExact,
+    FunctionCall {
+        name: String,
+        assigned: Vec<QueryStructureVertexResponse>,
+        arguments: Vec<QueryStructureVertexResponse>,
+    },
+    Expression {
+        text: String,
+        assigned: Vec<QueryStructureVertexResponse>,
+        arguments: Vec<QueryStructureVertexResponse>,
+    },
+}
 
-    Assigned(String),
-    Argument(String),
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub enum QueryStructureConstraintExactness {
+    Exact,
+    Subtypes,
 }
 
 #[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct QueryStructureEdgeSpan {
+struct QueryStructureConstraintSpan {
     begin: usize,
     end: usize,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct QueryStructureEdgeResponse {
-    r#type: QueryStructureEdgeTypeResponse,
-    from: QueryStructureVertexResponse,
-    to: QueryStructureVertexResponse,
-    span: Option<QueryStructureEdgeSpan>,
+pub struct QueryStructureConstraintResponse {
+    span: Option<QueryStructureConstraintSpan>,
+    #[serde(flatten)]
+    constraint: QueryStructureConstraint,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-#[serde(rename_all = "camelCase", tag = "kind", content = "value")]
+#[serde(rename_all = "camelCase", tag = "tag")]
 pub enum QueryStructureVertexResponse {
     Variable { variable: String },
-    Label(serde_json::Value),
+    Label { r#type: serde_json::Value },
     Value(ValueResponse),
     Expression { repr: String },
     FunctionCall { repr: String },
     UnavailableVariable { variable: String },
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct QueryStructureRolePlayerResponse {
+    player: QueryStructureVertexResponse,
+    role: QueryStructureVertexResponse,
+    span: Option<QueryStructureConstraintSpan>,
 }
 
 pub(crate) fn encode_query_structure(
@@ -134,139 +182,164 @@ fn encode_query_structure_branch(
     query_structure: &QueryStructure,
     branch: &[Constraint<Variable>],
 ) -> Result<QueryStructureBranchResponse, Box<ConceptReadError>> {
-    let mut edges = Vec::new();
+    let mut constraints = Vec::new();
     let role_names = branch
         .iter()
         .filter_map(|constraint| constraint.as_role_name())
         .map(|rolename| (rolename.type_().as_variable().unwrap(), rolename.name().to_owned()))
         .collect();
     let context = QueryStructureContext { query_structure, snapshot, type_manager, role_names };
-    branch
-        .iter()
-        .enumerate()
-        .try_for_each(|(index, constraint)| query_structure_edge(&context, constraint, &mut edges, index))?;
-    Ok(QueryStructureBranchResponse { edges })
+    branch.iter().enumerate().try_for_each(|(index, constraint)| {
+        query_structure_constraint(&context, constraint, &mut constraints, index)
+    })?;
+    Ok(QueryStructureBranchResponse { constraints })
 }
 
-macro_rules! push_edge {
-    ($edges:ident, $query_structure:expr, $span:expr, $from:expr, $to:expr, $variant:ident) => {{
-        let from = query_structure_vertex($query_structure, $from)?;
-        let to = query_structure_vertex($query_structure, $to)?;
-        $edges.push(QueryStructureEdgeResponse {
-            r#type: QueryStructureEdgeTypeResponse::$variant,
-            from,
-            to,
-            span: $span,
-        });
-    }};
-    ($edges:ident, $query_structure:expr, $span:expr, $from:expr, $to:expr, $variant:ident($param:expr)) => {{
-        let from = query_structure_vertex($query_structure, $from)?;
-        let to = query_structure_vertex($query_structure, $to)?;
-        $edges.push(QueryStructureEdgeResponse {
-            r#type: QueryStructureEdgeTypeResponse::$variant($param),
-            from,
-            to,
-            span: $span,
-        });
-    }};
-}
-
-fn query_structure_edge(
+fn query_structure_constraint(
     context: &QueryStructureContext<'_, impl ReadableSnapshot>,
     constraint: &Constraint<Variable>,
-    edges: &mut Vec<QueryStructureEdgeResponse>,
+    constraints: &mut Vec<QueryStructureConstraintResponse>,
     index: usize,
 ) -> Result<(), Box<ConceptReadError>> {
-    let span =
-        constraint.source_span().map(|span| QueryStructureEdgeSpan { begin: span.begin_offset, end: span.end_offset });
+    let span = constraint
+        .source_span()
+        .map(|span| QueryStructureConstraintSpan { begin: span.begin_offset, end: span.end_offset });
     match constraint {
         Constraint::Links(links) => {
-            let role_type = query_structure_role_type_as_vertex(context, links.role_type())?;
-            push_edge!(edges, context, span, links.relation(), links.player(), Links(role_type))
+            constraints.push(QueryStructureConstraintResponse {
+                span,
+                constraint: QueryStructureConstraint::Links {
+                    relation: query_structure_vertex(context, links.relation())?,
+                    player: query_structure_vertex(context, links.player())?,
+                    role: query_structure_role_type_as_vertex(context, links.role_type())?,
+                    exactness: QueryStructureConstraintExactness::Subtypes,
+                },
+            });
         }
-        Constraint::Has(has) => push_edge!(edges, context, span, has.owner(), has.attribute(), Has),
-        Constraint::Isa(isa) => match isa.isa_kind() {
-            IsaKind::Exact => push_edge!(edges, context, span, isa.thing(), isa.type_(), IsaExact),
-            IsaKind::Subtype => push_edge!(edges, context, span, isa.thing(), isa.type_(), Isa),
-        },
-        Constraint::Sub(sub) => match sub.sub_kind() {
-            SubKind::Exact => push_edge!(edges, context, span, sub.subtype(), sub.supertype(), SubExact),
-            SubKind::Subtype => push_edge!(edges, context, span, sub.subtype(), sub.supertype(), Sub),
-        },
-        Constraint::Owns(owns) => push_edge!(edges, context, span, owns.owner(), owns.attribute(), Owns),
+        Constraint::Has(has) => {
+            constraints.push(QueryStructureConstraintResponse {
+                span,
+                constraint: QueryStructureConstraint::Has {
+                    owner: query_structure_vertex(context, has.owner())?,
+                    attribute: query_structure_vertex(context, has.attribute())?,
+                    exactness: QueryStructureConstraintExactness::Subtypes,
+                },
+            });
+        }
+        Constraint::Isa(isa) => {
+            let exactness = match isa.isa_kind() {
+                IsaKind::Exact => QueryStructureConstraintExactness::Exact,
+                IsaKind::Subtype => QueryStructureConstraintExactness::Subtypes,
+            };
+            constraints.push(QueryStructureConstraintResponse {
+                span,
+                constraint: QueryStructureConstraint::Isa {
+                    instance: query_structure_vertex(context, isa.thing())?,
+                    r#type: query_structure_vertex(context, isa.type_())?,
+                    exactness,
+                },
+            })
+        }
+        Constraint::Sub(sub) => {
+            let exactness = match sub.sub_kind() {
+                SubKind::Exact => QueryStructureConstraintExactness::Exact,
+                SubKind::Subtype => QueryStructureConstraintExactness::Subtypes,
+            };
+            constraints.push(QueryStructureConstraintResponse {
+                span,
+                constraint: QueryStructureConstraint::Sub {
+                    subtype: query_structure_vertex(context, sub.subtype())?,
+                    supertype: query_structure_vertex(context, sub.supertype())?,
+                    exactness,
+                },
+            })
+        }
+        Constraint::Owns(owns) => constraints.push(QueryStructureConstraintResponse {
+            span,
+            constraint: QueryStructureConstraint::Owns {
+                owner: query_structure_vertex(context, owns.owner())?,
+                attribute: query_structure_vertex(context, owns.attribute())?,
+                exactness: QueryStructureConstraintExactness::Subtypes,
+            },
+        }),
         Constraint::Relates(relates) => {
-            push_edge!(edges, context, span, relates.relation(), relates.role_type(), Relates)
+            constraints.push(QueryStructureConstraintResponse {
+                span,
+                constraint: QueryStructureConstraint::Relates {
+                    relation: query_structure_vertex(context, relates.relation())?,
+                    role: query_structure_vertex(context, relates.role_type())?,
+                    exactness: QueryStructureConstraintExactness::Subtypes,
+                },
+            });
         }
-        Constraint::Plays(plays) => push_edge!(edges, context, span, plays.player(), plays.role_type(), Plays),
+        Constraint::Plays(plays) => {
+            constraints.push(QueryStructureConstraintResponse {
+                span,
+                constraint: QueryStructureConstraint::Plays {
+                    player: query_structure_vertex(context, plays.player())?,
+                    role: query_structure_vertex(context, plays.role_type())?,
+                    exactness: QueryStructureConstraintExactness::Subtypes,
+                },
+            });
+        }
         Constraint::IndexedRelation(indexed) => {
-            let role_type_1 = query_structure_role_type_as_vertex(context, indexed.role_type_1())?;
-            let role_type_2 = query_structure_role_type_as_vertex(context, indexed.role_type_2())?;
             let span_1 = indexed
                 .source_span_1()
-                .map(|span| QueryStructureEdgeSpan { begin: span.begin_offset, end: span.end_offset });
+                .map(|span| QueryStructureConstraintSpan { begin: span.begin_offset, end: span.end_offset });
             let span_2 = indexed
                 .source_span_2()
-                .map(|span| QueryStructureEdgeSpan { begin: span.begin_offset, end: span.end_offset });
-            push_edge!(edges, context, span_1, indexed.relation(), indexed.player_1(), Links(role_type_1));
-            push_edge!(edges, context, span_2, indexed.relation(), indexed.player_2(), Links(role_type_2));
+                .map(|span| QueryStructureConstraintSpan { begin: span.begin_offset, end: span.end_offset });
+            constraints.push(QueryStructureConstraintResponse {
+                span: span_1,
+                constraint: QueryStructureConstraint::Links {
+                    relation: query_structure_vertex(context, indexed.relation())?,
+                    player: query_structure_vertex(context, indexed.player_1())?,
+                    role: query_structure_role_type_as_vertex(context, indexed.role_type_1())?,
+                    exactness: QueryStructureConstraintExactness::Subtypes,
+                },
+            });
+            constraints.push(QueryStructureConstraintResponse {
+                span: span_2,
+                constraint: QueryStructureConstraint::Links {
+                    relation: query_structure_vertex(context, indexed.relation())?,
+                    player: query_structure_vertex(context, indexed.player_2())?,
+                    role: query_structure_role_type_as_vertex(context, indexed.role_type_2())?,
+                    exactness: QueryStructureConstraintExactness::Subtypes,
+                },
+            });
         }
         Constraint::ExpressionBinding(expr) => {
-            let repr =
+            let text =
                 context.get_call_syntax(constraint).map_or_else(|| format!("Expression#{index}"), |text| text.clone());
-            let expr_vertex = QueryStructureVertexResponse::Expression { repr };
-            expr.ids_assigned().try_for_each(|variable| {
-                let assigned = query_structure_vertex(context, &Vertex::Variable(variable))?;
-                let assigned_name = context.get_variable_name(&variable).unwrap_or_else(|| variable.to_string());
-                let edge_type = QueryStructureEdgeTypeResponse::Assigned(assigned_name);
-                edges.push(QueryStructureEdgeResponse {
-                    r#type: edge_type,
-                    from: expr_vertex.clone(),
-                    to: assigned,
-                    span,
-                });
-                Ok::<_, Box<ConceptReadError>>(())
-            })?;
-            expr.required_ids().try_for_each(|variable| {
-                let argument = query_structure_vertex(context, &Vertex::Variable(variable))?;
-                let arg_name = context.get_variable_name(&variable).unwrap_or_else(|| variable.to_string());
-                let edge_type = QueryStructureEdgeTypeResponse::Argument(arg_name);
-                edges.push(QueryStructureEdgeResponse {
-                    r#type: edge_type,
-                    from: argument,
-                    to: expr_vertex.clone(),
-                    span,
-                });
-                Ok::<_, Box<ConceptReadError>>(())
-            })?;
+            let assigned = expr
+                .ids_assigned()
+                .map(|variable| query_structure_vertex(context, &Vertex::Variable(variable)))
+                .collect::<Result<Vec<_>, _>>()?;
+            let arguments = expr
+                .required_ids()
+                .map(|variable| query_structure_vertex(context, &Vertex::Variable(variable)))
+                .collect::<Result<Vec<_>, _>>()?;
+            constraints.push(QueryStructureConstraintResponse {
+                span,
+                constraint: QueryStructureConstraint::Expression { text, assigned, arguments },
+            });
         }
         Constraint::FunctionCallBinding(function_call) => {
-            let repr =
+            let text =
                 context.get_call_syntax(constraint).map_or_else(|| format!("Function#{index}"), |text| text.clone());
-            let func_vertex = QueryStructureVertexResponse::FunctionCall { repr };
-            function_call.ids_assigned().try_for_each(|variable| {
-                let assigned = query_structure_vertex(context, &Vertex::Variable(variable))?;
-                let assigned_name = context.get_variable_name(&variable).unwrap_or_else(|| variable.to_string());
-                let edge_type = QueryStructureEdgeTypeResponse::Assigned(assigned_name);
-                edges.push(QueryStructureEdgeResponse {
-                    r#type: edge_type,
-                    from: func_vertex.clone(),
-                    to: assigned,
-                    span,
-                });
-                Ok::<_, Box<ConceptReadError>>(())
-            })?;
-            function_call.required_ids().try_for_each(|variable| {
-                let argument = query_structure_vertex(context, &Vertex::Variable(variable))?;
-                let arg_name = context.get_variable_name(&variable).unwrap_or_else(|| variable.to_string());
-                let edge_type = QueryStructureEdgeTypeResponse::Argument(arg_name);
-                edges.push(QueryStructureEdgeResponse {
-                    r#type: edge_type,
-                    from: argument,
-                    to: func_vertex.clone(),
-                    span,
-                });
-                Ok::<_, Box<ConceptReadError>>(())
-            })?;
+            let assigned = function_call
+                .ids_assigned()
+                .map(|variable| query_structure_vertex(context, &Vertex::Variable(variable)))
+                .collect::<Result<Vec<_>, _>>()?;
+            let arguments = function_call
+                .function_call()
+                .argument_ids()
+                .map(|variable| query_structure_vertex(context, &Vertex::Variable(variable)))
+                .collect::<Result<Vec<_>, _>>()?;
+            constraints.push(QueryStructureConstraintResponse {
+                span,
+                constraint: QueryStructureConstraint::FunctionCall { name: text, assigned, arguments },
+            });
         }
         | Constraint::Comparison(_) => {}
         Constraint::RoleName(_) => {} // Handled separately via resolved_role_names
@@ -298,11 +371,13 @@ fn query_structure_vertex(
         }
         Vertex::Label(label) => {
             let type_ = context.get_type(label).unwrap();
-            QueryStructureVertexResponse::Label(serde_json::json!(encode_type_concept(
-                &type_,
-                context.snapshot,
-                context.type_manager
-            )?))
+            QueryStructureVertexResponse::Label {
+                r#type: serde_json::json!(encode_type_concept(
+                    &type_,
+                    context.snapshot,
+                    context.type_manager
+                )?)
+            }
         }
         Vertex::Parameter(param) => {
             let value = context.get_parameter_value(param).unwrap();
@@ -318,7 +393,7 @@ fn query_structure_role_type_as_vertex(
 ) -> Result<QueryStructureVertexResponse, Box<ConceptReadError>> {
     if let Some(label) = context.get_role_type(&role_type.as_variable().unwrap()) {
         // At present rolename could resolve to multiple types - Manually encode.
-        Ok(QueryStructureVertexResponse::Label(serde_json::json!(RoleTypeResponse { label: label.to_owned() })))
+        Ok(QueryStructureVertexResponse::Label { r#type: serde_json::json!(RoleTypeResponse { label: label.to_owned() }) })
     } else {
         query_structure_vertex(context, role_type)
     }
