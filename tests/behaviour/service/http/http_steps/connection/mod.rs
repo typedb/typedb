3e1be6b11335d7348ce7384da65ed2c537dacaf4
Ganeshwara Hananda <ganesh@vaticle.com>
2025-05-22 17:06:24 +0100

    Fix formatting with rustfmt

--- tests/behaviour/service/http/http_steps/connection/mod.rs
+++ tests/behaviour/service/http/http_steps/connection/mod.rs
@@ -28,11 +28,7 @@ mod user;
 
 const GRPC_ADDRESS: &str = "0.0.0.0:1729";
 const HTTP_ADDRESS: &str = "0.0.0.0:8000";
-const SERVER_INFO: ServerInfo = ServerInfo {
-    logo: "logo",
-    distribution: "TypeDB CE TEST",
-    version: "0.0.0"
-};
+const SERVER_INFO: ServerInfo = ServerInfo { logo: "logo", distribution: "TypeDB CE TEST", version: "0.0.0" };
 
 pub(crate) async fn start_typedb(
 ) -> (tokio::sync::watch::Sender<()>, std::thread::JoinHandle<Result<(), ServerOpenError>>) {
