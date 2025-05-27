1501efab888ef9075ed2c629927d6837a1266631
Ganeshwara Hananda <ganesh@vaticle.com>
2025-05-27 18:30:54 +0100

    ServerBuilder - wip

--- server/server.rs
+++ server/server.rs
@@ -16,13 +16,45 @@ use tokio::{
     net::lookup_host,
     sync::watch::{Receiver, Sender},
 };
-
+use tokio::sync::watch::channel;
 use crate::{
     error::ServerOpenError,
     parameters::config::{Config, EncryptionConfig},
     service::{grpc, http},
     state::{BoxServerState, LocalServerState},
 };
+
+pub struct ServerBuilder {
+    server_info: Option<ServerInfo>,
+    shutdown_channel: Option<(Sender<()>, Receiver<()>)>,
+}
+
+impl ServerBuilder {
+    pub fn server_info(mut self, server_info: ServerInfo) -> Self {
+        self.server_info = Some(server_info);
+        self
+    }
+    
+    pub fn shutdown_channel(mut self, shutdown_channel: (Sender<()>, Receiver<()>)) -> Self {
+        self.shutdown_channel = Some(shutdown_channel);
+        self
+    }
+    
+    pub fn build(self, config: Config, server_state: BoxServerState) -> Server {
+        let server_info = self.server_info.unwrap_or(SERVER_INFO);
+        let (shutdown_sender, shutdown_receiver) = self.shutdown_channel
+            .unwrap_or_else(|| { channel(()) });
+        Server::new(server_info, config, Arc::new(server_state), shutdown_sender, shutdown_receiver)
+    }
+    
+    pub async fn build_core(self, config: Config) -> Result<Server, ServerOpenError> {
+        let server_info = self.server_info.unwrap_or(SERVER_INFO);
+        let (shutdown_sender, shutdown_receiver) = self.shutdown_channel
+            .unwrap_or_else(|| { channel(()) });
+        let server_state = Box::new(LocalServerState::new(SERVER_INFO, config.clone(), None, shutdown_receiver.clone()).await?);
+        Ok(Server::new(server_info, config, Arc::new(server_state), shutdown_sender, shutdown_receiver))
+    }
+}
 
 #[derive(Debug)]
 pub struct Server {
