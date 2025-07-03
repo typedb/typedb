d6437f8dc05e36181d369ce5cf2a2a01ce643f16
Krishnan Govindraj <4918770+krishnangovindraj@users.noreply.github.com>
2025-07-03 16:40:15 +0530

    Simplify PartialCostHash and beam search implementation (#7495)
    ## Product change and motivation
    We simplify the hash used to detect redundant states during the plan-search, loosening the definition of equivalent plans for more aggressive pruning.
    
    ## Implementation
    We update the `PartialCostHash` to have a stronger definition of 
    Since we want to choose the plan with the lowest cost amongst all plans corresponding to the same state, we convert `new_plans_heap` to a min_heap so the first one we see is the best one.

--- compiler/executable/match_/planner/plan.rs
+++ compiler/executable/match_/planner/plan.rs
@@ -6,7 +6,7 @@
 
 use std::{
     any::type_name_of_val,
-    cmp::Ordering,
+    cmp::{Ordering, Reverse},
     collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet},
     fmt,
     hash::{DefaultHasher, Hash, Hasher},
@@ -669,11 +669,12 @@ impl<'a> ConjunctionPlanBuilder<'a> {
         ));
 
         let mut extension_heap = BinaryHeap::with_capacity(extension_width); // reused
+        let mut new_plans_heap = BinaryHeap::with_capacity(beam_width);
+        let mut new_plans_hashset = HashSet::with_capacity(beam_width);
         for i in 0..num_patterns {
             event!(Level::TRACE, "{INDENT:4}PLANNER STEP {}", i);
-            let mut new_plans_heap = BinaryHeap::with_capacity(beam_width);
-            let mut new_plans_hashset = HashSet::with_capacity(beam_width);
 
+            // TODO: Do we need this?
             if i % BEAM_REDUCTION_CYCLE == 0 {
                 beam_width = usize::max(beam_width.saturating_sub(1), 2);
             }
@@ -681,6 +682,7 @@ impl<'a> ConjunctionPlanBuilder<'a> {
                 extension_width = usize::max(extension_width.saturating_sub(1), 2);
             } // Narrow the beam until it greedy at the tail (for large queries)
 
+            new_plans_heap.clear();
             for plan in best_partial_plans.drain(..) {
                 event!(
                     Level::TRACE,
@@ -693,87 +695,32 @@ impl<'a> ConjunctionPlanBuilder<'a> {
                     plan.cumulative_cost.chain(plan.ongoing_step_cost),
                     plan.heuristic
                 );
-
+                debug_assert!(extension_heap.is_empty());
+                // Add best k extensions from this plan to new_plan_heap (k = extension_width)
                 for extension in plan.extensions_iter(&self.graph)? {
                     if extension.is_trivial(&self.graph) {
-                        event!(
-                            Level::TRACE,
-                            "{INDENT:12}Stash {:?} = {} <-- cost: {:?} heuristic: {:?}",
-                            extension.pattern_id,
-                            self.graph.elements[&VertexId::Pattern(extension.pattern_id)],
-                            extension.step_cost.cost,
-                            extension.heuristic
-                        );
-                        let mut plan = plan.clone();
-                        plan.add_to_stash(extension.pattern_id, &self.graph);
-                        if new_plans_heap.len() < beam_width {
-                            new_plans_heap.push(plan);
-                        } else if let Some(top) = new_plans_heap.peek() {
-                            if plan < *top {
-                                new_plans_heap.pop();
-                                new_plans_heap.push(plan);
-                            }
-                        }
                         extension_heap.clear();
+                        extension_heap.push(Reverse(extension));
                         break;
-                    }
-
-                    if extension_heap.len() < extension_width {
-                        extension_heap.push(extension);
-                    } else if let Some(top) = extension_heap.peek() {
-                        if extension < *top {
-                            extension_heap.pop();
-                            extension_heap.push(extension);
-                        }
+                    } else {
+                        extension_heap.push(Reverse(extension));
                     }
                 }
-
-                for extension in extension_heap.drain() {
-                    event!(
-                        Level::TRACE,
-                        "{INDENT:12}Choice {:?} = {} <-- join: {:?}, cost: {:?}, heuristic: {:?} metadata: {:?}",
-                        extension.pattern_id,
-                        self.graph.elements[&VertexId::Pattern(extension.pattern_id)],
-                        extension
-                            .step_join_var
-                            .map(|v| self.graph.elements[&VertexId::Variable(v)].as_variable().unwrap().variable()),
-                        extension.step_cost,
-                        extension.heuristic,
-                        extension.pattern_metadata
-                    );
-                    let new_plan = if !extension.is_constraint(&self.graph) {
-                        plan.clone_and_extend_with_new_step(extension, &self.graph)
-                    } else if extension.step_join_var.is_some()
-                        && (plan.ongoing_step_join_var.is_none()
-                            || plan.ongoing_step_join_var == extension.step_join_var)
-                    {
-                        plan.clone_and_extend_with_continued_step(extension, &self.graph)
-                    } else {
-                        plan.clone_and_extend_with_new_step(extension, &self.graph)
-                    };
-
-                    let new_plan_hash = new_plan.hash();
-                    if !new_plans_hashset.contains(&new_plan_hash) {
-                        if new_plans_heap.len() < beam_width {
-                            new_plans_heap.push(new_plan);
-                            new_plans_hashset.insert(new_plan_hash);
-                            event!(Level::TRACE, "{INDENT:16}(added)");
-                        } else if let Some(top) = new_plans_heap.peek() {
-                            if new_plan < *top {
-                                new_plans_heap.pop();
-                                new_plans_heap.push(new_plan);
-                                new_plans_hashset.insert(new_plan_hash);
-                                event!(Level::TRACE, "{INDENT:16}(added)");
-                            } else {
-                                event!(Level::TRACE, "{INDENT:16}(discarded)");
-                            }
-                        }
-                    } else {
-                        event!(Level::TRACE, "{INDENT:16}(hash collision)");
+                for Reverse(extension) in drain_sorted(&mut extension_heap).take(extension_width) {
+                    new_plans_heap.push(Reverse(plan.extend_with(&self.graph, extension)));
+                }
+            }
+            // Pick best (k = beam_width) plans to beam.
+            debug_assert!(best_partial_plans.is_empty());
+            new_plans_hashset.clear();
+            for Reverse(plan) in drain_sorted(&mut new_plans_heap) {
+                if new_plans_hashset.insert(plan.hash()) {
+                    best_partial_plans.push(plan);
+                    if best_partial_plans.len() >= beam_width {
+                        break;
                     }
                 }
             }
-            best_partial_plans = new_plans_heap.into_vec();
         }
 
         let best_plan =
@@ -807,6 +754,28 @@ impl<'a> ConjunctionPlanBuilder<'a> {
             element_to_order,
             planner_statistics,
         })
+    }
+}
+
+struct DrainSorted<'a, T: Ord> {
+    heap: &'a mut BinaryHeap<T>,
+}
+
+fn drain_sorted<T: Ord>(heap: &mut BinaryHeap<T>) -> impl Iterator<Item = T> + '_ {
+    DrainSorted { heap }
+}
+
+impl<'a, T: Ord> Iterator for DrainSorted<'a, T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.heap.pop()
+    }
+}
+
+impl<'a, T: Ord> Drop for DrainSorted<'a, T> {
+    fn drop(&mut self) {
+        self.heap.clear();
     }
 }
 
@@ -981,6 +950,45 @@ impl PartialCostPlan {
                 })
             })
             .collect::<Result<Vec<_>, _>>()
+    }
+
+    pub(crate) fn extend_with(&self, graph: &Graph<'_>, extension: StepExtension) -> PartialCostPlan {
+        const INDENT: &str = "";
+        if extension.is_trivial(graph) {
+            event!(
+                Level::TRACE,
+                "{INDENT:12}Stash {:?} = {} <-- cost: {:?} heuristic: {:?}",
+                extension.pattern_id,
+                graph.elements[&VertexId::Pattern(extension.pattern_id)],
+                extension.step_cost.cost,
+                extension.heuristic
+            );
+            let mut new_plan = self.clone();
+            new_plan.add_to_stash(extension.pattern_id, graph);
+            new_plan
+        } else {
+            event!(
+                Level::TRACE,
+                "{INDENT:12}Choice {:?} = {} <-- join: {:?}, cost: {:?}, heuristic: {:?} metadata: {:?}",
+                extension.pattern_id,
+                graph.elements[&VertexId::Pattern(extension.pattern_id)],
+                extension
+                    .step_join_var
+                    .map(|v| graph.elements[&VertexId::Variable(v)].as_variable().unwrap().variable()),
+                extension.step_cost,
+                extension.heuristic,
+                extension.pattern_metadata
+            );
+            if !extension.is_constraint(graph) {
+                self.clone_and_extend_with_new_step(extension, graph)
+            } else if extension.step_join_var.is_some()
+                && (self.ongoing_step_join_var.is_none() || self.ongoing_step_join_var == extension.step_join_var)
+            {
+                self.clone_and_extend_with_continued_step(extension, graph)
+            } else {
+                self.clone_and_extend_with_new_step(extension, graph)
+            }
+        }
     }
 
     fn determine_joinability(&self, graph: &Graph<'_>, pattern: PatternVertexId) -> Option<VariableVertexId> {
@@ -1206,13 +1214,12 @@ impl PartialCostPlan {
         }
     }
 
-    fn hash(&self) -> PartialCostHash {
-        PartialCostHash {
-            all_vars: self.remaining_patterns.clone(),
-            ongoing_vars: self.ongoing_step.clone(),
-            approx_io: (self.cumulative_cost.io_ratio * self.ongoing_step_cost.io_ratio) as u64, // TODO: improve rounding/hashing (make relative)
-            approx_cost: self.cumulative_cost.chain(self.ongoing_step_cost).cost as u64, // TODO: improve rounding/hashing (make relative)
-            ongoing_join: self.ongoing_step_join_var.is_some(),
+    fn hash(&self) -> PartialPlanHash {
+        PartialPlanHash {
+            n_remaining_patterns: self.remaining_patterns.len() as u32,
+            planned_patterns: self.vertex_ordering.iter().filter_map(|v| v.as_pattern_id()).collect::<BTreeSet<_>>(),
+            ongoing_step_join_var: self.ongoing_step_join_var,
+            ongoing_non_trivial_patterns: self.ongoing_step.iter().copied().collect::<BTreeSet<_>>(),
         }
     }
 }
@@ -1231,35 +1238,12 @@ impl Ord for PartialCostPlan {
     }
 }
 
-#[derive(Clone, PartialEq, Eq)]
-pub(super) struct PartialCostHash {
-    all_vars: HashSet<PatternVertexId>,
-    ongoing_vars: HashSet<PatternVertexId>,
-    approx_io: u64,
-    approx_cost: u64,
-    ongoing_join: bool,
-}
-
-impl Hash for PartialCostHash {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        let mut acc = 0;
-        for pattern in &self.all_vars {
-            let mut hasher = DefaultHasher::new();
-            pattern.hash(&mut hasher);
-            acc ^= hasher.finish();
-        }
-        acc.hash(state);
-        let mut acc = 0;
-        for pattern in &self.ongoing_vars {
-            let mut hasher = DefaultHasher::new();
-            pattern.hash(&mut hasher);
-            acc ^= hasher.finish();
-        }
-        acc.hash(state);
-        self.approx_io.hash(state);
-        self.approx_cost.hash(state);
-        self.ongoing_join.hash(state);
-    }
+#[derive(Clone, Hash, PartialEq, Eq)]
+pub(super) struct PartialPlanHash {
+    n_remaining_patterns: u32, // Needed for continuous search (A*), but not step-based (beam)
+    planned_patterns: BTreeSet<PatternVertexId>,
+    ongoing_non_trivial_patterns: BTreeSet<PatternVertexId>,
+    ongoing_step_join_var: Option<VariableVertexId>,
 }
 
 #[derive(Clone, PartialEq, Debug)]
