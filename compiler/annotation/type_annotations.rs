016e92f1adc7ad85c2c56dc80c35e36941563202
Krishnan Govindraj <4918770+krishnangovindraj@users.noreply.github.com>
2025-07-09 22:13:57 +0530

    Separate out optional & negation type inference (#7520)
    ## Product change and motivation
    Since negated & optional patterns cannot influence the type-annotations of the containing conjunction, we need not interleave their type-inference  with that of the parent conjunction. Hence, We refactor the type-inference procedure so that type-inference on a conjunction & its disjunctions is completed before we begin the process on negated & optional subpatterns. 
    
    ## Implementation
    * Rip out `nested_negations` & `nested_optionals` from `TypeInferenceGraph`, the type-seeding process, and the pruning process.
    * For brevity,  disjunctions means a root conjunction and all its nested disjunctions (recursively). negations means any negation (or optional) in the root conjunction or any of its nested disjunctions.  negations may contain more disjunctions.
      - infer_types used to do seeding, pruning & validation of disjunctions & negations interleaved.
      - Now infer_types is first done on all the disjunctions, followed by the negations.
      - This makes the logic easier to follow & makes it obvious that negations don't influence disjunctions.
     * At the end of type-inference of a conjunction, we recurse into all subpatterns and copy annotations for any optional variables into their parent's annotations.

--- compiler/annotation/type_annotations.rs
+++ compiler/annotation/type_annotations.rs
@@ -65,6 +65,10 @@ impl TypeAnnotations {
         self.vertex.get(vertex)
     }
 
+    pub(super) fn vertex_annotations_mut(&mut self) -> &mut BTreeMap<Vertex<Variable>, Arc<BTreeSet<Type>>> {
+        &mut self.vertex
+    }
+
     pub fn constraint_annotations(&self) -> &HashMap<Constraint<Variable>, ConstraintTypeAnnotations> {
         &self.constraints
     }
