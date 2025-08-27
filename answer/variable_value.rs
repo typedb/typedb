55ed584932f4c8d9f96e63935dd47210aaa9bbb2
Krishnan Govindraj <4918770+krishnangovindraj@users.noreply.github.com>
2025-08-27 15:10:19 +0200

    Support inserting attributes from values of other attributes  (#7559)
    ## Product change and motivation
    Support inserting attributes from values of other attributes 
    
    ```typeql
    match $x isa src-attr;
    insert $y isa dst-attr == $x;
    ```
    
    fixes #7551

--- answer/variable_value.rs
+++ answer/variable_value.rs
@@ -47,7 +47,6 @@ impl<'a> VariableValue<'a> {
     pub fn as_value(&self) -> &Value<'a> {
         match self {
             VariableValue::Value(value) => value,
-            // TODO: Do we want to implicit cast from attributes?
             _ => panic!("VariableValue is not a value: {:?}", self),
         }
     }
