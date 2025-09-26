2d61617f3ac890a56367b26c82bd76e631f45647
Georgii Novoselov <georgii@typedb.com>
2025-09-26 15:47:54 +0100

    Reenable Sentry crash reporting

--- main.rs
+++ main.rs
@@ -12,6 +12,7 @@ use std::path::PathBuf;
 use clap::Parser;
 use logger::initialise_logging_global;
 use resource::constants::server::{DEFAULT_CONFIG_PATH, SENTRY_REPORTING_URI, SERVER_INFO};
+use sentry::ClientInitGuard as SentryGuard;
 use server::{
     parameters::{
         cli::CLIArgs,
@@ -32,7 +33,9 @@ fn main() {
     config_builder.override_with_cliargs(cli_args);
     let config = config_builder.build().expect("Error validating config file overridden with cli args");
     initialise_logging_global(&config.logging.directory);
-    may_initialise_error_reporting(&config);
+    // This guard sends Sentry reports when it's dropped. It has to be alive while the server is running.
+    // DO NOT delete!
+    let _guard = may_initialise_error_reporting(&config);
     create_tokio_runtime().block_on(async {
         let server = ServerBuilder::default().server_info(SERVER_INFO).build(config).await.unwrap();
         match server.serve().await {
@@ -52,13 +55,15 @@ fn initialise_abort_on_panic() {
     });
 }
 
-fn may_initialise_error_reporting(config: &Config) {
+fn may_initialise_error_reporting(config: &Config) -> Option<SentryGuard> {
     if config.diagnostics.reporting.report_errors && !config.development_mode.enabled {
-        let opts = (
+        let options = (
             SENTRY_REPORTING_URI,
             sentry::ClientOptions { release: Some(SERVER_INFO.version.into()), ..Default::default() },
         );
-        let _ = sentry::init(opts);
+        Some(sentry::init(options))
+    } else {
+        None
     }
 }
 
