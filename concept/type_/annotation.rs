typedb/typedb

commit 5337bf5d4722db45896f65fc76aa5ae76c39af08

Remove server domain-leak in the main function (#7391)
## Release notes: product changes
- The main and server module is refactored for better maintainability

## Motivation
- The `main` function contain functionalities that is supposed to be in
the `server` module

## Implementation
- Minimise domain leak by moving functionalities into the `Server`
struct appropriately. The `main` function is made to only contain code
that is not server-specific such as panic handler, logging, and Sentry
error reporting. Diagnostics initialisation and printing TypeDB logo is
now moved into Server
- Tidy up naming and method placement in Server, such that methods that
belongs to a particular domain sits next to one another

main.rs
@@ -7,34 +7,28 @@
 #![deny(unused_must_use)]
 #![deny(elided_lifetimes_in_paths)]
 
-use std::{path::PathBuf, str::FromStr};
-
 use clap::Parser;
 use logger::initialise_logging_global;
-use resource::constants::server::{ASCII_LOGO, DISTRIBUTION, SENTRY_REPORTING_URI, VERSION};
-use sentry::ClientInitGuard as SentryGuard;
-use server::parameters::{
-    cli::CLIArgs,
-    config::{Config, DiagnosticsConfig, EncryptionConfig, ServerConfig},
-};
+use resource::constants::server::{DISTRIBUTION, SENTRY_REPORTING_URI, VERSION};
+use server::parameters::config::Config;
+use server::parameters::cli::CLIArgs;
+use server::server::Server;
+use tokio::runtime::Runtime;
 
 fn main() {
-    setup_abort_on_panic();
+    let config = CLIArgs::parse().to_config();
+    initialise_abort_on_panic();
     initialise_logging_global();
-
-    let cli_args = server::parameters::cli::CLIArgs::parse();
-    print_ascii_logo(); // very important
-
-    let deployment_id = None;
-    let is_error_reporting_enabled = cli_args.diagnostics_reporting_errors;
-    let config = get_configuration(cli_args);
-    let _error_reporting_guard = setup_error_reporting(config.server_config(), is_error_reporting_enabled);
-
-    tokio::runtime::Builder::new_multi_thread().enable_all().build().expect("Expected a main tokio runtime").block_on(
+    may_initialise_error_reporting(&config);
+    create_tokio_runtime().block_on(
         async {
-            let open_result = server::typedb::Server::open(config, DISTRIBUTION, VERSION, deployment_id).await;
-            let result = open_result.unwrap().serve().await;
-            match result {
+            let server = Server::create(
+                config,
+                DISTRIBUTION,
+                VERSION,
+                None
+            ).await.unwrap();
+            match server.serve().await {
                 Ok(_) => println!("Exited."),
                 Err(err) => println!("Exited with error: {:?}", err),
             }
@@ -42,33 +36,7 @@ fn main() {
     );
 }
 
-fn get_configuration(cli_args: CLIArgs) -> Config {
-    let encryption_config = EncryptionConfig {
-        enabled: cli_args.server_encryption_enabled,
-        cert: cli_args.server_encryption_cert.map(|path| PathBuf::from_str(path.as_str()).unwrap()),
-        cert_key: cli_args.server_encryption_cert_key.map(|path| PathBuf::from_str(path.as_str()).unwrap()),
-        root_ca: cli_args.server_encryption_root_ca.map(|path| PathBuf::from_str(path.as_str()).unwrap()),
-    };
-    let diagnostics_config = DiagnosticsConfig {
-        is_monitoring_enabled: cli_args.diagnostics_monitoring_enable,
-        monitoring_port: cli_args.diagnostics_monitoring_port,
-        is_reporting_enabled: cli_args.diagnostics_reporting_metrics,
-    };
-    let data_dir = cli_args.storage_data.map(|dir| PathBuf::from_str(dir.as_str()).unwrap());
-    Config::customised(
-        cli_args.server_address,
-        Some(encryption_config),
-        Some(diagnostics_config),
-        data_dir,
-        cli_args.development_mode_enabled,
-    )
-}
-
-fn print_ascii_logo() {
-    println!("{ASCII_LOGO}");
-}
-
-fn setup_abort_on_panic() {
+fn initialise_abort_on_panic() {
     std::panic::set_hook({
         let default_panic = std::panic::take_hook();
         Box::new(move |info| {
@@ -78,13 +46,19 @@ fn setup_abort_on_panic() {
     });
 }
 
-fn setup_error_reporting(server_config: &ServerConfig, is_error_reporting_enabled: bool) -> Option<SentryGuard> {
-    if is_error_reporting_enabled && !server_config.is_development_mode {
-        Some(sentry::init((
+fn may_initialise_error_reporting(config: &Config) {
+    if config.diagnostics.is_reporting_error_enabled && !config.server_config().is_development_mode {
+        let opts = (
             SENTRY_REPORTING_URI,
             sentry::ClientOptions { release: Some(VERSION.into()), ..Default::default() },
-        )))
-    } else {
-        None
+        );
+        let _ = sentry::init(opts);
     }
+}
+
+fn create_tokio_runtime() -> Runtime {
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .expect("Expected a main tokio runtime")
 }

server/error.rs
@@ -1,0 +1,29 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+use std::net::SocketAddr;
+use std::sync::Arc;
+use error::typedb_error;
+use database::DatabaseOpenError;
+use std::io;
+
+typedb_error! {
+    pub ServerOpenError(component = "Server open", prefix = "SRO") {
+        NotADirectory(1, "Invalid path '{path}': not a directory.", path: String),
+        CouldNotReadServerIDFile(2, "Could not read data from server ID file '{path}'.", path: String, source: Arc<io::Error>),
+        CouldNotCreateServerIDFile(3, "Could not write data to server ID file '{path}'.", path: String, source: Arc<io::Error>),
+        CouldNotCreateDataDirectory(4, "Could not create data directory in '{path}'.", path: String, source: Arc<io::Error>),
+        InvalidServerID(5, "Server ID read from '{path}' is invalid. Delete the corrupted file and try again.", path: String),
+        DatabaseOpen(6, "Could not open database.", typedb_source: DatabaseOpenError),
+        Serve(7, "Could not serve on {address}.", address: SocketAddr, source: Arc<tonic::transport::Error>),
+        MissingTLSCertificate(8, "TLS certificate path must be specified when encryption is enabled."),
+        MissingTLSCertificateKey(9, "TLS certificate key path must be specified when encryption is enabled."),
+        CouldNotReadTLSCertificate(10, "Could not read TLS certificate from '{path}'.", path: String, source: Arc<io::Error>),
+        CouldNotReadTLSCertificateKey(11, "Could not read TLS certificate key from '{path}'.", path: String, source: Arc<io::Error>),
+        CouldNotReadRootCA(12, "Could not read root CA from '{path}'.", path: String, source: Arc<io::Error>),
+        TLSConfigError(13, "Failed to configure TLS.", source: Arc<tonic::transport::Error>),
+    }
+}

server/lib.rs
@@ -11,4 +11,5 @@ mod authenticator;
 pub mod authenticator_cache;
 pub mod parameters;
 mod service;
-pub mod typedb;
+pub mod server;
+mod error;

server/parameters/cli.rs
@@ -4,9 +4,11 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 use std::net::SocketAddr;
-
+use std::str::FromStr;
+use std::path::PathBuf;
 use clap::{ArgAction, Parser};
 use resource::constants::server::{MONITORING_DEFAULT_PORT, VERSION};
+use crate::parameters::config::{Config, DiagnosticsConfig, EncryptionConfig};
 
 /// TypeDB CE usage
 #[derive(Parser, Debug)]
@@ -58,4 +60,30 @@ pub struct CLIArgs {
     /// logging, restricted functionalities, and reduced performance
     #[arg(long = "development-mode.enabled", hide = true)]
     pub development_mode_enabled: bool,
+}
+
+impl CLIArgs {
+    pub fn to_config(&self) -> Config {
+        let encryption_config = EncryptionConfig {
+            enabled: self.server_encryption_enabled,
+            cert: self.server_encryption_cert.clone().map(|path| PathBuf::from_str(path.as_str()).unwrap()),
+            cert_key: self.server_encryption_cert_key.clone().map(|path| PathBuf::from_str(path.as_str()).unwrap()),
+            root_ca: self.server_encryption_root_ca.clone().map(|path| PathBuf::from_str(path.as_str()).unwrap()),
+        };
+        let diagnostics_config = DiagnosticsConfig {
+            is_reporting_error_enabled: self.diagnostics_reporting_errors,
+            is_reporting_metric_enabled: self.diagnostics_reporting_metrics,
+            is_monitoring_enabled: self.diagnostics_monitoring_enable,
+            monitoring_port: self.diagnostics_monitoring_port,
+        };
+        let data_dir = self.storage_data.clone().map(|dir| PathBuf::from_str(dir.as_str()).unwrap());
+        Config::customised(
+            self.server_address.clone(),
+            Some(encryption_config),
+            Some(diagnostics_config),
+            data_dir,
+            self.development_mode_enabled,
+        )
+    }
+
 }

server/parameters/config.rs
@@ -15,7 +15,8 @@ use tokio::net::lookup_host;
 
 #[derive(Debug)]
 pub struct Config {
-    pub(crate) server: ServerConfig,
+    pub server: ServerConfig,
+    pub diagnostics: DiagnosticsConfig,
     pub(crate) storage: StorageConfig,
 }
 
@@ -34,9 +35,9 @@ impl Config {
             server: ServerConfig {
                 address: DEFAULT_ADDRESS.to_string(),
                 encryption: EncryptionConfig::default(),
-                diagnostics: DiagnosticsConfig::default(),
                 is_development_mode: ServerConfig::IS_DEVELOPMENT_MODE_FORCED,
             },
+            diagnostics: DiagnosticsConfig::default(),
             storage: StorageConfig { data: typedb_dir_or_current.join(PathBuf::from_str(DEFAULT_DATA_DIR).unwrap()) },
         }
     }
@@ -74,9 +75,9 @@ impl Config {
             server: ServerConfig {
                 address: server_address,
                 encryption: encryption_config,
-                diagnostics: diagnostics_config,
                 is_development_mode,
             },
+            diagnostics: diagnostics_config,
             storage: StorageConfig { data: data_directory.to_owned() },
         }
     }
@@ -90,8 +91,6 @@ impl Config {
 pub struct ServerConfig {
     pub(crate) address: String,
     pub(crate) encryption: EncryptionConfig,
-    pub(crate) diagnostics: DiagnosticsConfig,
-
     pub is_development_mode: bool,
 }
 
@@ -129,14 +128,20 @@ pub(crate) struct StorageConfig {
 
 #[derive(Debug)]
 pub struct DiagnosticsConfig {
+    pub is_reporting_error_enabled: bool,
+    pub is_reporting_metric_enabled: bool,
     pub is_monitoring_enabled: bool,
     pub monitoring_port: u16,
-    pub is_reporting_enabled: bool,
 }
 
 impl DiagnosticsConfig {
     pub fn enabled() -> Self {
-        Self { is_monitoring_enabled: true, monitoring_port: MONITORING_DEFAULT_PORT, is_reporting_enabled: true }
+        Self {
+            is_reporting_error_enabled: true,
+            is_reporting_metric_enabled: true,
+            is_monitoring_enabled: true,
+            monitoring_port: MONITORING_DEFAULT_PORT,
+        }
     }
 }
 

server/server.rs
@@ -1,0 +1,307 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+use std::{
+    fs,
+    net::SocketAddr,
+    path::{Path, PathBuf},
+    sync::Arc,
+};
+
+use concurrency::IntervalRunner;
+use database::database_manager::DatabaseManager;
+use diagnostics::{diagnostics_manager::DiagnosticsManager, Diagnostics};
+use rand::seq::SliceRandom;
+use resource::constants::server::{
+    DATABASE_METRICS_UPDATE_INTERVAL, GRPC_CONNECTION_KEEPALIVE, SERVER_ID_ALPHABET, SERVER_ID_FILE_NAME,
+    SERVER_ID_LENGTH,
+};
+use system::initialise_system_database;
+use tokio::net::lookup_host;
+use tonic::transport::{Certificate, Identity, ServerTlsConfig};
+use user::{initialise_default_user, user_manager::UserManager};
+
+use crate::{
+    authenticator::Authenticator,
+    authenticator_cache::AuthenticatorCache,
+    parameters::config::{Config, DiagnosticsConfig, EncryptionConfig},
+    service::typedb_service::TypeDBService,
+};
+use resource::constants::server::ASCII_LOGO;
+use crate::error::ServerOpenError;
+
+#[derive(Debug)]
+pub struct Server {
+    id: String,
+    deployment_id: String,
+    distribution: &'static str,
+    version: &'static str,
+    config: Config,
+    address: SocketAddr,
+    data_directory: PathBuf,
+    diagnostics_manager: Arc<DiagnosticsManager>,
+    database_diagnostics_updater: IntervalRunner,
+    user_manager: Arc<UserManager>,
+    authenticator_cache: Arc<AuthenticatorCache>,
+    typedb_service: Option<TypeDBService>,
+    shutdown_sender: tokio::sync::watch::Sender<()>,
+}
+
+impl Server {
+    pub async fn create(
+        config: Config,
+        distribution: &'static str,
+        version: &'static str,
+        deployment_id: Option<String>,
+    ) -> Result<Self, ServerOpenError> {
+        let storage_directory = &config.storage.data;
+        let server_config = &config.server;
+        let diagnostics_config = &config.diagnostics;
+
+        Self::may_initialise_storage_directory(storage_directory)?;
+
+        let server_id = Self::may_initialise_server_id(storage_directory)?;
+
+        let deployment_id = deployment_id.unwrap_or(server_id.clone());
+
+        let server_address = Self::resolve_address(server_config.address.clone()).await;
+
+        let diagnostics_manager = Arc::new(
+            Self::initialise_diagnostics(
+                deployment_id.clone(),
+                server_id.clone(),
+                distribution,
+                version,
+                &diagnostics_config,
+                storage_directory.clone(),
+                server_config.is_development_mode,
+            ).await
+        );
+
+        let database_manager = DatabaseManager::new(storage_directory)
+            .map_err(|typedb_source| ServerOpenError::DatabaseOpen { typedb_source })?;
+        let system_database = initialise_system_database(&database_manager);
+
+        let user_manager = Arc::new(UserManager::new(system_database));
+        initialise_default_user(&user_manager);
+
+        let authenticator_cache = Arc::new(AuthenticatorCache::new());
+
+        let (shutdown_sender, shutdown_receiver) = tokio::sync::watch::channel(());
+
+        let typedb_service = TypeDBService::new(
+            &server_address,
+            database_manager.clone(),
+            user_manager.clone(),
+            authenticator_cache.clone(),
+            diagnostics_manager.clone(),
+            shutdown_receiver,
+        );
+
+        Ok(Self {
+            id: server_id,
+            deployment_id,
+            distribution,
+            version,
+            address: server_address,
+            data_directory: storage_directory.to_owned(),
+            diagnostics_manager: diagnostics_manager.clone(),
+            database_diagnostics_updater: IntervalRunner::new(
+                move || Self::synchronize_database_metrics(diagnostics_manager.clone(), database_manager.clone()),
+                DATABASE_METRICS_UPDATE_INTERVAL,
+            ),
+            user_manager,
+            authenticator_cache,
+            typedb_service: Some(typedb_service),
+            shutdown_sender,
+            config,
+        })
+    }
+
+    fn may_initialise_server_id(storage_directory: &Path) -> Result<String, ServerOpenError> {
+        let server_id_file = storage_directory.join(SERVER_ID_FILE_NAME);
+        if server_id_file.exists() {
+            let server_id = fs::read_to_string(&server_id_file)
+                .map_err(|source| ServerOpenError::CouldNotReadServerIDFile {
+                    path: server_id_file.to_str().unwrap_or("").to_owned(),
+                    source: Arc::new(source),
+                })?
+                .trim()
+                .to_owned();
+            if server_id.is_empty() {
+                Err(ServerOpenError::InvalidServerID { path: server_id_file.to_str().unwrap_or("").to_owned() })
+            } else {
+                Ok(server_id)
+            }
+        } else {
+            let server_id = Self::generate_server_id();
+            assert!(!server_id.is_empty(), "Generated server ID should not be empty");
+            fs::write(server_id_file.clone(), &server_id).map_err(|source| {
+                ServerOpenError::CouldNotCreateServerIDFile {
+                    path: server_id_file.to_str().unwrap_or("").to_owned(),
+                    source: Arc::new(source),
+                }
+            })?;
+            Ok(server_id)
+        }
+    }
+
+    fn generate_server_id() -> String {
+        let mut rng = rand::thread_rng();
+        (0..SERVER_ID_LENGTH).map(|_| SERVER_ID_ALPHABET.choose(&mut rng).unwrap()).collect()
+    }
+
+    fn may_initialise_storage_directory(storage_directory: &Path) -> Result<(), ServerOpenError> {
+        if !storage_directory.exists() {
+            Self::create_storage_directory(storage_directory)
+        } else if !storage_directory.is_dir() {
+            Err(ServerOpenError::NotADirectory { path: storage_directory.to_str().unwrap_or("").to_owned() })
+        } else {
+            Ok(())
+        }
+    }
+
+    fn create_storage_directory(storage_directory: &Path) -> Result<(), ServerOpenError> {
+        fs::create_dir_all(storage_directory).map_err(|source| ServerOpenError::CouldNotCreateDataDirectory {
+            path: storage_directory.to_str().unwrap_or("").to_owned(),
+            source: Arc::new(source),
+        })?;
+        Ok(())
+    }
+
+    async fn initialise_diagnostics(
+        deployment_id: String,
+        server_id: String,
+        distribution: &'static str,
+        version: &'static str,
+        config: &DiagnosticsConfig,
+        storage_directory: PathBuf,
+        is_development_mode: bool,
+    ) -> DiagnosticsManager {
+        let diagnostics = Diagnostics::new(
+            deployment_id,
+            server_id,
+            distribution.to_owned(),
+            version.to_owned(),
+            storage_directory,
+            config.is_reporting_metric_enabled,
+        );
+        let diagnostics_manager = DiagnosticsManager::new(diagnostics, config.monitoring_port, config.is_monitoring_enabled, is_development_mode);
+        diagnostics_manager.may_start_monitoring().await;
+        diagnostics_manager.may_start_reporting().await;
+
+        diagnostics_manager
+    }
+
+    fn synchronize_database_metrics(diagnostics_manager: Arc<DiagnosticsManager>, database_manager: Arc<DatabaseManager>) {
+        let metrics = database_manager
+            .databases()
+            .values()
+            .filter(|database| DatabaseManager::is_user_database(database.name()))
+            .map(|database| database.get_metrics())
+            .collect();
+        diagnostics_manager.submit_database_metrics(metrics);
+    }
+
+    pub async fn serve(mut self) -> Result<(), ServerOpenError> {
+        let service = typedb_protocol::type_db_server::TypeDbServer::new(self.typedb_service.take().unwrap());
+        let authenticator = Authenticator::new(
+            self.user_manager.clone(),
+            self.authenticator_cache.clone(),
+            self.diagnostics_manager.clone(),
+        );
+
+        Self::print_hello(self.distribution, self.version, self.config.server.is_development_mode);
+
+        Self::create_tonic_server(&self.config.server.encryption)?
+            .layer(&authenticator)
+            .add_service(service)
+            .serve_with_shutdown(self.address, async {
+                // The tonic server starts a shutdown process when this closure execution finishes
+                Self::shutdown_handler(self.shutdown_sender).await;
+            })
+            .await
+            .map_err(|source| ServerOpenError::Serve { address: self.address, source: Arc::new(source) })
+    }
+
+    fn create_tonic_server(encryption_config: &EncryptionConfig) -> Result<tonic::transport::Server, ServerOpenError> {
+        let mut tonic_server =
+            Self::configure_tonic_server_encryption(tonic::transport::Server::builder(), encryption_config)?;
+        Ok(tonic_server.http2_keepalive_interval(Some(GRPC_CONNECTION_KEEPALIVE)))
+    }
+
+    fn configure_tonic_server_encryption(
+        server: tonic::transport::Server,
+        encryption_config: &EncryptionConfig,
+    ) -> Result<tonic::transport::Server, ServerOpenError> {
+        if !encryption_config.enabled {
+            return Ok(server);
+        }
+
+        let cert_path = encryption_config.cert.as_ref().ok_or_else(|| ServerOpenError::MissingTLSCertificate {})?;
+        let cert = fs::read_to_string(cert_path).map_err(|source| ServerOpenError::CouldNotReadTLSCertificate {
+            path: cert_path.display().to_string(),
+            source: Arc::new(source),
+        })?;
+        let cert_key_path =
+            encryption_config.cert_key.as_ref().ok_or_else(|| ServerOpenError::MissingTLSCertificateKey {})?;
+        let cert_key =
+            fs::read_to_string(cert_key_path).map_err(|source| ServerOpenError::CouldNotReadTLSCertificateKey {
+                path: cert_key_path.display().to_string(),
+                source: Arc::new(source),
+            })?;
+        let mut tls_config = ServerTlsConfig::new().identity(Identity::from_pem(cert, cert_key));
+
+        if let Some(root_ca_path) = &encryption_config.root_ca {
+            let root_ca = fs::read_to_string(root_ca_path).map_err(|source| ServerOpenError::CouldNotReadRootCA {
+                path: root_ca_path.display().to_string(),
+                source: Arc::new(source),
+            })?;
+            tls_config = tls_config.client_ca_root(Certificate::from_pem(root_ca)).client_auth_optional(true);
+        }
+        server.tls_config(tls_config).map_err(|source| ServerOpenError::TLSConfigError { source: Arc::new(source) })
+    }
+
+    async fn resolve_address(address: String) -> SocketAddr {
+        lookup_host(address.clone())
+            .await
+            .unwrap()
+            .next()
+            .unwrap_or_else(|| panic!("Unable to map address '{}' to any IP addresses", address))
+    }
+
+    fn print_hello(distribution: &'static str, version: &'static str, is_development_mode_enabled: bool) {
+        println!("{ASCII_LOGO}"); // very important
+        if is_development_mode_enabled {
+            println!("Running {distribution} {version} in development mode.");
+        } else {
+            println!("Running {distribution} {version}.");
+        }
+        println!("Ready!");
+    }
+
+    async fn shutdown_handler(shutdown_signal_sender: tokio::sync::watch::Sender<()>) {
+        Self::listen_to_ctrl_c_signal().await;
+        println!("\nReceived CTRL-C. Initiating shutdown...");
+        shutdown_signal_sender.send(()).expect("Expected a successful shutdown signal");
+
+        tokio::spawn(Self::forced_shutdown_handler());
+    }
+
+    async fn forced_shutdown_handler() {
+        Self::listen_to_ctrl_c_signal().await;
+        println!("\nReceived CTRL-C. Forcing shutdown...");
+        std::process::exit(1);
+    }
+
+    async fn listen_to_ctrl_c_signal() {
+        tokio::signal::ctrl_c().await.expect("Failed to listen for CTRL-C signal");
+    }
+
+    pub fn database_manager(&self) -> &DatabaseManager {
+        self.typedb_service.as_ref().unwrap().database_manager()
+    }
+}

commit fdc866369d40d41a497fe2c9bbc587b5c8381e7a

Reenable Sentry crash reporting (#7590)
## Product change and motivation
Fix a bug when TypeDB Server did not send crash reports for diagnostics
even when `--diagnostics.reporting.errors` was enabled.

## Implementation
Fixes the bug introduced in https://github.com/typedb/typedb/pull/7391.

Preserve the ownership of the Sentry's `Guard` until the `main` function
is exited. To avoid the same mistake, we encapsulate all the guards and
other things required to remain alive while the App is running inside a
new struct called `ServerApplication`.

concept/type_/annotation.rs
@@ -21,7 +21,7 @@ use encoding::{
     value::{value::Value, value_type::ValueType, ValueEncodable},
 };
 use error::typedb_error;
-use regex::{Regex};
+use regex::Regex;
 use resource::constants::snapshot::BUFFER_VALUE_INLINE;
 use serde::{Deserialize, Serialize};
 
@@ -247,7 +247,7 @@ impl AnnotationRegex {
         if !self.regex.is_empty() {
             match Regex::new(&self.regex) {
                 Ok(_) => Ok(()),
-                Err(err) => Err(err)
+                Err(err) => Err(err),
             }
         } else {
             Err(regex::Error::Syntax(String::from("Unexpected empty regex")))

concept/type_/type_manager/validation/operation_time_validation.rs
@@ -1576,7 +1576,7 @@ impl OperationTimeValidation {
     pub(crate) fn validate_regex_arguments(regex: AnnotationRegex) -> Result<(), Box<SchemaValidationError>> {
         match regex.validate() {
             Ok(_) => Ok(()),
-            Err(err) => Err(Box::new(SchemaValidationError::InvalidRegexArguments { regex, source: err})),
+            Err(err) => Err(Box::new(SchemaValidationError::InvalidRegexArguments { regex, source: err })),
         }
     }
 

executor/read/stream_modifier.rs
@@ -274,9 +274,7 @@ impl LastMapper {
 impl StreamModifierResultMapperTrait for LastMapper {
     fn map_output(&mut self, subquery_result: Option<FixedBatch>) -> Option<FixedBatch> {
         if let Some(input_batch) = subquery_result {
-            self.last_row = (!input_batch.is_empty()).then(|| {
-                input_batch.get_row(input_batch.len() - 1).into_owned()
-            });
+            self.last_row = (!input_batch.is_empty()).then(|| input_batch.get_row(input_batch.len() - 1).into_owned());
             Some(FixedBatch::EMPTY) // Retry this instruction without returning any rows
         } else {
             self.last_row.take().map(FixedBatch::from)

main.rs
@@ -12,6 +12,7 @@ use std::path::PathBuf;
 use clap::Parser;
 use logger::initialise_logging_global;
 use resource::constants::server::{DEFAULT_CONFIG_PATH, SENTRY_REPORTING_URI, SERVER_INFO};
+use sentry::ClientInitGuard as SentryGuard;
 use server::{
     parameters::{
         cli::CLIArgs,
@@ -32,14 +33,32 @@ fn main() {
     config_builder.override_with_cliargs(cli_args);
     let config = config_builder.build().expect("Error validating config file overridden with cli args");
     initialise_logging_global(&config.logging.directory);
-    may_initialise_error_reporting(&config);
-    create_tokio_runtime().block_on(async {
-        let server = ServerBuilder::default().server_info(SERVER_INFO).build(config).await.unwrap();
-        match server.serve().await {
-            Ok(_) => println!("Exited."),
-            Err(err) => println!("Exited with error: {:?}", err),
-        }
-    });
+
+    ServerApplication::new(config).run()
+}
+
+struct ServerApplication {
+    runtime: Runtime,
+    config: Config,
+    // This guard sends Sentry reports when it's dropped. It has to be alive for the lifetime of the app.
+    _error_reporting_guard: Option<SentryGuard>,
+}
+
+impl ServerApplication {
+    fn new(config: Config) -> Self {
+        let error_reporting_guard = may_initialise_error_reporting(&config);
+        Self { config, runtime: create_tokio_runtime(), _error_reporting_guard: error_reporting_guard }
+    }
+
+    fn run(self) {
+        self.runtime.block_on(async {
+            let server = ServerBuilder::default().server_info(SERVER_INFO).build(self.config).await.unwrap();
+            match server.serve().await {
+                Ok(_) => println!("Exited."),
+                Err(err) => println!("Exited with error: {:?}", err),
+            }
+        })
+    }
 }
 
 fn initialise_abort_on_panic() {
@@ -52,13 +71,15 @@ fn initialise_abort_on_panic() {
     });
 }
 
-fn may_initialise_error_reporting(config: &Config) {
+fn may_initialise_error_reporting(config: &Config) -> Option<SentryGuard> {
     if config.diagnostics.reporting.report_errors && !config.development_mode.enabled {
-        let opts = (
+        let options = (
             SENTRY_REPORTING_URI,
             sentry::ClientOptions { release: Some(SERVER_INFO.version.into()), ..Default::default() },
         );
-        let _ = sentry::init(opts);
+        Some(sentry::init(options))
+    } else {
+        None
     }
 }
 