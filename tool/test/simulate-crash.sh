fe73575f8f2351ee79d813437072370416a7139e
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2025-12-16 19:44:39 +0000

    add 10 min timeout

--- tool/test/simulate-crash.sh
+++ tool/test/simulate-crash.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-tar -xf 
+TIMEOUT=600 # 10 minutes
+START=$(date +%s)
 
 while true; do
+    END=$(date +%s)
+    ELAPSED=$((END-START))
+    if [[ $ELAPSED -ge $TIMEOUT ]]; then exit 0; fi
+
     cid=$(docker run --detach bazel:assemble-linux-x86_64)
     sleep 10 # give time to start up
     typedb-all-linux-x86_64/typedb console --username=admin --password=password --address=localhost:1729 --tls-disabled --script=tests/assembly/script.tql
