12981ee13f9b8d38121316b17791df45c94e3caa
joshua <joshua@typedb.com>
2026-06-01 11:54:00 -0400

    WIP

--- encoding/layout/prefix.rs
+++ encoding/layout/prefix.rs
@@ -20,7 +20,7 @@ impl PrefixID {
 }
 
 macro_rules! make_prefix_enum {
-    ($($name:ident => $byte:literal = $hex:literal, $fixed_width_keys:literal);*) => {
+    ($($name:ident => $byte:literal = $hex:literal, $width:???, $domain:???);*) => {
         // assert that $byte and $hex are the same literal
         $(const _: [(); $byte] = [(); $hex];)*
 
@@ -54,12 +54,30 @@ macro_rules! make_prefix_enum {
             /// Note: two different prefixes with fixed width are not necessarily the same fixed widths!
             ///
            pub const fn fixed_width_keys(&self) -> bool {
-                match self {
-                    $(Self::$name => $fixed_width_keys,)*
-                }
+                let width = match self {
+                    $(Self::$name => $width,)*
+                };
+                matches!(width, Width::Fixed)
+            }
+
+            pub const fn is_schema(&self) -> bool {
+                let domain = match self {
+                    $(Self::$name => $domain,)*
+                };
+                matches!(domain, MetaDomain::Schema)
             }
         }
     };
+}
+
+enum Width {
+    Fixed,
+    Variable
+}
+
+enum MetaDomain {
+    Schema,
+    Data
 }
 
 impl Prefix {
@@ -82,41 +100,41 @@ impl Prefix {
 
 make_prefix_enum! {
     // Reserved: 0-9 = 0x00-0x09
-    VertexEntityType => 10 = 0x0A, true;
-    VertexRelationType => 11 = 0x0B, true;
-    VertexAttributeType => 12 = 0x0C, true;
-    VertexRoleType => 15 = 0x0F, true;
-    DefinitionStruct => 20 = 0x14, true;
-    DefinitionFunction => 21 = 0x15, true;
+    VertexEntityType => 10 = 0x0A, Width::Fixed;
+    VertexRelationType => 11 = 0x0B, Width::Fixed;
+    VertexAttributeType => 12 = 0x0C, Width::Fixed;
+    VertexRoleType => 15 = 0x0F, Width::Fixed;
+    DefinitionStruct => 20 = 0x14, Width::Fixed;
+    DefinitionFunction => 21 = 0x15, Width::Fixed;
 
     // All objects are stored consecutively for iteration
-    VertexEntity => 30 = 0x1E, true;
-    VertexRelation => 31 = 0x1F, true;
+    VertexEntity => 30 = 0x1E, Width::Fixed;
+    VertexRelation => 31 = 0x1F, Width::Fixed;
     VertexAttribute => 32 = 0x20, false;
 
-    EdgeSub => 100 = 0x64, true;
-    EdgeSubReverse => 101 = 0x65, true;
-    EdgeOwns => 102 = 0x66, true;
-    EdgeOwnsReverse => 103 = 0x67, true;
-    EdgePlays => 104 = 0x68, true;
-    EdgePlaysReverse => 105 = 0x69, true;
-    EdgeRelates => 106 = 0x6A, true;
-    EdgeRelatesReverse => 107 = 0x6B, true;
+    EdgeSub => 100 = 0x64, Width::Fixed;
+    EdgeSubReverse => 101 = 0x65, Width::Fixed;
+    EdgeOwns => 102 = 0x66, Width::Fixed;
+    EdgeOwnsReverse => 103 = 0x67, Width::Fixed;
+    EdgePlays => 104 = 0x68, Width::Fixed;
+    EdgePlaysReverse => 105 = 0x69, Width::Fixed;
+    EdgeRelates => 106 = 0x6A, Width::Fixed;
+    EdgeRelatesReverse => 107 = 0x6B, Width::Fixed;
 
     EdgeHas => 130 = 0x82, false;
     EdgeHasReverse => 131 = 0x83, false;
-    EdgeLinks => 132 = 0x84, true;
-    EdgeLinksReverse => 133 = 0x85, true;
-    EdgeLinksIndex => 140 = 0x8C, true;
+    EdgeLinks => 132 = 0x84, Width::Fixed;
+    EdgeLinksReverse => 133 = 0x85, Width::Fixed;
+    EdgeLinksIndex => 140 = 0x8C, Width::Fixed;
 
-    PropertyTypeVertex => 160 = 0xA0, true;
-    PropertyTypeEdge => 162 = 0xA2, true;
-    PropertyObjectVertex => 163 = 0xA3, true;
+    PropertyTypeVertex => 160 = 0xA0, Width::Fixed;
+    PropertyTypeEdge => 162 = 0xA2, Width::Fixed;
+    PropertyObjectVertex => 163 = 0xA3, Width::Fixed;
 
-    IndexLabelToType => 182 = 0xB6, false;
-    IndexNameToDefinitionStruct => 183 = 0xB7, false;
-    IndexNameToDefinitionFunction => 184 = 0xB8, false;
+    IndexLabelToType => 182 = 0xB6, Width::Variable;
+    IndexNameToDefinitionStruct => 183 = 0xB7, Width::Variable;
+    IndexNameToDefinitionFunction => 184 = 0xB8, Width::Variable;
 
-    IndexValueToStruct => 190 = 0xBE, false
+    IndexValueToStruct => 190 = 0xBE, Width::Variable
     // Reserved: 200-255 = 0xC8-0xFF
 }
