0aee459d9c442f30fddcb120412a106ac4b43d49
Krishnan Govindraj <krishnan@typedb.com>
2026-06-02 13:07:20 +0200

    Add SHA256

--- .circleci/windows/prepare.bat
+++ .circleci/windows/prepare.bat
@@ -16,3 +16,4 @@ REM permanently set variables for Bazel build
 SETX BAZEL_SH "C:\Program Files\Git\usr\bin\bash.exe"
 SETX CARGO_NET_GIT_FETCH_WITH_CLI true
 SETX CARGO_BAZEL_GENERATOR_URL="https://repo.typedb.com/public/public-tools/raw/versions/0.0.0/cargo-bazel-win-x86_64"
+SETX CARGO_BAZEL_GENERATOR_SHA256="238e01c1bc97ce2671dddaa7435e56ebffcbbc43f5d6fb3f3d18f6afad148fbf"
