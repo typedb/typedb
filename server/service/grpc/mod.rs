80009517abad541f2e711d2aa66a3f462535583f
Krishnan Govindraj <krishnan@typedb.com>
2026-06-24 12:16:39 +0100

    Fix protocol extension mismatch error handling

--- server/service/grpc/mod.rs
+++ server/service/grpc/mod.rs
@@ -3,6 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
+use std::fmt;
 
 pub use error::{IntoGrpcStatus, IntoProtocolErrorMessage};
 
@@ -21,3 +22,27 @@ mod row;
 pub(crate) mod transaction_service;
 pub(crate) mod typedb_service;
 pub(crate) type ConnectionID = uuid::Bytes;
+
+#[derive(Clone, PartialEq, Eq, Ord, PartialOrd)]
+pub struct GrpcProtocolVersion {
+    version: i32,
+    extension: i32,
+}
+
+impl GrpcProtocolVersion {
+    pub(crate) fn new(version: i32, extension: i32) -> GrpcProtocolVersion {
+        Self { version, extension }
+    }
+}
+
+impl fmt::Display for GrpcProtocolVersion {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Debug::fmt(self, f)
+    }
+}
+
+impl fmt::Debug for GrpcProtocolVersion {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}.{}", self.version as i32, self.extension as i32)
+    }
+}
