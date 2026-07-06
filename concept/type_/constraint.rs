6ec56f9bee3b16e71bb33e5031c4d81f64f4bd68
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2026-07-06 16:30:07 +0100

    doc and metadata annotations don't need validation (#7866)
    ## Product change and motivation
    
    We mark `@doc` and `@meta` annotations as not requiring validation.
    Otherwise the operation level validation of schema queries fails in
    debug mode as there are unhandled constraints.
    
    ## Implementation

--- concept/type_/constraint.rs
+++ concept/type_/constraint.rs
@@ -163,6 +163,8 @@ impl ConstraintDescription {
         match self {
             ConstraintDescription::Cardinality(cardinality) => cardinality.requires_validation(),
             ConstraintDescription::Independent(_) => false,
+            ConstraintDescription::Doc(_) => false,
+            ConstraintDescription::Meta(_) => false,
             _ => true,
         }
     }
@@ -171,6 +173,8 @@ impl ConstraintDescription {
         match self {
             ConstraintDescription::Cardinality(_) => false, // only commit time
             ConstraintDescription::Independent(_) => false, // never
+            ConstraintDescription::Doc(_) => false,         // never
+            ConstraintDescription::Meta(_) => false,        // never
             _ => true,
         }
     }
