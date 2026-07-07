2660c77fdcb68f97040b7f24eb092498464f342e
Krishnan Govindraj <krishnan@typedb.com>
2026-07-07 12:05:24 +0200

    WIP: Brew snapshots

---
+++ config/brew/typedb-snapshot.rb
@@ -0,0 +1,26 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+# IMPORTANT: any changes to the formula should be propagated to Homebrew/homebrew-core
+class Typedb < Formula
+  desc "The power of programming, in your database"
+  homepage "https://typedb.com"
+
+  on_arm do
+    url "https://repo.typedb.com/public/public-snapshot/raw/names/typedb-all-mac-arm64/versions/{version}/typedb-all-mac-arm64-{version}.zip"
+    sha256 "{sha256-arm64}"
+  end
+
+  on_intel do
+    url "https://repo.typedb.com/public/public-snapshot/raw/names/typedb-all-mac-x86_64/versions/{version}/typedb-all-mac-x86_64-{version}.zip"
+    sha256 "{sha256-x86_64}"
+  end
+
+  license "MPL-2.0"
+
+  def install
+    libexec.install Dir["*"]
+    bin.install_symlink libexec / "typedb"
+  end
+end
