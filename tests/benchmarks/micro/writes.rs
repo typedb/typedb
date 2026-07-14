c970f7bb2059cdde6f90a808d0822121dfcc25c0
Krishnan Govindraj <krishnan@typedb.com>
2026-07-14 18:02:58 +0200

    WIP: Just setup things so they run

---
+++ tests/benchmarks/micro/writes.rs
@@ -0,0 +1,67 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+use std::{fs::File, os::raw::c_int, path::Path};
+
+use criterion::{Criterion, criterion_group, criterion_main, profiler::Profiler};
+use pprof::ProfilerGuard;
+
+fn do_something() -> u64 {
+    1
+}
+
+fn do_something_else() -> u64 {
+    lib_common::get_int()
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    println!("In criterion benchmark");
+    c.bench_function("do-something", |b| b.iter(|| do_something()));
+    c.bench_function("do-something_else", |b| b.iter(|| do_something_else()));
+}
+
+// --- Code to generate flamegraphs copied from https://www.jibbow.com/posts/criterion-flamegraphs/ ---
+// This causes a SIGBUS on (mac) arm64 if the frequency is set too high.
+
+pub struct FlamegraphProfiler<'a> {
+    frequency: c_int,
+    active_profiler: Option<ProfilerGuard<'a>>,
+}
+
+impl<'a> FlamegraphProfiler<'a> {
+    #[allow(dead_code)]
+    pub fn new(frequency: c_int) -> Self {
+        FlamegraphProfiler { frequency, active_profiler: None }
+    }
+}
+
+impl<'a> Profiler for FlamegraphProfiler<'a> {
+    fn start_profiling(&mut self, _benchmark_id: &str, _benchmark_dir: &Path) {
+        self.active_profiler = Some(ProfilerGuard::new(self.frequency).unwrap());
+    }
+
+    fn stop_profiling(&mut self, _benchmark_id: &str, benchmark_dir: &Path) {
+        std::fs::create_dir_all(benchmark_dir).unwrap();
+        let flamegraph_path = benchmark_dir.join("flamegraph.svg");
+        let flamegraph_file = File::create(&flamegraph_path).expect("File system error while creating flamegraph.svg");
+        if let Some(profiler) = self.active_profiler.take() {
+            profiler.report().build().unwrap().flamegraph(flamegraph_file).expect("Error writing flamegraph");
+            println!("Wrote flamegraph to {}", flamegraph_path.display());
+        }
+    }
+}
+
+fn profiled() -> Criterion {
+    Criterion::default().with_profiler(FlamegraphProfiler::new(100))
+}
+
+criterion_group!(
+    name = benches;
+    config = profiled();
+    targets = criterion_benchmark
+);
+
+criterion_main!(benches);
