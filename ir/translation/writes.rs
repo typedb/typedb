9db69431a05b878c3d2e10eae09efc70f8ea8038
Krishnan Govindraj <krishnan@typedb.com>
2026-08-31 22:15:08 +0200

    Where did that come from

--- ir/translation/writes.rs
+++ ir/translation/writes.rs
@@ -3,7 +3,6 @@
 * License, v. 2.0. If a copy of the MPL was not distributed with this
 * file, You can obtain one at https://mozilla.org/MPL/2.0/.
 */
-use std::os::macos::raw::stat;
 
 use answer::variable::Variable;
 use error::UnimplementedFeature;
