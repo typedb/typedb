c2c95fbae2013d9a22271eb78fbb95b6af916fd3
Krishnan Govindraj <krishnan@typedb.com>
2026-09-10 20:08:14 +0200

    Replace conditional write required_variables with CheckInstructions

--- compiler/executable/update/executable.rs
+++ compiler/executable/update/executable.rs
@@ -17,7 +17,7 @@ use crate::{
     VariablePosition,
     annotation::type_annotations::{BlockAnnotations, TypeAnnotations},
     executable::{
-        RequiredVariablesForWrite, WriteCompilationError,
+        WriteCompilationError, WritePatternCondition,
         insert::{
             VariableSource,
             executable::{
@@ -89,7 +89,7 @@ pub fn compile(
 pub struct ConditionalUpdate {
     pub concept_instructions: Vec<ConceptInstruction>,
     pub connection_instructions: Vec<ConnectionInstruction>,
-    pub required_input_variables: RequiredVariablesForWrite,
+    pub condition: WritePatternCondition,
 }
 
 impl ConditionalUpdate {
@@ -111,11 +111,11 @@ impl ConditionalUpdate {
         let connection_instructions =
             add_connections(conjunction, block_annotations, variable_positions, variable_registry)?;
 
-        let required_input_variables = RequiredVariablesForWrite::build(conjunction, variable_positions);
+        let required_input_variables = WritePatternCondition::build(conjunction, variable_positions);
 
         let concept_instructions = concept_instructions_map_to_vec(concept_instruction_map);
 
-        Ok(Self { concept_instructions, connection_instructions, required_input_variables })
+        Ok(Self { concept_instructions, connection_instructions, condition: required_input_variables })
     }
 }
 
