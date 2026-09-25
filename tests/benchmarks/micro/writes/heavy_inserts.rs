ceb4c91dd9012fe5b05138c7290b261d4b103d11
Krishnan Govindraj <krishnan@typedb.com>
2026-09-26 01:18:10 +0200

    Introduce standardized run_configs

--- tests/benchmarks/micro/writes/heavy_inserts.rs
+++ tests/benchmarks/micro/writes/heavy_inserts.rs
@@ -3,104 +3,100 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-use std::{sync::Arc, time::Instant};
 
-use database::{Database, transaction::TransactionWrite};
+use encoding::graph::type_::vertex::TypeID;
 use lib_benchmark::{
-    QueryAnswer,
-    benchmark::{RunDescriptor, TypeDBMicroBenchmark},
-    commit, execute_write_query_in,
-    profiling::{MultiQueryTxProfile, MultiTxMultiQueryProfile},
+    benchmark::{QueryDescriptor, RunDescriptor, TypeDBWorkloadBenchmark, WorkloadInstance},
+    datagen::RandomDataGen,
     runner::{BenchmarkRunner, BenchmarkRunnerGroup},
-    utils::{CountResults, unpack_result},
 };
-use options::TransactionOptions;
-use query::given_rows::GivenRowsSimple;
-use storage::durability_client::WALClient;
+use query::given_rows::GivenRowEntry;
 
-use crate::{given_rows_with, no_initial_data};
+use crate::{
+    run_configs::{
+        PARALLEL_MANY_LARGE, PARALLEL_MANY_MEDIUM, PARALLEL_MANY_SMALL, SERIAL_FEW_LARGE, SERIAL_MANY_SMALL,
+    },
+    simple_inserts::{SCHEMA as SIMPLE_SCHEMA, no_initial_data},
+};
 
-type HeavyInsertBenchmark = TypeDBMicroBenchmark<Option<GivenRowsSimple>, MultiTxMultiQueryProfile>;
 pub(crate) fn run_all(runner: &mut impl BenchmarkRunner) {
-    let mut group = runner.new_group("simple_inserts");
-    group.run_benchmark(many_small_tx());
-    group.run_benchmark(many_queries_per_tx());
-    group.run_benchmark(single_large_query_per_tx());
-    group.run_benchmark(ten_large_query_per_tx());
-    group.run_benchmark(many_average_tx());
+    let mut group = runner.new_group("serial_inserts");
+    group.run_benchmark(serial_entities_few_large());
+    group.run_benchmark(serial_entities_few_large_10q());
+    group.run_benchmark(serial_entities_many_small());
+
+    let mut group = runner.new_group("parallel_inserts");
+    group.run_benchmark(parallel_entities_many_small());
+    group.run_benchmark(parallel_entities_many_large());
+    group.run_benchmark(parallel_relations_many_medium());
 }
 
-fn parametrised_entity_insert(
-    name: &'static str,
-    n_txns: usize,
-    n_queries_per_tx: usize,
-    n_entities_per_query: usize,
-) -> HeavyInsertBenchmark {
-    let query = "given; insert $x isa person;";
-    let query_owned = query.to_owned();
-    let run_descriptor =
-        RunDescriptor { parallelism: 1, total_txns: n_txns, n_queries_per_tx, n_rows_per_query: n_entities_per_query };
-    let benchmark_fn =
-        Box::new(move |database: Arc<Database<WALClient>>, given_rows_to_clone: Option<GivenRowsSimple>| {
-            let mut profiles = Vec::with_capacity(n_txns);
-            let very_beginning = Instant::now();
-            for _ in 0..n_txns {
-                let start = Instant::now();
-                let mut query_profiles = Vec::with_capacity(n_queries_per_tx);
-                let mut tx = TransactionWrite::open(database.clone(), TransactionOptions::default()).unwrap();
-                for _ in 0..n_queries_per_tx {
-                    let given_rows = given_rows_to_clone
-                        .as_ref()
-                        .map(|g| GivenRowsSimple { variables: g.variables.clone(), rows: g.rows.clone() });
-                    let (query_result, tx_returned) = unpack_result(execute_write_query_in::<_, CountResults>(
-                        tx,
-                        query_owned.as_str(),
-                        given_rows,
-                        true,
-                    ));
-                    tx = tx_returned;
-                    let QueryAnswer { profile: query_profile, answer: rows } = query_result.unwrap();
-                    assert_eq!(rows, n_entities_per_query);
-                    query_profiles.push(query_profile);
-                }
-                let tx_profile = commit(tx).unwrap();
-                let time_elapsed = start.elapsed();
-                profiles.push(MultiQueryTxProfile { tx_profile, query_profiles, time_elapsed });
-            }
-            MultiTxMultiQueryProfile {
-                name,
-                profiles,
-                run_descriptor: run_descriptor.clone(),
-                total_wall_time: very_beginning.elapsed(),
-            }
-        });
-    TypeDBMicroBenchmark {
-        name,
-        schema: crate::simple_inserts::SCHEMA,
-        warmup_fn: None, // TODO
-        preload_data_fn: no_initial_data(),
-        prepare_run_fn: given_rows_with(n_entities_per_query, vec![], |_| vec![]),
-        benchmark_fn,
+fn parametrised_entity_insert(name: &'static str, run_descriptor: RunDescriptor) -> TypeDBWorkloadBenchmark {
+    let query_descriptor =
+        QueryDescriptor { query: "given; insert $x isa person;", variables: vec![], produce_row: Some(|_| vec![]) };
+    TypeDBWorkloadBenchmark::new(name, SIMPLE_SCHEMA, no_initial_data(), query_descriptor, run_descriptor)
+}
+
+// Serial
+
+fn serial_entities_few_large() -> TypeDBWorkloadBenchmark {
+    parametrised_entity_insert("serial_entities_few_large", SERIAL_FEW_LARGE)
+}
+
+fn serial_entities_few_large_10q() -> TypeDBWorkloadBenchmark {
+    // FLAGGED: 10 queries/tx has no constant in run_configs
+    parametrised_entity_insert(
+        "serial_entities_few_large_10q",
+        RunDescriptor { n_queries_per_tx: 10, ..SERIAL_FEW_LARGE },
+    )
+}
+
+fn serial_entities_many_small() -> TypeDBWorkloadBenchmark {
+    parametrised_entity_insert("serial_entities_many_small", SERIAL_MANY_SMALL)
+}
+
+// Parallel
+fn parallel_entities_many_small() -> TypeDBWorkloadBenchmark {
+    parametrised_entity_insert("parallel_entities_many_small", PARALLEL_MANY_SMALL)
+}
+
+fn parallel_entities_many_large() -> TypeDBWorkloadBenchmark {
+    parametrised_entity_insert("parallel_entities_many_large", PARALLEL_MANY_LARGE)
+}
+
+fn parallel_relations_many_medium() -> TypeDBWorkloadBenchmark {
+    parametrised_parallel_binary_relation("parallel_relations_many_medium", PARALLEL_MANY_MEDIUM)
+}
+
+fn parametrised_parallel_binary_relation(name: &'static str, run_descriptor: RunDescriptor) -> TypeDBWorkloadBenchmark {
+    const N_ENTITIES: usize = 100_000;
+    fn produce_row(rng: &mut RandomDataGen) -> Vec<GivenRowEntry> {
+        vec![
+            rng.entry_entity_raw_in(TypeID::new(0), 0, (N_ENTITIES - 1) as u64),
+            rng.entry_entity_raw_in(TypeID::new(1), 0, (N_ENTITIES - 1) as u64),
+        ]
     }
-}
+    let schema = r#"
+    define
+        relation r1, relates e1, relates e2;
+        entity e1, plays r1:e1;
+        entity e2, plays r1:e2;
+    "#;
 
-// Instantiations
-fn many_small_tx() -> HeavyInsertBenchmark {
-    parametrised_entity_insert("many_small_tx", 100_000, 1, 1)
-}
+    let preload_data_fn = WorkloadInstance::make_preload_data_fn(
+        "given; insert $_ isa e1; $_ isa e2;",
+        vec![],
+        |_| vec![],
+        N_ENTITIES,
+        10_000,
+    );
 
-fn many_queries_per_tx() -> HeavyInsertBenchmark {
-    parametrised_entity_insert("many_queries_per_tx", 100, 1000, 1)
-}
+    let query = r#"
+        given $e1:e1, $e2: e2;
+        insert $r isa r1, links (e1: $e1, e2: $e2);
+       "#;
+    let variables = vec!["e1".to_owned(), "e2".to_owned()];
+    let query_descriptor = QueryDescriptor { query, variables, produce_row: Some(produce_row) };
 
-fn single_large_query_per_tx() -> HeavyInsertBenchmark {
-    parametrised_entity_insert("single_large_query_per_tx", 100, 1, 10_000)
-}
-
-fn ten_large_query_per_tx() -> HeavyInsertBenchmark {
-    parametrised_entity_insert("ten_large_query_per_tx", 100, 10, 10_000)
-}
-
-fn many_average_tx() -> HeavyInsertBenchmark {
-    parametrised_entity_insert("many_average_tx", 1_000, 1, 100)
+    TypeDBWorkloadBenchmark::new(name, schema, Some(preload_data_fn), query_descriptor, run_descriptor)
 }
